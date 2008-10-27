b4b169c937975564560b70154c315646d6294a79
Arjen Poutsma <apoutsma@vmware.com>
2008-10-27 23:43:25 +0000

    Removed unused import

--- org.springframework.core/src/main/java/org/springframework/core/CollectionFactory.java
+++ org.springframework.core/src/main/java/org/springframework/core/CollectionFactory.java
@@ -18,7 +18,6 @@ package org.springframework.core;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
