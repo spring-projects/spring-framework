df74a87bcaca4a4f605b40222b045ddafab9f6b0
Arjen Poutsma <apoutsma@vmware.com>
2008-10-29 12:17:13 +0000

    Moved tests over from testsuite to core
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@179 50f2f4bb-b051-0410-bef5-90022cba6387

---
+++ org.springframework.testsuite/src/test/java/org/springframework/web/util/MockLog4jAppender.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.web.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * @author Alef Arendsen
+ */
+public class MockLog4jAppender extends AppenderSkeleton {
+
+	public static final List loggingStrings = new ArrayList();
+
+	public static boolean closeCalled = false;
+
+	/* (non-Javadoc)
+	 * @see org.apache.log4j.AppenderSkeleton#append(org.apache.log4j.spi.LoggingEvent)
+	 */
+	protected void append(LoggingEvent evt) {
+		//System.out.println("Adding " + evt.getMessage());
+		loggingStrings.add(evt.getMessage());
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.log4j.Appender#close()
+	 */
+	public void close() {
+		closeCalled = true;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.log4j.Appender#requiresLayout()
+	 */
+	public boolean requiresLayout() {
+		return false;
+	}
+
+
+
+
+}
