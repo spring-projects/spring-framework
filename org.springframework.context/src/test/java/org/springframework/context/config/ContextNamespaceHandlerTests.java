100a80e11254c1e4596c9542b0bc71053d8b8fe0
Arjen Poutsma <apoutsma@vmware.com>
2008-10-30 16:24:14 +0000

    Moved tests from testsuite to context and context support

--- org.springframework.context/src/test/java/org/springframework/context/config/ContextNamespaceHandlerTests.java
+++ org.springframework.context/src/test/java/org/springframework/context/config/ContextNamespaceHandlerTests.java
@@ -19,7 +19,11 @@ package org.springframework.context.config;
 import java.util.Date;
 import java.util.Map;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import org.junit.Before;
+import org.junit.Test;
+
 import org.springframework.beans.factory.config.PropertyOverrideConfigurer;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.ApplicationContext;
@@ -29,22 +33,25 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author Arjen Poutsma
  * @since 2.5.6
  */
-public class ContextNamespaceHandlerTests extends TestCase {
+public class ContextNamespaceHandlerTests {
 
 	private ApplicationContext applicationContext;
 
-	protected void setUp() throws Exception {
+	@Before
+	public void createAppContext() {
 		applicationContext = new ClassPathXmlApplicationContext("contextNamespaceHandlerTests.xml", getClass());
 	}
 
-	public void testPropertyPlaceholder() throws Exception {
+	@Test
+	public void propertyPlaceholder() throws Exception {
 		Map beans = applicationContext.getBeansOfType(PropertyPlaceholderConfigurer.class);
 		assertFalse("No PropertyPlaceHolderConfigurer found", beans.isEmpty());
 		String s = (String) applicationContext.getBean("string");
 		assertEquals("No properties replaced", "bar", s);
 	}
 
-	public void testPropertyOverride() throws Exception {
+	@Test
+	public void propertyOverride() throws Exception {
 		Map beans = applicationContext.getBeansOfType(PropertyOverrideConfigurer.class);
 		assertFalse("No PropertyOverrideConfigurer found", beans.isEmpty());
 		Date date = (Date) applicationContext.getBean("date");
