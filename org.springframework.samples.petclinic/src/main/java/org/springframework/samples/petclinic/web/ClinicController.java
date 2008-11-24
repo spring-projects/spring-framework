49f0189671e3948f9469868ad82a7d92f08338ae
Chris Beams <cbeams@vmware.com>
2008-11-24 22:16:21 +0000

    + Added necessary JPA dependencies to make Petclinic JUnit tests pass + Removed unused imports for @RequestParam Note: While unit tests pass in eclipse, NoClassDefFound errors are still being thrown when tests are run from the command line.
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@330 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.samples.petclinic/src/main/java/org/springframework/samples/petclinic/web/ClinicController.java
+++ org.springframework.samples.petclinic/src/main/java/org/springframework/samples/petclinic/web/ClinicController.java
@@ -6,7 +6,6 @@ import org.springframework.samples.petclinic.Clinic;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
--- org.springframework.samples.petclinic/src/main/java/org/springframework/samples/petclinic/web/EditOwnerForm.java
+++ org.springframework.samples.petclinic/src/main/java/org/springframework/samples/petclinic/web/EditOwnerForm.java
@@ -10,7 +10,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
