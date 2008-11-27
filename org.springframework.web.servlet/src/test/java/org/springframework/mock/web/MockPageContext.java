fda71008660d208c32703ff1e6d5d08297179d37
Juergen Hoeller <jhoeller@vmware.com>
2008-11-27 21:49:10 +0000

    consistent compilation against Servlet 2.5, JSP 2.1 and JUnit 4.5

--- org.springframework.web.servlet/src/test/java/org/springframework/mock/web/MockPageContext.java
+++ org.springframework.web.servlet/src/test/java/org/springframework/mock/web/MockPageContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ package org.springframework.mock.web;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Hashtable;
-
+import javax.el.ELContext;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -273,6 +273,10 @@ public class MockPageContext extends PageContext {
 
 	public ExpressionEvaluator getExpressionEvaluator() {
 		return new MockExpressionEvaluator(this);
+	}
+
+	public ELContext getELContext() {
+		throw new UnsupportedOperationException("getELContext");
 	}
 
 	public VariableResolver getVariableResolver() {
