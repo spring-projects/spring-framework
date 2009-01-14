1dec645383385e5728e47db9ae836b104701a66b
Arjen Poutsma <apoutsma@vmware.com>
2009-01-14 10:03:28 +0000

    Polish

--- org.springframework.core/src/test/java/org/springframework/util/AntPathMatcherTests.java
+++ org.springframework.core/src/test/java/org/springframework/util/AntPathMatcherTests.java
@@ -278,7 +278,8 @@ public class AntPathMatcherTests {
 		assertEquals("cvs/commit", pathMatcher.extractPathWithinPattern("/docs/*", "/docs/cvs/commit"));
 		assertEquals("commit.html", pathMatcher.extractPathWithinPattern("/docs/cvs/*.html", "/docs/cvs/commit.html"));
 		assertEquals("cvs/commit", pathMatcher.extractPathWithinPattern("/docs/**", "/docs/cvs/commit"));
-		assertEquals("cvs/commit.html", pathMatcher.extractPathWithinPattern("/docs/**/*.html", "/docs/cvs/commit.html"));
+		assertEquals("cvs/commit.html",
+				pathMatcher.extractPathWithinPattern("/docs/**/*.html", "/docs/cvs/commit.html"));
 		assertEquals("commit.html", pathMatcher.extractPathWithinPattern("/docs/**/*.html", "/docs/commit.html"));
 		assertEquals("commit.html", pathMatcher.extractPathWithinPattern("/*.html", "/commit.html"));
 		assertEquals("docs/commit.html", pathMatcher.extractPathWithinPattern("/*.html", "/docs/commit.html"));
@@ -288,15 +289,16 @@ public class AntPathMatcherTests {
 		assertEquals("/docs/commit.html", pathMatcher.extractPathWithinPattern("*", "/docs/commit.html"));
 
 		assertEquals("docs/cvs/commit", pathMatcher.extractPathWithinPattern("/d?cs/*", "/docs/cvs/commit"));
-		assertEquals("cvs/commit.html", pathMatcher.extractPathWithinPattern("/docs/c?s/*.html", "/docs/cvs/commit.html"));
+		assertEquals("cvs/commit.html",
+				pathMatcher.extractPathWithinPattern("/docs/c?s/*.html", "/docs/cvs/commit.html"));
 		assertEquals("docs/cvs/commit", pathMatcher.extractPathWithinPattern("/d?cs/**", "/docs/cvs/commit"));
-		assertEquals("docs/cvs/commit.html", pathMatcher.extractPathWithinPattern("/d?cs/**/*.html", "/docs/cvs/commit.html"));
+		assertEquals("docs/cvs/commit.html",
+				pathMatcher.extractPathWithinPattern("/d?cs/**/*.html", "/docs/cvs/commit.html"));
 	}
 
 	@Test
 	public void extractUriTemplateVariables() throws Exception {
-		Map<String,String> result;
-		result = pathMatcher.extractUriTemplateVariables("/hotels/{hotel}", "/hotels/1");
+		Map<String, String> result = pathMatcher.extractUriTemplateVariables("/hotels/{hotel}", "/hotels/1");
 		assertEquals(Collections.singletonMap("hotel", "1"), result);
 
 		result = pathMatcher.extractUriTemplateVariables("/h?tels/{hotel}", "/hotels/1");
