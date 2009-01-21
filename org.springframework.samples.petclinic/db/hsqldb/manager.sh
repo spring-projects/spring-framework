24688edcd7559ff9367ee1b120239a00f38719bb
Arjen Poutsma <apoutsma@vmware.com>
2009-01-21 21:04:15 +0000

    SPR-5335: manager.sh classpath for petclinic should be updated
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@564 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.samples.petclinic/db/hsqldb/manager.sh
+++ org.springframework.samples.petclinic/db/hsqldb/manager.sh
@@ -1,1 +1,1 @@
-java -cp ../../../../lib/hsqldb/hsqldb.jar org.hsqldb.util.DatabaseManager
+java -classpath ../../../ivy-cache/repository/org.hsqldb/com.springsource.org.hsqldb/1.8.0.9/com.springsource.org.hsqldb-1.8.0.9.jar org.hsqldb.util.DatabaseManager
