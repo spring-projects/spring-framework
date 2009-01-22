a768f116342f4c2940d87a7660a3434962abfb81
Arjen Poutsma <apoutsma@vmware.com>
2009-01-22 15:41:29 +0000

    SPR-2581: Provide out of the box implementation of the thread scope
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@574 50f2f4bb-b051-0410-bef5-90022cba6387

---
+++ org.springframework.context/src/main/java/org/springframework/context/support/SimpleThreadScope.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.context.support;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.Scope;
+import org.springframework.core.NamedThreadLocal;
+
+/**
+ * Thread-backed {@link Scope} implementation.
+ *
+ * <p><strong>Note</strong> that the <code>SimpleThreadScope</code> <em>does not clean up any objects</em> associated
+ * with it. As such, it's typically preferable to use the {@link org.springframework.web.context.request.RequestScope}
+ * in Web environments.
+ *
+ * <p>For a implementation of a thread-based <code>Scope</code> with support for destruction callbacks, refer to <a
+ * href="http://www.springbyexample.org/twiki/bin/view/Example/CustomThreadScopeModule">this module</a>.
+ *
+ * @author Eugene Kuleshov
+ * @author Arjen Poutsma
+ * @since 3.0
+ */
+public class SimpleThreadScope implements Scope {
+
+	private static final Log logger = LogFactory.getLog(SimpleThreadScope.class);
+
+	private static final ThreadLocal<Map<String, Object>> threadScope =
+			new NamedThreadLocal<Map<String, Object>>("SimpleThreadScope") {
+				@Override
+				protected Map<String, Object> initialValue() {
+					return new HashMap<String, Object>();
+				}
+			};
+
+	public Object get(String name, ObjectFactory objectFactory) {
+		Map<String, Object> scope = threadScope.get();
+		Object object = scope.get(name);
+		if (object == null) {
+			object = objectFactory.getObject();
+			scope.put(name, object);
+		}
+		return object;
+	}
+
+	public Object remove(String name) {
+		Map<String, Object> scope = threadScope.get();
+		return scope.remove(name);
+	}
+
+	public void registerDestructionCallback(String name, Runnable callback) {
+		logger.warn("SimpleThreadScope does not support descruction callbacks. " +
+				"Consider using a RequestScope in a Web environment");
+	}
+
+	public Object resolveContextualObject(String key) {
+		return null;
+	}
+
+	public String getConversationId() {
+		return Thread.currentThread().getName();
+	}
+}
---
+++ org.springframework.context/src/test/java/org/springframework/context/support/SimpleThreadScopeTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.context.support;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.springframework.beans.TestBean;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @author Arjen Poutsma
+ */
+public class SimpleThreadScopeTest {
+
+	private ApplicationContext applicationContext;
+
+	@Before
+	public void setUp() {
+		applicationContext = new ClassPathXmlApplicationContext("simpleThreadScopeTests.xml", getClass());
+	}
+
+	@Test
+	public void getFromScope() throws Exception {
+		String name = "threadScopedObject";
+		TestBean bean = (TestBean) this.applicationContext.getBean(name);
+		assertNotNull(bean);
+		assertSame(bean, this.applicationContext.getBean(name));
+		TestBean bean2 = (TestBean) this.applicationContext.getBean(name);
+		assertSame(bean, bean2);
+	}
+
+	@Test
+	public void getMultipleInstances() throws Exception {
+		final TestBean[] beans = new TestBean[2];
+		Thread thread1 = new Thread(new Runnable() {
+			public void run() {
+				beans[0] = applicationContext.getBean("threadScopedObject", TestBean.class);
+			}
+		});
+		Thread thread2 = new Thread(new Runnable() {
+			public void run() {
+				beans[1] = applicationContext.getBean("threadScopedObject", TestBean.class);
+			}
+		});
+		thread1.start();
+		thread2.start();
+
+		Thread.sleep(200);
+
+		assertNotNull(beans[0]);
+		assertNotNull(beans[1]);
+
+		assertNotSame(beans[0], beans[1]);
+	}
+
+}
