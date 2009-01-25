eef60863258eb7fe53f3f8fb533082a5d50be7f7
Juergen Hoeller <jhoeller@vmware.com>
2009-01-25 23:28:01 +0000

    polishing

--- org.springframework.jms/src/main/java/org/springframework/jms/support/destination/JndiDestinationResolver.java
+++ org.springframework.jms/src/main/java/org/springframework/jms/support/destination/JndiDestinationResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,7 +108,7 @@ public class JndiDestinationResolver extends JndiLocatorSupport implements Cachi
 		}
 		else {
 			try {
-				dest = (Destination) lookup(destinationName, Destination.class);
+				dest = lookup(destinationName, Destination.class);
 				validateDestination(dest, destinationName, pubSubDomain);
 			}
 			catch (NamingException ex) {
