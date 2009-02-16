4c5ab74c4151baff0395829fba212056fbf5153e
Juergen Hoeller <jhoeller@vmware.com>
2009-02-16 01:36:13 +0000

    pre-converted property values are preserved more eagerly for re-created beans (SPR-5293)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@641 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.beans/src/main/java/org/springframework/beans/PropertyValue.java
+++ org.springframework.beans/src/main/java/org/springframework/beans/PropertyValue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,6 +80,8 @@ public class PropertyValue extends BeanMetadataAttributeAccessor implements Seri
 		this.name = original.getName();
 		this.value = original.getValue();
 		this.source = original.getSource();
+		this.converted = original.converted;
+		this.convertedValue = original.convertedValue;
 		this.conversionNecessary = original.conversionNecessary;
 		this.resolvedTokens = original.resolvedTokens;
 		this.resolvedDescriptor = original.resolvedDescriptor;
