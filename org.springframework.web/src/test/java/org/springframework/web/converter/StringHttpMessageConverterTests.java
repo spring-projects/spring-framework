975fb59d4b25f3e8698595df7cca57d2a38503af
Arjen Poutsma <apoutsma@vmware.com>
2009-02-22 10:15:32 +0000

    Fixing tests
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@665 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.web/src/test/java/org/springframework/web/converter/StringHttpMessageConverterTests.java
+++ org.springframework.web/src/test/java/org/springframework/web/converter/StringHttpMessageConverterTests.java
@@ -55,12 +55,12 @@ public class StringHttpMessageConverterTests {
 		Charset charset = Charset.forName("UTF-8");
 		converter.setSupportedMediaTypes(Collections.singletonList(new MediaType("text", "plain", charset)));
 		MockHttpOutputMessage outputMessage = new MockHttpOutputMessage();
-		String body = "HŽllo W¿rld";
+		String body = "Hello World";
 		converter.write(body, outputMessage);
 		assertEquals("Invalid result", body, outputMessage.getBodyAsString(charset));
 		assertEquals("Invalid content-type", new MediaType("text", "plain", charset),
 				outputMessage.getHeaders().getContentType());
-		assertEquals("Invalid content-length", 13, outputMessage.getHeaders().getContentLength());
+		assertEquals("Invalid content-length", 11, outputMessage.getHeaders().getContentLength());
 		assertFalse("Invalid accept-charset", outputMessage.getHeaders().getAcceptCharset().isEmpty());
 	}
 }
