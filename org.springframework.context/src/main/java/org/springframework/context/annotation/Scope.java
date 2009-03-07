873723313c821c7a4d5514b474015f5728a2dcad
Chris Beams <cbeams@vmware.com>
2009-03-07 03:37:10 +0000

    @Scope is now allowed on methods (for use in conjunction with @Bean)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@745 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.context/src/main/java/org/springframework/context/annotation/Scope.java
+++ org.springframework.context/src/main/java/org/springframework/context/annotation/Scope.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,9 +31,10 @@ import org.springframework.beans.factory.config.BeanDefinition;
  * '<code>singleton</code>', '<code>prototype</code>', and so forth.
  * 
  * @author Mark Fisher
+ * @author Chris Beams
  * @since 2.5
  */
-@Target(ElementType.TYPE)
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Scope {
