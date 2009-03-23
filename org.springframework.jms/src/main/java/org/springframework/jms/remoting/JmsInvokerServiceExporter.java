c82a66b59c1c77415b4c850c64607596b34d0826
Juergen Hoeller <jhoeller@vmware.com>
2009-03-23 11:19:25 +0000

    SessionAwareMessageListener supports generic declaration of concrete JMS Message type

--- org.springframework.jms/src/main/java/org/springframework/jms/remoting/JmsInvokerServiceExporter.java
+++ org.springframework.jms/src/main/java/org/springframework/jms/remoting/JmsInvokerServiceExporter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,7 +52,7 @@ import org.springframework.remoting.support.RemoteInvocationResult;
  * @see JmsInvokerProxyFactoryBean
  */
 public class JmsInvokerServiceExporter extends RemoteInvocationBasedExporter
-		implements SessionAwareMessageListener, InitializingBean {
+		implements SessionAwareMessageListener<Message>, InitializingBean {
 
 	private MessageConverter messageConverter = new SimpleMessageConverter();
 
