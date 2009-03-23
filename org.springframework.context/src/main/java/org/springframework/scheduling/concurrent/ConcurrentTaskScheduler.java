df99929e21baa93fca2f2fb9e9a6a77e89c405fe
Juergen Hoeller <jhoeller@vmware.com>
2009-03-23 14:38:55 +0000

    added TaskScheduler interface and Trigger abstraction; added ConcurrentTaskScheduler and ThreadPoolTaskScheduler; added CommonJ TimerManagerTaskScheduler; added CronTrigger implementation for cron expression support

---
+++ org.springframework.context/src/main/java/org/springframework/scheduling/concurrent/ConcurrentTaskScheduler.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.scheduling.concurrent;
+
+import java.util.Date;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.core.task.TaskRejectedException;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.Trigger;
+
+/**
+ * Adapter that takes a JDK 1.5 <code>java.util.concurrent.ScheduledExecutorService</code>
+ * and exposes a Spring {@link org.springframework.scheduling.TaskScheduler} for it.
+ * Extends {@link ConcurrentTaskExecutor} in order to implement the
+ * {@link org.springframework.scheduling.SchedulingTaskExecutor} interface as well.
+ *
+ * <p>Note that there is a pre-built {@link ThreadPoolTaskScheduler} that allows for
+ * defining a JDK 1.5 {@link java.util.concurrent.ScheduledThreadPoolExecutor} in bean style,
+ * exposing it as a Spring {@link org.springframework.scheduling.TaskScheduler} directly.
+ * This is a convenient alternative to a raw ScheduledThreadPoolExecutor definition with
+ * a separate definition of the present adapter class.
+ *
+ * @author Juergen Hoeller
+ * @since 3.0
+ * @see java.util.concurrent.ScheduledExecutorService
+ * @see java.util.concurrent.ScheduledThreadPoolExecutor
+ * @see java.util.concurrent.Executors
+ * @see ThreadPoolTaskScheduler
+ */
+public class ConcurrentTaskScheduler extends ConcurrentTaskExecutor implements TaskScheduler {
+
+	private ScheduledExecutorService scheduledExecutor;
+
+
+	/**
+	 * Create a new ConcurrentTaskScheduler,
+	 * using a single thread executor as default.
+	 * @see java.util.concurrent.Executors#newSingleThreadScheduledExecutor()
+	 */
+	public ConcurrentTaskScheduler() {
+		super();
+		setScheduledExecutor(null);
+	}
+
+	/**
+	 * Create a new ConcurrentTaskScheduler,
+	 * using the given JDK 1.5 executor as shared delegate.
+	 * @param scheduledExecutor the JDK 1.5 scheduled executor to delegate to
+	 * for {@link org.springframework.scheduling.SchedulingTaskExecutor} as well
+	 * as {@link TaskScheduler} invocations
+	 */
+	public ConcurrentTaskScheduler(ScheduledExecutorService scheduledExecutor) {
+		super(scheduledExecutor);
+		setScheduledExecutor(scheduledExecutor);
+	}
+
+	/**
+	 * Create a new ConcurrentTaskScheduler,
+	 * using the given JDK 1.5 executors as delegates.
+	 * @param concurrentExecutor the JDK 1.5 concurrent executor to delegate to
+	 * for {@link org.springframework.scheduling.SchedulingTaskExecutor} invocations
+	 * @param scheduledExecutor the JDK 1.5 scheduled executor to delegate to
+	 * for {@link TaskScheduler} invocations
+	 */
+	public ConcurrentTaskScheduler(Executor concurrentExecutor, ScheduledExecutorService scheduledExecutor) {
+		super(concurrentExecutor);
+		setScheduledExecutor(scheduledExecutor);
+	}
+
+
+	/**
+	 * Specify the JDK 1.5 scheduled executor to delegate to.
+	 * <p>Note: This will only apply to {@link TaskScheduler} invocations.
+	 * If you want the given executor to apply to
+	 * {@link org.springframework.scheduling.SchedulingTaskExecutor} invocations
+	 * as well, pass the same executor reference to {@link #setConcurrentExecutor}.
+	 * @see #setConcurrentExecutor
+	 */
+	public final void setScheduledExecutor(ScheduledExecutorService scheduledExecutor) {
+		this.scheduledExecutor =
+				(scheduledExecutor != null ? scheduledExecutor : Executors.newSingleThreadScheduledExecutor());
+	}
+
+
+	public ScheduledFuture schedule(Runnable task, Trigger trigger) {
+		try {
+			return new ReschedulingRunnable(task, trigger, this.scheduledExecutor).schedule();
+		}
+		catch (RejectedExecutionException ex) {
+			throw new TaskRejectedException("Executor [" + this.scheduledExecutor + "] did not accept task: " + task, ex);
+		}
+	}
+
+	public ScheduledFuture schedule(Runnable task, Date startTime) {
+		long initialDelay = startTime.getTime() - System.currentTimeMillis();
+		try {
+			return this.scheduledExecutor.schedule(task, initialDelay, TimeUnit.MILLISECONDS);
+		}
+		catch (RejectedExecutionException ex) {
+			throw new TaskRejectedException("Executor [" + this.scheduledExecutor + "] did not accept task: " + task, ex);
+		}
+	}
+
+	public ScheduledFuture scheduleAtFixedRate(Runnable task, Date startTime, long period) {
+		long initialDelay = startTime.getTime() - System.currentTimeMillis();
+		try {
+			return this.scheduledExecutor.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.MILLISECONDS);
+		}
+		catch (RejectedExecutionException ex) {
+			throw new TaskRejectedException("Executor [" + this.scheduledExecutor + "] did not accept task: " + task, ex);
+		}
+	}
+
+	public ScheduledFuture scheduleAtFixedRate(Runnable task, long period) {
+		try {
+			return this.scheduledExecutor.scheduleAtFixedRate(task, 0, period, TimeUnit.MILLISECONDS);
+		}
+		catch (RejectedExecutionException ex) {
+			throw new TaskRejectedException("Executor [" + this.scheduledExecutor + "] did not accept task: " + task, ex);
+		}
+	}
+
+	public ScheduledFuture scheduleWithFixedDelay(Runnable task, Date startTime, long delay) {
+		long initialDelay = startTime.getTime() - System.currentTimeMillis();
+		try {
+			return this.scheduledExecutor.scheduleWithFixedDelay(task, initialDelay, delay, TimeUnit.MILLISECONDS);
+		}
+		catch (RejectedExecutionException ex) {
+			throw new TaskRejectedException("Executor [" + this.scheduledExecutor + "] did not accept task: " + task, ex);
+		}
+	}
+
+	public ScheduledFuture scheduleWithFixedDelay(Runnable task, long delay) {
+		try {
+			return this.scheduledExecutor.scheduleWithFixedDelay(task, 0, delay, TimeUnit.MILLISECONDS);
+		}
+		catch (RejectedExecutionException ex) {
+			throw new TaskRejectedException("Executor [" + this.scheduledExecutor + "] did not accept task: " + task, ex);
+		}
+	}
+
+}
---
+++ org.springframework.context/src/main/java/org/springframework/scheduling/support/SimpleTriggerContext.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.scheduling.support;
+
+import java.util.Date;
+
+import org.springframework.scheduling.TriggerContext;
+
+/**
+ * Simple data holder implementation of the {@link TriggerContext} interface.
+ *
+ * @author Juergen Hoeller
+ * @since 3.0
+ */
+public class SimpleTriggerContext implements TriggerContext {
+
+	private volatile Date lastScheduledExecutionTime;
+
+	private volatile Date lastActualExecutionTime;
+
+	private volatile Date lastCompletionTime;
+
+
+	/**
+	 * Update this holder's state with the latest time values.
+ 	 * @param lastScheduledExecutionTime last <i>scheduled</i> execution time
+	 * @param lastActualExecutionTime last <i>actual</i> execution time
+	 * @param lastCompletionTime last completion time
+	 */
+	public void update(Date lastScheduledExecutionTime, Date lastActualExecutionTime, Date lastCompletionTime) {
+		this.lastScheduledExecutionTime = lastScheduledExecutionTime;
+		this.lastActualExecutionTime = lastActualExecutionTime;
+		this.lastCompletionTime = lastCompletionTime;
+	}
+
+
+	public Date lastScheduledExecutionTime() {
+		return this.lastScheduledExecutionTime;
+	}
+
+	public Date lastActualExecutionTime() {
+		return this.lastActualExecutionTime;
+	}
+
+	public Date lastCompletionTime() {
+		return this.lastCompletionTime;
+	}
+
+}
