463f1e07debbfe57c14eb95dcf13afb7082d6e70
Chris Beams <cbeams@vmware.com>
2009-03-30 16:02:04 +0000

    polishing @Configuration tests
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@872 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.context/src/test/java/org/springframework/context/annotation/configuration/ConfigurationClassProcessingTests.java
+++ org.springframework.context/src/test/java/org/springframework/context/annotation/configuration/ConfigurationClassProcessingTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.context.annotation.configuration;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -21,7 +37,13 @@ import test.beans.ITestBean;
 import test.beans.TestBean;
 
 
-public class BasicTests {
+/**
+ * Miscellaneous system tests covering {@link Bean} naming, aliases, scoping and error
+ * handling within {@link Configuration} class definitions.
+ *
+ * @author Chris Beams
+ */
+public class ConfigurationClassProcessingTests {
 
 	/**
 	 * Creates a new {@link BeanFactory}, populates it with a {@link BeanDefinition} for
--- org.springframework.context/src/test/java/org/springframework/context/annotation/configuration/PolymorphicConfigurationTests.java
+++ org.springframework.context/src/test/java/org/springframework/context/annotation/configuration/PolymorphicConfigurationTests.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.context.annotation.configuration;
 
 import java.lang.annotation.Inherited;
