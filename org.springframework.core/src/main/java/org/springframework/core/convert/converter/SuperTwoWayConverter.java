d38c0d301cd5f3b091d5921a10ef1c00fd93c7a6
Keith Donald <kdonald@vmware.com>
2009-03-31 15:55:42 +0000

    polish

--- org.springframework.core/src/main/java/org/springframework/core/convert/converter/SuperTwoWayConverter.java
+++ org.springframework.core/src/main/java/org/springframework/core/convert/converter/SuperTwoWayConverter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.core.convert.converter;
 
 import org.springframework.core.convert.ConversionException;
@@ -14,10 +29,10 @@ import org.springframework.core.convert.ConversionService;
 public interface SuperTwoWayConverter<S, T> extends SuperConverter<S, T> {
 
 	/**
-	 * Convert the target of type T to an instance of S.
+	 * Convert the target of type T to an instance of RS.
 	 * @param target the target object to convert, whose class must be equal to or a subclass of T
 	 * @param sourceClass the requested source class to convert to, which must be equal to S or extend from S
-	 * @return the converted object, which must be an instance of S
+	 * @return the converted object, which must be an instance of RS
 	 * @throws Exception an exception occurred performing the conversion; may be any checked exception, the conversion
 	 * system will handle wrapping the failure in a {@link ConversionException} that provides a consistent type
 	 * conversion error context
