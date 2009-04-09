884ff14359418ecacebd05f3af1b3ebff12edc95
Juergen Hoeller <jhoeller@vmware.com>
2009-04-09 18:24:07 +0000

    fixed javadoc
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@962 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.beans/src/main/java/org/springframework/beans/BeanWrapper.java
+++ org.springframework.beans/src/main/java/org/springframework/beans/BeanWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,7 +39,6 @@ import java.beans.PropertyDescriptor;
  * @author Rod Johnson
  * @author Juergen Hoeller
  * @since 13 April 2001
- * @see #setExtractOldValueForEditor
  * @see PropertyAccessor
  * @see PropertyEditorRegistry
  * @see PropertyAccessorFactory#forBeanPropertyAccess
@@ -77,6 +76,6 @@ public interface BeanWrapper extends ConfigurablePropertyAccessor {
 	 * @return the property descriptor for the specified property
 	 * @throws InvalidPropertyException if there is no such property
 	 */
-	PropertyDescriptor getPropertyDescriptor(String propertyName) throws BeansException;
+	PropertyDescriptor getPropertyDescriptor(String propertyName) throws InvalidPropertyException;
 
 }
