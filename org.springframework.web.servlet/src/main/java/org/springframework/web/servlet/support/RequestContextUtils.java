0926cf79e6544ec4e57ebff9972d032e921dedba
Juergen Hoeller <jhoeller@vmware.com>
2009-04-19 22:28:31 +0000

    revised WebApplicationContext lookup
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1032 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.web.servlet/src/main/java/org/springframework/web/servlet/support/RequestContextUtils.java
+++ org.springframework.web.servlet/src/main/java/org/springframework/web/servlet/support/RequestContextUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package org.springframework.web.servlet.support;
 
 import java.util.Locale;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
@@ -79,10 +78,7 @@ public abstract class RequestContextUtils {
 			if (servletContext == null) {
 				throw new IllegalStateException("No WebApplicationContext found: not in a DispatcherServlet request?");
 			}
-			webApplicationContext = WebApplicationContextUtils.getWebApplicationContext(servletContext);
-			if (webApplicationContext == null) {
-				throw new IllegalStateException("No WebApplicationContext found: no ContextLoaderListener registered?");
-			}
+			webApplicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
 		}
 		return webApplicationContext;
 	}
--- org.springframework.web.servlet/src/main/java/org/springframework/web/servlet/view/tiles2/SpringBeanPreparerFactory.java
+++ org.springframework.web.servlet/src/main/java/org/springframework/web/servlet/view/tiles2/SpringBeanPreparerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,7 @@ public class SpringBeanPreparerFactory extends AbstractSpringPreparerFactory {
 
 	@Override
 	protected ViewPreparer getPreparer(String name, WebApplicationContext context) throws TilesException {
-		return (ViewPreparer) context.getBean(name, ViewPreparer.class);
+		return context.getBean(name, ViewPreparer.class);
 	}
 
 }
