1b8294a3793db32fe3e3ff0fa9ddd0530e3df0d9
Juergen Hoeller <jhoeller@vmware.com>
2009-04-19 20:35:32 +0000

    moved Scope annotation to local test package
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1029 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.core/src/test/java/org/springframework/core/type/Scope.java
+++ org.springframework.core/src/test/java/org/springframework/core/type/Scope.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.context.annotation;
+package org.springframework.core.type;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
