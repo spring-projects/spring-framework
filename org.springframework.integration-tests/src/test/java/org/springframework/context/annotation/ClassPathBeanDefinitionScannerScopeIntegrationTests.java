b1ecb0de451a0da135e9c98003b5c967e8ce5aee
Juergen Hoeller <jhoeller@vmware.com>
2009-04-19 20:36:33 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1030 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.integration-tests/src/test/java/org/springframework/context/annotation/ClassPathBeanDefinitionScannerScopeIntegrationTests.java
+++ org.springframework.integration-tests/src/test/java/org/springframework/context/annotation/ClassPathBeanDefinitionScannerScopeIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,11 +16,11 @@
 
 package org.springframework.context.annotation;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
