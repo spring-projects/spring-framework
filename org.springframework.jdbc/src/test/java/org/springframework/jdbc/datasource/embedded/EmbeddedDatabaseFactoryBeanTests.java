4bfcd161303df9f1d4027fb01d25892cf340f631
Keith Donald <kdonald@vmware.com>
2009-05-06 09:14:25 +0000

    polish

--- org.springframework.jdbc/src/test/java/org/springframework/jdbc/datasource/embedded/EmbeddedDatabaseFactoryBeanTests.java
+++ org.springframework.jdbc/src/test/java/org/springframework/jdbc/datasource/embedded/EmbeddedDatabaseFactoryBeanTests.java
@@ -6,16 +6,19 @@ import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class EmbeddedDatabaseFactoryBeanTests {
-	
+
 	@Test
 	public void testFactoryBeanLifecycle() throws Exception {
 		EmbeddedDatabaseFactoryBean bean = new EmbeddedDatabaseFactoryBean();
 		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("db-schema.sql", getClass()));
-		populator.addScript(new ClassPathResource("db-test-data.sql", getClass()));
+		populator.setScripts(new Resource[] {
+			new ClassPathResource("db-schema.sql", getClass()),
+			new ClassPathResource("db-test-data.sql", getClass())
+		});
 		bean.setDatabasePopulator(populator);
 		bean.afterPropertiesSet();
 		DataSource ds = bean.getObject();
