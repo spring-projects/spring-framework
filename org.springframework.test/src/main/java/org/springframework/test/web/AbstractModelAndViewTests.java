a600ccc38ceec3cd43c276be7a6cbf7f12a99f7b
Sam Brannen <sam@sambrannen.com>
2009-05-06 06:25:03 +0000

    [SPR-5145] Updated reference manual regarding upgrade to JUnit 4.5; additional improvements in the testing chapter as well.
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1110 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.test/src/main/java/org/springframework/test/web/AbstractModelAndViewTests.java
+++ org.springframework.test/src/main/java/org/springframework/test/web/AbstractModelAndViewTests.java
@@ -28,11 +28,11 @@ import org.springframework.web.servlet.ModelAndView;
 /**
  * Convenient JUnit 3.8 base class for tests dealing with Spring Web MVC
  * {@link org.springframework.web.servlet.ModelAndView ModelAndView} objects.
- *
+ * 
  * <p>All <code>assert*()</code> methods throw {@link AssertionFailedError}s.
- *
+ * 
  * <p>Consider the use of {@link ModelAndViewAssert} with JUnit 4 and TestNG.
- *
+ * 
  * @author Alef Arendsen
  * @author Bram Smeets
  * @author Sam Brannen
@@ -41,6 +41,7 @@ import org.springframework.web.servlet.ModelAndView;
  * @see ModelAndViewAssert
  * @deprecated as of Spring 3.0, in favor of using the listener-based test context framework
  * ({@link org.springframework.test.context.junit38.AbstractJUnit38SpringContextTests})
+ * or {@link ModelAndViewAssert} with JUnit 4 and TestNG.
  */
 @Deprecated
 public abstract class AbstractModelAndViewTests extends TestCase {
