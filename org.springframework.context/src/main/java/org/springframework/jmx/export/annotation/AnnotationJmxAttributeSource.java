5a29d0355e8ac538718c3b91e924e48ec374538c
Juergen Hoeller <jhoeller@vmware.com>
2009-05-12 16:14:09 +0000

    removed outdated Attributes references
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1158 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.context/src/main/java/org/springframework/jmx/export/annotation/AnnotationJmxAttributeSource.java
+++ org.springframework.context/src/main/java/org/springframework/jmx/export/annotation/AnnotationJmxAttributeSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,8 +45,6 @@ import org.springframework.util.StringUtils;
  * @see org.springframework.jmx.export.annotation.ManagedResource
  * @see org.springframework.jmx.export.annotation.ManagedAttribute
  * @see org.springframework.jmx.export.annotation.ManagedOperation
- * @see org.springframework.jmx.export.metadata.AttributesJmxAttributeSource
- * @see org.springframework.metadata.commons.CommonsAttributes
  */
 public class AnnotationJmxAttributeSource implements JmxAttributeSource {
 
