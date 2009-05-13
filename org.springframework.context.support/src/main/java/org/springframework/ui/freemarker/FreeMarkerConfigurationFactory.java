e0384568cc66c18fb26c57b1fb844ab6ed75dd2e
Juergen Hoeller <jhoeller@vmware.com>
2009-05-13 14:26:56 +0000

    Velocity/FreeMarker/TilesViewResolver only return a view if the target resource exists now
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1172 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.context.support/src/main/java/org/springframework/ui/freemarker/FreeMarkerConfigurationFactory.java
+++ org.springframework.context.support/src/main/java/org/springframework/ui/freemarker/FreeMarkerConfigurationFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -383,7 +383,7 @@ public class FreeMarkerConfigurationFactory {
 	 * @see #createConfiguration()
 	 * @see #setPostTemplateLoaders
 	 */
-	protected void postProcessTemplateLoaders(List templateLoaders) {
+	protected void postProcessTemplateLoaders(List<TemplateLoader> templateLoaders) {
 	}
 
 	/**
