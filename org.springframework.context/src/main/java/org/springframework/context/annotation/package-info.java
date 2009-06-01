b0d11e8c1518f0f15e42a6822efd97ffc459339d
Mark Fisher <markfisher@vmware.com>
2009-06-01 18:47:52 +0000

    Added package declarations and comments to package-info.java files where they had been missing.

---
+++ org.springframework.context/src/main/java/org/springframework/context/annotation/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * 
+ * Annotation support for the Application Context, including JSR-250 "common"
+ * annotations, component-scanning, and Java-based metadata for creating
+ * Spring-managed objects.
+ * 
+ */
+package org.springframework.context.annotation;
+
