c254924b4c65edf575f367f89ffc0afd447fdc9e
Arjen Poutsma <apoutsma@vmware.com>
2009-06-15 09:30:46 +0000

    Added @Override

--- org.springframework.web.servlet/src/test/java/org/springframework/mock/web/DelegatingServletOutputStream.java
+++ org.springframework.web.servlet/src/test/java/org/springframework/mock/web/DelegatingServletOutputStream.java
@@ -54,15 +54,18 @@ public class DelegatingServletOutputStream extends ServletOutputStream {
 	}
 
 
+	@Override
 	public void write(int b) throws IOException {
 		this.targetStream.write(b);
 	}
 
+	@Override
 	public void flush() throws IOException {
 		super.flush();
 		this.targetStream.flush();
 	}
 
+	@Override
 	public void close() throws IOException {
 		super.close();
 		this.targetStream.close();
