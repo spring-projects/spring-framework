398729cda1da9ddea58eb1a506c4bad8f77e63c3
Arjen Poutsma <apoutsma@vmware.com>
2009-06-24 13:55:36 +0000

    SPR-5853 - JSON formatting view for Spring MVC

---
+++ org.springframework.web.servlet/src/main/java/org/springframework/web/servlet/view/json/BindingJacksonJsonView.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.web.servlet.view.json;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.JsonEncoding;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializerFactory;
+
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.AbstractView;
+
+/**
+ * Spring-MVC {@link View} that renders JSON content by serializing the model for the current request using <a
+ * href="http://jackson.codehaus.org/">Jackson's</a> {@link ObjectMapper}.
+ *
+ * <p>By default, the entire contents of the model map (with the exception of framework-specific classes) will be
+ * encoded as JSON. For cases where the contents of the map need to be filtered, users may specify a specific set of
+ * model attributes to encode via the {@link #setRenderedAttributes(Set) includeAttributes} property.
+ *
+ * @author Jeremy Grelle
+ * @author Arjen Poutsma
+ * @see org.springframework.http.converter.json.BindingJacksonHttpMessageConverter
+ * @since 3.0
+ */
+public class BindingJacksonJsonView extends AbstractView {
+
+	/**
+	 * Default content type. Overridable as bean property.
+	 */
+	public static final String DEFAULT_CONTENT_TYPE = "application/json";
+
+	private ObjectMapper objectMapper = new ObjectMapper();
+
+	private JsonEncoding encoding = JsonEncoding.UTF8;
+
+	private boolean prefixJson = false;
+
+	private Set<String> renderedAttributes;
+
+	/**
+	 * Construct a new {@code JacksonJsonView}, setting the content type to {@code application/json}.
+	 */
+	public BindingJacksonJsonView() {
+		setContentType(DEFAULT_CONTENT_TYPE);
+	}
+
+	/**
+	 * Sets the {@code ObjectMapper} for this view. If not set, a default {@link ObjectMapper#ObjectMapper() ObjectMapper}
+	 * is used.
+	 *
+	 * <p>Setting a custom-configured {@code ObjectMapper} is one way to take further control of the JSON serialization
+	 * process. For example, an extended {@link SerializerFactory} can be configured that provides custom serializers for
+	 * specific types. The other option for refining the serialization process is to use Jackson's provided annotations on
+	 * the types to be serialized, in which case a custom-configured ObjectMapper is unnecessary.
+	 */
+	public void setObjectMapper(ObjectMapper objectMapper) {
+		Assert.notNull(objectMapper, "'objectMapper' must not be null");
+		this.objectMapper = objectMapper;
+	}
+
+	/**
+	 * Sets the {@code JsonEncoding} for this converter. By default, {@linkplain JsonEncoding#UTF8 UTF-8} is used.
+	 */
+	public void setEncoding(JsonEncoding encoding) {
+		Assert.notNull(encoding, "'encoding' must not be null");
+		this.encoding = encoding;
+	}
+
+	/**
+	 * Indicates whether the JSON output by this view should be prefixed with "{@code {} &&}". Default is false.
+	 *
+	 * <p> Prefixing the JSON string in this manner is used to help prevent JSON Hijacking. The prefix renders the string
+	 * syntactically invalid as a script so that it cannot be hijacked. This prefix does not affect the evaluation of JSON,
+	 * but if JSON validation is performed on the string, the prefix would need to be ignored.
+	 */
+	public void setPrefixJson(boolean prefixJson) {
+		this.prefixJson = prefixJson;
+	}
+
+	/**
+	 * Sets the attributes in the model that should be rendered by this view. When set, all other model attributes will be
+	 * ignored.
+	 */
+	public void setRenderedAttributes(Set<String> renderedAttributes) {
+		this.renderedAttributes = renderedAttributes;
+	}
+
+	@Override
+	protected void prepareResponse(HttpServletRequest request, HttpServletResponse response) {
+		response.setContentType(getContentType());
+		response.setCharacterEncoding(encoding.getJavaName());
+	}
+
+	@Override
+	protected void renderMergedOutputModel(Map<String, Object> model,
+			HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		model = filterModel(model);
+		JsonGenerator generator =
+				objectMapper.getJsonFactory().createJsonGenerator(response.getOutputStream(), encoding);
+		if (prefixJson) {
+			generator.writeRaw("{} && ");
+		}
+		objectMapper.writeValue(generator, model);
+	}
+
+	/**
+	 * Filters out undesired attributes from the given model.
+	 *
+	 * <p>Default implementation removes {@link BindingResult} instances and entries not included in the {@link
+	 * #setRenderedAttributes(Set) renderedAttributes} property.
+	 */
+	protected Map<String, Object> filterModel(Map<String, Object> model) {
+		Map<String, Object> result = new HashMap<String, Object>(model.size());
+		if (CollectionUtils.isEmpty(renderedAttributes)) {
+			renderedAttributes = model.keySet();
+		}
+		for (Map.Entry<String, Object> entry : model.entrySet()) {
+			if (!(entry instanceof BindingResult) && renderedAttributes.contains(entry.getKey())) {
+				result.put(entry.getKey(), entry.getValue());
+			}
+		}
+		return result;
+	}
+
+}
