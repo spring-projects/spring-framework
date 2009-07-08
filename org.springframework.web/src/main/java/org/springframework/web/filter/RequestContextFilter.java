5c19909c610d7ba44858b1be72ffda94b776b2c5
Juergen Hoeller <jhoeller@vmware.com>
2009-07-08 15:11:15 +0000

    preserve custom pre-bound RequestAttributes implementations (for Grails)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1490 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.web/src/main/java/org/springframework/web/filter/RequestContextFilter.java
+++ org.springframework.web/src/main/java/org/springframework/web/filter/RequestContextFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package org.springframework.web.filter;
 
 import java.io.IOException;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -84,8 +83,8 @@ public class RequestContextFilter extends OncePerRequestFilter {
 			filterChain.doFilter(request, response);
 		}
 		finally {
-			RequestContextHolder.resetRequestAttributes();
 			LocaleContextHolder.resetLocaleContext();
+			RequestContextHolder.resetRequestAttributes();
 			attributes.requestCompleted();
 			if (logger.isDebugEnabled()) {
 				logger.debug("Cleared thread-bound request context: " + request);
