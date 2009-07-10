0d98baa5c1b83d99cdf45f9d3c2a55902630f678
Mark Pollack <mpollack@vmware.com>
2009-07-10 04:02:08 +0000

    SPR-5619

--- org.springframework.beans/src/test/java/org/springframework/beans/factory/config/PropertyResourceConfigurerTests.java
+++ org.springframework.beans/src/test/java/org/springframework/beans/factory/config/PropertyResourceConfigurerTests.java
@@ -659,29 +659,6 @@ public final class PropertyResourceConfigurerTests {
 		assertEquals("mytest", tb.getTouchy());
 	}
 
-	@Ignore // this test was breaking after the 3.0 repackaging
-	@Test
-	public void testPropertyPlaceholderConfigurerWithAutowireByType() {
-//		StaticApplicationContext ac = new StaticApplicationContext();
-//		MutablePropertyValues pvs = new MutablePropertyValues();
-//		pvs.addPropertyValue("touchy", "${test}");
-//		ac.registerSingleton("tb", TestBean.class, pvs);
-//		pvs = new MutablePropertyValues();
-//		pvs.addPropertyValue("target", new RuntimeBeanReference("tb"));
-//		// uncomment when fixing this test
-//		// ac.registerSingleton("tbProxy", org.springframework.aop.framework.ProxyFactoryBean.class, pvs);
-//		pvs = new MutablePropertyValues();
-//		Properties props = new Properties();
-//		props.put("test", "mytest");
-//		pvs.addPropertyValue("properties", new Properties(props));
-//		RootBeanDefinition ppcDef = new RootBeanDefinition(PropertyPlaceholderConfigurer.class, pvs);
-//		ppcDef.setAutowireMode(RootBeanDefinition.AUTOWIRE_BY_TYPE);
-//		ac.registerBeanDefinition("configurer", ppcDef);
-//		ac.refresh();
-//		TestBean tb = (TestBean) ac.getBean("tb");
-//		assertEquals("mytest", tb.getTouchy());
-	}
-
 	@Test
 	public void testPropertyPlaceholderConfigurerWithAliases() {
 		factory.registerBeanDefinition("tb",
