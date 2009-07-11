29327798c11c6631009ab91d1e49d263fd8c7553
Sam Brannen <sam@sambrannen.com>
2009-07-11 16:45:48 +0000

    [SPR-5699] Investigating issues surrounding automatic flushing of the underlying ORM tool's session via TransactionalTestExecutionListener.

---
+++ org.springframework.test/src/test/java/org/springframework/test/context/junit4/orm/db-schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE drivers_license IF EXISTS;
+DROP TABLE person IF EXISTS;
+
+CREATE TABLE person (
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	drivers_license_id INTEGER NOT NULL
+);
+CREATE UNIQUE INDEX person_name ON person(name);
+CREATE UNIQUE INDEX person_drivers_license_id ON person(drivers_license_id);
+
+CREATE TABLE drivers_license (
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	license_number INTEGER NOT NULL
+);
+CREATE UNIQUE INDEX drivers_license_license_number ON drivers_license(license_number);
