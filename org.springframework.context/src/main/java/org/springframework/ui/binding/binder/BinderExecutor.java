14018541e2ebd7ed5340447bf1e5135b4ecb2968
Keith Donald <kdonald@vmware.com>
2009-07-23 13:43:23 +0000

    polish

--- org.springframework.context/src/main/java/org/springframework/ui/binding/binder/BinderExecutor.java
+++ org.springframework.context/src/main/java/org/springframework/ui/binding/binder/BinderExecutor.java
@@ -1,20 +1,57 @@
+/*
+ * Copyright 2004-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.ui.binding.binder;
 
 import org.springframework.ui.binding.config.BindingRuleConfiguration;
 
+/**
+ * A SPI interface that lets you configure a model binder, then execute it.
+ * Hides details about the source of binder source values.
+ * @author Keith Donald
+ * @param <M> the type of model to bind to
+ */
 public interface BinderExecutor<M> {
 	
+	/**
+	 * Configure the model object to bind to.
+	 * @param model the model
+	 */
 	void setModel(M model);
 	
+	/**
+	 * Add a binding rule for the model property.
+	 * @param property the model property
+	 * @return a builder API for configuring the rule
+	 */
 	BindingRuleConfiguration bindingRule(String property);
 
 	// TODO allow injection of pre-created BindingRules
-	
+
+	/**
+	 * Execute the bind operation.
+	 * @return the binding results
+	 */
 	BindingResults bind();
 
 	// TODO return validation results
 	void validate();
 	
+	/**
+	 * The model that was bound to.
+	 */
 	M getModel();
 	
 }
