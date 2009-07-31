53c8646b54e09226ee33db9e0d4146eef9dcf7ab
Keith Donald <kdonald@vmware.com>
2009-07-31 04:10:22 +0000

    moved multipart and bind to web project since independent of servlet and required by new native web request parameter map
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1666 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.web.servlet/src/main/java/org/springframework/web/bind/annotation/package-info.java
+++
@@ -1,9 +0,0 @@
-
-/**
- *
- * Annotations for binding requests to controllers and handler methods
- * as well as for binding request parameters to method arguments.
- *
- */
-package org.springframework.web.bind.annotation;
-
