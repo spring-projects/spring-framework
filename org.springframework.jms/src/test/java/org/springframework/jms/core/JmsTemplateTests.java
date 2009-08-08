6bb68a80f76be69aa51fe0b2b4cb1a17605f13c7
Juergen Hoeller <jhoeller@vmware.com>
2009-08-08 01:33:01 +0000

    JMS SingleConnectionFactory uses minimized number of start calls (for Oracle AQ; SPR-5987)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1718 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.jms/src/test/java/org/springframework/jms/core/JmsTemplateTests.java
+++ org.springframework.jms/src/test/java/org/springframework/jms/core/JmsTemplateTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -275,7 +275,7 @@ public class JmsTemplateTests extends TestCase {
 		template.setConnectionFactory(scf);
 
 		mockConnection.start();
-		connectionControl.setVoidCallable(3);
+		connectionControl.setVoidCallable(1);
 		// We're gonna call getTransacted 3 times, i.e. 2 more times.
 		mockSession.getTransacted();
 		sessionControl.setReturnValue(useTransactedSession(), 2);
