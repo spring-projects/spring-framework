427edfe93cf1649c85ae04757bd16230262e756c
Juergen Hoeller <jhoeller@vmware.com>
2009-08-24 13:36:32 +0000

    fixed package-info

--- org.springframework.context/src/main/java/org/springframework/ui/format/support/package-info.java
+++ org.springframework.context/src/main/java/org/springframework/ui/format/support/package-info.java
@@ -1,6 +1,5 @@
 /**
- * Support classes for the formatting package, providing
- * common implementations as well as adapters.
+ * Support classes for the formatting package,
+ * providing common implementations as well as adapters.
  */
-package org.springframework.ui.format;
-
+package org.springframework.ui.format.support;
