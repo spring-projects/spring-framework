9183af2c2e97641344acb379d0b620275fd8e295
Sam Brannen <sam@sambrannen.com>
2009-08-24 12:14:57 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1749 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.test/src/main/java/org/springframework/test/context/junit4/AbstractTransactionalJUnit4SpringContextTests.java
+++ org.springframework.test/src/main/java/org/springframework/test/context/junit4/AbstractTransactionalJUnit4SpringContextTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,7 +77,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @see org.springframework.test.context.junit38.AbstractTransactionalJUnit38SpringContextTests
  * @see org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests
  */
-@TestExecutionListeners( { TransactionalTestExecutionListener.class })
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
 @Transactional
 public abstract class AbstractTransactionalJUnit4SpringContextTests extends AbstractJUnit4SpringContextTests {
 
