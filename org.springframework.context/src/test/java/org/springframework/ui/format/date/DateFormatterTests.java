04b619ebfbc1e08a9e9bdc54263b3ece21f4aa7f
Juergen Hoeller <jhoeller@vmware.com>
2009-08-24 13:48:21 +0000

    fixed accidental test failures

--- org.springframework.context/src/test/java/org/springframework/ui/format/date/DateFormatterTests.java
+++ org.springframework.context/src/test/java/org/springframework/ui/format/date/DateFormatterTests.java
@@ -1,17 +1,34 @@
-package org.springframework.ui.format.date;
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import static org.junit.Assert.assertEquals;
+package org.springframework.ui.format.date;
 
 import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Locale;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
-import org.springframework.ui.format.date.DateFormatter;
 
+/**
+ * @author Keith Donald
+ */
 public class DateFormatterTests {
 
-	private DateFormatter formatter = new DateFormatter();
+	private DateFormatter formatter = new DateFormatter("yyyy-MM-dd");
 	
 	@Test
 	public void formatValue() {
