9bfc5b6d284fe31c2882bd0b7f51ff72a90a81d7
Sam Brannen <sam@sambrannen.com>
2009-08-26 23:23:07 +0000

    [SPR-6043] @NotTransactional is now deprecated
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1769 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.test/src/main/java/org/springframework/test/annotation/NotTransactional.java
+++ org.springframework.test/src/main/java/org/springframework/test/annotation/NotTransactional.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,14 +23,21 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Test annotation to indicate that a method is not transactional.
+ * Test annotation to indicate that a method is <i>not transactional</i>.
  * 
  * @author Rod Johnson
  * @author Sam Brannen
  * @since 2.0
+ * @deprecated as of Spring 3.0, in favor of moving the non-transactional test
+ * method to a separate (non-transactional) test class or to a
+ * {@link org.springframework.test.context.transaction.BeforeTransaction
+ * &#64;BeforeTransaction} or
+ * {@link org.springframework.test.context.transaction.AfterTransaction
+ * &#64;AfterTransaction} method.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
+@Deprecated
 public @interface NotTransactional {
 }
