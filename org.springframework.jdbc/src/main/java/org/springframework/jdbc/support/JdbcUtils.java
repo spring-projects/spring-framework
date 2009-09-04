5eb2138a6173a2d2d2d6ec47785df39f4f5e0847
Thomas Risberg <trisberg@vmware.com>
2009-09-04 18:37:50 +0000

    revised the detection of Sybase (SPR-6053)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1821 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.jdbc/src/main/java/org/springframework/jdbc/support/JdbcUtils.java
+++ org.springframework.jdbc/src/main/java/org/springframework/jdbc/support/JdbcUtils.java
@@ -387,7 +387,9 @@ public abstract class JdbcUtils {
 			name = "DB2";
 		}
 		else if ("Sybase SQL Server".equals(source) ||
-				"Adaptive Server Enterprise".equals(source) || "sql server".equals(source) ) {
+				"Adaptive Server Enterprise".equals(source) || 
+				"ASE".equals(source) || 
+				"sql server".equalsIgnoreCase(source) ) {
 			name = "Sybase";
 		}
 		return name;
