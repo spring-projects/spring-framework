86ccb01bc2c376f9c37f662a0091ca1cee28f43a
Rob Harrop <rharrop@vmware.com>
2009-09-08 16:29:43 +0000

    [SPR-6025] added SystemPropertyUtilsTests
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1846 50f2f4bb-b051-0410-bef5-90022cba6387

---
+++ org.springframework.core/src/test/java/org/springframework/util/SystemPropertyUtilsTests.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.util;
+
+import java.util.Map;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/** @author Rob Harrop */
+public class SystemPropertyUtilsTests {
+
+	@Test
+	public void testReplaceFromSystemProperty() {
+		System.setProperty("test.prop", "bar");
+		String resolved = SystemPropertyUtils.resolvePlaceholders("${test.prop}");
+		assertEquals("bar", resolved);
+	}
+
+	@Test
+	public void testReplaceFromEnv() {
+		Map<String,String> env = System.getenv();
+		if(env.containsKey("PATH")) {
+			String text = "${PATH}";
+			assertEquals(env.get("PATH"), SystemPropertyUtils.resolvePlaceholders(text));
+		}
+	}
+}
