ceca0db6459fca13519336ad8e62691fa1ace332
Thomas Risberg <trisberg@vmware.com>
2009-09-08 16:19:34 +0000

    added synchronization to compile method (SPR-6001)

--- org.springframework.jdbc/src/main/java/org/springframework/jdbc/core/simple/AbstractJdbcCall.java
+++ org.springframework.jdbc/src/main/java/org/springframework/jdbc/core/simple/AbstractJdbcCall.java
@@ -249,7 +249,7 @@ public abstract class AbstractJdbcCall {
 	 * @throws org.springframework.dao.InvalidDataAccessApiUsageException if the object hasn't
 	 * been correctly initialized, for example if no DataSource has been provided
 	 */
-	public final void compile() throws InvalidDataAccessApiUsageException {
+	public synchronized final void compile() throws InvalidDataAccessApiUsageException {
 		if (!isCompiled()) {
 			if (getProcedureName() == null) {
 				throw new InvalidDataAccessApiUsageException("Procedure or Function name is required");
