4342f509469233d96910185b97a236cfbc1eef29
Keith Donald <kdonald@vmware.com>
2009-09-18 20:08:45 +0000

    generic converter updates
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1933 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.core/src/main/java/org/springframework/core/convert/support/MatchableGenericConverter.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.core.convert.support;
-
-import org.springframework.core.convert.TypeDescriptor;
-
-public interface MatchableGenericConverter extends GenericConverter {
-
-	boolean matches(TypeDescriptor sourceType, TypeDescriptor targetType);
-
-}
