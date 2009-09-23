5446706ec7693667c1641de8a4cd92c67f074bd7
Juergen Hoeller <jhoeller@vmware.com>
2009-09-23 13:51:14 +0000

    AbstractBeanDefinitionParser can deal with null return value as well (SPR-5872)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1968 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.beans/src/main/java/org/springframework/beans/factory/xml/AbstractBeanDefinitionParser.java
+++ org.springframework.beans/src/main/java/org/springframework/beans/factory/xml/AbstractBeanDefinitionParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,7 +54,7 @@ public abstract class AbstractBeanDefinitionParser implements BeanDefinitionPars
 
 	public final BeanDefinition parse(Element element, ParserContext parserContext) {
 		AbstractBeanDefinition definition = parseInternal(element, parserContext);
-		if (!parserContext.isNested()) {
+		if (definition != null && !parserContext.isNested()) {
 			try {
 				String id = resolveId(element, definition, parserContext);
 				if (!StringUtils.hasText(id)) {
