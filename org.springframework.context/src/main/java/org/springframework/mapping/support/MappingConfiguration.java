092ff94ce73dcb804aa351b1f69886f965283a8f
Juergen Hoeller <jhoeller@vmware.com>
2009-09-24 22:53:17 +0000

    removed 3.1 feature classes
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@1996 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.context/src/main/java/org/springframework/mapping/support/MappingConfiguration.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.mapping.support;
-
-import org.springframework.core.convert.converter.Converter;
-
-public interface MappingConfiguration {
-	MappingConfiguration setConverter(Converter converter);
-}
--- org.springframework.context/src/main/java/org/springframework/model/binder/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * API for binding submitted field values in a single batch operation.
- */
-package org.springframework.model.binder;
-
