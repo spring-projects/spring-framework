a75a975219f9b5760579fb33c14f93fae497629d
Sam Brannen <sam@sambrannen.com>
2009-10-11 15:02:24 +0000

    Pet Clinic test suite now includes VisitsAtomViewTest.

--- org.springframework.samples.petclinic/src/test/java/org/springframework/samples/petclinic/PetClinicSuiteTests.java
+++ org.springframework.samples.petclinic/src/test/java/org/springframework/samples/petclinic/PetClinicSuiteTests.java
@@ -3,12 +3,12 @@ package org.springframework.samples.petclinic;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
 import org.springframework.samples.petclinic.hibernate.HibernateClinicTests;
 import org.springframework.samples.petclinic.jdbc.SimpleJdbcClinicTests;
 import org.springframework.samples.petclinic.jpa.EntityManagerClinicTests;
 import org.springframework.samples.petclinic.jpa.HibernateEntityManagerClinicTests;
 import org.springframework.samples.petclinic.jpa.OpenJpaEntityManagerClinicTests;
+import org.springframework.samples.petclinic.web.VisitsAtomViewTest;
 
 /**
  * JUnit 4 based test suite for all PetClinic tests.
@@ -22,7 +22,8 @@ import org.springframework.samples.petclinic.jpa.OpenJpaEntityManagerClinicTests
 		HibernateClinicTests.class,
 		EntityManagerClinicTests.class,
 		HibernateEntityManagerClinicTests.class,
-		OpenJpaEntityManagerClinicTests.class
+		OpenJpaEntityManagerClinicTests.class,
+		VisitsAtomViewTest.class
 })
 public class PetClinicSuiteTests {
 
