8f6d46d92737fec28f70753b9e8586fa328403f5
Juergen Hoeller <jhoeller@vmware.com>
2009-10-13 20:51:01 +0000

    check against MultipartRequest mixin interface
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2097 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.web/src/main/java/org/springframework/web/bind/ServletRequestDataBinder.java
+++ org.springframework.web/src/main/java/org/springframework/web/bind/ServletRequestDataBinder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import javax.servlet.ServletRequest;
 
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.validation.BindException;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartRequest;
 
 /**
  * Special {@link org.springframework.validation.DataBinder} to perform data binding
@@ -103,8 +103,8 @@ public class ServletRequestDataBinder extends WebDataBinder {
 	 */
 	public void bind(ServletRequest request) {
 		MutablePropertyValues mpvs = new ServletRequestParameterPropertyValues(request);
-		if (request instanceof MultipartHttpServletRequest) {
-			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+		if (request instanceof MultipartRequest) {
+			MultipartRequest multipartRequest = (MultipartRequest) request;
 			bindMultipartFiles(multipartRequest.getFileMap(), mpvs);
 		}
 		doBind(mpvs);
