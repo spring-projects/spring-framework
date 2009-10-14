53100357000e46c388919baf6e1e2c0e9954454f
Arjen Poutsma <apoutsma@vmware.com>
2009-10-14 10:46:29 +0000

    SPR-6214 - Using @ResponseBody throws HttpMediaTypeNotAcceptableException when it supports writing an acceptable MediaType

--- org.springframework.web.servlet/src/test/java/org/springframework/web/servlet/mvc/annotation/ServletAnnotationControllerTests.java
+++ org.springframework.web.servlet/src/test/java/org/springframework/web/servlet/mvc/annotation/ServletAnnotationControllerTests.java
@@ -988,6 +988,20 @@ public class ServletAnnotationControllerTests {
 	}
 
 	@Test
+	public void responseBodyWildCardMediaType() throws ServletException, IOException {
+		initServlet(RequestBodyController.class);
+
+		MockHttpServletRequest request = new MockHttpServletRequest("PUT", "/something");
+		String requestBody = "Hello World";
+		request.setContent(requestBody.getBytes("UTF-8"));
+		request.addHeader("Content-Type", "text/plain; charset=utf-8");
+		request.addHeader("Accept", "*/*");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		servlet.service(request, response);
+		assertEquals(requestBody, response.getContentAsString());
+	}
+
+	@Test
 	public void unsupportedRequestBody() throws ServletException, IOException {
 		initServlet(RequestBodyController.class);
 
