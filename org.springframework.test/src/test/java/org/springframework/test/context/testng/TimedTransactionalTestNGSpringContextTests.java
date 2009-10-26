b3f3c03505271e5378ab0eecf8b73ef4eafa13e9
Sam Brannen <sam@sambrannen.com>
2009-10-26 13:38:59 +0000

    [SPR-6124] Verified that timed transactional tests fail with the TestNG integration in the TestContext framework.
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2182 50f2f4bb-b051-0410-bef5-90022cba6387

---
+++ org.springframework.test/src/test/java/org/springframework/test/context/testng/TimedTransactionalTestNGSpringContextTests.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.test.context.testng;
+
+import static org.springframework.test.transaction.TransactionTestUtils.assertInTransaction;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.Test;
+
+/**
+ * Timed integration tests for
+ * {@link AbstractTransactionalTestNGSpringContextTests}; used to verify claim
+ * raised in <a href="http://jira.springframework.org/browse/SPR-6124"
+ * target="_blank">SPR-6124</a>.
+ * 
+ * @author Sam Brannen
+ * @since 3.0
+ */
+@ContextConfiguration
+public class TimedTransactionalTestNGSpringContextTests extends AbstractTransactionalTestNGSpringContextTests {
+
+	@Test
+	public void testWithoutTimeout() {
+		assertInTransaction(true);
+	}
+
+	// TODO Enable TestNG test with timeout once we have a solution.
+	@Test(timeOut = 10000, enabled = false)
+	public void testWithTimeout() {
+		assertInTransaction(true);
+	}
+
+}
