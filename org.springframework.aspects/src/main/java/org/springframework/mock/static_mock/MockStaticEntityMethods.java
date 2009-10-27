6749e502055328814c8e4824e6d676cdf1f82cf7
Ramnivas Laddad <rladdad@vmware.com>
2009-10-27 17:16:02 +0000

    Moved over static mock testing from Roo; added tests
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2201 50f2f4bb-b051-0410-bef5-90022cba6387

---
+++ org.springframework.aspects/src/main/java/org/springframework/mock/static_mock/MockStaticEntityMethods.java
@@ -0,0 +1,21 @@
+package org.springframework.mock.static_mock;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to indicate a test class for whose @Test methods
+ * static methods on Entity classes should be mocked.
+ * 
+ * @see AbstractMethodMockingControl
+ * 
+ * @author Rod Johnson
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface MockStaticEntityMethods {
+
+}
---
+++ org.springframework.aspects/src/test/java/org/springframework/mock/static_mock/Person.java
@@ -0,0 +1,8 @@
+package org.springframework.mock.static_mock;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Person {
+}
+
