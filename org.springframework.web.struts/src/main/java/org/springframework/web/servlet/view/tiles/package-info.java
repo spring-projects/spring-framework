68ddf0e1dc2a00ef111b4197e11c98176c838b7c
Chris Beams <cbeams@vmware.com>
2009-11-06 23:13:15 +0000

    SPR-5523: converted a few additional package.html artifacts to package-info.java
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2279 50f2f4bb-b051-0410-bef5-90022cba6387

---
+++ org.springframework.web.struts/src/main/java/org/springframework/web/servlet/view/tiles/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Support classes for the integration of
+ * <a href="http://www.lifl.fr/~dumoulin/tiles">Tiles</a>
+ * (included in Struts) as Spring web view technology.
+ * Contains a View implementation for Tiles definitions.
+ */
+
+package org.springframework.web.servlet.view.tiles;
