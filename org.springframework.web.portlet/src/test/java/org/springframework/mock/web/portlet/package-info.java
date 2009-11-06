ca0b590bb949e6cb4c669cbce61a6a9916a3ac79
Chris Beams <cbeams@vmware.com>
2009-11-06 23:13:15 +0000

    SPR-5523: converted a few additional package.html artifacts to package-info.java

---
+++ org.springframework.web.portlet/src/test/java/org/springframework/mock/web/portlet/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * A comprehensive set of Portlet API mock objects,
+ * targeted at usage with Spring's web MVC framework.
+ * Useful for testing web contexts and controllers.
+ *
+ * <p>More convenient to use than dynamic mock objects
+ * (<a href="http://www.easymock.org">EasyMock</a>) or
+ * existing Portlet API mock objects.
+ */
+
+package org.springframework.mock.web.portlet;
