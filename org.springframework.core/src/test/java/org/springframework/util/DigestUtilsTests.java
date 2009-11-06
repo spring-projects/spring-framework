d415d3619230217f926ca66df0ff35718ce06aad
Arjen Poutsma <apoutsma@vmware.com>
2009-11-06 11:52:53 +0000

    Renamed Md5HashUtils to DigestUtils

--- org.springframework.core/src/test/java/org/springframework/util/DigestUtilsTests.java
+++ org.springframework.core/src/test/java/org/springframework/util/DigestUtilsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright ${YEAR} the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,12 +18,11 @@ package org.springframework.util;
 
 import java.io.UnsupportedEncodingException;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class Md5HashUtilsTests {
+public class DigestUtilsTests {
 
 	private byte[] bytes;
 
@@ -33,23 +32,23 @@ public class Md5HashUtilsTests {
 	}
 
 	@Test
-	public void hash() {
-		byte[] result = Md5HashUtils.getHash(bytes);
+	public void md5() {
+		byte[] result = DigestUtils.md5Digest(bytes);
 		byte[] expected = new byte[]{-0x4f, 0xa, -0x73, -0x4f, 0x64, -0x20, 0x75, 0x41, 0x5, -0x49, -0x57, -0x65, -0x19,
 				0x2e, 0x3f, -0x1b};
 		assertArrayEquals("Invalid hash", expected, result);
 	}
 
 	@Test
-	public void hashString() throws UnsupportedEncodingException {
-		String hash = Md5HashUtils.getHashString(bytes);
+	public void md5Hex() throws UnsupportedEncodingException {
+		String hash = DigestUtils.md5DigestAsHex(bytes);
 		assertEquals("Invalid hash", "b10a8db164e0754105b7a99be72e3fe5", hash);
 	}
 
 	@Test
-	public void hashStringBuilder() throws UnsupportedEncodingException {
+	public void md5StringBuilder() throws UnsupportedEncodingException {
 		StringBuilder builder = new StringBuilder();
-		Md5HashUtils.appendHashString(bytes, builder);
+		DigestUtils.appendMd5DigestAsHex(bytes, builder);
 		assertEquals("Invalid hash", "b10a8db164e0754105b7a99be72e3fe5", builder.toString());
 	}
 
