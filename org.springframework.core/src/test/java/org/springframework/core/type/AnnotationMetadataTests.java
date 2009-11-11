9d14faa6763079e2665bb74fa8f67f6d34ad8c10
Juergen Hoeller <jhoeller@vmware.com>
2009-11-11 19:24:43 +0000

    AnnotationMetadata returns Class values by default (again), allowing for explicit retrieval of String class names where preferred (SPR-5827)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2346 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.core/src/test/java/org/springframework/core/type/AnnotationMetadataTests.java
+++ org.springframework.core/src/test/java/org/springframework/core/type/AnnotationMetadataTests.java
@@ -79,8 +79,11 @@ public class AnnotationMetadataTests extends TestCase {
 		assertEquals("myScope", scopeAttrs.get("value"));
 		Map<String, Object> specialAttrs = metadata.getAnnotationAttributes(SpecialAttr.class.getName());
 		assertEquals(2, specialAttrs.size());
-		assertEquals(String.class.getName(), specialAttrs.get("clazz"));
+		assertEquals(String.class, specialAttrs.get("clazz"));
 		assertEquals(Thread.State.NEW, specialAttrs.get("state"));
+		Map<String, Object> specialAttrsString = metadata.getAnnotationAttributes(SpecialAttr.class.getName(), true);
+		assertEquals(String.class.getName(), specialAttrsString .get("clazz"));
+		assertEquals(Thread.State.NEW, specialAttrsString.get("state"));
 	}
 
 	private void doTestMethodAnnotationInfo(AnnotationMetadata classMetadata) {
