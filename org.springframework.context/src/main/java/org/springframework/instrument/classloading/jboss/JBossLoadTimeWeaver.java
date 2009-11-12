d377afe8ed2eef987697215593df1d69416d763e
Costin Leau <cleau@vmware.com>
2009-11-12 01:25:39 +0000

    SPR-5764 + redone the JBossLTW (no dependency on the JBoss API) - removed JBoss jars - removed JBoss imports (Spring code doesn't rely on it)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2364 50f2f4bb-b051-0410-bef5-90022cba6387

---
+++ org.springframework.context/src/main/java/org/springframework/instrument/classloading/jboss/JBossLoadTimeWeaver.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.instrument.classloading.jboss;
+
+import java.lang.instrument.ClassFileTransformer;
+
+import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.instrument.classloading.SimpleThrowawayClassLoader;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+
+/**
+ * {@link LoadTimeWeaver} implementation for JBoss's instrumentable ClassLoader.
+ *
+ * <p><b>NOTE:</b> Requires JBoss AS version 5.0.0 or higher.
+ * 
+ * <p>Thanks to Ales Justin and Marius Bogoevici for the initial prototype.</p> 
+ * 
+ * @author Costin Leau
+ */
+public class JBossLoadTimeWeaver implements LoadTimeWeaver {
+
+	private final JBossClassLoaderAdapter classLoader;
+
+	/**
+	 * Creates a new instance of the {@link JBossLoadTimeWeaver} class using
+	 * the default {@link ClassLoader class loader}.
+	 * @see org.springframework.util.ClassUtils#getDefaultClassLoader()
+	 */
+	public JBossLoadTimeWeaver() {
+		this(ClassUtils.getDefaultClassLoader());
+	}
+
+	/**
+	 * Creates a new instance of the {@link JBossLoadTimeWeaver} class using
+	 * the supplied {@link ClassLoader}.
+	 * @param classLoader the <code>ClassLoader</code> to delegate to for
+	 * weaving (must not be <code>null</code>)
+	 */
+	public JBossLoadTimeWeaver(ClassLoader classLoader) {
+		Assert.notNull(classLoader, "ClassLoader must not be null");
+		this.classLoader = new JBossClassLoaderAdapter(classLoader);
+	}
+
+	public void addTransformer(ClassFileTransformer transformer) {
+		this.classLoader.addTransformer(transformer);
+	}
+
+	public ClassLoader getInstrumentableClassLoader() {
+		return this.classLoader.getClassLoader();
+	}
+
+	public ClassLoader getThrowawayClassLoader() {
+		return new SimpleThrowawayClassLoader(getInstrumentableClassLoader());
+	}
+}
---
+++ org.springframework.context/src/main/java/org/springframework/instrument/classloading/jboss/package-info.java
@@ -0,0 +1,8 @@
+
+/**
+ *
+ * Support for class instrumentation on JBoss AS 5.x / JBoss MC 2.0.x.
+ *
+ */
+package org.springframework.instrument.classloading.jboss;
+
