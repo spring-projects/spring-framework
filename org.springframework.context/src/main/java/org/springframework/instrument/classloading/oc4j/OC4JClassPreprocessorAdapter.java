9ad4fbbb7ee41a3098a8fe88169f4354c10a9fd6
Costin Leau <cleau@vmware.com>
2009-11-19 20:28:16 +0000

    SPR-6333 SPR-6393 + eliminated compile-time dependency on oc4j classes
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2459 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.context/src/main/java/org/springframework/instrument/classloading/oc4j/OC4JClassPreprocessorAdapter.java
+++ org.springframework.context/src/main/java/org/springframework/instrument/classloading/oc4j/OC4JClassPreprocessorAdapter.java
@@ -1,60 +1,77 @@
 /*
- * Copyright 2002-2006 the original author or authors.
- *
+ * Copyright 2006-2009 the original author or authors.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.springframework.instrument.classloading.oc4j;
-
-import oracle.classloader.util.ClassPreprocessor;
-import org.springframework.util.Assert;
 
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.security.ProtectionDomain;
 
 /**
- * {@link ClassPreprocessor} adapter for OC4J, delegating to a standard
- * JDK {@link ClassFileTransformer} underneath.
+ * Adapter that implements OC4J ClassPreProcessor interface, delegating to a
+ * standard JDK {@link ClassFileTransformer} underneath.
  *
- * <p>Many thanks to <a href="mailto:mike.keith@oracle.com">Mike Keith</a>
- * for his assistance.
- *
+ * <p>To avoid compile time checks again the vendor API, a dynamic proxy is
+ * being used.
+ * 
  * @author Costin Leau
- * @since 2.0
  */
-class OC4JClassPreprocessorAdapter implements ClassPreprocessor {
+class OC4JClassPreProcessorAdapter implements InvocationHandler {
 
 	private final ClassFileTransformer transformer;
 
-
 	/**
-	 * Creates a new instance of the {@link OC4JClassPreprocessorAdapter} class.
-	 * @param transformer the {@link ClassFileTransformer} to be adapted (must not be <code>null</code>)
-	 * @throws IllegalArgumentException if the supplied <code>transformer</code> is <code>null</code>
+	 * Creates a new {@link OC4JClassPreProcessorAdapter}.
+	 * @param transformer the {@link ClassFileTransformer} to be adapted (must
+	 * not be <code>null</code>)
 	 */
-	public OC4JClassPreprocessorAdapter(ClassFileTransformer transformer) {
-		Assert.notNull(transformer, "Transformer must not be null");
+	public OC4JClassPreProcessorAdapter(ClassFileTransformer transformer) {
 		this.transformer = transformer;
 	}
 
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		String name = method.getName();
 
-	public ClassPreprocessor initialize(ClassLoader loader) {
-		return this;
+		if ("equals".equals(name)) {
+			return (Boolean.valueOf(proxy == args[0]));
+		} else if ("hashCode".equals(name)) {
+			return hashCode();
+		} else if ("toString".equals(name)) {
+			return toString();
+		} else if ("initialize".equals(name)) {
+			initialize(proxy, (ClassLoader) args[0]);
+			return null;
+		} else if ("processClass".equals(name)) {
+			return processClass((String) args[0], (byte[]) args[1], (Integer) args[2], (Integer) args[3],
+					(ProtectionDomain) args[4], (ClassLoader) args[5]);
+		} else {
+			throw new IllegalArgumentException("Unknown method: " + method);
+		}
+	}
+
+	// maps to oracle.classloader.util.ClassPreprocessor#initialize
+	// the proxy is passed since it implements the Oracle interface which
+	// is asked as a return type
+	public Object initialize(Object proxy, ClassLoader loader) {
+		return proxy;
 	}
 
 	public byte[] processClass(String className, byte origClassBytes[], int offset, int length, ProtectionDomain pd,
-							   ClassLoader loader) {
+			ClassLoader loader) {
 		try {
 			byte[] tempArray = new byte[length];
 			System.arraycopy(origClassBytes, offset, tempArray, 0, length);
@@ -63,12 +80,10 @@ class OC4JClassPreprocessorAdapter implements ClassPreprocessor {
 			// transformer expects a VM, "/" format
 			byte[] result = this.transformer.transform(loader, className.replace('.', '/'), null, pd, tempArray);
 			return (result != null ? result : origClassBytes);
-		}
-		catch (IllegalClassFormatException ex) {
+		} catch (IllegalClassFormatException ex) {
 			throw new IllegalStateException("Cannot transform because of illegal class format", ex);
 		}
 	}
-
 
 	@Override
 	public String toString() {
@@ -77,5 +92,4 @@ class OC4JClassPreprocessorAdapter implements ClassPreprocessor {
 		builder.append(this.transformer);
 		return builder.toString();
 	}
-
 }
