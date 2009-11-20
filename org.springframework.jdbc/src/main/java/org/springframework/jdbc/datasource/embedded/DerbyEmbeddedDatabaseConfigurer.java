27747d30863a11a1913e215842888e847c52749c
Juergen Hoeller <jhoeller@vmware.com>
2009-11-20 14:21:48 +0000

    Derby requires a publicly accessible stream creation method (SPR-6346)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2471 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.jdbc/src/main/java/org/springframework/jdbc/datasource/embedded/DerbyEmbeddedDatabaseConfigurer.java
+++ org.springframework.jdbc/src/main/java/org/springframework/jdbc/datasource/embedded/DerbyEmbeddedDatabaseConfigurer.java
@@ -18,7 +18,6 @@ package org.springframework.jdbc.datasource.embedded;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.sql.SQLException;
 import java.util.Properties;
 import javax.sql.DataSource;
@@ -54,8 +53,8 @@ final class DerbyEmbeddedDatabaseConfigurer implements EmbeddedDatabaseConfigure
 	public static synchronized DerbyEmbeddedDatabaseConfigurer getInstance() throws ClassNotFoundException {
 		if (INSTANCE == null) {
 			// disable log file
-			System.setProperty("derby.stream.error.method", 
-					DerbyEmbeddedDatabaseConfigurer.class.getName() + ".getNoopOutputStream");
+			System.setProperty("derby.stream.error.method",
+					OutputStreamFactory.class.getName() + ".getNoopOutputStream");
 			INSTANCE = new DerbyEmbeddedDatabaseConfigurer();
 		}
 		return INSTANCE;
@@ -101,17 +100,4 @@ final class DerbyEmbeddedDatabaseConfigurer implements EmbeddedDatabaseConfigure
 		}
 	}
 
-
-	/**
-	 * Returns an {@link OutputStream} that ignores all data given to it.
-	 * Used by {@link #getInstance()} to prevent writing to Derby.log file.
-	 */
-	public static OutputStream getNoopOutputStream() {
-		return new OutputStream() {
-			public void write(int b) throws IOException {
-				// ignore the output
-			}
-		};
-	}
-	
 }
