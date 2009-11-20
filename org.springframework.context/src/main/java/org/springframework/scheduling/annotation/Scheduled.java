af674bfac47f839161e9155200fa5ce553fa8d45
David Syer <dsyer@vmware.com>
2009-11-20 12:36:02 +0000

    RESOLVED - issue SPR-6398: Document attributes in @Scheduled and friends http://jira.springframework.org/browse/SPR-6398

--- org.springframework.context/src/main/java/org/springframework/scheduling/annotation/Scheduled.java
+++ org.springframework.context/src/main/java/org/springframework/scheduling/annotation/Scheduled.java
@@ -31,6 +31,7 @@ import java.lang.annotation.Target;
  * <code>void</code> return type.
  *
  * @author Mark Fisher
+ * @author Dave Syer
  * @since 3.0
  * @see ScheduledAnnotationBeanPostProcessor
  */
@@ -39,10 +40,29 @@ import java.lang.annotation.Target;
 @Documented
 public @interface Scheduled {
 
+	/**
+	 * A cron-like expression, extending the usual UN*X definition to include
+	 * triggers on the second as well as minute, hour, day of month, month
+	 * and day of week.  e.g. <code>"0 * * * * MON-FRI"</code> means once 
+	 * per minute on weekdays (at the top of the minute - the 0th second).
+	 * 
+	 * @return an expression that can be parsed to a cron schedule
+	 */
 	String cron() default "";
 
+	/**
+	 * Execute the annotated method with a fixed period between the end
+	 * of the last invocation and the start of the next.
+	 * 
+	 * @return the delay in milliseconds
+	 */
 	long fixedDelay() default -1;
 
+	/**
+	 * Execute the annotated method with a fixed period between invocations.
+	 * 
+	 * @return the period in milliseconds
+	 */
 	long fixedRate() default -1;
 
 }
