1ac7b95c1dcc1792b4f351a6700dd42363f9f1e0
Mark Fisher <markfisher@vmware.com>
2009-11-27 18:16:08 +0000

    SPR-6460 Added "phase" attribute to the JMS namespace's "jca-listener-container" element.

--- org.springframework.jms/src/main/java/org/springframework/jms/config/AbstractListenerContainerParser.java
+++ org.springframework.jms/src/main/java/org/springframework/jms/config/AbstractListenerContainerParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,6 +82,8 @@ abstract class AbstractListenerContainerParser implements BeanDefinitionParser {
 	protected static final String TRANSACTION_MANAGER_ATTRIBUTE = "transaction-manager";
 
 	protected static final String CONCURRENCY_ATTRIBUTE = "concurrency";
+
+	protected static final String PHASE_ATTRIBUTE = "phase";
 
 	protected static final String PREFETCH_ATTRIBUTE = "prefetch";
 
