5900cd6daa1749e760565e456c19d9b188e9a483
Chris Beams <cbeams@vmware.com>
2009-12-08 15:29:27 +0000

    Renamed tests for AnnotationConfig[Web]ApplicationContext; added tests for scan() and register() methods.
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2604 50f2f4bb-b051-0410-bef5-90022cba6387

---
+++ org.springframework.context/src/test/java/org/springframework/context/annotation6/ComponentForScanning.java
@@ -0,0 +1,7 @@
+package org.springframework.context.annotation6;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentForScanning {
+}
---
+++ org.springframework.context/src/test/java/org/springframework/context/annotation6/Jsr330NamedForScanning.java
@@ -0,0 +1,8 @@
+package org.springframework.context.annotation6;
+
+import javax.inject.Named;
+
+@Named
+public class Jsr330NamedForScanning {
+
+}
