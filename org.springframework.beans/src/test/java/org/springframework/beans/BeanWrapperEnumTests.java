5f9b4443194d3aa3948d76956897c0a1d918d546
Juergen Hoeller <jhoeller@vmware.com>
2009-12-13 13:21:30 +0000

    bean properties of type enum array/collection can be populated with comma-separated String (SPR-6547)

--- org.springframework.beans/src/test/java/org/springframework/beans/BeanWrapperEnumTests.java
+++ org.springframework.beans/src/test/java/org/springframework/beans/BeanWrapperEnumTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,9 +17,7 @@
 package org.springframework.beans;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 import test.beans.CustomEnum;
 import test.beans.GenericBean;
 
@@ -51,6 +49,64 @@ public final class BeanWrapperEnumTests {
 		BeanWrapper bw = new BeanWrapperImpl(gb);
 		bw.setPropertyValue("customEnum", "");
 		assertEquals(null, gb.getCustomEnum());
+	}
+
+	@Test
+	public void testCustomEnumArrayWithSingleValue() {
+		GenericBean<?> gb = new GenericBean<Object>();
+		BeanWrapper bw = new BeanWrapperImpl(gb);
+		bw.setPropertyValue("customEnumArray", "VALUE_1");
+		assertEquals(1, gb.getCustomEnumArray().length);
+		assertEquals(CustomEnum.VALUE_1, gb.getCustomEnumArray()[0]);
+	}
+
+	@Test
+	public void testCustomEnumArrayWithMultipleValues() {
+		GenericBean<?> gb = new GenericBean<Object>();
+		BeanWrapper bw = new BeanWrapperImpl(gb);
+		bw.setPropertyValue("customEnumArray", new String[] {"VALUE_1", "VALUE_2"});
+		assertEquals(2, gb.getCustomEnumArray().length);
+		assertEquals(CustomEnum.VALUE_1, gb.getCustomEnumArray()[0]);
+		assertEquals(CustomEnum.VALUE_2, gb.getCustomEnumArray()[1]);
+	}
+
+	@Test
+	public void testCustomEnumArrayWithMultipleValuesAsCsv() {
+		GenericBean<?> gb = new GenericBean<Object>();
+		BeanWrapper bw = new BeanWrapperImpl(gb);
+		bw.setPropertyValue("customEnumArray", "VALUE_1,VALUE_2");
+		assertEquals(2, gb.getCustomEnumArray().length);
+		assertEquals(CustomEnum.VALUE_1, gb.getCustomEnumArray()[0]);
+		assertEquals(CustomEnum.VALUE_2, gb.getCustomEnumArray()[1]);
+	}
+
+	@Test
+	public void testCustomEnumSetWithSingleValue() {
+		GenericBean<?> gb = new GenericBean<Object>();
+		BeanWrapper bw = new BeanWrapperImpl(gb);
+		bw.setPropertyValue("customEnumSet", "VALUE_1");
+		assertEquals(1, gb.getCustomEnumSet().size());
+		assertTrue(gb.getCustomEnumSet().contains(CustomEnum.VALUE_1));
+	}
+
+	@Test
+	public void testCustomEnumSetWithMultipleValues() {
+		GenericBean<?> gb = new GenericBean<Object>();
+		BeanWrapper bw = new BeanWrapperImpl(gb);
+		bw.setPropertyValue("customEnumSet", new String[] {"VALUE_1", "VALUE_2"});
+		assertEquals(2, gb.getCustomEnumSet().size());
+		assertTrue(gb.getCustomEnumSet().contains(CustomEnum.VALUE_1));
+		assertTrue(gb.getCustomEnumSet().contains(CustomEnum.VALUE_2));
+	}
+
+	@Test
+	public void testCustomEnumSetWithMultipleValuesAsCsv() {
+		GenericBean<?> gb = new GenericBean<Object>();
+		BeanWrapper bw = new BeanWrapperImpl(gb);
+		bw.setPropertyValue("customEnumSet", "VALUE_1,VALUE_2");
+		assertEquals(2, gb.getCustomEnumSet().size());
+		assertTrue(gb.getCustomEnumSet().contains(CustomEnum.VALUE_1));
+		assertTrue(gb.getCustomEnumSet().contains(CustomEnum.VALUE_2));
 	}
 
 }
