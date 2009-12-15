7fb19d658b62b54bf2886325589b9b0703efc9d9
Keith Donald <kdonald@vmware.com>
2009-12-15 19:55:10 +0000

    primitive desc caching

--- org.springframework.core/src/main/java/org/springframework/core/convert/TypeDescriptor.java
+++ org.springframework.core/src/main/java/org/springframework/core/convert/TypeDescriptor.java
@@ -43,15 +43,31 @@ public class TypeDescriptor {
 	private static final Map<Class<?>, TypeDescriptor> typeDescriptorCache = new HashMap<Class<?>, TypeDescriptor>();
 
 	static {
-		typeDescriptorCache.put(String.class, new TypeDescriptor(String.class));
+		typeDescriptorCache.put(byte.class, new TypeDescriptor(byte.class));
 		typeDescriptorCache.put(Byte.class, new TypeDescriptor(Byte.class));
+		
+		typeDescriptorCache.put(char.class, new TypeDescriptor(char.class));
 		typeDescriptorCache.put(Character.class, new TypeDescriptor(Character.class));
+		
+		typeDescriptorCache.put(boolean.class, new TypeDescriptor(boolean.class));
 		typeDescriptorCache.put(Boolean.class, new TypeDescriptor(Boolean.class));
+		
+		typeDescriptorCache.put(short.class, new TypeDescriptor(short.class));
 		typeDescriptorCache.put(Short.class, new TypeDescriptor(Short.class));
+		
+		typeDescriptorCache.put(int.class, new TypeDescriptor(int.class));
 		typeDescriptorCache.put(Integer.class, new TypeDescriptor(Integer.class));
+		
+		typeDescriptorCache.put(long.class, new TypeDescriptor(long.class));
 		typeDescriptorCache.put(Long.class, new TypeDescriptor(Long.class));
+		
+		typeDescriptorCache.put(float.class, new TypeDescriptor(float.class));
 		typeDescriptorCache.put(Float.class, new TypeDescriptor(Float.class));
+		
+		typeDescriptorCache.put(double.class, new TypeDescriptor(double.class));
 		typeDescriptorCache.put(Double.class, new TypeDescriptor(Double.class));
+		
+		typeDescriptorCache.put(String.class, new TypeDescriptor(String.class));
 	}
 	
 	private Object value;
