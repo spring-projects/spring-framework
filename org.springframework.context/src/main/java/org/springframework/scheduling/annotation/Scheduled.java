1284086ffad7eccc43b883c865d72e58de83c905
Mark Fisher <markfisher@vmware.com>
2010-01-11 18:36:48 +0000

    SPR-6669 @Scheduled may now be used as a meta-annotation

--- org.springframework.context/src/main/java/org/springframework/scheduling/annotation/Scheduled.java
+++ org.springframework.context/src/main/java/org/springframework/scheduling/annotation/Scheduled.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ import java.lang.annotation.Target;
  * @since 3.0
  * @see ScheduledAnnotationBeanPostProcessor
  */
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Scheduled {
