4b46db27a9cc875e265986f3f349c21193293f1a
Juergen Hoeller <jhoeller@vmware.com>
2010-01-12 15:31:30 +0000

    fixed type assignability check for wildcarded Map (SPR-6676)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2806 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.core/src/main/java/org/springframework/core/convert/TypeDescriptor.java
+++ org.springframework.core/src/main/java/org/springframework/core/convert/TypeDescriptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -456,7 +456,7 @@ public class TypeDescriptor {
 
 	private boolean isTypeAssignableTo(Class<?> clazz) {
 		Class<?> type = getType();
-		return (type != null && ClassUtils.isAssignable(clazz, type));
+		return (type != null && (clazz == null || ClassUtils.isAssignable(clazz, type)));
 	}
 
 
