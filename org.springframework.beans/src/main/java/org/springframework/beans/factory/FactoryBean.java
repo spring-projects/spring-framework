7553d32b246a632f8345a7e3a11ecfb8685b816b
Juergen Hoeller <jhoeller@vmware.com>
2010-01-31 16:01:14 +0000

    widened FactoryBean's "getObjectType" signature to return any Class<?> (SPR-6692)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2885 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.beans/src/main/java/org/springframework/beans/factory/FactoryBean.java
+++ org.springframework.beans/src/main/java/org/springframework/beans/factory/FactoryBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,7 +90,7 @@ public interface FactoryBean<T> {
 	 * or <code>null</code> if not known at the time of the call
 	 * @see ListableBeanFactory#getBeansOfType
 	 */
-	Class<? extends T> getObjectType();
+	Class<?> getObjectType();
 
 	/**
 	 * Is the object managed by this factory a singleton? That is,
