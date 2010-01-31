6a41d6b2a04e5be9e7d002200c7d19569f786072
Juergen Hoeller <jhoeller@vmware.com>
2010-01-31 14:12:48 +0000

    BeanNameAutoProxyCreator detects alias matches for specified bean names as well (SPR-6774)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2880 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.aop/src/main/java/org/springframework/aop/framework/autoproxy/BeanNameAutoProxyCreator.java
+++ org.springframework.aop/src/main/java/org/springframework/aop/framework/autoproxy/BeanNameAutoProxyCreator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -83,6 +83,15 @@ public class BeanNameAutoProxyCreator extends AbstractAutoProxyCreator {
 				}
 				if (isMatch(beanName, mappedName)) {
 					return PROXY_WITHOUT_ADDITIONAL_INTERCEPTORS;
+				}
+				BeanFactory beanFactory = getBeanFactory();
+				if (beanFactory != null) {
+					String[] aliases = beanFactory.getAliases(beanName);
+					for (String alias : aliases) {
+						if (isMatch(alias, mappedName)) {
+							return PROXY_WITHOUT_ADDITIONAL_INTERCEPTORS;
+						}
+					}
 				}
 			}
 		}
