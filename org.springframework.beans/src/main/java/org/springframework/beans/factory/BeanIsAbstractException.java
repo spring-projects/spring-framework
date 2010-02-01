a983d181aaf3d6979e1336f76f6b60a82feb2c11
Juergen Hoeller <jhoeller@vmware.com>
2010-02-01 17:56:03 +0000

    fixed MBeanExporter regression: do not try to expose abstract beans (SPR-6784)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2896 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.beans/src/main/java/org/springframework/beans/factory/BeanIsAbstractException.java
+++ org.springframework.beans/src/main/java/org/springframework/beans/factory/BeanIsAbstractException.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2010 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,8 +17,9 @@
 package org.springframework.beans.factory;
 
 /**
- * Exception thrown when a bean instance has been requested for a bean
- * which has been defined as abstract
+ * Exception thrown when a bean instance has been requested for
+ * a bean definition which has been marked as abstract.
+ *
  * @author Juergen Hoeller
  * @since 1.1
  * @see org.springframework.beans.factory.support.AbstractBeanDefinition#setAbstract
