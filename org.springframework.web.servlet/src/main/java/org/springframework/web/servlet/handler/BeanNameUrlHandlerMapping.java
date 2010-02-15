a125120b8f92f0ef0074aee3ab79755fec7d343d
Juergen Hoeller <jhoeller@vmware.com>
2010-02-15 17:37:57 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2983 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.web.servlet/src/main/java/org/springframework/web/servlet/handler/BeanNameUrlHandlerMapping.java
+++ org.springframework.web.servlet/src/main/java/org/springframework/web/servlet/handler/BeanNameUrlHandlerMapping.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,9 +60,9 @@ public class BeanNameUrlHandlerMapping extends AbstractDetectingUrlHandlerMappin
 			urls.add(beanName);
 		}
 		String[] aliases = getApplicationContext().getAliases(beanName);
-		for (int i = 0; i < aliases.length; i++) {
-			if (aliases[i].startsWith("/")) {
-				urls.add(aliases[i]);
+		for (String alias : aliases) {
+			if (alias.startsWith("/")) {
+				urls.add(alias);
 			}
 		}
 		return StringUtils.toStringArray(urls);
