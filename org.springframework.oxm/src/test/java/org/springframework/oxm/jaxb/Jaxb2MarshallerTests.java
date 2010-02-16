0bd10c979a7cea7b9491cc19ef3857651aed4d71
Arjen Poutsma <apoutsma@vmware.com>
2010-02-16 14:25:55 +0000

    Improved Jaxb2Marshaller.supports()
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@2990 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.oxm/src/test/java/org/springframework/oxm/jaxb/Jaxb2MarshallerTests.java
+++ org.springframework.oxm/src/test/java/org/springframework/oxm/jaxb/Jaxb2MarshallerTests.java
@@ -237,6 +237,19 @@ public class Jaxb2MarshallerTests extends AbstractMarshallerTests {
 	}
 
 	@Test
+	public void supportsXmlRootElement() throws Exception {
+		marshaller = new Jaxb2Marshaller();
+		marshaller.setClassesToBeBound(new Class[]{DummyRootElement.class, DummyType.class});
+		marshaller.afterPropertiesSet();
+		assertTrue("Jaxb2Marshaller does not support XmlRootElement class", marshaller.supports(DummyRootElement.class));
+		assertTrue("Jaxb2Marshaller does not support XmlRootElement generic type", marshaller.supports((Type)DummyRootElement.class));
+
+		assertFalse("Jaxb2Marshaller supports DummyType class", marshaller.supports(DummyType.class));
+		assertFalse("Jaxb2Marshaller supports DummyType type", marshaller.supports((Type)DummyType.class));
+	}
+	
+
+	@Test
 	public void marshalAttachments() throws Exception {
 		marshaller = new Jaxb2Marshaller();
 		marshaller.setClassesToBeBound(new Class[]{BinaryObject.class});
