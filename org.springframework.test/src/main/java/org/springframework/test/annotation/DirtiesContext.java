80b8fb8b83aa63c149a731e8d0274f52e19906b5
Sam Brannen <sam@sambrannen.com>
2010-02-23 17:43:00 +0000

    [SPR-6879] @DirtiesContext is now an @Inherited annotation.

--- org.springframework.test/src/main/java/org/springframework/test/annotation/DirtiesContext.java
+++ org.springframework.test/src/main/java/org/springframework/test/annotation/DirtiesContext.java
@@ -18,6 +18,7 @@ package org.springframework.test.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -54,6 +55,7 @@ import java.lang.annotation.Target;
  * @since 2.0
  */
 @Documented
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target( { ElementType.TYPE, ElementType.METHOD })
 public @interface DirtiesContext {
