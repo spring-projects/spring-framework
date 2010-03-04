77f8b070cd12523db393f7b620811703290e1501
Juergen Hoeller <jhoeller@vmware.com>
2010-03-04 13:58:02 +0000

    polishing

--- org.springframework.beans/src/main/java/org/springframework/beans/factory/support/FactoryBeanRegistrySupport.java
+++ org.springframework.beans/src/main/java/org/springframework/beans/factory/support/FactoryBeanRegistrySupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -123,8 +123,6 @@ public abstract class FactoryBeanRegistrySupport extends DefaultSingletonBeanReg
 			final FactoryBean factory, final String beanName, final boolean shouldPostProcess)
 			throws BeanCreationException {
 
-		
-		
 		Object object;
 		try {
 			if (System.getSecurityManager() != null) {
@@ -135,7 +133,8 @@ public abstract class FactoryBeanRegistrySupport extends DefaultSingletonBeanReg
 								return factory.getObject();
 							}
 						}, acc);
-				} catch (PrivilegedActionException pae) {
+				}
+				catch (PrivilegedActionException pae) {
 					throw pae.getException();
 				}
 			}
