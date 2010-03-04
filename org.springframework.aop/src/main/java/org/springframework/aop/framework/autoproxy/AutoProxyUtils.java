5b7217b7049c375ef84295dd5f6518ccd700a833
Juergen Hoeller <jhoeller@vmware.com>
2010-03-04 22:33:59 +0000

    fixed @Configurable issue with null bean name (SPR-6947)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3057 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.aop/src/main/java/org/springframework/aop/framework/autoproxy/AutoProxyUtils.java
+++ org.springframework.aop/src/main/java/org/springframework/aop/framework/autoproxy/AutoProxyUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,7 +52,7 @@ public abstract class AutoProxyUtils {
 	 * @return whether the given bean should be proxied with its target class
 	 */
 	public static boolean shouldProxyTargetClass(ConfigurableListableBeanFactory beanFactory, String beanName) {
-		if (beanFactory.containsBeanDefinition(beanName)) {
+		if (beanName != null && beanFactory.containsBeanDefinition(beanName)) {
 			BeanDefinition bd = beanFactory.getBeanDefinition(beanName);
 			return Boolean.TRUE.equals(bd.getAttribute(PRESERVE_TARGET_CLASS_ATTRIBUTE));
 		}
