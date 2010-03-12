4c4f19ec94d8832136772e567285da6ffe233176
Christian Dupuis <cdupuis@vmware.com>
2010-03-12 16:10:03 +0000

    SPR-6977: updated copyright; added method comment

--- org.springframework.core/src/main/java/org/springframework/core/GenericTypeResolver.java
+++ org.springframework.core/src/main/java/org/springframework/core/GenericTypeResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -164,6 +164,9 @@ public abstract class GenericTypeResolver {
 		return null;
 	}
 	
+	/**
+	 * Extract a class instance from given Type.
+	 */
 	private static Class extractClass(Class ownerClass, Type arg) {
 		if (arg instanceof TypeVariable) {
 			TypeVariable tv = (TypeVariable) arg;
