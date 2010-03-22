35fc6771ab184c6e5fbee35be244efbbd67d0566
Juergen Hoeller <jhoeller@vmware.com>
2010-03-22 18:31:41 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3137 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.aop/src/main/java/org/springframework/aop/framework/JdkDynamicAopProxy.java
+++ org.springframework.aop/src/main/java/org/springframework/aop/framework/JdkDynamicAopProxy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -146,7 +146,7 @@ final class JdkDynamicAopProxy implements AopProxy, InvocationHandler, Serializa
 	 * unless a hook method throws an exception.
 	 */
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		MethodInvocation invocation = null;
+		MethodInvocation invocation;
 		Object oldProxy = null;
 		boolean setProxyContext = false;
 
@@ -169,7 +169,7 @@ final class JdkDynamicAopProxy implements AopProxy, InvocationHandler, Serializa
 				return AopUtils.invokeJoinpointUsingReflection(this.advised, method, args);
 			}
 
-			Object retVal = null;
+			Object retVal;
 
 			if (this.advised.exposeProxy) {
 				// Make invocation available if necessary.
@@ -239,7 +239,7 @@ final class JdkDynamicAopProxy implements AopProxy, InvocationHandler, Serializa
 			return false;
 		}
 
-		JdkDynamicAopProxy otherProxy = null;
+		JdkDynamicAopProxy otherProxy;
 		if (other instanceof JdkDynamicAopProxy) {
 			otherProxy = (JdkDynamicAopProxy) other;
 		}
