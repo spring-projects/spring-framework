6118d67321b07bf09799559ad4e9e26e1600e5b1
Chris Beams <cbeams@vmware.com>
2010-03-24 10:52:10 +0000

    updated JavaDoc regarding short-circuiting the BeanPostProcessor chain when returning null from BPP methods (SPR-6926)

--- org.springframework.beans/src/main/java/org/springframework/beans/factory/config/BeanPostProcessor.java
+++ org.springframework.beans/src/main/java/org/springframework/beans/factory/config/BeanPostProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,7 +48,8 @@ public interface BeanPostProcessor {
 	 * The returned bean instance may be a wrapper around the original.
 	 * @param bean the new bean instance
 	 * @param beanName the name of the bean
-	 * @return the bean instance to use, either the original or a wrapped one
+	 * @return the bean instance to use, either the original or a wrapped one; if
+	 * <code>null</code>, no subsequent BeanPostProcessors will be invoked
 	 * @throws org.springframework.beans.BeansException in case of errors
 	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet
 	 */
@@ -68,7 +69,8 @@ public interface BeanPostProcessor {
 	 * in contrast to all other BeanPostProcessor callbacks.
 	 * @param bean the new bean instance
 	 * @param beanName the name of the bean
-	 * @return the bean instance to use, either the original or a wrapped one
+	 * @return the bean instance to use, either the original or a wrapped one; if
+	 * <code>null</code>, no subsequent BeanPostProcessors will be invoked
 	 * @throws org.springframework.beans.BeansException in case of errors
 	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet
 	 * @see org.springframework.beans.factory.FactoryBean
