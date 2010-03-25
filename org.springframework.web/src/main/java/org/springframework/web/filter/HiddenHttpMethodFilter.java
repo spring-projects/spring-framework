d2b69d6d7c1e3bd91b3e438386d5f10579a52414
Arjen Poutsma <apoutsma@vmware.com>
2010-03-25 13:02:19 +0000

    Added @Override annotations.
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3175 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.web/src/main/java/org/springframework/web/filter/HiddenHttpMethodFilter.java
+++ org.springframework.web/src/main/java/org/springframework/web/filter/HiddenHttpMethodFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,6 +58,7 @@ public class HiddenHttpMethodFilter extends OncePerRequestFilter {
 		this.methodParam = methodParam;
 	}
 
+	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
@@ -86,6 +87,7 @@ public class HiddenHttpMethodFilter extends OncePerRequestFilter {
 			this.method = method;
 		}
 
+		@Override
 		public String getMethod() {
 			return this.method;
 		}
