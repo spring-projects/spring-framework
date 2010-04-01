5d87fe71fa7c0f2921905129a5b6b8caced66a66
Juergen Hoeller <jhoeller@vmware.com>
2010-04-01 10:59:58 +0000

    fixed TemplateParserContext documentation (SPR-7059)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3216 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.expression/src/main/java/org/springframework/expression/common/TemplateParserContext.java
+++ org.springframework.expression/src/main/java/org/springframework/expression/common/TemplateParserContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,8 +31,9 @@ public class TemplateParserContext implements ParserContext {
 
 	private final String expressionSuffix;
 
+
 	/**
-	 * Creates a new TemplateParserContext with the default #{ prefix and } suffix.
+	 * Create a new TemplateParserContext with the default "#{" prefix and "}" suffix.
 	 */
 	public TemplateParserContext() {
 		this("#{", "}");
