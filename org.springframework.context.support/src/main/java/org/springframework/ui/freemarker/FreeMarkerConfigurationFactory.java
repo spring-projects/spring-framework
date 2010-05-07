026523d24b962d6cf27808390605c0f08fdccbdf
Juergen Hoeller <jhoeller@vmware.com>
2010-05-07 15:06:27 +0000

    set shared variables with configured ObjectWrapper (SPR-7165)

--- org.springframework.context.support/src/main/java/org/springframework/ui/freemarker/FreeMarkerConfigurationFactory.java
+++ org.springframework.context.support/src/main/java/org/springframework/ui/freemarker/FreeMarkerConfigurationFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -286,7 +286,7 @@ public class FreeMarkerConfigurationFactory {
 		}
 
 		if (!CollectionUtils.isEmpty(this.freemarkerVariables)) {
-			config.setAllSharedVariables(new SimpleHash(this.freemarkerVariables));
+			config.setAllSharedVariables(new SimpleHash(this.freemarkerVariables, config.getObjectWrapper()));
 		}
 
 		if (this.defaultEncoding != null) {
