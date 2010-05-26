f0e971c755352f1dad2012e2e5d39932926e2d0d
Juergen Hoeller <jhoeller@vmware.com>
2010-05-26 20:35:42 +0000

    CronTrigger defensively protects itself against accidental re-fires if a task runs too early (SPR-7004)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3373 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.context/src/main/java/org/springframework/scheduling/support/CronTrigger.java
+++ org.springframework.context/src/main/java/org/springframework/scheduling/support/CronTrigger.java
@@ -57,7 +57,16 @@ public class CronTrigger implements Trigger {
 
 	public Date nextExecutionTime(TriggerContext triggerContext) {
 		Date date = triggerContext.lastCompletionTime();
-		if (date == null) {
+		if (date != null) {
+			Date scheduled = triggerContext.lastScheduledExecutionTime();
+			if (scheduled != null && date.before(scheduled)) {
+				// Previous task apparently executed too early...
+				// Let's simply use the last calculated execution time then,
+				// in order to prevent accidental re-fires in the same second.
+				date = scheduled;
+			}
+		}
+		else {
 			date = new Date();
 		}
 		return this.sequenceGenerator.next(date);
