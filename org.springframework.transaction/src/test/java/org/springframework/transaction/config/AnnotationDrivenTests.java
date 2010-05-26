6c6004a93b7bad4644b5b04ec97a25e5ce24f0f4
Juergen Hoeller <jhoeller@vmware.com>
2010-05-26 09:46:03 +0000

    @Transactional qualifier value matches against @Qualifier annotations on @Bean methods as well (SPR-7232)

--- org.springframework.transaction/src/test/java/org/springframework/transaction/config/AnnotationDrivenTests.java
+++ org.springframework.transaction/src/test/java/org/springframework/transaction/config/AnnotationDrivenTests.java
@@ -54,6 +54,26 @@ public class AnnotationDrivenTests extends TestCase {
 		assertEquals(2, tm2.commits);
 	}
 
+	public void testWithConfigurationClass() throws Exception {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("annotationDrivenConfigurationClassTests.xml", getClass());
+		CallCountingTransactionManager tm1 = context.getBean("transactionManager1", CallCountingTransactionManager.class);
+		CallCountingTransactionManager tm2 = context.getBean("transactionManager2", CallCountingTransactionManager.class);
+		TransactionalService service = context.getBean("service", TransactionalService.class);
+		assertTrue(AopUtils.isCglibProxy(service));
+		service.setSomething("someName");
+		assertEquals(1, tm1.commits);
+		assertEquals(0, tm2.commits);
+		service.doSomething();
+		assertEquals(1, tm1.commits);
+		assertEquals(1, tm2.commits);
+		service.setSomething("someName");
+		assertEquals(2, tm1.commits);
+		assertEquals(1, tm2.commits);
+		service.doSomething();
+		assertEquals(2, tm1.commits);
+		assertEquals(2, tm2.commits);
+	}
+
 	public void testSerializableWithPreviousUsage() throws Exception {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("annotationDrivenProxyTargetClassTests.xml", getClass());
 		TransactionalService service = context.getBean("service", TransactionalService.class);
