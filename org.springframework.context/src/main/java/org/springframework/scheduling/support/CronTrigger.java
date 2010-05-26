a818ab8c60e2457cb03f17d5d95e3f23c48b5658
David Syer <dsyer@vmware.com>
2010-05-26 17:41:54 +0000

    SPR-7239: fix CronTrigger
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3371 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.context/src/main/java/org/springframework/scheduling/support/CronTrigger.java
+++ org.springframework.context/src/main/java/org/springframework/scheduling/support/CronTrigger.java
@@ -74,5 +74,10 @@ public class CronTrigger implements Trigger {
 	public int hashCode() {
 		return this.sequenceGenerator.hashCode();
 	}
+	
+	@Override
+	public String toString() {
+		return sequenceGenerator.toString();
+	}
 
 }
