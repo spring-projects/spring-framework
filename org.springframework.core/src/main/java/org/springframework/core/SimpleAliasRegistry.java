5e35ea0a5a00ab6b124415efcee804b8391cf177
Juergen Hoeller <jhoeller@vmware.com>
2010-06-07 22:15:20 +0000

    registerDependentBean resolves to the canonical bean name in order to handle alias references (SPR-7254)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3391 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.core/src/main/java/org/springframework/core/SimpleAliasRegistry.java
+++ org.springframework.core/src/main/java/org/springframework/core/SimpleAliasRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -145,8 +145,8 @@ public class SimpleAliasRegistry implements AliasRegistry {
 	 */
 	public String canonicalName(String name) {
 		String canonicalName = name;
-		// Handle aliasing.
-		String resolvedName = null;
+		// Handle aliasing...
+		String resolvedName;
 		do {
 			resolvedName = this.aliasMap.get(canonicalName);
 			if (resolvedName != null) {
