78c906caaadb1c41e35e2ee4b2f0377d60e97d1c
Juergen Hoeller <jhoeller@vmware.com>
2010-06-07 20:32:52 +0000

    JSP HiddenInputTag allows for using common HTML attributes (SPR-7249)

--- org.springframework.web.servlet/src/main/java/org/springframework/web/servlet/tags/form/AbstractHtmlElementTag.java
+++ org.springframework.web.servlet/src/main/java/org/springframework/web/servlet/tags/form/AbstractHtmlElementTag.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@ package org.springframework.web.servlet.tags.form;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.DynamicAttributes;
 
@@ -40,7 +39,7 @@ import org.springframework.util.StringUtils;
  * @author Jeremy Grelle
  * @since 2.0
  */
-public abstract class AbstractHtmlElementTag extends AbstractDataBoundFormElementTag implements DynamicAttributes{
+public abstract class AbstractHtmlElementTag extends AbstractDataBoundFormElementTag implements DynamicAttributes {
 
 	public static final String CLASS_ATTRIBUTE = "class";
 
