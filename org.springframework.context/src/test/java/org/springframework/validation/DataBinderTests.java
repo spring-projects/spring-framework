07e9f1775be0b0fbf2e617f9964501b8af5a2aa2
Juergen Hoeller <jhoeller@vmware.com>
2010-06-20 19:11:36 +0000

    added test for invalid binding to ClassLoader

--- org.springframework.context/src/test/java/org/springframework/validation/DataBinderTests.java
+++ org.springframework.context/src/test/java/org/springframework/validation/DataBinderTests.java
@@ -225,6 +225,22 @@ public class DataBinderTests extends TestCase {
 		}
 	}
 
+	public void testBindingWithSystemFieldError() throws Exception {
+		TestBean rod = new TestBean();
+		DataBinder binder = new DataBinder(rod, "person");
+		MutablePropertyValues pvs = new MutablePropertyValues();
+		pvs.add("class.classLoader.URLs[0]", "http://myserver");
+		binder.setIgnoreUnknownFields(false);
+
+		try {
+			binder.bind(pvs);
+			fail("Should have thrown NotWritablePropertyException");
+		}
+		catch (NotWritablePropertyException ex) {
+			assertTrue(ex.getMessage().contains("classLoader"));
+		}
+	}
+
 	public void testBindingWithErrorsAndCustomEditors() throws Exception {
 		TestBean rod = new TestBean();
 		DataBinder binder = new DataBinder(rod, "person");
