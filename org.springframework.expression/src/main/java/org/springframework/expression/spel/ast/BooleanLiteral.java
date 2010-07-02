0c1424acb6f832b0de543157934733c9cc270ac7
Andy Clement <aclement@vmware.com>
2010-07-02 16:06:59 +0000

    polish

--- org.springframework.expression/src/main/java/org/springframework/expression/spel/ast/BooleanLiteral.java
+++ org.springframework.expression/src/main/java/org/springframework/expression/spel/ast/BooleanLiteral.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.springframework.expression.spel.ast;
 
 import org.springframework.expression.spel.support.BooleanTypedValue;
