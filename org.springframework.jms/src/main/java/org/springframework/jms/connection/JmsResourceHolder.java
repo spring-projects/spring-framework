ee3a08cefa7d2d48284d64e81ef84805f3ddfcd2
Juergen Hoeller <jhoeller@vmware.com>
2010-07-08 12:15:35 +0000

    JmsResourceHolder does not ignore IllegalStateException from locally synchronized transaction (SPR-7360)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3478 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.jms/src/main/java/org/springframework/jms/connection/JmsResourceHolder.java
+++ org.springframework.jms/src/main/java/org/springframework/jms/connection/JmsResourceHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -183,9 +183,7 @@ public class JmsResourceHolder extends ResourceHolderSupport {
 			catch (TransactionInProgressException ex) {
 				// Ignore -> can only happen in case of a JTA transaction.
 			}
-			catch (javax.jms.IllegalStateException ex) {
-				// Ignore -> can only happen in case of a JTA transaction.
-			}
+			// Let IllegalStateException through: It might point out an unexpectedly closed session.
 		}
 	}
 
