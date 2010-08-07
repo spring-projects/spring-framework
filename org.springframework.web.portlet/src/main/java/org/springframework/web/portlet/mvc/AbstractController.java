753cda3d810e2affe93af2bd6dd7010b2217d561
Juergen Hoeller <jhoeller@vmware.com>
2010-08-07 11:46:15 +0000

    Portlet AbstractController consistently uses session mutex if "synchronizeOnSession" is active

--- org.springframework.web.portlet/src/main/java/org/springframework/web/portlet/mvc/AbstractController.java
+++ org.springframework.web.portlet/src/main/java/org/springframework/web/portlet/mvc/AbstractController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -188,7 +188,8 @@ public abstract class AbstractController extends PortletContentGenerator impleme
 		if (this.synchronizeOnSession) {
 			PortletSession session = request.getPortletSession(false);
 			if (session != null) {
-				synchronized (session) {
+				Object mutex = PortletUtils.getSessionMutex(session);
+				synchronized (mutex) {
 					handleActionRequestInternal(request, response);
 					return;
 				}
