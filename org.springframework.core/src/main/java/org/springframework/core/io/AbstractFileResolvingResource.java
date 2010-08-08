ca58577eb154304d14c2153133b7aa846099c5f6
Juergen Hoeller <jhoeller@vmware.com>
2010-08-08 23:21:41 +0000

    implement file resolution for isReadable() as well
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3547 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.core/src/main/java/org/springframework/core/io/AbstractFileResolvingResource.java
+++ org.springframework.core/src/main/java/org/springframework/core/io/AbstractFileResolvingResource.java
@@ -111,6 +111,24 @@ public abstract class AbstractFileResolvingResource extends AbstractResource {
 	}
 
 	@Override
+	public boolean isReadable() {
+		try {
+			URL url = getURL();
+			if (ResourceUtils.isFileURL(url)) {
+				// Proceed with file system resolution...
+				File file = getFile();
+				return (file.canRead() && !file.isDirectory());
+			}
+			else {
+				return true;
+			}
+		}
+		catch (IOException ex) {
+			return false;
+		}
+	}
+
+	@Override
 	public int contentLength() throws IOException {
 		URL url = getURL();
 		if (ResourceUtils.isFileURL(url)) {
