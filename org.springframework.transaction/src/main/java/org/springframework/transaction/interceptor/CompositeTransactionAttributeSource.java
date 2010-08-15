d5149d86f4117597c787c69fa3ab280d3e71112f
Juergen Hoeller <jhoeller@vmware.com>
2010-08-15 21:19:52 +0000

    use generic Class<?> in TransactionAttributeSource signature
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3581 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.transaction/src/main/java/org/springframework/transaction/interceptor/CompositeTransactionAttributeSource.java
+++ org.springframework.transaction/src/main/java/org/springframework/transaction/interceptor/CompositeTransactionAttributeSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,9 +51,8 @@ public class CompositeTransactionAttributeSource implements TransactionAttribute
 	}
 
 
-	public TransactionAttribute getTransactionAttribute(Method method, Class targetClass) {
-		for (int i = 0; i < this.transactionAttributeSources.length; i++) {
-			TransactionAttributeSource tas = this.transactionAttributeSources[i];
+	public TransactionAttribute getTransactionAttribute(Method method, Class<?> targetClass) {
+		for (TransactionAttributeSource tas : this.transactionAttributeSources) {
 			TransactionAttribute ta = tas.getTransactionAttribute(method, targetClass);
 			if (ta != null) {
 				return ta;
