71c052155eb3294e9ff8b725984793889619e28c
Juergen Hoeller <jhoeller@vmware.com>
2010-08-16 20:39:14 +0000

    consistent converting and escaping of label values (following AbstractMultiCheckedElementTag's SPR-7174)
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3599 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.web.servlet/src/main/java/org/springframework/web/servlet/tags/form/AbstractSingleCheckedElementTag.java
+++ org.springframework.web.servlet/src/main/java/org/springframework/web/servlet/tags/form/AbstractSingleCheckedElementTag.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,7 +90,7 @@ public abstract class AbstractSingleCheckedElementTag extends AbstractCheckedEle
 		if (resolvedLabel != null) {
 			tagWriter.startTag("label");
 			tagWriter.writeAttribute("for", id);
-			tagWriter.appendValue(resolvedLabel.toString());
+			tagWriter.appendValue(convertToDisplayString(resolvedLabel));
 			tagWriter.endTag();
 		}
 
