6f69b7b75251a5e78a15b1c5c41b329fb2d482d0
Chris Beams <cbeams@vmware.com>
2010-09-08 15:30:48 +0000

    Allow class-relative resource loading in GenericXmlApplicationContext (SPR-7530)
    Before:
    
        - new GenericXmlApplicationContext("com/acme/path/to/resource.xml");
    
        - GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
          ctx.load("com/acme/path/to/resource.xml");
          ctx.refresh();
    
    After:
    
        - The above remain supported, as well as new class-relative variants
    
        - import com.acme.path.to.Foo;
          new GenericXmlApplicationContext(Foo.class, "resource.xml");
    
        - import com.acme.path.to.Foo;
          GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
          ctx.load(Foo.class, "resource.xml");
          ctx.refresh();
    
    These changes are generally aligned with signatures long available in
    ClassPathXmlApplicationContext. As GenericXmlApplicationContext is
    intended to be a more flexible successor to CPXAC (and FSXAC), it's
    important that all the same conveniences are available.

---
+++ org.springframework.context/src/test/java/org/springframework/context/support/GenericXmlApplicationContextTests.java
@@ -0,0 +1,53 @@
+package org.springframework.context.support;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.util.ClassUtils;
+
+/**
+ * Unit tests for {@link GenericXmlApplicationContext}.
+ * 
+ * See SPR-7530.
+ *
+ * @author Chris Beams
+ */
+public class GenericXmlApplicationContextTests {
+
+	private static final Class<?> RELATIVE_CLASS = GenericXmlApplicationContextTests.class;
+	private static final String RESOURCE_BASE_PATH = ClassUtils.classPackageAsResourcePath(RELATIVE_CLASS);
+	private static final String RESOURCE_NAME = GenericXmlApplicationContextTests.class.getSimpleName() + "-context.xml";
+	private static final String FQ_RESOURCE_PATH = RESOURCE_BASE_PATH + '/' + RESOURCE_NAME;
+	private static final String TEST_BEAN_NAME = "testBean";
+
+
+	@Test
+	public void classRelativeResourceLoading_ctor() {
+		ApplicationContext ctx = new GenericXmlApplicationContext(RELATIVE_CLASS, RESOURCE_NAME);
+		assertThat(ctx.containsBean(TEST_BEAN_NAME), is(true));
+	}
+
+	@Test
+	public void classRelativeResourceLoading_load() {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load(RELATIVE_CLASS, RESOURCE_NAME);
+		ctx.refresh();
+		assertThat(ctx.containsBean(TEST_BEAN_NAME), is(true));
+	}
+
+	@Test
+	public void fullyQualifiedResourceLoading_ctor() {
+		ApplicationContext ctx = new GenericXmlApplicationContext(FQ_RESOURCE_PATH);
+		assertThat(ctx.containsBean(TEST_BEAN_NAME), is(true));
+	}
+
+	@Test
+	public void fullyQualifiedResourceLoading_load() {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load(FQ_RESOURCE_PATH);
+		ctx.refresh();
+		assertThat(ctx.containsBean(TEST_BEAN_NAME), is(true));
+	}
+}
