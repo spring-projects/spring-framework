c046419acd4d00d4ccf53b78cdabe0f99c9ec4ad
Mark Fisher <markfisher@vmware.com>
2010-10-11 17:36:18 +0000

    SPR-7627 added Serializer and Deserializer strategies, Converter adapters, and default implementations

---
+++ org.springframework.core/src/test/java/org/springframework/core/serializer/SerializationConverterTests.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.core.serializer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.NotSerializableException;
+import java.io.Serializable;
+
+import org.junit.Test;
+
+import org.springframework.core.serializer.DeserializationFailureException;
+import org.springframework.core.serializer.DeserializingConverter;
+import org.springframework.core.serializer.SerializationFailureException;
+import org.springframework.core.serializer.SerializingConverter;
+
+/**
+ * @author Gary Russell
+ * @author Mark Fisher
+ * @since 3.0.5
+ */
+public class SerializationConverterTests {
+
+	@Test
+	public void serializeAndDeserializeString() {
+		SerializingConverter toBytes = new SerializingConverter();
+		byte[] bytes = toBytes.convert("Testing");
+		DeserializingConverter fromBytes = new DeserializingConverter();
+		assertEquals("Testing", fromBytes.convert(bytes));
+	}
+
+	@Test
+	public void nonSerializableObject() {
+		SerializingConverter toBytes = new SerializingConverter();
+		try {
+			toBytes.convert(new Object());
+			fail("Expected IllegalArgumentException");
+		}
+		catch (SerializationFailureException e) {
+			assertNotNull(e.getCause());
+			assertTrue(e.getCause() instanceof IllegalArgumentException);
+		}
+	}
+
+	@Test
+	public void nonSerializableField() {
+		SerializingConverter toBytes = new SerializingConverter();
+		try {
+			toBytes.convert(new UnSerializable());
+			fail("Expected SerializationFailureException");
+		}
+		catch (SerializationFailureException e) {
+			assertNotNull(e.getCause());
+			assertTrue(e.getCause() instanceof NotSerializableException);
+		}
+	}
+
+	@Test(expected = DeserializationFailureException.class)
+	public void deserializationFailure() {
+		DeserializingConverter fromBytes = new DeserializingConverter();
+		fromBytes.convert("Junk".getBytes());
+	}
+
+
+	class UnSerializable implements Serializable {
+
+		private static final long serialVersionUID = 1L;
+
+		@SuppressWarnings("unused")
+		private Object object;
+	}
+
+}
