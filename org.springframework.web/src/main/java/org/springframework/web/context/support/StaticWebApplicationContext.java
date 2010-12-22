a32894ca313c8ad3ed3dedd0a7c77e897b0fad31
Arjen Poutsma <apoutsma@vmware.com>
2010-12-22 10:23:34 +0000

    Removed JDK 1.6 usage
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@3830 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.web/src/main/java/org/springframework/web/context/support/StaticWebApplicationContext.java
+++ org.springframework.web/src/main/java/org/springframework/web/context/support/StaticWebApplicationContext.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,7 +17,6 @@
 package org.springframework.web.context.support;
 
 import java.util.LinkedList;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 
@@ -170,8 +169,8 @@ public class StaticWebApplicationContext extends StaticApplicationContext
 	protected void onRefresh() {
 		this.themeSource = UiApplicationContextUtils.initThemeSource(this);
 		LinkedList<PropertySource<?>> propertySources = this.getEnvironment().getPropertySources();
-		propertySources.push(new ServletContextPropertySource(servletContext));
-		propertySources.push(new ServletConfigPropertySource(servletConfig));
+		propertySources.addFirst(new ServletContextPropertySource(servletContext));
+		propertySources.addFirst(new ServletConfigPropertySource(servletConfig));
 	}
 
 	public Theme getTheme(String themeName) {
