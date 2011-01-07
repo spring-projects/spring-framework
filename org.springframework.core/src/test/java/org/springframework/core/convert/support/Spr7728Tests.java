b040606cfad97f5fafb2289814d9e8e291bc16f4
Keith Donald <kdonald@vmware.com>
2011-01-07 14:21:54 +0000

    spr-7728: empty collection conversion can return value not assignable to targetType

---
+++ org.springframework.core/src/test/java/org/springframework/core/convert/support/Spr7728Tests.java
@@ -0,0 +1,52 @@
+package org.springframework.core.convert.support;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Vector;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.convert.TypeDescriptor;
+
+public class Spr7728Tests
+{
+    private CollectionToCollectionConverter theConverter;
+    private Vector<String> theSrcVector;
+    private TypeDescriptor theSrcType;
+    private TypeDescriptor theTargetType;
+
+    @Before
+    public void setup()
+    {
+        theSrcVector = new Vector<String>();
+        theSrcType = TypeDescriptor.forObject(theSrcVector);
+        theTargetType = TypeDescriptor.forObject(new ArrayList());
+        theConverter = new CollectionToCollectionConverter(new GenericConversionService());
+    }
+
+    @Test
+    public void convertEmptyVector_shouldReturnEmptyArrayList()
+        throws Exception
+    {
+        theSrcVector.add("Element");
+
+        testCollectionConversionToArrayList(theSrcVector);
+    }
+
+    @Test
+    public void convertNonEmptyVector_shouldReturnNonEmptyArrayList()
+        throws Exception
+    {
+        testCollectionConversionToArrayList(theSrcVector);
+    }
+
+    private void testCollectionConversionToArrayList(Collection<String> aSource)
+    {
+        Object myConverted = theConverter.convert(aSource, theSrcType, theTargetType);
+
+        Assert.assertTrue(myConverted instanceof ArrayList<?>);
+        Assert.assertEquals(aSource.size(), ((ArrayList<?>) myConverted).size());
+    }
+
+}
