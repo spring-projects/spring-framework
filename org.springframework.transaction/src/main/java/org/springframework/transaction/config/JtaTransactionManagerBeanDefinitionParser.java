df6be9f694c68247a9a5eb2193103e7b48034024
Juergen Hoeller <jhoeller@vmware.com>
2011-02-10 01:58:21 +0000

    turned to package visibility

--- org.springframework.transaction/src/main/java/org/springframework/transaction/config/JtaTransactionManagerBeanDefinitionParser.java
+++ org.springframework.transaction/src/main/java/org/springframework/transaction/config/JtaTransactionManagerBeanDefinitionParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@ import org.springframework.util.ClassUtils;
  * @author Christian Dupuis
  * @since 2.5
  */
-public class JtaTransactionManagerBeanDefinitionParser extends AbstractSingleBeanDefinitionParser  {
+class JtaTransactionManagerBeanDefinitionParser extends AbstractSingleBeanDefinitionParser  {
 
 	private static final String WEBLOGIC_JTA_TRANSACTION_MANAGER_CLASS_NAME =
 			"org.springframework.transaction.jta.WebLogicJtaTransactionManager";
