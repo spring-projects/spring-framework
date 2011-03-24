e633c97bffb21c3f88a3bac3fe1f9ef5fee683f8
Agim Emruli <agimemruli@gmail.com>
2011-03-24 11:06:32 +0000

    fixed compile error in test with javac compiler
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@4110 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.beans/src/test/java/org/springframework/beans/factory/xml/NestedBeansElementAttributeRecursionTests.java
+++ org.springframework.beans/src/test/java/org/springframework/beans/factory/xml/NestedBeansElementAttributeRecursionTests.java
@@ -63,18 +63,18 @@ public class NestedBeansElementAttributeRecursionTests {
 
 		TestBean topLevel = bf.getBean("topLevelConcreteTestBean", TestBean.class);
 		// has the concrete child bean values
-		assertThat(topLevel.getSomeList(), hasItems("charlie", "delta"));
+		assertThat((Iterable<String>) topLevel.getSomeList(), hasItems("charlie", "delta"));
 		// but does not merge the parent values
-		assertThat(topLevel.getSomeList(), not(hasItems("alpha", "bravo")));
+		assertThat((Iterable<String>) topLevel.getSomeList(), not(hasItems("alpha", "bravo")));
 
 		TestBean firstLevel = bf.getBean("firstLevelNestedTestBean", TestBean.class);
 		// merges all values
-		assertThat(firstLevel.getSomeList(),
+		assertThat((Iterable<String>) firstLevel.getSomeList(),
 				hasItems("charlie", "delta", "echo", "foxtrot"));
 
 		TestBean secondLevel = bf.getBean("secondLevelNestedTestBean", TestBean.class);
 		// merges all values
-		assertThat(secondLevel.getSomeList(),
+		assertThat((Iterable<String>)secondLevel.getSomeList(),
 				hasItems("charlie", "delta", "echo", "foxtrot", "golf", "hotel"));
 	}
 
