619ac47b0093619f2af28855d54d6b404a2e392c
Sam Brannen <sam@sambrannen.com>
2011-04-03 17:20:02 +0000

    Removed unnecessary declaration of @TestExecutionListeners.
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@4163 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.test/src/test/java/org/springframework/test/context/junit4/spr3896/DefaultLocationsBaseTests.java
+++ org.springframework.test/src/test/java/org/springframework/test/context/junit4/spr3896/DefaultLocationsBaseTests.java
@@ -21,13 +21,10 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.beans.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 /**
  * <p>
@@ -43,7 +40,6 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-@TestExecutionListeners( { DependencyInjectionTestExecutionListener.class })
 public class DefaultLocationsBaseTests {
 
 	@Autowired
