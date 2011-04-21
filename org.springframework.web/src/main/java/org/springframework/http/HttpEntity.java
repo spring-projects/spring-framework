ab654a7a0623957fb4808a1fe0d2eaebd6616108
Arjen Poutsma <apoutsma@vmware.com>
2011-04-21 11:39:20 +0000

    added toString to HttpEntity

--- org.springframework.web/src/main/java/org/springframework/http/HttpEntity.java
+++ org.springframework.web/src/main/java/org/springframework/http/HttpEntity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -122,4 +122,19 @@ public class HttpEntity<T> {
 		return (this.body != null);
 	}
 
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder("<");
+		if (body != null) {
+			builder.append(body);
+			if (headers != null) {
+				builder.append(',');
+			}
+		}
+		if (headers != null) {
+			builder.append(headers);
+		}
+		builder.append('>');
+		return builder.toString();
+	}
 }
