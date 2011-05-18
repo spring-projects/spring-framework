a618bcc8cde89b7c7cc6e9fbc4348465387a8e99
Arjen Poutsma <apoutsma@vmware.com>
2011-05-18 11:24:57 +0000

    Exposing HttpServletRequest/Response in ServletServerHttpRequest/Response

--- org.springframework.web/src/main/java/org/springframework/http/server/ServletServerHttpRequest.java
+++ org.springframework.web/src/main/java/org/springframework/http/server/ServletServerHttpRequest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -64,6 +64,13 @@ public class ServletServerHttpRequest implements ServerHttpRequest {
 	public ServletServerHttpRequest(HttpServletRequest servletRequest) {
 		Assert.notNull(servletRequest, "'servletRequest' must not be null");
 		this.servletRequest = servletRequest;
+	}
+
+	/**
+	 * Returns the {@code HttpServletRequest} this object is based on.
+	 */
+	public HttpServletRequest getServletRequest() {
+		return servletRequest;
 	}
 
 	public HttpMethod getMethod() {
--- org.springframework.web/src/main/java/org/springframework/http/server/ServletServerHttpResponse.java
+++ org.springframework.web/src/main/java/org/springframework/http/server/ServletServerHttpResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,6 +50,13 @@ public class ServletServerHttpResponse implements ServerHttpResponse {
 		this.servletResponse = servletResponse;
 	}
 
+
+	/**
+	 * Returns the {@code HttpServletResponse} this object is based on.
+	 */
+	public HttpServletResponse getServletResponse() {
+		return servletResponse;
+	}
 
 	public void setStatusCode(HttpStatus status) {
 		this.servletResponse.setStatus(status.value());
