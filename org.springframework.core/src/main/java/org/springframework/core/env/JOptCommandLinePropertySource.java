1eb581134794ba7d4926f6d10d4dc8a70de247ec
Chris Beams <cbeams@vmware.com>
2011-06-30 22:33:56 +0000

    Introduce CommandLinePropertySource and impls
    Users may now work with command line arguments as a source of
    properties for use with the PropertySource and Environment APIs.
    An implementation based on the jopt library and a "simple"
    implementation requiring no external libraries are are provided
    out-of-the box.
    
    See Javadoc for CommandLinePropertySource, JOptCommandLinePropertySource
    and SimpleCommandLinePropertySource for details.
    
    Issue: SPR-8482

---
+++ org.springframework.core/src/main/java/org/springframework/core/env/JOptCommandLinePropertySource.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.core.env;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import joptsimple.OptionSet;
+
+/**
+ * {@link CommandLinePropertySource} implementation backed by a JOpt {@link OptionSet}.
+ *
+ * <h2>Typical usage</h2>
+ * Configure and execute an {@code OptionParser} against the {@code String[]} of arguments
+ * supplied to the {@code main} method, and create a {@link JOptCommandLinePropertySource}
+ * using the resulting {@code OptionSet} object:
+ * <pre class="code">
+ * public static void main(String[] args) {
+ *     OptionParser parser = new OptionParser();
+ *     parser.accepts("option1");
+ *     parser.accepts("option2").withRequiredArg();
+ *     OptionSet options = parser.parse(args);
+ *     PropertySource<?> ps = new JOptCommandLinePropertySource(options);
+ *     // ...
+ * }</pre>
+ *
+ * See {@link CommandLinePropertySource} for complete general usage examples.
+ *
+ * <h3>Requirements</h3>
+ *
+ * <p>Use of this class requires adding the jopt-simple JAR to your application classpath.
+ * Versions 3.0 and better are supported.
+ *
+ * @author Chris Beams
+ * @since 3.1
+ * @see CommandLinePropertySource
+ * @see joptsimple.OptionParser
+ * @see joptsimple.OptionSet
+ */
+public class JOptCommandLinePropertySource extends CommandLinePropertySource<OptionSet> {
+
+	/**
+	 * Create a new {@code JOptCommandLinePropertySource} having the default name
+	 * and backed by the given {@code OptionSet}.
+	 * @see CommandLinePropertySource#DEFAULT_COMMAND_LINE_PROPERTY_SOURCE_NAME
+	 * @see CommandLinePropertySource#CommandLinePropertySource(Object)
+	 */
+	public JOptCommandLinePropertySource(OptionSet options) {
+		super(options);
+	}
+
+	/**
+	 * Create a new {@code JOptCommandLinePropertySource} having the given name
+	 * and backed by the given {@code OptionSet}.
+	 */
+	public JOptCommandLinePropertySource(String name, OptionSet options) {
+		super(name, options);
+	}
+
+	@Override
+	protected boolean containsOption(String key) {
+		return this.source.has(key);
+	}
+
+	@Override
+	public List<String> getOptionValues(String key) {
+		List<?> argValues = this.source.valuesOf(key);
+		List<String> stringArgValues = new ArrayList<String>();
+		for(Object argValue : argValues) {
+			if (!(argValue instanceof String)) {
+				throw new IllegalArgumentException("argument values must be of type String");
+			}
+			stringArgValues.add((String)argValue);
+		}
+		if (stringArgValues.size() == 0) {
+			if (this.source.has(key)) {
+				return Collections.emptyList();
+			}
+			else {
+				return null;
+			}
+		}
+		return Collections.unmodifiableList(stringArgValues);
+	}
+
+	@Override
+	protected List<String> getNonOptionArgs() {
+		return this.source.nonOptionArguments();
+	}
+
+}
---
+++ org.springframework.core/src/test/java/org/springframework/core/env/SimpleCommandLineParserTests.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.core.env;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+public class SimpleCommandLineParserTests {
+
+	@Test
+	public void withNoOptions() {
+		SimpleCommandLineArgsParser parser = new SimpleCommandLineArgsParser();
+		assertThat(parser.parse().getOptionValues("foo"), nullValue());
+	}
+
+	@Test
+	public void withSingleOptionAndNoValue() {
+		SimpleCommandLineArgsParser parser = new SimpleCommandLineArgsParser();
+		CommandLineArgs args = parser.parse("--o1");
+		assertThat(args.containsOption("o1"), is(true));
+		assertThat(args.getOptionValues("o1"), equalTo(Collections.EMPTY_LIST));
+	}
+
+	@Test
+	public void withSingleOptionAndValue() {
+		SimpleCommandLineArgsParser parser = new SimpleCommandLineArgsParser();
+		CommandLineArgs args = parser.parse("--o1=v1");
+		assertThat(args.containsOption("o1"), is(true));
+		assertThat(args.getOptionValues("o1").get(0), equalTo("v1"));
+	}
+
+	@Test
+	public void withMixOfOptionsHavingValueAndOptionsHavingNoValue() {
+		SimpleCommandLineArgsParser parser = new SimpleCommandLineArgsParser();
+		CommandLineArgs args = parser.parse("--o1=v1", "--o2");
+		assertThat(args.containsOption("o1"), is(true));
+		assertThat(args.containsOption("o2"), is(true));
+		assertThat(args.containsOption("o3"), is(false));
+		assertThat(args.getOptionValues("o1").get(0), equalTo("v1"));
+		assertThat(args.getOptionValues("o2"), equalTo(Collections.EMPTY_LIST));
+		assertThat(args.getOptionValues("o3"), nullValue());
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void withEmptyOptionText() {
+		SimpleCommandLineArgsParser parser = new SimpleCommandLineArgsParser();
+		parser.parse("--");
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void withEmptyOptionName() {
+		SimpleCommandLineArgsParser parser = new SimpleCommandLineArgsParser();
+		parser.parse("--=v1");
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void withEmptyOptionValue() {
+		SimpleCommandLineArgsParser parser = new SimpleCommandLineArgsParser();
+		parser.parse("--o1=");
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void withEmptyOptionNameAndEmptyOptionValue() {
+		SimpleCommandLineArgsParser parser = new SimpleCommandLineArgsParser();
+		parser.parse("--=");
+	}
+
+	@Test
+	public void withNonOptionArguments() {
+		SimpleCommandLineArgsParser parser = new SimpleCommandLineArgsParser();
+		CommandLineArgs args = parser.parse("--o1=v1", "noa1", "--o2=v2", "noa2");
+		assertThat(args.getOptionValues("o1").get(0), equalTo("v1"));
+		assertThat(args.getOptionValues("o2").get(0), equalTo("v2"));
+
+		List<String> nonOptions = args.getNonOptionArgs();
+		assertThat(nonOptions.get(0), equalTo("noa1"));
+		assertThat(nonOptions.get(1), equalTo("noa2"));
+		assertThat(nonOptions.size(), equalTo(2));
+	}
+
+	@Test(expected=UnsupportedOperationException.class)
+	public void assertOptionNamesIsUnmodifiable() {
+		CommandLineArgs args = new SimpleCommandLineArgsParser().parse();
+		args.getOptionNames().add("bogus");
+	}
+
+	@Test(expected=UnsupportedOperationException.class)
+	public void assertNonOptionArgsIsUnmodifiable() {
+		CommandLineArgs args = new SimpleCommandLineArgsParser().parse();
+		args.getNonOptionArgs().add("foo");
+	}
+
+}
