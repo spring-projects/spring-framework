6c6594137eb5989624c2cf8892527044f8c6d1b8
Chris Beams <cbeams@vmware.com>
2011-07-13 23:30:57 +0000

    Allow ConfigurationCPP to process multiple registries
    Prior to this change, an instance of ConfigurationClassPostProcessor
    would throw IllegalStateException if its
    postProcessBeanDefinitionRegistry method were called more than once.
    This check is important to ensure that @Configuration classes are
    not proxied by CGLIB multiple times, and works for most normal use
    cases.
    
    However, if the same CCPP instance is used to process multiple
    registries/factories/contexts, this check creates a false negative
    because it does not distinguish between invocations of
    postProcessBeanDefinitionRegistry across different registries.
    
    A use case for this, though admittedly uncommon, would be creating
    a CCPP instance and registering it via
    ConfigurableApplicationContext#addBeanDefinitionPostProcessor against
    several ApplicationContexts. In such a case, the same CCPP instance
    will post-process multiple different registry instances, and throw the
    above mentioned exception.
    
    With this change, CCPP now performs lightweight tracking of the
    registries/beanFactories that it has already processed by recording
    the identity hashcodes of these objects.  This is only slightly more
    complex than the previous boolean-based 'already processed' flags, and
    prevents this issue (however rare it may be) from occurring.
    
    Issue: SPR-8527
    
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@4699 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.context/src/test/java/org/springframework/context/annotation/ConfigurationClassPostProcessorTests.java
+++ org.springframework.context/src/test/java/org/springframework/context/annotation/ConfigurationClassPostProcessorTests.java
@@ -82,6 +82,25 @@ public class ConfigurationClassPostProcessorTests {
 		assertSame(foo, bar.foo);
 	}
 
+	@Test
+	public void testProcessingAllowedOnlyOncePerProcessorRegistryPair() {
+		DefaultListableBeanFactory bf1 = new DefaultListableBeanFactory();
+		DefaultListableBeanFactory bf2 = new DefaultListableBeanFactory();
+		ConfigurationClassPostProcessor pp = new ConfigurationClassPostProcessor();
+		pp.postProcessBeanFactory(bf1); // first invocation -- should succeed
+		try {
+			pp.postProcessBeanFactory(bf1); // second invocation for bf1 -- should throw
+			fail("expected exception");
+		} catch (IllegalStateException ex) {
+		}
+		pp.postProcessBeanFactory(bf2); // first invocation for bf2 -- should succeed
+		try {
+			pp.postProcessBeanFactory(bf2); // second invocation for bf2 -- should throw
+			fail("expected exception");
+		} catch (IllegalStateException ex) {
+		}
+	}
+
 
 	@Configuration
 	static class SingletonBeanConfig {
