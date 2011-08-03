8f23c66512720ee3077e8500bf976549504fcd8f
Juergen Hoeller <jhoeller@vmware.com>
2011-08-03 10:19:27 +0000

    SpringBeanAutowiringSupport is able to process @Value annotations on any given target instance (SPR-8574)

---
+++ org.springframework.web/src/test/java/org/springframework/web/context/support/SpringBeanAutowiringSupportTests.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.web.context.support;
+
+import org.junit.Test;
+
+import org.springframework.beans.ITestBean;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.TestBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Juergen Hoeller
+ */
+public class SpringBeanAutowiringSupportTests {
+
+	@Test
+	public void testProcessInjectionBasedOnServletContext() {
+		MockServletContext sc = new MockServletContext();
+		StaticWebApplicationContext wac = new StaticWebApplicationContext();
+		MutablePropertyValues pvs = new MutablePropertyValues();
+		pvs.add("name", "tb");
+		wac.registerSingleton("testBean", TestBean.class, pvs);
+		wac.setServletContext(sc);
+		wac.refresh();
+		sc.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, wac);
+		InjectionTarget target = new InjectionTarget();
+		SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(target, sc);
+		assertTrue(target.testBean instanceof TestBean);
+		assertEquals("tb", target.name);
+	}
+
+
+	public static class InjectionTarget {
+
+		@Autowired
+		public ITestBean testBean;
+
+		@Value("#{testBean.name}")
+		public String name;
+	}
+
+}
