da3724c6e36a705c27621fb4aa3f8b91fa20bae6
Chris Beams <cbeams@vmware.com>
2011-08-20 03:02:20 +0000

    Introduce ConfigurableEnvironment#addActiveProfile
    Provides a convenient mechanism for activating an additional profile
    while preserving those that are already active, as opposed to
    calling #setActiveProfiles with the contents of #getActiveProfiles plus
    the new profile.
    
    Issue: SPR-8548
    
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@4907 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.core/src/test/java/org/springframework/core/env/EnvironmentTests.java
+++ org.springframework.core/src/test/java/org/springframework/core/env/EnvironmentTests.java
@@ -121,6 +121,21 @@ public class EnvironmentTests {
 	}
 
 	@Test
+	public void addActiveProfile() {
+		assertThat(environment.getActiveProfiles().length, is(0));
+		environment.setActiveProfiles("local", "embedded");
+		assertThat(Arrays.asList(environment.getActiveProfiles()), hasItems("local", "embedded"));
+		assertThat(environment.getActiveProfiles().length, is(2));
+		environment.addActiveProfile("p1");
+		assertThat(Arrays.asList(environment.getActiveProfiles()), hasItems("p1"));
+		assertThat(environment.getActiveProfiles().length, is(3));
+		environment.addActiveProfile("p2");
+		environment.addActiveProfile("p3");
+		assertThat(Arrays.asList(environment.getActiveProfiles()), hasItems("p2", "p3"));
+		assertThat(environment.getActiveProfiles().length, is(5));
+	}
+
+	@Test
 	public void reservedDefaultProfile() {
 		assertThat(environment.getDefaultProfiles(), equalTo(new String[]{RESERVED_DEFAULT_PROFILE_NAME}));
 		System.setProperty(DEFAULT_PROFILES_PROPERTY_NAME, "d0");
