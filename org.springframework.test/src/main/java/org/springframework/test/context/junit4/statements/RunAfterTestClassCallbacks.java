f6483cad3c6334885b25a0b2280ad99f453106e8
Sam Brannen <sam@sambrannen.com>
2011-09-26 11:13:59 +0000

    [SPR-8222] Suppressing deprecation warnings for org.junit.internal.runners.model.MultipleFailureException, which has been deprecated in JUnit 4.9.

--- org.springframework.test/src/main/java/org/springframework/test/context/junit4/statements/RunAfterTestClassCallbacks.java
+++ org.springframework.test/src/main/java/org/springframework/test/context/junit4/statements/RunAfterTestClassCallbacks.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,7 @@ import org.springframework.test.context.TestContextManager;
  * @author Sam Brannen
  * @since 3.0
  */
+@SuppressWarnings("deprecation")
 public class RunAfterTestClassCallbacks extends Statement {
 
 	private final Statement next;
