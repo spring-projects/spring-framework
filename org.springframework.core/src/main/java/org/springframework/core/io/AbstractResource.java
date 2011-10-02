7c9bd2e869aeba7f84475d68b53cf8c680102e50
Chris Beams <cbeams@vmware.com>
2011-10-02 19:45:52 +0000

    Fix error in AbstractResource#contentLength Javadoc
    Issue: SPR-8734
    
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@5016 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.core/src/main/java/org/springframework/core/io/AbstractResource.java
+++ org.springframework.core/src/main/java/org/springframework/core/io/AbstractResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,7 +108,7 @@ public abstract class AbstractResource implements Resource {
 	}
 
 	/**
-	 * This implementation checks the timestamp of the underlying File,
+	 * This implementation checks the length of the underlying File,
 	 * if available.
 	 * @see #getFile()
 	 */
