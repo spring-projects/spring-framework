4055e9a8d47504041bdc9833af9edcb7eee425a2
Thomas Risberg <trisberg@vmware.com>
2011-10-06 23:58:38 +0000

    SPR-8652 Fixing queryForLong and queryForInt implementation to use correct expected type
    git-svn-id: https://src.springframework.org/svn/spring-framework/trunk@5023 50f2f4bb-b051-0410-bef5-90022cba6387

--- org.springframework.jdbc/src/main/java/org/springframework/jdbc/core/namedparam/NamedParameterJdbcTemplate.java
+++ org.springframework.jdbc/src/main/java/org/springframework/jdbc/core/namedparam/NamedParameterJdbcTemplate.java
@@ -206,7 +206,7 @@ public class NamedParameterJdbcTemplate implements NamedParameterJdbcOperations 
 	}
 
 	public long queryForLong(String sql, SqlParameterSource paramSource) throws DataAccessException {
-		Number number = queryForObject(sql, paramSource, Number.class);
+		Number number = queryForObject(sql, paramSource, Long.class);
 		return (number != null ? number.longValue() : 0);
 	}
 
@@ -215,7 +215,7 @@ public class NamedParameterJdbcTemplate implements NamedParameterJdbcOperations 
 	}
 
 	public int queryForInt(String sql, SqlParameterSource paramSource) throws DataAccessException {
-		Number number = queryForObject(sql, paramSource, Number.class);
+		Number number = queryForObject(sql, paramSource, Integer.class);
 		return (number != null ? number.intValue() : 0);
 	}
 
