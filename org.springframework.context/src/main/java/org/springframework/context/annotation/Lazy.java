3920c5a158530f6278631648e4fd3761daf3c5d0
Chris Beams <cbeams@vmware.com>
2011-10-11 18:51:15 +0000

    Polish @ComponentScan/@Configuration/@Lazy Javadoc

--- org.springframework.context/src/main/java/org/springframework/context/annotation/Lazy.java
+++ org.springframework.context/src/main/java/org/springframework/context/annotation/Lazy.java
@@ -26,23 +26,22 @@ import java.lang.annotation.Inherited;
 /**
  * Indicates whether a bean is to be lazily initialized.
  *
- * <p>May be used on any class directly or indirectly annotated with
- * {@link org.springframework.stereotype.Component} or on methods annotated with
- * {@link Bean}.
+ * <p>May be used on any class directly or indirectly annotated with {@link
+ * org.springframework.stereotype.Component @Component} or on methods annotated with
+ * {@link Bean @Bean}.
  *
- * <p>If this annotation is not present on a Component or Bean definition, eager
- * initialization will occur. If present and set to {@code true}, the
- * Bean/Component will not be initialized until referenced by another bean or
- * explicitly retrieved from the enclosing
- * {@link org.springframework.beans.factory.BeanFactory}. If present and set to
- * {@code false}, the bean will be instantiated on startup by bean factories
- * that perform eager initialization of singletons.
+ * <p>If this annotation is not present on a {@code @Component} or {@code @Bean} definition,
+ * eager initialization will occur. If present and set to {@code true}, the {@code @Bean} or
+ * {@code @Component} will not be initialized until referenced by another bean or explicitly
+ * retrieved from the enclosing {@link org.springframework.beans.factory.BeanFactory
+ * BeanFactory}. If present and set to {@code false}, the bean will be instantiated on
+ * startup by bean factories that perform eager initialization of singletons.
  *
  * <p>If Lazy is present on a {@link Configuration @Configuration} class, this
- * indicates that all {@link Bean @Bean} methods within that {@code @Configuration}
- * should be lazily initialized. If Lazy is present and false on a Bean method
- * within a Lazy-annotated Configuration class, this indicates overriding the
- * 'default lazy' behavior and that the bean should be eagerly initialized.
+ * indicates that all {@code @Bean} methods within that {@code @Configuration}
+ * should be lazily initialized. If {@code @Lazy} is present and false on a {@code @Bean}
+ * method within a {@code @Lazy}-annotated {@code @Configuration} class, this indicates
+ * overriding the 'default lazy' behavior and that the bean should be eagerly initialized.
  *
  * @author Chris Beams
  * @since 3.0
