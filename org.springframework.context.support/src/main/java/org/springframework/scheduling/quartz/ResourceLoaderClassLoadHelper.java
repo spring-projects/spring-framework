0b078f2ff907451238859b6d6bc408017f61eb6f
Juergen Hoeller <jhoeller@vmware.com>
2011-11-11 09:54:28 +0000

    added Quartz 2.1 loadClass variant (SPR-8727)

--- org.springframework.context.support/src/main/java/org/springframework/scheduling/quartz/ResourceLoaderClassLoadHelper.java
+++ org.springframework.context.support/src/main/java/org/springframework/scheduling/quartz/ResourceLoaderClassLoadHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,6 +74,11 @@ public class ResourceLoaderClassLoadHelper implements ClassLoadHelper {
 	public Class loadClass(String name) throws ClassNotFoundException {
 		return this.resourceLoader.getClassLoader().loadClass(name);
 	}
+
+	@SuppressWarnings("unchecked")
+	public <T> Class<? extends T> loadClass(String name, Class<T> clazz) throws ClassNotFoundException {
+        return loadClass(name);
+    }
 
 	public URL getResource(String name) {
 		Resource resource = this.resourceLoader.getResource(name);
