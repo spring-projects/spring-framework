124662189e1883d9cf2688eb82c1c9ef43a21017
Chris Beams <cbeams@vmware.com>
2011-11-16 04:20:22 +0000

    Polish @EnableAsync imports, etc

--- org.springframework.context/src/test/java/org/springframework/context/annotation/EnableAspectJAutoProxyTests.java
+++ org.springframework.context/src/test/java/org/springframework/context/annotation/EnableAspectJAutoProxyTests.java
@@ -1,12 +1,10 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-import org.springframework.aop.aspectj.annotation.EnableAspectJAutoProxy;
-
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
