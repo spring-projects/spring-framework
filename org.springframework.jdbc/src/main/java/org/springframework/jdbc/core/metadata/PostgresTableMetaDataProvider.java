70c28a0bc5b35e9851a4ee3b757729df74fe3eb2
Chris Beams <cbeams@vmware.com>
2011-11-16 18:23:56 +0000

    Add Apache license header where missing in src/main

--- org.springframework.jdbc/src/main/java/org/springframework/jdbc/core/metadata/PostgresTableMetaDataProvider.java
+++ org.springframework.jdbc/src/main/java/org/springframework/jdbc/core/metadata/PostgresTableMetaDataProvider.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.jdbc.core.metadata;
 
 import java.sql.DatabaseMetaData;
@@ -16,7 +32,6 @@ public class PostgresTableMetaDataProvider extends GenericTableMetaDataProvider 
 		super(databaseMetaData);
 	}
 
-
 	@Override
 	public boolean isGetGeneratedKeysSimulated() {
 		if (getDatabaseVersion().compareTo("8.2.0") >= 0) {
@@ -27,7 +42,6 @@ public class PostgresTableMetaDataProvider extends GenericTableMetaDataProvider 
 			return false;
 		}
 	}
-
 
 	@Override
 	public String getSimpleQueryForGetGeneratedKey(String tableName, String keyColumnName) {
--- org.springframework.web/src/main/java/org/springframework/web/bind/annotation/PathVariable.java
+++ org.springframework.web/src/main/java/org/springframework/web/bind/annotation/PathVariable.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.web.bind.annotation;
 
 import java.lang.annotation.Documented;
@@ -7,8 +23,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation which indicates that a method parameter should be bound to a URI template variable. Supported for {@link
- * RequestMapping} annotated handler methods in Servlet environments.
+ * Annotation which indicates that a method parameter should be bound to a URI template
+ * variable. Supported for {@link RequestMapping} annotated handler methods in Servlet
+ * environments.
  *
  * @author Arjen Poutsma
  * @see RequestMapping
