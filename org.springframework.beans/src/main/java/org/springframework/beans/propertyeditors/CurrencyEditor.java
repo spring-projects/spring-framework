8a43dd172c7737e82c31831114c772f4c3b8472e
Juergen Hoeller <jhoeller@vmware.com>
2011-11-28 21:14:47 +0000

    fixed getAsText javadoc (SPR-7890)

--- org.springframework.beans/src/main/java/org/springframework/beans/propertyeditors/CurrencyEditor.java
+++ org.springframework.beans/src/main/java/org/springframework/beans/propertyeditors/CurrencyEditor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,10 +34,6 @@ public class CurrencyEditor extends PropertyEditorSupport {
 		setValue(Currency.getInstance(text));
 	}
 
-	/**
-	 * This implementation returns <code>null</code> to indicate that
-	 * there is no appropriate text representation.
-	 */
 	@Override
 	public String getAsText() {
 		Currency value = (Currency) getValue();
