e0231438cdfa75bcbd34bae28765e2afcb0c4121
Juergen Hoeller <jhoeller@vmware.com>
2011-12-21 12:41:37 +0100

    fixed "packagesToScan" to avoid additional provider scan (SPR-8936)

--- org.springframework.orm/src/main/java/org/springframework/orm/jpa/persistenceunit/DefaultPersistenceUnitManager.java
+++ org.springframework.orm/src/main/java/org/springframework/orm/jpa/persistenceunit/DefaultPersistenceUnitManager.java
@@ -390,7 +390,7 @@ public class DefaultPersistenceUnitManager
 	private SpringPersistenceUnitInfo buildDefaultPersistenceUnitInfo() {
 		SpringPersistenceUnitInfo scannedUnit = new SpringPersistenceUnitInfo();
 		scannedUnit.setPersistenceUnitName(this.defaultPersistenceUnitName);
-		scannedUnit.excludeUnlistedClasses();
+		scannedUnit.setExcludeUnlistedClasses(true);
 		if (this.packagesToScan != null) {
 			for (String pkg : this.packagesToScan) {
 				try {
