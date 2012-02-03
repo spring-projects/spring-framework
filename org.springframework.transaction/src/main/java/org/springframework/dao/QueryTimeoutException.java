e202d89c29fe9380b7ebc485eb16353b45937096
Thomas Risberg <trisberg@vmware.com>
2012-02-03 11:21:19 -0500

    SPR-7680 Adding QueryTimeoutException to the DataAccessException hierarchy

---
+++ org.springframework.transaction/src/main/java/org/springframework/dao/QueryTimeoutException.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.dao;
+
+/**
+ * Exception to be thrown on a query timeout. This could have different causes depending on
+ * the database API in use but most likely thrown after the database interrupts or stops
+ * the processing of a query before it has completed.
+ *
+ * <p>This exception can be thrown by user code trapping the native database exception or
+ * by exception translation.
+ *
+ * @author Thomas Risberg
+ * @since 3.1
+ */
+public class QueryTimeoutException extends TransientDataAccessException {
+
+	/**
+	 * Constructor for QueryTimeoutException.
+	 * @param msg the detail message
+	 */
+	public QueryTimeoutException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * Constructor for QueryTimeoutException.
+	 * @param msg the detail message
+	 * @param cause the root cause from the data access API in use
+	 */
+	public QueryTimeoutException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
