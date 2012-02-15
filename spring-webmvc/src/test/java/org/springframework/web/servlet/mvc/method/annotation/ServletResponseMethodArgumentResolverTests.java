dbeb7610651a5a098fe51b38e4475b52ce6180b7
Stevo Slavic <sslavic@gmail.com>
2012-02-15 13:33:05 +0100

    [fix:SPR-8983]Fixes NPE when resolving ServletResponse arguments of init-binder method

--- spring-webmvc/src/test/java/org/springframework/web/servlet/mvc/method/annotation/ServletResponseMethodArgumentResolverTests.java
+++ spring-webmvc/src/test/java/org/springframework/web/servlet/mvc/method/annotation/ServletResponseMethodArgumentResolverTests.java
@@ -71,6 +71,16 @@ public class ServletResponseMethodArgumentResolverTests {
 	}
 
 	@Test
+	public void servletResponseParamInInitBinderMethod() throws Exception {
+		MethodParameter servletResponseParameter = new MethodParameter(method, 0);
+
+		assertTrue("ServletResponse not supported", resolver.supportsParameter(servletResponseParameter));
+
+		Object result = resolver.resolveArgument(servletResponseParameter, null, webRequest, null);
+		assertSame("Invalid result", servletResponse, result);
+	}
+
+	@Test
 	public void outputStream() throws Exception {
 		MethodParameter outputStreamParameter = new MethodParameter(method, 1);
 
