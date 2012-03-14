e25183eab1b5fbce42750e92e95248c0c24445b0
Stevo Slavic <sslavic@gmail.com>
2012-03-15 00:22:51 +0100

    Use configured encoding during JiBX unmarshalling
    Before this change JibxMarshaller did not use the configured encoding
    when unmarshalling XML. This caused issues when content being
    unmarshalled was not encoded using the default encoding.
    
    This commit fixes the issue by passing configured encoding to JiBX so
    it gets used when unmarshalling instead of the default encoding.
    
    Issue: SPR-7865

--- spring-oxm/src/main/java/org/springframework/oxm/jibx/JibxMarshaller.java
+++ spring-oxm/src/main/java/org/springframework/oxm/jibx/JibxMarshaller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2002-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -349,7 +349,7 @@ public class JibxMarshaller extends AbstractMarshaller implements InitializingBe
 	protected Object unmarshalInputStream(InputStream inputStream) throws XmlMappingException, IOException {
 		try {
 			IUnmarshallingContext unmarshallingContext = createUnmarshallingContext();
-			return unmarshallingContext.unmarshalDocument(inputStream, null);
+			return unmarshallingContext.unmarshalDocument(inputStream, encoding);
 		}
 		catch (JiBXException ex) {
 			throw convertJibxException(ex, false);
