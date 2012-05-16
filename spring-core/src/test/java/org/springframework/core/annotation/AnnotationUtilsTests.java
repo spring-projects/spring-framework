a788dee218863a6d2184ad1a0ff0f0ea425134c4
Petr Janecek <JanecekPetr@seznam.cz>
2012-05-16 22:05:59 +0200

    Fix annotation search ending too early
    At AnnotationUtils.findAnnotation(Method, Class), the search for method
    annotation fails if:
    
     - the original method does not have the annotation
    
     - an abstract superclass does not have an equivalent method declared
    
     - an interface implemented by the superclass has the method and
       the annotation - it is not found
    
    This happened because of the try-catch block being too wide. Therefore,
    cl.getDeclaredMethod() can throw NoSuchMethodException and skip
    the searchOnInterfaces() call prematurely.
    
    The try-catch block was made narrower to allow searchOnInterfaces()
    to run even if the abstract class does not have the method declared
    at all.
    
    Issue: SPR-9342

--- spring-core/src/test/java/org/springframework/core/annotation/AnnotationUtilsTests.java
+++ spring-core/src/test/java/org/springframework/core/annotation/AnnotationUtilsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -219,6 +219,14 @@ public class AnnotationUtilsTests {
 		assertNotNull(order);
 	}
 
+	@Test
+	public void testFindAnnotationFromInterfaceWhenSuperDoesNotImplementMethod() throws Exception {
+		
+		Method method = SubOfAbstractImplementsInterfaceWithAnnotatedMethod.class.getMethod("foo");
+		Order order = findAnnotation(method, Order.class);
+		assertNotNull(order);
+	}
+
 
 	@Component(value="meta1")
 	@Retention(RetentionPolicy.RUNTIME)
@@ -348,6 +356,15 @@ public class AnnotationUtilsTests {
 	}
 
 	public static class SubOfImplementsInterfaceWithAnnotatedMethod extends ImplementsInterfaceWithAnnotatedMethod {
+
+		public void foo() {
+		}
+	}
+
+	public abstract static class AbstractDoesNotImplementInterfaceWithAnnotatedMethod implements InterfaceWithAnnotatedMethod {
+	}
+
+	public static class SubOfAbstractImplementsInterfaceWithAnnotatedMethod extends AbstractDoesNotImplementInterfaceWithAnnotatedMethod {
 
 		public void foo() {
 		}
