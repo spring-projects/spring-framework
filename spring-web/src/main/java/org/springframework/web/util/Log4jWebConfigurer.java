59e3223c849fd04c1415f5ddd9bfb9acb9a17b60
Sam Brannen <sam@sambrannen.com>
2012-05-17 18:05:38 +0200

    Polish recent changes to Log4jWebConfigurer
    Reordered inline comments so that they now apply to current
    state of the code.
    
    Added logger entry in testlog4j.properties to avoid console
    warning.
    
    Issue: SPR-9417

--- spring-web/src/main/java/org/springframework/web/util/Log4jWebConfigurer.java
+++ spring-web/src/main/java/org/springframework/web/util/Log4jWebConfigurer.java
@@ -122,11 +122,14 @@ public abstract class Log4jWebConfigurer {
 		if (location != null) {
 			// Perform actual log4j initialization; else rely on log4j's default initialization.
 			try {
-				// Return a URL (e.g. "classpath:" or "file:") as-is;
-				// consider a plain file path as relative to the web application root directory.
+				// Resolve system property placeholders before potentially
+				// resolving a real path.
 				location = SystemPropertyUtils.resolvePlaceholders(location);
+
+				// Leave a URL (e.g. "classpath:" or "file:") as-is.
 				if (!ResourceUtils.isUrl(location)) {
-					// Resolve system property placeholders before resolving real path.
+					// Consider a plain file path as relative to the web
+					// application root directory.
 					location = WebUtils.getRealPath(servletContext, location);
 				}
 
