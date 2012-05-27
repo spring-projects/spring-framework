83fa8e12f0ea1aa3f5564de48a801b4d5d171898
Juergen Hoeller <jhoeller@vmware.com>
2012-05-28 00:21:33 +0200

    Add initial support for JCache-compliant providers
    Issue: SPR-8774

---
+++ spring-context/src/main/java/org/springframework/cache/jcache/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Implementation package for JSR-107 (javax.cache aka "JCache") based caches.
+ * Provides a {@link org.springframework.cache.CacheManager CacheManager}
+ * and {@link org.springframework.cache.Cache Cache} implementation for
+ * use in a Spring context, using a JSR-107 compliant cache provider.
+ */
+package org.springframework.cache.jcache;
