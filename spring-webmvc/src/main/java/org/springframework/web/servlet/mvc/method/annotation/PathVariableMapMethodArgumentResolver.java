7dff02b2c145a94ff47da3e8931d34d05f05f331
Rossen Stoyanchev <rstoyanchev@vmware.com>
2012-06-26 18:07:12 -0400

    Fix import issue introduced in prior commit
    Issue: SPR-9289

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/PathVariableMapMethodArgumentResolver.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/PathVariableMapMethodArgumentResolver.java
@@ -16,6 +16,7 @@
 
 package org.springframework.web.servlet.mvc.method.annotation;
 
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -29,8 +30,6 @@ import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.HandlerMapping;
-
-import edu.emory.mathcs.backport.java.util.Collections;
 
 /**
  * Resolves {@link Map} method arguments annotated with an @{@link PathVariable}
