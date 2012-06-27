a76c7ca29990e5242eb6ee2d22dadbdbde0759ca
Chris Beams <cbeams@vmware.com>
2012-06-27 21:35:01 +0200

    Test meta-@Async executor qualification
    Prove that Async#value is respected even when using @Async as a meta
    annotation.
    
    Issue: SPR-9443
    Backport-Issue: SPR-6847
    Backport-Commit: 37e024c6ebfdc57874e5024b3e9efa69de05fa44

--- org.springframework.context/src/test/java/org/springframework/scheduling/annotation/AnnotationAsyncExecutionInterceptorTests.java
+++ org.springframework.context/src/test/java/org/springframework/scheduling/annotation/AnnotationAsyncExecutionInterceptorTests.java
@@ -16,6 +16,9 @@
 
 package org.springframework.scheduling.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -25,7 +28,7 @@ import static org.junit.Assert.*;
  * Unit tests for {@link AnnotationAsyncExecutionInterceptor}.
  *
  * @author Chris Beams
- * @since 3.1.2
+ * @since 3.2
  */
 public class AnnotationAsyncExecutionInterceptorTests {
 
@@ -33,21 +36,29 @@ public class AnnotationAsyncExecutionInterceptorTests {
 	@SuppressWarnings("unused")
 	public void testGetExecutorQualifier() throws SecurityException, NoSuchMethodException {
 		AnnotationAsyncExecutionInterceptor i = new AnnotationAsyncExecutionInterceptor(null);
-		{
+		{ // method level
 			class C { @Async("qMethod") void m() { } }
 			assertThat(i.getExecutorQualifier(C.class.getDeclaredMethod("m")), is("qMethod"));
 		}
-		{
+		{ // class level
 			@Async("qClass") class C { void m() { } }
 			assertThat(i.getExecutorQualifier(C.class.getDeclaredMethod("m")), is("qClass"));
 		}
-		{
+		{ // method and class level -> method value overrides
 			@Async("qClass") class C { @Async("qMethod") void m() { } }
 			assertThat(i.getExecutorQualifier(C.class.getDeclaredMethod("m")), is("qMethod"));
 		}
-		{
+		{ // method and class level -> method value, even if empty, overrides
 			@Async("qClass") class C { @Async void m() { } }
 			assertThat(i.getExecutorQualifier(C.class.getDeclaredMethod("m")), is(""));
 		}
+		{ // meta annotation with qualifier
+			@MyAsync class C { void m() { } }
+			assertThat(i.getExecutorQualifier(C.class.getDeclaredMethod("m")), is("qMeta"));
+		}
 	}
+
+	@Async("qMeta")
+	@Retention(RetentionPolicy.RUNTIME)
+	@interface MyAsync { }
 }
