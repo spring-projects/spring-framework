23fe1e2934336351615556f243de694e06b5e553
Juergen Hoeller <jhoeller@vmware.com>
2012-07-05 01:20:43 +0200

    Cannot amend properties in RequestMappingHandlerMapping (e.g. useSuffixPatternMatch) using a bean post processor as ApplicationContextAwareProcessor always fires first initialising RequestMappingHandlerMapping
    Issue: SPR-9371

--- org.springframework.web.servlet/src/test/java/org/springframework/web/servlet/handler/HandlerMethodMappingTests.java
+++ org.springframework.web.servlet/src/test/java/org/springframework/web/servlet/handler/HandlerMethodMappingTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2002-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -98,12 +98,14 @@ public class HandlerMethodMappingTests {
 		
 		AbstractHandlerMethodMapping<String> mapping1 = new MyHandlerMethodMapping();
 		mapping1.setApplicationContext(new StaticApplicationContext(cxt));
+		mapping1.afterPropertiesSet();
 
 		assertEquals(0, mapping1.getHandlerMethods().size());
 
 		AbstractHandlerMethodMapping<String> mapping2 = new MyHandlerMethodMapping();
 		mapping2.setDetectHandlerMethodsInAncestorContexts(true);
 		mapping2.setApplicationContext(new StaticApplicationContext(cxt));
+		mapping2.afterPropertiesSet();
 
 		assertEquals(2, mapping2.getHandlerMethods().size());
 	}
