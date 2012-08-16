8059625670aedbdb1f5b9689ef1a9cbc41b050ec
Sam Brannen <sam@sambrannen.com>
2012-08-16 13:14:35 +0200

    Implement invalidate() properly in MockHttpSession
    The invalidate() method in MockHttpSession is currently implemented
    incorrectly. According to the Servlet specification, the method should
    throw an IllegalStateException if it is invoked on an already
    invalidated session. However, invoking invalidate() on the same
    MockHttpSession instance multiple times does not throw an exception.
    
    This commits addresses this issue by checking the invalid field and
    throwing an IllegalStateException if it has already been set to true.
    
    Issue: SPR-9686

--- spring-test/src/main/java/org/springframework/mock/web/MockHttpSession.java
+++ spring-test/src/main/java/org/springframework/mock/web/MockHttpSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2002-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,7 @@ import org.springframework.util.Assert;
  * @author Juergen Hoeller
  * @author Rod Johnson
  * @author Mark Fisher
+ * @author Sam Brannen
  * @since 1.0.2
  */
 @SuppressWarnings("deprecation")
@@ -186,7 +187,17 @@ public class MockHttpSession implements HttpSession {
 		}
 	}
 
+	/**
+	 * Invalidates this session then unbinds any objects bound to it.
+	 *
+	 * @throws IllegalStateException if this method is called on an already invalidated session
+	 */
 	public void invalidate() {
+		if (this.invalid) {
+			throw new IllegalStateException("The session has already been invalidated");
+		}
+
+		// else
 		this.invalid = true;
 		clearAttributes();
 	}
