408110d724b89c52da00d142a87df9486163f8d1
Rob Winch <rwinch@vmware.com>
2012-08-31 12:59:26 -0500

    Add MockFilterChain support Filter's and Servlet
    Currently the MockFilterChain
     - Performs basic validation on the request/response
     - Makes the request/response available as members
     - Ensures doFilter is only invoked once
    
    This commit adds the ability to allow the MockFilterChain to invoke
    a List of Filter's and/or a Servlet.
    
    Issue: SPR-9745

--- spring-test/src/main/java/org/springframework/mock/web/MockFilterChain.java
+++ spring-test/src/main/java/org/springframework/mock/web/MockFilterChain.java
@@ -16,7 +16,16 @@
 
 package org.springframework.mock.web;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.RandomAccess;
+
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
@@ -29,6 +38,7 @@ import org.springframework.util.Assert;
  * custom {@link javax.servlet.Filter} implementations.
  *
  * @author Juergen Hoeller
+ * @author Rob Winch
  * @since 2.0.3
  * @see MockFilterConfig
  * @see PassThroughFilterChain
@@ -39,32 +49,153 @@ public class MockFilterChain implements FilterChain {
 
 	private ServletResponse response;
 
+	private final List<Filter> filters;
+
+	private int index = 0;
 
 	/**
-	 * Records the request and response.
+	 * Registers a single do-nothing {@link Filter} implementation. This means multiple invocations of
+	 * {@link #doFilter(ServletRequest, ServletResponse)} will result in an {@link IllegalStateException}.
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response) {
+	public MockFilterChain() {
+		this(new FilterAdapter());
+	}
+
+	/**
+	 * Create a {@link FilterChain} with one or more {@link Filter}s.
+	 *
+	 * @param filters the {@link Filter}'s to use in this  {@link FilterChain}
+	 *
+	 * @since 3.2
+	 */
+	public MockFilterChain(Filter... filters) {
+		Assert.notEmpty(filters, "filters cannot be null or empty. Got "+filters);
+		this.filters = createFilters(filters);
+	}
+
+	/**
+	 * Creates a FilterChain with {@link Servlet}
+	 *
+	 * @param servlet the {@link Servlet} to use in this {@link FilterChain}
+	 *
+	 * @since 3.2
+	 */
+	public MockFilterChain(Servlet servlet) {
+		this(new ServletFilterProxy(servlet));
+	}
+
+	/**
+	 * Creates a FilterChain with one or more {@link Filter}s and a {@link Servlet}. The {@link Filter}s will be invoked
+	 * first and then the {@link Servlet}.
+	 *
+	 * @param servlet the {@link Servlet} to use in this {@link FilterChain}
+	 * @param filters the {@link Filter}'s to use in this {@link FilterChain}
+	 *
+	 * @since 3.2
+	 */
+	public MockFilterChain(Servlet servlet, Filter... filters) {
+		this.filters = createFilters(filters);
+		this.filters.add(new ServletFilterProxy(servlet));
+	}
+
+	/**
+	 * Invokes the next registered {@link Filter} or the {@link Servlet}. Records the request and response.
+	 *
+	 * @throws ServletException
+	 * @throws IOException
+	 * @throws IllegalStateException if all of the registered {@link Filter} and {@link Servlet} implementations have
+	 * been invoked.
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response) throws IOException, ServletException {
 		Assert.notNull(request, "Request must not be null");
 		Assert.notNull(response, "Response must not be null");
-		if (this.request != null) {
-			throw new IllegalStateException("This FilterChain has already been called!");
+		if (index == filters.size()) {
+			throw new IllegalStateException("Cannot invoke doFilter with index "
+					+ index + " due to too many invocations of doFilter. Original chain is " + filters);
 		}
+		else {
+			Filter nextFilter = filters.get(index);
+			index++;
+
+			nextFilter.doFilter(request, response, this);
+		}
+
 		this.request = request;
 		this.response = response;
 	}
 
 	/**
-	 * Return the request that {@link #doFilter} has been called with.
+	 * Return the most recent request that {@link #doFilter} has been called with.
 	 */
 	public ServletRequest getRequest() {
 		return this.request;
 	}
 
 	/**
-	 * Return the response that {@link #doFilter} has been called with.
+	 * Return the most recent response that {@link #doFilter} has been called with.
 	 */
 	public ServletResponse getResponse() {
 		return this.response;
 	}
 
+	/**
+	 * Validates the filters and creates a List<Filter> from them that implements {@link RandomAccess} and can be
+	 * modified.
+	 * @param filters
+	 * @return
+	 */
+	private static List<Filter> createFilters(Filter...filters) {
+		Assert.notNull(filters, "filters cannot be null");
+		Assert.noNullElements(filters, "fiters cannot contain null values");
+		List<Filter> result = new ArrayList<Filter>(filters.length + 1);
+		for(Filter filter : filters) {
+			result.add(filter);
+		}
+		return result;
+	}
+
+	/**
+	 * A do nothing implementation of {@link Filter}. Subclasses can selectively implement only methods of interest.
+	 *
+	 * @author Rob Winch
+	 *
+	 */
+	private static class FilterAdapter implements Filter {
+		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+				ServletException {
+		}
+
+		public void init(FilterConfig filterConfig) throws ServletException {
+		}
+
+		public void destroy() {
+		}
+	}
+
+	/**
+	 * Adapts a {@link Servlet#service(ServletRequest, ServletResponse)} to be invoked by
+	 * {@link Filter#doFilter(ServletRequest, ServletResponse, FilterChain)}. This simplifies the implementation of
+	 * {@link #doFilter(ServletRequest, ServletResponse, FilterChain)}.
+	 *
+	 * @author Rob Winch
+	 *
+	 */
+	private static class ServletFilterProxy extends FilterAdapter {
+		private final Servlet delegateServlet;
+
+		private ServletFilterProxy(Servlet servlet) {
+			Assert.notNull(servlet, "servlet cannot be null");
+			this.delegateServlet = servlet;
+		}
+
+		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+				ServletException {
+			delegateServlet.service(request, response);
+		}
+
+		@Override
+		public String toString() {
+			return delegateServlet.toString();
+		}
+	}
 }
