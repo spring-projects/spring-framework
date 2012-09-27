5c13739afafcd45471e1954c5e05c3cfa2e723da
Rossen Stoyanchev <rstoyanchev@vmware.com>
2012-09-27 17:07:14 -0400

    Add mock implementations of Http[Input|Output]Message

---
+++ spring-test/src/main/java/org/springframework/mock/http/client/MockClientHttpRequest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.mock.http.client;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.mock.http.MockHttpOutputMessage;
+
+/**
+ * Mock implementation of {@link ClientHttpRequest}.
+ *
+ * @author Rossen Stoyanchev
+ * @since 3.2
+ */
+public class MockClientHttpRequest extends MockHttpOutputMessage implements ClientHttpRequest {
+
+	private URI uri;
+
+	private HttpMethod httpMethod;
+
+	private boolean executed = false;
+
+	private ClientHttpResponse clientHttpResponse;
+
+
+	/**
+	 * Default constructor.
+	 */
+	public MockClientHttpRequest() {
+	}
+
+	/**
+	 * Create an instance with the given HttpMethod and URI.
+	 */
+	public MockClientHttpRequest(HttpMethod httpMethod, URI uri) {
+		this.httpMethod = httpMethod;
+		this.uri = uri;
+	}
+
+	public URI getURI() {
+		return this.uri;
+	}
+
+	public void setURI(URI uri) {
+		this.uri = uri;
+	}
+
+	public HttpMethod getMethod() {
+		return this.httpMethod;
+	}
+
+	public void setMethod(HttpMethod httpMethod) {
+		this.httpMethod = httpMethod;
+	}
+
+	public void setResponse(ClientHttpResponse clientHttpResponse) {
+		this.clientHttpResponse = clientHttpResponse;
+	}
+
+	/**
+	 * Whether the execute method was invoked.
+	 */
+	public boolean isExecuted() {
+		return this.executed;
+	}
+
+	/**
+	 * Sets the {@link #isExecuted() executed} flag to true and returns the
+	 * configured {@link #setResponse(ClientHttpResponse) response}.
+	 */
+	public ClientHttpResponse execute() throws IOException {
+		this.executed = true;
+		return this.clientHttpResponse;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		if (this.httpMethod != null) {
+			sb.append(this.httpMethod);
+		}
+		if (this.uri != null) {
+			sb.append(" ").append(this.uri);
+		}
+		if (!getHeaders().isEmpty()) {
+			sb.append(", headers : ").append(getHeaders());
+		}
+		if (sb.length() == 0) {
+			sb.append("Not yet initialized");
+		}
+		return sb.toString();
+	}
+
+}
