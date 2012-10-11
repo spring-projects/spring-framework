138fa8af0ef3df3df5dbb2a6db4643cff36043d1
Juergen Hoeller <jhoeller@vmware.com>
2012-10-11 21:39:39 +0200

    OpDivide does not return a TypedValue for its operate result (consistent with OpMultiply)
    Issue: SPR-9869

--- spring-expression/src/main/java/org/springframework/expression/spel/ast/OpMultiply.java
+++ spring-expression/src/main/java/org/springframework/expression/spel/ast/OpMultiply.java
@@ -48,13 +48,12 @@ public class OpMultiply extends Operator {
 	 * Implements the {@code multiply} operator directly here for certain types
 	 * of supported operands and otherwise delegates to any registered overloader
 	 * for types not supported here.
-	 * 
 	 * <p>Supported operand types:
 	 * <ul>
 	 * <li>doubles
 	 * <li>longs
 	 * <li>integers
-	 * <li>string and int ('abc' * 2 == 'abcabc')
+	 * <li>String and int ('abc' * 2 == 'abcabc')
 	 * </ul>
 	 */
 	@Override
