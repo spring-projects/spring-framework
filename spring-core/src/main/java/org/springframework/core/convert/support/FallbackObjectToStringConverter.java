a27d1a28ffb69aed618869d3815a3af7120effa2
Phillip Webb <pwebb@vmware.com>
2012-10-26 17:43:41 -0700

    Bypass conversion when possible
    Prior to this commit conversion between like types would often result in
    a copy of the object. This can be problematic in the case of large byte
    arrays and objects that do not have a default constructor.
    
    The ConversionService SPI now includes canBypassConvert methods that can
    be used to deduce when conversion is not needed. Several existing
    converters have been updated to ensure they only apply when source and
    target types differ.
    
    This change introduces new methods to the ConversionService that will
    break existing implementations. However, it anticipated that most users
    are consuming the ConversionService interface rather then extending it.
    
    Issue: SPR-9566

--- spring-core/src/main/java/org/springframework/core/convert/support/FallbackObjectToStringConverter.java
+++ spring-core/src/main/java/org/springframework/core/convert/support/FallbackObjectToStringConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,6 +41,9 @@ final class FallbackObjectToStringConverter implements ConditionalGenericConvert
 
 	public boolean matches(TypeDescriptor sourceType, TypeDescriptor targetType) {
 		Class<?> sourceClass = sourceType.getObjectType();
+		if (String.class.equals(sourceClass)) {
+			return false;
+		}
 		return CharSequence.class.isAssignableFrom(sourceClass) || StringWriter.class.isAssignableFrom(sourceClass) ||
 			ObjectToObjectConverter.hasValueOfMethodOrConstructor(sourceClass, String.class);
 	}
