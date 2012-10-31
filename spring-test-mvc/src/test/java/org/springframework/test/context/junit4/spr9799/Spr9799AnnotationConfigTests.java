0b45daf5e9b4587ce9817e7872ab274572dd9ce5
Sam Brannen <sam@sambrannen.com>
2012-10-31 17:41:25 +0100

    Assess claims made in SPR-9799
    This commit validates the claims made in SPR-9799.
    
     - Spr9799XmlConfigTests demonstrates that a WAC is not always necessary
       when integration testing with XML configuration that uses
       <mvc:annotation-driven />.
    
     - Spr9799AnnotationConfigTests demonstrates that a WAC is in fact
       necessary when integration testing with a configuration class that
       uses @EnableWebMvc.
    
    Issue: SPR-9799

---
+++ spring-test-mvc/src/test/java/org/springframework/test/context/junit4/spr9799/Spr9799AnnotationConfigTests.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.test.context.junit4.spr9799;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Integration tests used to assess claims raised in
+ * <a href="https://jira.springsource.org/browse/SPR-9799" target="_blank">SPR-9799</a>.
+ * 
+ * <p>Note: this test class would normally reside in the {@code spring-test} source
+ * tree; however, due to classpath issues with slf4j, this class must reside in
+ * the {@code spring-test-mvc} source tree.
+ * 
+ * @author Sam Brannen
+ * @since 3.2
+ * @see Spr9799XmlConfigTests
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+// NOTE: if we omit the @WebAppConfiguration declaration, the ApplicationContext will fail
+// to load since @EnableWebMvc requires that the context be a WebApplicationContext.
+@WebAppConfiguration
+public class Spr9799AnnotationConfigTests {
+
+	@Configuration
+	@EnableWebMvc
+	static class Config {
+		/* intentionally no beans defined */
+	}
+
+
+	@Test
+	public void applicationContextLoads() {
+		// no-op
+	}
+
+}
