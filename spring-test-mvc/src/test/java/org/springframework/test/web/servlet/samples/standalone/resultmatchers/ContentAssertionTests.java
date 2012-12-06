31cb14e03a2802d1425504b00f97d83471bde7c7
Rossen Stoyanchev <rstoyanchev@vmware.com>
2012-12-05 19:10:13 -0500

    Remove Spring Security sample tests in spring-test-mvc
    Those tests as well as Spring Hateoas tests will be added to respective
    projects instead.

--- spring-test-mvc/src/test/java/org/springframework/test/web/servlet/samples/standalone/resultmatchers/ContentAssertionTests.java
+++ spring-test-mvc/src/test/java/org/springframework/test/web/servlet/samples/standalone/resultmatchers/ContentAssertionTests.java
@@ -19,23 +19,14 @@ package org.springframework.test.web.servlet.samples.standalone.resultmatchers;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import java.nio.charset.Charset;
-import java.util.Collections;
-import java.util.Map;
-
-import javax.xml.bind.annotation.XmlRootElement;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.ResourceSupport;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.test.web.servlet.MockMvc;
@@ -116,20 +107,6 @@ public class ContentAssertionTests {
 			.andExpect(content().bytes("\u3053\u3093\u306b\u3061\u306f\u4e16\u754c\uff01".getBytes("UTF-8")));
 	}
 
-	@Test
-	public void testSpringHateoasJsonLink() throws Exception {
-		this.mockMvc.perform(get("/handle").accept(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$.links[?(@.rel == 'self')].href").value("http://myhost/people"));
-	}
-
-	@Test
-	public void testSpringHateoasXmlLink() throws Exception {
-		Map<String, String> ns = Collections.singletonMap("ns", "http://www.w3.org/2005/Atom");
-		this.mockMvc.perform(get("/handle").accept(MediaType.APPLICATION_XML))
-			.andDo(print())
-			.andExpect(xpath("/person/ns:link[@rel='self']/@href", ns).string("http://myhost/people"));
-	}
-
 
 	@Controller
 	private static class SimpleController {
@@ -145,20 +122,6 @@ public class ContentAssertionTests {
 		public String handleWithCharset() {
 			return "\u3053\u3093\u306b\u3061\u306f\u4e16\u754c\uff01";	// "Hello world! (Japanese)
 		}
-
-		@RequestMapping(value="/handle", produces={"application/json", "application/xml"})
-		@ResponseBody
-		public PersonResource handleJsonOrXml() {
-			PersonResource resource = new PersonResource();
-			resource.name = "Joe";
-			resource.add(new Link("http://myhost/people"));
-			return resource;
-		}
-	}
-
-	@XmlRootElement(name="person")
-	static class PersonResource extends ResourceSupport {
-		String name;
 	}
 
 }
