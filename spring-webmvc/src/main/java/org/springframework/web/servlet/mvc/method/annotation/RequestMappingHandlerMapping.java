2233949a0f93d4484b7d3388c88dcd4ee4f8660a
Sam Brannen <sam@sambrannen.com>
2012-12-07 12:06:21 +0100

    Fix typo

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/RequestMappingHandlerMapping.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/RequestMappingHandlerMapping.java
@@ -58,12 +58,13 @@ public class RequestMappingHandlerMapping extends RequestMappingInfoHandlerMappi
 
 	private final List<String> fileExtensions = new ArrayList<String>();
 
+	
 	/**
 	 * Whether to use suffix pattern match (".*") when matching patterns to
 	 * requests. If enabled a method mapped to "/users" also matches to "/users.*".
 	 * <p>The default value is {@code true}.
 	 * <p>Also see {@link #setUseRegisteredSuffixPatternMatch(boolean)} for
-	 * more fine-grained control over specific suffices to allow.
+	 * more fine-grained control over specific suffixes to allow.
 	 */
 	public void setUseSuffixPatternMatch(boolean useSuffixPatternMatch) {
 		this.useSuffixPatternMatch = useSuffixPatternMatch;
