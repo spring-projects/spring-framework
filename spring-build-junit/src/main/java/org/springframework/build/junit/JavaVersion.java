5affb9e09ae6ce3fe25664d28121a842ec7a943b
Phillip Webb <pwebb@vmware.com>
2012-12-11 17:35:09 -0800

    Add new shared JUnit build time project
    Introduce new 'spring-build-junit' project containing shared
    JUnit utilities and classes to be used by other test-cases. This
    project is internal and will not be published to Maven.
    
    The initial code includes support for JUnit Assumptions that can
    be used to determine when a test should run. Tests can be skipped
    based on the running JDK version, logging level or based on specific
    'groups' that have activated via a Gradle property.
    
    It is intended that the 'spring-build-junit' project will eventually
    contain the sample bean classes that are currently duplicated
    throughout the existing tests.

---
+++ spring-build-junit/src/main/java/org/springframework/build/junit/JavaVersion.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.build.junit;
+
+/**
+ * Enumeration of known JDK versions.
+ *
+ * @author Phillip Webb
+ * @see #runningVersion()
+ */
+public enum JavaVersion {
+
+
+	/**
+	 * Java 1.5
+	 */
+	JAVA_15("1.5", 15),
+
+	/**
+	 * Java 1.6
+	 */
+	JAVA_16("1.6", 16),
+
+	/**
+	 * Java 1.7
+	 */
+	JAVA_17("1.7", 17),
+
+	/**
+	 * Java 1.8
+	 */
+	JAVA_18("1.8", 18);
+
+
+	private static final JavaVersion runningVersion = findRunningVersion();
+
+	private static JavaVersion findRunningVersion() {
+		String version = System.getProperty("java.version");
+		for (JavaVersion candidate : values()) {
+			if (version.startsWith(candidate.version)) {
+				return candidate;
+			}
+		}
+		return JavaVersion.JAVA_15;
+	}
+
+
+	private String version;
+
+	private int value;
+
+
+	private JavaVersion(String version, int value) {
+		this.version = version;
+		this.value = value;
+	}
+
+
+	@Override
+	public String toString() {
+		return version;
+	}
+
+	/**
+	 * Determines if the specified version is the same as or greater than this version.
+	 * @param version the version to check
+	 * @return {@code true} if the specified version is at least this version
+	 */
+	public boolean isAtLeast(JavaVersion version) {
+		return this.value >= version.value;
+	}
+
+
+	/**
+	 * Returns the current running JDK version. If the current version cannot be
+	 * determined {@link #JAVA_15} will be returned.
+	 * @return the JDK version
+	 */
+	public static JavaVersion runningVersion() {
+		return runningVersion;
+	}
+}
---
+++ spring-build-junit/src/main/java/org/springframework/build/junit/TestGroup.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.build.junit;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A test group used to limit when certain tests are run.
+ *
+ * @see Assume#running(TestGroup)
+ * @author Phillip Webb
+ */
+public enum TestGroup {
+
+
+	/**
+	 * Performance related tests that may take a considerable time to run.
+	 */
+	PERFORMANCE;
+
+
+	/**
+	 * Parse the specified comma separates string of groups.
+	 * @param value the comma separated string of groups
+	 * @return a set of groups
+	 */
+	public static Set<TestGroup> parse(String value) {
+		if (value == null || "".equals(value)) {
+			return Collections.emptySet();
+		}
+		if("ALL".equalsIgnoreCase(value)) {
+			return EnumSet.allOf(TestGroup.class);
+		}
+		Set<TestGroup> groups = new HashSet<TestGroup>();
+		for (String group : value.split(",")) {
+			try {
+				groups.add(valueOf(group.trim().toUpperCase()));
+			} catch (IllegalArgumentException e) {
+				throw new IllegalArgumentException("Unable to find test group '" + group.trim()
+						+ "' when parsing '" + value + "'");
+			}
+		}
+		return groups;
+	}
+}
