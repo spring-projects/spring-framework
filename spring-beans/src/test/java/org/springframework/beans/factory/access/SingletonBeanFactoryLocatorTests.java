44a474a014788e7850cee3a5318ac3916e55e756
Chris Beams <cbeams@vmware.com>
2012-12-17 10:16:03 +0100

    Various updates to support IDEA
    Remove the 'final' modifier from SingletonBeanFactoryLocatorTests
    to work around the "cannot extend final class" error issued when
    running all tests. The error was due to confusion with IDEA between
    the two variants of SingletonBeanFactoryLocatorTests across
    spring-context and spring-beans.
    
    Rename one of the GroovyMessenger classes to GroovyMessenger2.
    Previously there were multiple Groovy classes named
    'GroovyMessenger', causing a compilation error in certain IDE
    arrangements.
    
    Update import-into-idea.md documentation
    
    Add various IDEA artifacts to .gitignore
     - ignore derby.log wherever it is written
     - ignore IDEA's test-output directory
     - ignore IDEA's Atlassian connector config file

--- spring-beans/src/test/java/org/springframework/beans/factory/access/SingletonBeanFactoryLocatorTests.java
+++ spring-beans/src/test/java/org/springframework/beans/factory/access/SingletonBeanFactoryLocatorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ import org.springframework.util.ClassUtils;
  * @author Colin Sampaleanu
  * @author Chris Beams
  */
-public final class SingletonBeanFactoryLocatorTests {
+public class SingletonBeanFactoryLocatorTests {
 	private static final Class<?> CLASS = SingletonBeanFactoryLocatorTests.class;
 	private static final String REF1_XML = CLASS.getSimpleName() + "-ref1.xml";
 
