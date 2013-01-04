51b307681a8ee7d89180f58f967856b9c4bf9232
Chris Beams <cbeams@vmware.com>
2013-01-04 11:04:54 +0100

    Fix warnings due to unused import statements
    Issue: SPR-9431

--- spring-beans/src/test/java/org/springframework/tests/beans/CollectingReaderEventListener.java
+++ spring-beans/src/test/java/org/springframework/tests/beans/CollectingReaderEventListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,6 @@ import org.springframework.beans.factory.parsing.ComponentDefinition;
 import org.springframework.beans.factory.parsing.DefaultsDefinition;
 import org.springframework.beans.factory.parsing.ImportDefinition;
 import org.springframework.beans.factory.parsing.ReaderEventListener;
-import org.springframework.core.CollectionFactory;
 
 /**
  * @author Rob Harrop
