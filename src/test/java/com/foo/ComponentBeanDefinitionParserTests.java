676231644ddd38d14826dd961b20672f6ea1bda8
Chris Beams <cbeams@vmware.com>
2013-01-04 11:57:22 +0100

    Update Apache license headers for moved files
    Issue: SPR-9431

--- src/test/java/com/foo/ComponentBeanDefinitionParserTests.java
+++ src/test/java/com/foo/ComponentBeanDefinitionParserTests.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.foo;
 
 import static org.hamcrest.CoreMatchers.equalTo;
