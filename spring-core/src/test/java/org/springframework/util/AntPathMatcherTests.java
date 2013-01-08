f185c3a3c18f11770494060182780af61a8c9e55
Rossen Stoyanchev <rstoyanchev@vmware.com>
2013-01-08 12:03:51 -0500

    Add option to not trim path segments in AntPathMatch
    Issue: SPR-8687

--- spring-core/src/test/java/org/springframework/util/AntPathMatcherTests.java
+++ spring-core/src/test/java/org/springframework/util/AntPathMatcherTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -542,6 +542,14 @@ public class AntPathMatcherTests {
 		paths.clear();
 	}
 
+	// SPR-8687
 
+	@Test
+	public void trimTokensOff() {
+		pathMatcher.setTrimTokens(false);
+
+		assertTrue(pathMatcher.match("/group/{groupName}/members", "/group/sales/members"));
+		assertTrue(pathMatcher.match("/group/{groupName}/members", "/group/  sales/members"));
+	}
 
 }
