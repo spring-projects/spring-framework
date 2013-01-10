2427391286f489e11981e1072aaebbf78dd5d3f7
Sam Brannen <sam@sambrannen.com>
2013-01-10 17:33:59 +0100

    Polish Javadoc in CookieValue and RequestHeader

--- spring-web/src/main/java/org/springframework/web/bind/annotation/RequestHeader.java
+++ spring-web/src/main/java/org/springframework/web/bind/annotation/RequestHeader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,14 +49,14 @@ public @interface RequestHeader {
 	 * <p>Default is {@code true}, leading to an exception thrown in case
 	 * of the header missing in the request. Switch this to {@code false}
 	 * if you prefer a {@code null} in case of the header missing.
-	 * <p>Alternatively, provide a {@link #defaultValue() defaultValue},
-	 * which implicitely sets this flag to {@code false}.
+	 * <p>Alternatively, provide a {@link #defaultValue}, which implicitly sets
+	 * this flag to {@code false}.
 	 */
 	boolean required() default true;
 
 	/**
-	 * The default value to use as a fallback. Supplying a default value implicitely
-	 * sets {@link #required()} to false.
+	 * The default value to use as a fallback. Supplying a default value implicitly
+	 * sets {@link #required} to {@code false}.
 	 */
 	String defaultValue() default ValueConstants.DEFAULT_NONE;
 
