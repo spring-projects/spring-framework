20c4ba40d8735c0b86bc3e5dc20af4b7664b94ae
Juergen Hoeller <jhoeller@vmware.com>
2013-01-18 14:02:13 +0100

    AbstractDriverBasedDataSource does not rely on Properties chaining anymore
     Issue: SPR-9461

--- spring-jdbc/src/main/java/org/springframework/jdbc/datasource/AbstractDriverBasedDataSource.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/datasource/AbstractDriverBasedDataSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -139,7 +139,8 @@ public abstract class AbstractDriverBasedDataSource extends AbstractDataSource {
 	 * @see java.sql.Driver#connect(String, java.util.Properties)
 	 */
 	protected Connection getConnectionFromDriver(String username, String password) throws SQLException {
-		Properties props = new Properties(getConnectionProperties());
+		Properties props = new Properties();
+		props.putAll(getConnectionProperties());
 		if (username != null) {
 			props.setProperty("user", username);
 		}
