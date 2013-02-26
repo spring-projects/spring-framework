3100d89d0710c8306cf8fb363eef5bc34d0a5a38
Chris Beams <cbeams@vmware.com>
2013-02-26 12:33:05 +0100

    Clean up console output for spring-beans tests

--- spring-beans/src/test/java/org/springframework/beans/factory/parsing/CustomProblemReporterTests.java
+++ spring-beans/src/test/java/org/springframework/beans/factory/parsing/CustomProblemReporterTests.java
@@ -29,7 +29,6 @@ import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.Resource;
 import org.springframework.tests.sample.beans.TestBean;
 
-
 /**
  * @author Rob Harrop
  * @author Chris Beams
@@ -78,7 +77,6 @@ public final class CustomProblemReporterTests {
 
 		@Override
 		public void error(Problem problem) {
-			System.out.println(problem);
 			this.errors.add(problem);
 		}
 
@@ -88,7 +86,6 @@ public final class CustomProblemReporterTests {
 
 		@Override
 		public void warning(Problem problem) {
-			System.out.println(problem);
 			this.warnings.add(problem);
 		}
 	}
