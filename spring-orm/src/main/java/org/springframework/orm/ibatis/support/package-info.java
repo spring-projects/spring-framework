30a9dad5fe88f8458f18427f3907a07e1248764e
Juergen Hoeller <jhoeller@vmware.com>
2013-03-19 15:03:38 +0100

    Removed iBATIS SQL Maps support

--- spring-orm/src/main/java/org/springframework/orm/ibatis/support/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- *
- * Classes supporting the {@code org.springframework.orm.ibatis} package.
- * Contains a DAO base class for SqlMapClientTemplate usage.
- *
- */
-package org.springframework.orm.ibatis.support;
-
