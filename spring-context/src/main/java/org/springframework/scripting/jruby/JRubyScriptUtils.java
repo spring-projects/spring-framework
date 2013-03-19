90f79f3f4f0e65e848a1d2611be83a8a4c670c5e
Juergen Hoeller <jhoeller@vmware.com>
2013-03-19 16:46:14 +0100

    Require JRuby 1.6+ (avoiding the use of deprecated API)

--- spring-context/src/main/java/org/springframework/scripting/jruby/JRubyScriptUtils.java
+++ spring-context/src/main/java/org/springframework/scripting/jruby/JRubyScriptUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,8 +80,7 @@ public abstract class JRubyScriptUtils {
 		Ruby ruby = initializeRuntime();
 
 		Node scriptRootNode = ruby.parseEval(scriptSource, "", null, 0);
-		// keep using the deprecated runNormally variant for JRuby 1.1/1.2 compatibility...
-        IRubyObject rubyObject = ruby.runNormally(scriptRootNode, false);
+        IRubyObject rubyObject = ruby.runNormally(scriptRootNode);
 
 		if (rubyObject instanceof RubyNil) {
 			String className = findClassName(scriptRootNode);
