d7ec20a286608fcfa466c96538f7e0bf961aeac0
Phillip Webb <pwebb@vmware.com>
2013-06-03 09:37:43 -0700

    Test for property placeholder with defaults
    Test property placeholder with multiple locations and default values.
    
    Issue: SPR-10619

--- spring-context/src/test/java/org/springframework/context/support/PropertySourcesPlaceholderConfigurerTests.java
+++ spring-context/src/test/java/org/springframework/context/support/PropertySourcesPlaceholderConfigurerTests.java
@@ -265,4 +265,23 @@ public class PropertySourcesPlaceholderConfigurerTests {
 		thrown.expect(IllegalStateException.class);
 		ppc.getAppliedPropertySources();
 	}
+
+	@Test
+	public void multipleLocationsWithDefaultResolvedValue() throws Exception {
+		// SPR-10619
+		PropertySourcesPlaceholderConfigurer ppc = new PropertySourcesPlaceholderConfigurer();
+		ClassPathResource doesNotHave = new ClassPathResource("test.properties", getClass());
+		ClassPathResource setToTrue = new ClassPathResource("placeholder.properties", getClass());
+		ppc.setLocations(new Resource[] { doesNotHave, setToTrue });
+		ppc.setIgnoreResourceNotFound(true);
+		ppc.setIgnoreUnresolvablePlaceholders(true);
+		DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
+		bf.registerBeanDefinition("testBean",
+				genericBeanDefinition(TestBean.class)
+					.addPropertyValue("jedi", "${jedi:false}")
+					.getBeanDefinition());
+		ppc.postProcessBeanFactory(bf);
+		assertThat(bf.getBean(TestBean.class).isJedi(), equalTo(true));
+	}
+
 }
