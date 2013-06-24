f7f66f2e5c18a614a7d741b5ca3d6a07cb49fd8e
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2013-06-24 09:59:27 -0400

    Fix minor issue in ReactorMessageChannel

--- spring-websocket/src/main/java/org/springframework/web/messaging/support/ReactorMessageChannel.java
+++ spring-websocket/src/main/java/org/springframework/web/messaging/support/ReactorMessageChannel.java
@@ -104,7 +104,7 @@ public class ReactorMessageChannel implements SubscribableChannel<Message<?>, Me
 			logger.trace("Channel " + getName() + ", removing subscription for handler " + handler);
 		}
 
-		Registration<?> registration = this.registrations.get(handler);
+		Registration<?> registration = this.registrations.remove(handler);
 		if (registration == null) {
 			if (logger.isTraceEnabled()) {
 				logger.trace("Channel " + getName() + ", no subscription for handler " + handler);
