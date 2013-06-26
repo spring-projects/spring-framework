55f02db18e659e8523156465f70ac01b30ecab86
Phillip Webb <pwebb@vmware.com>
2013-06-26 10:35:13 -0700

    'Rename' HttpHeaders.getIfNotModifiedSince()
    Effectively rename HttpHeaders.getIfNotModifiedSince() to
    getIfModifiedSince() by adding a new method and deprecating the
    old one.
    
    Issue: SPR-10600

--- spring-web/src/main/java/org/springframework/http/HttpHeaders.java
+++ spring-web/src/main/java/org/springframework/http/HttpHeaders.java
@@ -1,5 +1,5 @@
 /*
-	 * Copyright 2002-2012 the original author or authors.
+	 * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -375,8 +375,19 @@ public class HttpHeaders implements MultiValueMap<String, String>, Serializable 
 	 * Returns the value of the {@code IfModifiedSince} header.
 	 * <p>The date is returned as the number of milliseconds since January 1, 1970 GMT. Returns -1 when the date is unknown.
 	 * @return the header value
+	 * @deprecated use {@link #getIfModifiedSince()}
 	 */
+	@Deprecated
 	public long getIfNotModifiedSince() {
+		return getIfModifiedSince();
+	}
+
+	/**
+	 * Returns the value of the {@code If-Modified-Since} header.
+	 * <p>The date is returned as the number of milliseconds since January 1, 1970 GMT. Returns -1 when the date is unknown.
+	 * @return the header value
+	 */
+	public long getIfModifiedSince() {
 		return getFirstDate(IF_MODIFIED_SINCE);
 	}
 
