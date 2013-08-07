f9325a83765ba959df72555e2a7418b1b2b0d89a
Juergen Hoeller <jhoeller@vmware.com>
2013-08-07 16:40:47 +0200

    Added Java 8 compliant @Schedules container annotation for @Scheduled
    Issue: SPR-10532

--- spring-context/src/main/java/org/springframework/scheduling/annotation/Scheduled.java
+++ spring-context/src/main/java/org/springframework/scheduling/annotation/Scheduled.java
@@ -18,6 +18,7 @@ package org.springframework.scheduling.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -45,6 +46,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Repeatable(Schedules.class)
 public @interface Scheduled {
 
 	/**
--- spring-context/src/test/java/org/springframework/scheduling/annotation/ScheduledAnnotationBeanPostProcessorTests.java
+++ spring-context/src/test/java/org/springframework/scheduling/annotation/ScheduledAnnotationBeanPostProcessorTests.java
@@ -129,6 +129,40 @@ public class ScheduledAnnotationBeanPostProcessorTests {
 	}
 
 	@Test
+	public void severalFixedRates() {
+		StaticApplicationContext context = new StaticApplicationContext();
+		BeanDefinition processorDefinition = new RootBeanDefinition(ScheduledAnnotationBeanPostProcessor.class);
+		BeanDefinition targetDefinition = new RootBeanDefinition(SeveralFixedRatesTestBean.class);
+		context.registerBeanDefinition("postProcessor", processorDefinition);
+		context.registerBeanDefinition("target", targetDefinition);
+		context.refresh();
+		Object postProcessor = context.getBean("postProcessor");
+		Object target = context.getBean("target");
+		ScheduledTaskRegistrar registrar = (ScheduledTaskRegistrar)
+				new DirectFieldAccessor(postProcessor).getPropertyValue("registrar");
+		@SuppressWarnings("unchecked")
+		List<IntervalTask> fixedRateTasks = (List<IntervalTask>)
+				new DirectFieldAccessor(registrar).getPropertyValue("fixedRateTasks");
+		assertEquals(2, fixedRateTasks.size());
+		IntervalTask task1 = fixedRateTasks.get(0);
+		ScheduledMethodRunnable runnable1 = (ScheduledMethodRunnable) task1.getRunnable();
+		Object targetObject = runnable1.getTarget();
+		Method targetMethod = runnable1.getMethod();
+		assertEquals(target, targetObject);
+		assertEquals("fixedRate", targetMethod.getName());
+		assertEquals(0, task1.getInitialDelay());
+		assertEquals(4000L, task1.getInterval());
+		IntervalTask task2 = fixedRateTasks.get(1);
+		ScheduledMethodRunnable runnable2 = (ScheduledMethodRunnable) task2.getRunnable();
+		targetObject = runnable2.getTarget();
+		targetMethod = runnable2.getMethod();
+		assertEquals(target, targetObject);
+		assertEquals("fixedRate", targetMethod.getName());
+		assertEquals(2000L, task2.getInitialDelay());
+		assertEquals(4000L, task2.getInterval());
+	}
+
+	@Test
 	public void cronTask() throws InterruptedException {
 		Assume.group(TestGroup.LONG_RUNNING);
 
@@ -399,6 +433,15 @@ public class ScheduledAnnotationBeanPostProcessorTests {
 	static class FixedRateWithInitialDelayTestBean {
 
 		@Scheduled(fixedRate=3000, initialDelay=1000)
+		public void fixedRate() {
+		}
+	}
+
+
+	static class SeveralFixedRatesTestBean {
+
+		@Scheduled(fixedRate=4000)
+		@Scheduled(fixedRate=4000, initialDelay=2000)
 		public void fixedRate() {
 		}
 	}
