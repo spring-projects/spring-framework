d9bfac393bc8f2df93a29cf685e7d81c222a59e7
Arjen Poutsma <apoutsma@gopivotal.com>
2013-08-12 12:26:34 +0200

    Added reference to CatchAllConverter
    Added reference to CatchAllConverter in both javadoc and reference docs.
    
    Issue: SPR-10821

--- spring-oxm/src/main/java/org/springframework/oxm/xstream/XStreamMarshaller.java
+++ spring-oxm/src/main/java/org/springframework/oxm/xstream/XStreamMarshaller.java
@@ -90,8 +90,9 @@ import org.springframework.util.xml.StaxUtils;
  * {@code XStreamMarshaller} to unmarshal XML from external sources</b> (i.e. the Web),
  * as this can result in <b>security vulnerabilities</b>. If you do use the
  * {@code XStreamMarshaller} to unmarshal external XML, set the
- * {@link #setConverters(ConverterMatcher[]) converters} and
- * {@link #setSupportedClasses(Class[]) supportedClasses} properties or override the
+ * {@link #setSupportedClasses(Class[]) supportedClasses} and
+ * {@link #setConverters(ConverterMatcher[]) converters} properties (possibly using a
+ * {@link CatchAllConverter} as the last converter in the list) or override the
  * {@link #customizeXStream(XStream)} method to make sure it only accepts the classes
  * you want it to support.
  *
