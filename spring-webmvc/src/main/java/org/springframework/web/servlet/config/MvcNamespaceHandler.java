db457e2d2646447a97ac5a207e66d921d822519f
sivaprasadvalluru <sivaprasad.valluru@gmail.com>
2013-08-28 21:25:53 +0530

    commit by siva
    Signed-off-by: sivaprasadvalluru <sivaprasad.valluru@gmail.com>

--- spring-webmvc/src/main/java/org/springframework/web/servlet/config/MvcNamespaceHandler.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/config/MvcNamespaceHandler.java
@@ -35,6 +35,7 @@ public class MvcNamespaceHandler extends NamespaceHandlerSupport {
 		registerBeanDefinitionParser("interceptors", new InterceptorsBeanDefinitionParser());
 		registerBeanDefinitionParser("resources", new ResourcesBeanDefinitionParser());
 		registerBeanDefinitionParser("view-controller", new ViewControllerBeanDefinitionParser());
+		registerBeanDefinitionParser("view-resolvers", new ViewResolversBeanDefinitionParser());
 	}
 
 }
