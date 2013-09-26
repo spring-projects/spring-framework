888e3c7e534d70a78a6ae9231aa5e1d3e2f1f637
Juergen Hoeller <jhoeller@vmware.com>
2013-09-26 21:48:50 +0200

    Polishing

--- spring-context/src/main/java/org/springframework/context/annotation/ComponentScan.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ComponentScan.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -142,7 +142,7 @@ public @interface ComponentScan {
 	@interface Filter {
 
 		/**
-		 * The type of filter to use.
+		 * The type of filter to use. Default is {@link FilterType#ANNOTATION}.
 		 */
 		FilterType type() default FilterType.ANNOTATION;
 
