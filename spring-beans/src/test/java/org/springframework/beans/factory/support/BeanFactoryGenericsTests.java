c8e8d6cd49e4edfc8ae9b093df667d8562444d99
Juergen Hoeller <jhoeller@vmware.com>
2013-10-28 13:25:45 +0100

    Defensive handling of exceptions during factory method type checking
    Also using ClassUtils.forName in AutowireUtils now in order to accept all common class name formats.
    
    Issue: SPR-11034
    (cherry picked from commit 8c1767e)

--- spring-beans/src/test/java/org/springframework/beans/factory/support/BeanFactoryGenericsTests.java
+++ spring-beans/src/test/java/org/springframework/beans/factory/support/BeanFactoryGenericsTests.java
@@ -660,8 +660,7 @@ public class BeanFactoryGenericsTests {
 	 * public static <T> T mock(Class<T> classToMock)
 	 * }
 	 * </pre>
-	 * <p>
-	 * See SPR-9493
+	 * <p>See SPR-9493
 	 */
 	@Test
 	public void parameterizedStaticFactoryMethod() {
@@ -734,6 +733,23 @@ public class BeanFactoryGenericsTests {
 
 		Map<String, Runnable> beans = bf.getBeansOfType(Runnable.class);
 		assertEquals(1, beans.size());
+	}
+
+	@Test
+	public void parameterizedInstanceFactoryMethodWithInvalidClassName() {
+		DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
+
+		RootBeanDefinition rbd = new RootBeanDefinition(MocksControl.class);
+		bf.registerBeanDefinition("mocksControl", rbd);
+
+		rbd = new RootBeanDefinition();
+		rbd.setFactoryBeanName("mocksControl");
+		rbd.setFactoryMethodName("createMock");
+		rbd.getConstructorArgumentValues().addGenericArgumentValue("x");
+		bf.registerBeanDefinition("mock", rbd);
+
+		Map<String, Runnable> beans = bf.getBeansOfType(Runnable.class);
+		assertEquals(0, beans.size());
 	}
 
 	@Test
