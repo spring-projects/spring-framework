23cc44f16ec1f935f798c60b85153d157eb6b3e8
Juergen Hoeller <jhoeller@vmware.com>
2013-11-01 22:38:55 +0100

    Javadoc polishing

--- spring-context/src/main/java/org/springframework/context/i18n/LocaleContextHolder.java
+++ spring-context/src/main/java/org/springframework/context/i18n/LocaleContextHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,7 +59,8 @@ public abstract class LocaleContextHolder {
 	/**
 	 * Associate the given LocaleContext with the current thread,
 	 * <i>not</i> exposing it as inheritable for child threads.
-	 * @param localeContext the current LocaleContext
+	 * @param localeContext the current LocaleContext,
+	 * or {@code null} to reset the thread-bound context
 	 */
 	public static void setLocaleContext(LocaleContext localeContext) {
 		setLocaleContext(localeContext, false);
