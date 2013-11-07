692e8221439b85270707633453820a078b7e1abf
Andy Clement <aclement@gopivotal.com>
2013-11-07 13:50:40 -0800

    Change SpEL equality operators to use .equals
    The SpEL operators == and != were using the java
    == comparison operator as part of their equality checking.
    It is more flexible to use the equals() method on Object.
    Under this commit the change to .equals() has been made
    and the equality checking code has been pushed into
    a common method in the Operator superclass. This commit
    also makes some tweaks to the other operator classes -
    the Float case was missing from OpGT.
    
    Issue: SPR-9194

--- spring-expression/src/main/java/org/springframework/expression/spel/ast/OpEQ.java
+++ spring-expression/src/main/java/org/springframework/expression/spel/ast/OpEQ.java
@@ -38,29 +38,7 @@ public class OpEQ extends Operator {
 			throws EvaluationException {
 		Object left = getLeftOperand().getValueInternal(state).getValue();
 		Object right = getRightOperand().getValueInternal(state).getValue();
-		if (left instanceof Number && right instanceof Number) {
-			Number op1 = (Number) left;
-			Number op2 = (Number) right;
-			if (op1 instanceof Double || op2 instanceof Double) {
-				return BooleanTypedValue.forValue(op1.doubleValue() == op2.doubleValue());
-			}
-			else if (op1 instanceof Float || op2 instanceof Float) {
-				return BooleanTypedValue.forValue(op1.floatValue() == op2.floatValue());
-			}
-			else if (op1 instanceof Long || op2 instanceof Long) {
-				return BooleanTypedValue.forValue(op1.longValue() == op2.longValue());
-			}
-			else {
-				return BooleanTypedValue.forValue(op1.intValue() == op2.intValue());
-			}
-		}
-		if (left != null && (left instanceof Comparable)) {
-			return BooleanTypedValue.forValue(state.getTypeComparator().compare(left,
-					right) == 0);
-		}
-		else {
-			return BooleanTypedValue.forValue(left == right);
-		}
+		return BooleanTypedValue.forValue(equalityCheck(state, left, right));
 	}
 
 }
