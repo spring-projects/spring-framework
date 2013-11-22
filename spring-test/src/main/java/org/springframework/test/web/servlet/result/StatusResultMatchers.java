665251fa2f87945488fdcfea3365e59af44a08a8
Sam Brannen <sam@sambrannen.com>
2013-11-22 12:55:12 +0100

    Suppress deprecation warning in StatusResultMatchers

--- spring-test/src/main/java/org/springframework/test/web/servlet/result/StatusResultMatchers.java
+++ spring-test/src/main/java/org/springframework/test/web/servlet/result/StatusResultMatchers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -211,7 +211,9 @@ public class StatusResultMatchers {
 
 	/**
 	 * Assert the response status code is {@code HttpStatus.MOVED_TEMPORARILY} (302).
+	 * @see #isFound()
 	 */
+	@SuppressWarnings("deprecation")
 	public ResultMatcher isMovedTemporarily() {
 		return matcher(HttpStatus.MOVED_TEMPORARILY);
 	}
