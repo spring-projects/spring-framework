e3017c30bbb2d33b16db9352288f0e6d6318f04b
Juergen Hoeller <jhoeller@vmware.com>
2013-12-23 22:00:30 +0100

    ResourceDatabasePopulator's setScripts takes varargs

--- spring-jdbc/src/main/java/org/springframework/jdbc/datasource/init/ResourceDatabasePopulator.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/datasource/init/ResourceDatabasePopulator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,7 +81,7 @@ public class ResourceDatabasePopulator implements DatabasePopulator {
 	 * Set the scripts to execute to populate the database.
 	 * @param scripts the scripts to execute
 	 */
-	public void setScripts(Resource[] scripts) {
+	public void setScripts(Resource... scripts) {
 		this.scripts = Arrays.asList(scripts);
 	}
 
