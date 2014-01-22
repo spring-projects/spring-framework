116e35db1dad48f35feb06e5eff944dae487c33d
Gary Russell <grussell@gopivotal.com>
2014-01-22 14:18:49 -0500

    SPR-11345 Restore MessageHandlingException Ctors
    JIRA: https://jira.springsource.org/browse/SPR-11345

--- spring-messaging/src/main/java/org/springframework/messaging/MessageHandlingException.java
+++ spring-messaging/src/main/java/org/springframework/messaging/MessageHandlingException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,8 +25,16 @@ package org.springframework.messaging;
 @SuppressWarnings("serial")
 public class MessageHandlingException extends MessagingException {
 
+	public MessageHandlingException(Message<?> failedMessage) {
+		super(failedMessage);
+	}
+
 	public MessageHandlingException(Message<?> message, String description) {
 		super(message, description);
+	}
+
+	public MessageHandlingException(Message<?> failedMessage, Throwable cause) {
+		super(failedMessage, cause);
 	}
 
 	public MessageHandlingException(Message<?> message, String description, Throwable cause) {
