3cb5828e21a1011ee2040452220453f3c178da31
Sam Brannen <sam@sambrannen.com>
2014-01-22 21:09:06 +0100

    Suppress warnings in MappingJacksonMsgConvTests

--- spring-jms/src/test/java/org/springframework/jms/support/converter/MappingJacksonMessageConverterTests.java
+++ spring-jms/src/test/java/org/springframework/jms/support/converter/MappingJacksonMessageConverterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,6 +28,7 @@ import javax.jms.TextMessage;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
@@ -38,6 +39,7 @@ import static org.mockito.BDDMockito.*;
  * @author Arjen Poutsma
  * @author Dave Syer
  */
+@SuppressWarnings("deprecation")
 public class MappingJacksonMessageConverterTests {
 
 	private MappingJacksonMessageConverter converter;
