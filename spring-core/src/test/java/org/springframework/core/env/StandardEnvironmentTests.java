23249cda982e2b9e3ee3237eeff384a6b58ecd8c
Juergen Hoeller <jhoeller@vmware.com>
2014-01-24 17:48:40 +0100

    Added test for SpringProperties setFlag method
    Issue: SPR-9014
    Issue: SPR-11297

--- spring-core/src/test/java/org/springframework/core/env/StandardEnvironmentTests.java
+++ spring-core/src/test/java/org/springframework/core/env/StandardEnvironmentTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -350,6 +350,13 @@ public class StandardEnvironmentTests {
 	@Test
 	public void suppressGetenvAccessThroughSpringProperty() {
 		SpringProperties.setProperty("spring.getenv.ignore", "true");
+		assertTrue(environment.getSystemEnvironment().isEmpty());
+		SpringProperties.setProperty("spring.getenv.ignore", null);
+	}
+
+	@Test
+	public void suppressGetenvAccessThroughSpringFlag() {
+		SpringProperties.setFlag("spring.getenv.ignore");
 		assertTrue(environment.getSystemEnvironment().isEmpty());
 		SpringProperties.setProperty("spring.getenv.ignore", null);
 	}
