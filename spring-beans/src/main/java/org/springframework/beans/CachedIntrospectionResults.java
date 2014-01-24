e07ad5a19b9b40dadbd1d37ee0f04ba6807b7829
Juergen Hoeller <jhoeller@vmware.com>
2014-01-25 00:15:00 +0100

    Polishing

--- spring-beans/src/main/java/org/springframework/beans/CachedIntrospectionResults.java
+++ spring-beans/src/main/java/org/springframework/beans/CachedIntrospectionResults.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -219,8 +219,8 @@ public class CachedIntrospectionResults {
 		synchronized (acceptedClassLoaders) {
 			acceptedLoaderArray = acceptedClassLoaders.toArray(new ClassLoader[acceptedClassLoaders.size()]);
 		}
-		for (ClassLoader registeredLoader : acceptedLoaderArray) {
-			if (isUnderneathClassLoader(classLoader, registeredLoader)) {
+		for (ClassLoader acceptedLoader : acceptedLoaderArray) {
+			if (isUnderneathClassLoader(classLoader, acceptedLoader)) {
 				return true;
 			}
 		}
