27ad1990e3026e30f7280de32ef6cfab7d79edc9
Sam Brannen <sam@sambrannen.com>
2014-02-13 00:44:10 +0100

    Fix imports in SourceHttpMessageConverterTests
    Replaced static import of org.junit.Assert.* with implicit imports in
    order to avoid ambiguity between JUnit's static Assert methods and those
    in org.custommonkey.xmlunit.XMLAssert.

--- spring-web/src/test/java/org/springframework/http/converter/xml/SourceHttpMessageConverterTests.java
+++ spring-web/src/test/java/org/springframework/http/converter/xml/SourceHttpMessageConverterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,7 @@ import org.springframework.util.FileCopyUtils;
 
 import static org.custommonkey.xmlunit.XMLAssert.*;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
