fe6a9826c57705149610fd678f71f0f6e423ff9f
Sam Brannen <sam@sambrannen.com>
2014-02-18 18:39:04 +0100

    Fix typo in AbstractDelegatingSmartContextLoader

--- spring-test/src/main/java/org/springframework/test/context/support/AbstractDelegatingSmartContextLoader.java
+++ spring-test/src/main/java/org/springframework/test/context/support/AbstractDelegatingSmartContextLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,7 +57,7 @@ import org.springframework.util.ObjectUtils;
  * files nor annotated classes and instead declare only {@linkplain
  * ContextConfiguration#initializers application context initializers}. In such
  * cases, an attempt will still be made to detect defaults, but their absence will
- * not result an an exception.
+ * not result in an exception.
  *
  * @author Sam Brannen
  * @since 3.2
