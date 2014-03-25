bbf101eef6b4692f2919ec0c2ed7249aa4c98564
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2014-03-25 09:59:09 -0400

    Fix issue in last commit

--- spring-websocket/src/main/java/org/springframework/web/socket/messaging/StompSubProtocolHandler.java
+++ spring-websocket/src/main/java/org/springframework/web/socket/messaging/StompSubProtocolHandler.java
@@ -187,7 +187,7 @@ public class StompSubProtocolHandler implements SubProtocolHandler, ApplicationE
 
 				message = MessageBuilder.withPayload(message.getPayload()).setHeaders(headers).build();
 
-				if (this.eventPublisher != null && StompCommand.CONNECT.equals(headers.getMessageType())) {
+				if (this.eventPublisher != null && StompCommand.CONNECT.equals(headers.getCommand())) {
 					publishEvent(new SessionConnectEvent(this, message));
 				}
 
@@ -265,7 +265,7 @@ public class StompSubProtocolHandler implements SubProtocolHandler, ApplicationE
 		try {
 			message = MessageBuilder.withPayload(message.getPayload()).setHeaders(headers).build();
 
-			if (this.eventPublisher != null && StompCommand.CONNECTED.equals(headers.getMessageType())) {
+			if (this.eventPublisher != null && StompCommand.CONNECTED.equals(headers.getCommand())) {
 				publishEvent(new SessionConnectedEvent(this, (Message<byte[]>) message));
 			}
 
--- spring-websocket/src/test/java/org/springframework/web/socket/messaging/StompSubProtocolHandlerTests.java
+++ spring-websocket/src/test/java/org/springframework/web/socket/messaging/StompSubProtocolHandlerTests.java
@@ -183,7 +183,7 @@ public class StompSubProtocolHandlerTests {
 
 		this.protocolHandler.afterSessionEnded(this.session, CloseStatus.BAD_DATA, this.channel);
 
-		assertEquals(3, publisher.events.size());
+		assertEquals("Unexpected events " + publisher.events, 3, publisher.events.size());
 		assertEquals(SessionConnectEvent.class, publisher.events.get(0).getClass());
 		assertEquals(SessionConnectedEvent.class, publisher.events.get(1).getClass());
 		assertEquals(SessionDisconnectEvent.class, publisher.events.get(2).getClass());
