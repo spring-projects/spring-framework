927201d213a6d44c4a1033eac1ece7bd11989f48
Morten Kjetland <mbk@kjetland.com>
2014-04-03 21:26:41 +0200

    Make Spring 3.2.x compatible with Java 8 bytecode Upgraded ASM from 4.0 to 5.0 and cglib from 3.0 to 3.1
    f

--- spring-core/src/main/java/org/springframework/asm/SpringAsmInfo.java
+++ spring-core/src/main/java/org/springframework/asm/SpringAsmInfo.java
@@ -33,6 +33,6 @@ public final class SpringAsmInfo {
 	 *
 	 * @see Opcodes#ASM4
 	 */
-	public static final int ASM_VERSION = Opcodes.ASM4;
+	public static final int ASM_VERSION = Opcodes.ASM5;
 
 }
