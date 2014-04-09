428aee5b727e9f0fb2a7e4dd48290e64d7c70460
Sam Brannen <sam@sambrannen.com>
2014-04-09 22:20:43 +0200

    Deprecate getDefTELClasses in TestContextMgr
    Since a dedicated bootstrap strategy will be introduced for the
    TestContext framework in Spring Framework 4.1, this commit officially
    deprecates TestContextManager.getDefaultTestExecutionListenerClasses()
    as a forewarning to developers or frameworks that may be using this
    obscure feature.
    
    Issue: SPR-11679

--- spring-test/src/main/java/org/springframework/test/context/TestContextManager.java
+++ spring-test/src/main/java/org/springframework/test/context/TestContextManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -242,7 +242,10 @@ public class TestContextManager {
 
 	/**
 	 * Determine the default {@link TestExecutionListener} classes.
+	 * @deprecated Spring Framework 4.1 will introduce a bootstrap strategy for
+	 * the TestContext framework at which point this method will be removed.
 	 */
+	@Deprecated
 	@SuppressWarnings("unchecked")
 	protected Set<Class<? extends TestExecutionListener>> getDefaultTestExecutionListenerClasses() {
 		Set<Class<? extends TestExecutionListener>> defaultListenerClasses = new LinkedHashSet<Class<? extends TestExecutionListener>>();
