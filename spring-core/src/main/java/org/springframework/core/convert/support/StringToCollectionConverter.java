0cdc7dfdc8623341a3feef90b1d20a54226d8eb5
MoonJongMin <jmmoon@lgcns.com>
2014-04-18 10:30:56 +0900

    With relation to [SPR-11126] problem converting empty parameter to List, I slightly chnaged convert(Object source, TypeDescriptor sourceType, TypeDescriptor targetType) method in the StringToCollectionConverter class.

--- spring-core/src/main/java/org/springframework/core/convert/support/StringToCollectionConverter.java
+++ spring-core/src/main/java/org/springframework/core/convert/support/StringToCollectionConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -61,7 +61,12 @@ final class StringToCollectionConverter implements ConditionalGenericConverter {
 			return null;
 		}
 		String string = (String) source;
-		String[] fields = StringUtils.commaDelimitedListToStringArray(string);
+		String[] fields = null;
+		if("".equals(string)) {
+			fields = new String[]{""};
+		} else {
+			fields = StringUtils.commaDelimitedListToStringArray(string);
+		}
 		Collection<Object> target = CollectionFactory.createCollection(targetType.getType(), fields.length);
 		if (targetType.getElementTypeDescriptor() == null) {
 			for (String field : fields) {
