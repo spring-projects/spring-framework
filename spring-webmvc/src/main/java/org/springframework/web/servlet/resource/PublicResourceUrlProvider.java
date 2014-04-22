10e1a805403883c9bc9ed19205e6306ffb01db73
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2014-04-22 11:34:46 -0400

    Update Javadoc for PublicResourceUrlProvider

--- spring-webmvc/src/main/java/org/springframework/web/servlet/resource/PublicResourceUrlProvider.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/resource/PublicResourceUrlProvider.java
@@ -34,9 +34,12 @@ import java.util.Map;
 
 
 /**
- * A central component for serving static resources that is aware of Spring MVC
- * handler mappings and provides methods to determine the public URL path that
- * a client should use to access a static resource.
+ * A central component to use to obtain the public URL path that clients should
+ * use to access a static resource.
+ *
+ * <p>This class is aware of Spring MVC handler mappings used to serve static
+ * resources and uses the {@code ResourceResolver} chains of the configured
+ * {@code ResourceHttpRequestHandler}s to make its decisions.
  *
  * @author Rossen Stoyanchev
  * @since 4.1
