a653c067112881428990ad1f535445838a25df55
Juergen Hoeller <jhoeller@vmware.com>
2014-05-01 00:49:22 +0200

    Fixed invalid code tag in javadoc (cherry picked from commit a139670)

--- spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanDefinition.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanDefinition.java
@@ -647,8 +647,8 @@ public abstract class AbstractBeanDefinition extends BeanMetadataAttributeAccess
 
 	/**
 	 * Specify whether to allow access to non-public constructors and methods,
-	 * for the case of externalized metadata pointing to those.
-	 * The default is {@code true}; switch this to {@false} for public access only.
+	 * for the case of externalized metadata pointing to those. The default is
+	 * {@code true}; switch this to {@code false} for public access only.
 	 * <p>This applies to constructor resolution, factory method resolution,
 	 * and also init/destroy methods. Bean property accessors have to be public
 	 * in any case and are not affected by this setting.
