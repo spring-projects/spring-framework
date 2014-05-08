9a7c72344dc6996eb62e55638a03789f6dbe4681
sergey-lagutin <sergey.v.lagutin@gmail.com>
2014-05-08 14:34:22 +0300

    Add auxiliary methods.

--- spring-webmvc/src/main/java/org/springframework/web/servlet/view/document/AbstractExcelXView.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/view/document/AbstractExcelXView.java
@@ -1,4 +1,10 @@
 package org.springframework.web.servlet.view.document;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.core.io.Resource;
 
@@ -34,5 +40,37 @@ public abstract class AbstractExcelXView extends AbstractPoiExcelView<XSSFWorkbo
     @Override
     protected XSSFWorkbook createWorkbookFromTemplate(Resource resource) throws IOException {
         return new XSSFWorkbook(resource.getInputStream());
+    }
+
+
+    /**
+     * Convenient method to obtain the cell in the given sheet, row and column.
+     * <p>Creates the row and the cell if they still doesn't already exist.
+     * Thus, the column can be passed as an int, the method making the needed downcasts.
+     * @param sheet a sheet object. The first sheet is usually obtained by workbook.getSheetAt(0)
+     * @param row thr row number
+     * @param col the column number
+     * @return the XSSFCell
+     */
+    protected XSSFCell getCell(XSSFSheet sheet, int row, int col) {
+        XSSFRow sheetRow = sheet.getRow(row);
+        if (sheetRow == null) {
+            sheetRow = sheet.createRow(row);
+        }
+        XSSFCell cell = sheetRow.getCell(col);
+        if (cell == null) {
+            cell = sheetRow.createCell(col);
+        }
+        return cell;
+    }
+
+    /**
+     * Convenient method to set a String as text content in a cell.
+     * @param cell the cell in which the text must be put
+     * @param text the text to put in the cell
+     */
+    protected void setText(XSSFCell cell, String text) {
+        cell.setCellType(XSSFCell.CELL_TYPE_STRING);
+        cell.setCellValue(text);
     }
 }
