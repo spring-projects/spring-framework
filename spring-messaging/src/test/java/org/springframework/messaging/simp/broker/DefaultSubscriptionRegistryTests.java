a9e40a6b7f97d970d7a90b9e6fc38d3143c0ccfc
Sebastien Deleuze <sdeleuze@gopivotal.com>
2014-07-01 11:55:02 +0200

    Fix DefaultSubscriptionRegistry cache cleanup with multiple subscriptions
    Issue: SPR-11931

--- spring-messaging/src/test/java/org/springframework/messaging/simp/broker/DefaultSubscriptionRegistryTests.java
+++ spring-messaging/src/test/java/org/springframework/messaging/simp/broker/DefaultSubscriptionRegistryTests.java
@@ -270,6 +270,22 @@ public class DefaultSubscriptionRegistryTests {
 		assertEquals(subscriptionIds, sort(actual.get(sessIds.get(2))));
 	}
 
+	// SPR-11931
+
+	@Test
+	public void registerTwiceAndUnregisterSubscriptions() {
+
+		this.registry.registerSubscription(subscribeMessage("sess01", "subs01", "/foo"));
+		this.registry.registerSubscription(subscribeMessage("sess01", "subs02", "/foo"));
+		MultiValueMap<String, String> actual = this.registry.findSubscriptions(message("/foo"));
+		assertEquals("Expected 1 element", 1, actual.size());
+
+		this.registry.unregisterSubscription(unsubscribeMessage("sess01", "subs01"));
+		this.registry.unregisterSubscription(unsubscribeMessage("sess01", "subs02"));
+		actual = this.registry.findSubscriptions(message("/foo"));
+		assertEquals("Expected no element", 0, actual.size());
+	}
+
 	@Test
 	public void unregisterAllSubscriptions() {
 
