134e5a2aecf913bcc184d5d3ae845a13d5369078
Juergen Hoeller <jhoeller@vmware.com>
2014-07-04 00:25:43 +0200

    Jms2MessageProducerInvocationHandler properly delegates to CachedMessageProducer.this
    Issue: SPR-11949

--- spring-jms/src/main/java/org/springframework/jms/connection/CachedMessageProducer.java
+++ spring-jms/src/main/java/org/springframework/jms/connection/CachedMessageProducer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -284,7 +284,7 @@ class CachedMessageProducer implements MessageProducer, QueueSender, TopicPublis
 								target, args[0], args[1], deliveryMode, priority, timeToLive, args[2]);
 					}
 				}
-				return method.invoke(target, args);
+				return method.invoke(CachedMessageProducer.this, args);
 			}
 			catch (InvocationTargetException ex) {
 				throw ex.getTargetException();
