2cbd5ba993b10b49be422759fbd51c72e557703f
Sam Brannen <sam@sambrannen.com>
2014-08-14 00:29:09 +0200

    Clean up regarding deprecated HttpStatus.MOVED_TEMPORARILY

--- spring-test/src/test/java/org/springframework/test/web/servlet/samples/standalone/resultmatchers/FlashAttributeAssertionTests.java
+++ spring-test/src/test/java/org/springframework/test/web/servlet/samples/standalone/resultmatchers/FlashAttributeAssertionTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,7 +48,7 @@ public class FlashAttributeAssertionTests {
 	@Before
 	public void setup() {
 		this.mockMvc = standaloneSetup(new PersonController())
-				.alwaysExpect(status().isMovedTemporarily())
+				.alwaysExpect(status().isFound())
 				.alwaysExpect(flash().attributeCount(3))
 				.build();
 	}
