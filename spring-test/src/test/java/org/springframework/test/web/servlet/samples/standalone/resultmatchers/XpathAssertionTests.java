e1d7f08d7b39ca26f5466e814ca22ff2c539bd94
Sam Brannen <sam@sambrannen.com>
2014-08-15 20:56:38 +0200

    Relax XML content type expectations in tests
    Requests in XmlContentAssertionTests and XpathAssertionTests now accept
    "application/xml;charset=UTF-8" in addition to "application/xml".

--- spring-test/src/test/java/org/springframework/test/web/servlet/samples/standalone/resultmatchers/XpathAssertionTests.java
+++ spring-test/src/test/java/org/springframework/test/web/servlet/samples/standalone/resultmatchers/XpathAssertionTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,7 +46,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
  * Examples of expectations on XML response content with XPath expressions.
  *
  * @author Rossen Stoyanchev
- *
+ * @author Sam Brannen
  * @see ContentAssertionTests
  * @see XmlContentAssertionTests
  */
@@ -60,9 +60,9 @@ public class XpathAssertionTests {
 	@Before
 	public void setup() throws Exception {
 		this.mockMvc = standaloneSetup(new MusicController())
-				.defaultRequest(get("/").accept(MediaType.APPLICATION_XML))
+				.defaultRequest(get("/").accept(MediaType.APPLICATION_XML, MediaType.parseMediaType("application/xml;charset=UTF-8")))
 				.alwaysExpect(status().isOk())
-				.alwaysExpect(content().contentType(MediaType.APPLICATION_XML))
+				.alwaysExpect(content().contentType(MediaType.parseMediaType("application/xml;charset=UTF-8")))
 				.build();
 	}
 
