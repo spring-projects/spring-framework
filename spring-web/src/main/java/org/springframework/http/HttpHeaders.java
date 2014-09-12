70412a9d0a3fe69f0f8f05f6f668782868c72e3f
Juergen Hoeller <jhoeller@vmware.com>
2014-09-12 16:44:56 +0200

    Accept empty Content-Type header
    Issue: SPR-12173

--- spring-web/src/main/java/org/springframework/http/HttpHeaders.java
+++ spring-web/src/main/java/org/springframework/http/HttpHeaders.java
@@ -556,7 +556,7 @@ public class HttpHeaders implements MultiValueMap<String, String>, Serializable 
 	 */
 	public MediaType getContentType() {
 		String value = getFirst(CONTENT_TYPE);
-		return (value != null ? MediaType.parseMediaType(value) : null);
+		return (StringUtils.hasLength(value) ? MediaType.parseMediaType(value) : null);
 	}
 
 	/**
