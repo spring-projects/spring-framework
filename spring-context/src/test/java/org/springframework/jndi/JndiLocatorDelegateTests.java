7f8d6115980c9325ff4b5227843f1972b4d15e44
Juergen Hoeller <jhoeller@vmware.com>
2014-09-19 00:11:44 +0200

    Defensive reset/restore of JNDI environment (avoiding test side effects)
    Issue: SPR-12223

--- spring-context/src/test/java/org/springframework/jndi/JndiLocatorDelegateTests.java
+++ spring-context/src/test/java/org/springframework/jndi/JndiLocatorDelegateTests.java
@@ -16,22 +16,35 @@
 
 package org.springframework.jndi;
 
+import java.lang.reflect.Field;
+import javax.naming.spi.NamingManager;
+
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
-
 import static org.junit.Assert.*;
 
 /**
  * Tests for {@link JndiLocatorDelegate}.
  *
  * @author Phillip Webb
+ * @author Juergen Hoeller
  */
 public class JndiLocatorDelegateTests {
 
 	@Test
-	public void isDefaultJndiEnvironmentAvailableFalse() {
-		assertThat(JndiLocatorDelegate.isDefaultJndiEnvironmentAvailable(), equalTo(false));
+	public void isDefaultJndiEnvironmentAvailableFalse() throws Exception {
+		Field builderField = NamingManager.class.getDeclaredField("initctx_factory_builder");
+		builderField.setAccessible(true);
+		Object oldBuilder = builderField.get(null);
+		builderField.set(null, null);
+
+		try {
+			assertThat(JndiLocatorDelegate.isDefaultJndiEnvironmentAvailable(), equalTo(false));
+		}
+		finally {
+			builderField.set(null, oldBuilder);
+		}
 	}
 
 }
