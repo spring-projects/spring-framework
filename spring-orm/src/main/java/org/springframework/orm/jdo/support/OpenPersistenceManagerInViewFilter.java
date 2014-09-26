b45d08f1929fb4efbb1e035214fc70ae0b22f746
Juergen Hoeller <jhoeller@vmware.com>
2014-09-26 21:40:28 +0200

    Remove outdated references to ContextLoaderServlet
    Issue: SPR-7725

--- spring-orm/src/main/java/org/springframework/orm/jdo/support/OpenPersistenceManagerInViewFilter.java
+++ spring-orm/src/main/java/org/springframework/orm/jdo/support/OpenPersistenceManagerInViewFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package org.springframework.orm.jdo.support;
 
 import java.io.IOException;
-
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 import javax.servlet.FilterChain;
@@ -46,9 +45,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
  *
  * <p>Looks up the PersistenceManagerFactory in Spring's root web application context.
  * Supports a "persistenceManagerFactoryBeanName" filter init-param in {@code web.xml};
- * the default bean name is "persistenceManagerFactory". Looks up the PersistenceManagerFactory
- * on each request, to avoid initialization order issues (when using ContextLoaderServlet,
- * the root application context will get initialized <i>after</i> this filter).
+ * the default bean name is "persistenceManagerFactory".
  *
  * @author Juergen Hoeller
  * @since 1.1
