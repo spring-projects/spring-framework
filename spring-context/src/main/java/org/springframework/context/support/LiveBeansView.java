3827e048d122fe553d69e9ab48be21a69b87ab12
Phillip Webb <pwebb@pivotal.io>
2014-09-26 12:32:04 -0700

    Protect against NPE when escaping LiveBeansView
    Issue: SPR-12252

--- spring-context/src/main/java/org/springframework/context/support/LiveBeansView.java
+++ spring-context/src/main/java/org/springframework/context/support/LiveBeansView.java
@@ -220,6 +220,9 @@ public class LiveBeansView implements LiveBeansViewMBean, ApplicationContextAwar
 	 */
 	protected String getEscapedResourceDescription(BeanDefinition bd) {
 		String resourceDescription = bd.getResourceDescription();
+		if (resourceDescription == null) {
+			return null;
+		}
 		StringBuilder result = new StringBuilder(resourceDescription.length() + 16);
 		for (int i = 0; i < resourceDescription.length(); i++) {
 			char character = resourceDescription.charAt(i);
