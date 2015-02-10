181b35243e3f38411ff8c692c5f7a2185e32f276
Juergen Hoeller <jhoeller@pivotal.io>
2015-02-10 19:26:19 +0100

    Properly honor conditions on overriding bean methods
    Issue: SPR-12694
    (cherry picked from commit 981aefc)

--- spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClass.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClass.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package org.springframework.context.annotation;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
@@ -62,6 +63,8 @@ final class ConfigurationClass {
 
 	private final Map<ImportBeanDefinitionRegistrar, AnnotationMetadata> importBeanDefinitionRegistrars =
 			new LinkedHashMap<ImportBeanDefinitionRegistrar, AnnotationMetadata>();
+
+	final Set<String> skippedBeans = new HashSet<String>();
 
 
 	/**
