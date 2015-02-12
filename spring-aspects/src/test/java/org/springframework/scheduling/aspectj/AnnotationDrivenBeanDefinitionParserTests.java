66735d01cf7eef64e630c353285233fd1c500b17
Stephane Nicoll <snicoll@pivotal.io>
2015-02-12 15:25:51 +0100

    Add exception-handler attribute support for AspectJ
    Previously, the exception-handler attribute was not taken care of when
    task:annotation-driven is used in AspectJ mode. This commit provides the
    expected behavior.
    
    Issue: SPR-12619

--- spring-aspects/src/test/java/org/springframework/scheduling/aspectj/AnnotationDrivenBeanDefinitionParserTests.java
+++ spring-aspects/src/test/java/org/springframework/scheduling/aspectj/AnnotationDrivenBeanDefinitionParserTests.java
@@ -14,25 +14,25 @@
  * limitations under the License.
  */
 
-package org.springframework.scheduling.config;
+package org.springframework.scheduling.aspectj;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import org.springframework.beans.DirectFieldAccessor;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.scheduling.config.TaskManagementConfigUtils;
 
 import static org.junit.Assert.*;
 
 /**
- * @author Mark Fisher
  * @author Stephane Nicoll
  */
 public class AnnotationDrivenBeanDefinitionParserTests {
 
-	private ApplicationContext context;
-
+	private ConfigurableApplicationContext context;
 
 	@Before
 	public void setup() {
@@ -40,36 +40,30 @@ public class AnnotationDrivenBeanDefinitionParserTests {
 				"annotationDrivenContext.xml", AnnotationDrivenBeanDefinitionParserTests.class);
 	}
 
-
-	@Test
-	public void asyncPostProcessorRegistered() {
-		assertTrue(context.containsBean(TaskManagementConfigUtils.ASYNC_ANNOTATION_PROCESSOR_BEAN_NAME));
+	@After
+	public void after() {
+		if (this.context != null) {
+			this.context.close();
+		}
 	}
 
 	@Test
-	public void scheduledPostProcessorRegistered() {
-		assertTrue(context.containsBean(TaskManagementConfigUtils.SCHEDULED_ANNOTATION_PROCESSOR_BEAN_NAME));
+	public void asyncAspectRegistered() {
+		assertTrue(context.containsBean(TaskManagementConfigUtils.ASYNC_EXECUTION_ASPECT_BEAN_NAME));
 	}
 
 	@Test
 	public void asyncPostProcessorExecutorReference() {
 		Object executor = context.getBean("testExecutor");
-		Object postProcessor = context.getBean(TaskManagementConfigUtils.ASYNC_ANNOTATION_PROCESSOR_BEAN_NAME);
-		assertSame(executor, new DirectFieldAccessor(postProcessor).getPropertyValue("executor"));
-	}
-
-	@Test
-	public void scheduledPostProcessorSchedulerReference() {
-		Object scheduler = context.getBean("testScheduler");
-		Object postProcessor = context.getBean(TaskManagementConfigUtils.SCHEDULED_ANNOTATION_PROCESSOR_BEAN_NAME);
-		assertSame(scheduler, new DirectFieldAccessor(postProcessor).getPropertyValue("scheduler"));
+		Object aspect = context.getBean(TaskManagementConfigUtils.ASYNC_EXECUTION_ASPECT_BEAN_NAME);
+		assertSame(executor, new DirectFieldAccessor(aspect).getPropertyValue("defaultExecutor"));
 	}
 
 	@Test
 	public void asyncPostProcessorExceptionHandlerReference() {
 		Object exceptionHandler = context.getBean("testExceptionHandler");
-		Object postProcessor = context.getBean(TaskManagementConfigUtils.ASYNC_ANNOTATION_PROCESSOR_BEAN_NAME);
-		assertSame(exceptionHandler, new DirectFieldAccessor(postProcessor).getPropertyValue("exceptionHandler"));
+		Object aspect = context.getBean(TaskManagementConfigUtils.ASYNC_EXECUTION_ASPECT_BEAN_NAME);
+		assertSame(exceptionHandler, new DirectFieldAccessor(aspect).getPropertyValue("exceptionHandler"));
 	}
 
 }
