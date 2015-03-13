f42c53d9be3e0fc34b30c9519da38d103694a3e4
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2015-03-13 13:02:52 -0400

    Support comma-separated X-Forwarded-Proto
    Issue: SPR-12816

--- spring-web/src/test/java/org/springframework/web/util/UriComponentsBuilderTests.java
+++ spring-web/src/test/java/org/springframework/web/util/UriComponentsBuilderTests.java
@@ -417,6 +417,25 @@ public class UriComponentsBuilderTests {
 		assertEquals("http://a.example.org/mvc-showcase", result.toString());
 	}
 
+	// SPR-12816
+
+	@Test
+	public void fromHttpRequestWithForwardedProtoMultiValueHeader() {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.setScheme("http");
+		request.setServerName("localhost");
+		request.setServerPort(8080);
+		request.setRequestURI("/mvc-showcase");
+		request.addHeader("X-Forwarded-Host", "a.example.org");
+		request.addHeader("X-Forwarded-Port", "443");
+		request.addHeader("X-Forwarded-Proto", "https,https");
+
+		HttpRequest httpRequest = new ServletServerHttpRequest(request);
+		UriComponents result = UriComponentsBuilder.fromHttpRequest(httpRequest).build();
+
+		assertEquals("https://a.example.org/mvc-showcase", result.toString());
+	}
+
 	@Test
 	public void path() throws URISyntaxException {
 		UriComponentsBuilder builder = UriComponentsBuilder.fromPath("/foo/bar");
