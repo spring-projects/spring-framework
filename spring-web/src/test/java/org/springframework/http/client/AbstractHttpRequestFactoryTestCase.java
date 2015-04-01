caee78aee3ebbb4425c2df4c039fa62536ed4277
Arjen Poutsma <apoutsma@pivotal.io>
2015-04-01 13:08:56 +0200

    Netty4ClientHttpRequest ignores query params
    Before this commit, Netty4ClientHttpRequest ignored query parameters
    (?foo=bar). This commit fixes that.
    
    Issue: SPR-12779

--- spring-web/src/test/java/org/springframework/http/client/AbstractHttpRequestFactoryTestCase.java
+++ spring-web/src/test/java/org/springframework/http/client/AbstractHttpRequestFactoryTestCase.java
@@ -180,4 +180,18 @@ public abstract class AbstractHttpRequestFactoryTestCase extends
 		}
 	}
 
+	@Test
+	public void queryParameters() throws Exception {
+		URI uri = new URI(baseUrl + "/params?param1=value&param2=value1&param2=value2");
+		ClientHttpRequest request = factory.createRequest(uri, HttpMethod.GET);
+
+		ClientHttpResponse response = request.execute();
+		try {
+			assertEquals("Invalid status code", HttpStatus.OK, response.getStatusCode());
+		}
+		finally {
+			response.close();
+		}
+	}
+
 }
