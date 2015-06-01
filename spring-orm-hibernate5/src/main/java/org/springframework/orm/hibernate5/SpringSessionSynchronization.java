8c337a48a973bb3522fca22910b379a3e0062bcc
Juergen Hoeller <jhoeller@pivotal.io>
2015-06-01 20:34:14 +0200

    SpringSessionSynchronization's beforeCompletion unbinds after disconnect failure as well
    Issue: SPR-13089

--- spring-orm-hibernate5/src/main/java/org/springframework/orm/hibernate5/SpringSessionSynchronization.java
+++ spring-orm-hibernate5/src/main/java/org/springframework/orm/hibernate5/SpringSessionSynchronization.java
@@ -112,18 +112,21 @@ public class SpringSessionSynchronization implements TransactionSynchronization,
 
 	@Override
 	public void beforeCompletion() {
-		Session session = this.sessionHolder.getSession();
-		if (this.sessionHolder.getPreviousFlushMode() != null) {
-			// In case of pre-bound Session, restore previous flush mode.
-			session.setFlushMode(this.sessionHolder.getPreviousFlushMode());
+		try {
+			Session session = this.sessionHolder.getSession();
+			if (this.sessionHolder.getPreviousFlushMode() != null) {
+				// In case of pre-bound Session, restore previous flush mode.
+				session.setFlushMode(this.sessionHolder.getPreviousFlushMode());
+			}
+			// Eagerly disconnect the Session here, to make release mode "on_close" work nicely.
+			session.disconnect();
 		}
-		// Eagerly disconnect the Session here, to make release mode "on_close" work nicely.
-		session.disconnect();
-
-		// Unbind at this point if it's a new Session...
-		if (this.newSession) {
-			TransactionSynchronizationManager.unbindResource(this.sessionFactory);
-			this.holderActive = false;
+		finally {
+			// Unbind at this point if it's a new Session...
+			if (this.newSession) {
+				TransactionSynchronizationManager.unbindResource(this.sessionFactory);
+				this.holderActive = false;
+			}
 		}
 	}
 
