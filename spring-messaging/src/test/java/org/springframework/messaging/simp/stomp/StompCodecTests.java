0dfb5d8bbfc199bc40111d72fed4788d578b21ba
Josh King <josh.king@xirrus.com>
2015-06-09 14:06:46 -0700

    Header values are optional in Stomp 1.1 and 1.2

--- spring-messaging/src/test/java/org/springframework/messaging/simp/stomp/StompCodecTests.java
+++ spring-messaging/src/test/java/org/springframework/messaging/simp/stomp/StompCodecTests.java
@@ -189,6 +189,23 @@ public class StompCodecTests {
 	}
 
 	@Test
+	public void decodeFrameWithHeaderWithEmptyValue() {
+		String accept = "accept-version:1.1\n";
+		String valuelessKey = "key:\n";
+
+		Message<byte[]> frame = decode("CONNECT\n" + accept + valuelessKey + "\n\0");
+		StompHeaderAccessor headers = StompHeaderAccessor.wrap(frame);
+
+		assertEquals(StompCommand.CONNECT, headers.getCommand());
+
+		assertEquals(2, headers.toNativeHeaderMap().size());
+		assertEquals("1.1", headers.getFirstNativeHeader("accept-version"));
+		assertEquals("", headers.getFirstNativeHeader("key"));
+
+		assertEquals(0, frame.getPayload().length);
+	}
+
+	@Test
 	public void decodeFrameWithIncompleteCommand() {
 		assertIncompleteDecode("MESSAG");
 	}
