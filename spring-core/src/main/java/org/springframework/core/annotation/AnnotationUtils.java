594c3302055748a31c99239080c4ddb45c002e97
Sam Brannen <sam@sambrannen.com>
2015-06-20 16:53:46 +0200

    Honor contract of @Repeatable in AnnotationUtils
    This commit introduces a minor bug fix for getRepeatableAnnotations()
    so that it fully complies with the contract of Java's
    getAnnotationsByType() method with regard to repeatable annotations
    declared on multiple superclasses.
    
    Issue: SPR-13068

--- spring-core/src/main/java/org/springframework/core/annotation/AnnotationUtils.java
+++ spring-core/src/main/java/org/springframework/core/annotation/AnnotationUtils.java
@@ -305,6 +305,13 @@ public abstract class AnnotationUtils {
 			return annotations;
 		}
 
+		if (annotatedElement instanceof Class) {
+			Class<?> superclass = ((Class<?>) annotatedElement).getSuperclass();
+			if ((superclass != null) && (Object.class != superclass)) {
+				return getRepeatableAnnotations(superclass, annotationType, containerAnnotationType);
+			}
+		}
+
 		return getRepeatableAnnotations(annotatedElement, annotationType, containerAnnotationType, false);
 	}
 
--- spring-core/src/test/java/org/springframework/core/annotation/AnnotationUtilsTests.java
+++ spring-core/src/test/java/org/springframework/core/annotation/AnnotationUtilsTests.java
@@ -587,6 +587,25 @@ public class AnnotationUtilsTests {
 	}
 
 	@Test
+	public void getRepeatableAnnotationsDeclaredOnMultipleSuperclasses() {
+		final Class<?> clazz = SubSubMyRepeatableWithAdditionalLocalDeclarationsClass.class;
+		final List<String> expectedValuesJava = Arrays.asList("X", "Y", "Z");
+		final List<String> expectedValuesSpring = Arrays.asList("X", "Y", "Z", "meta2");
+
+		// Java 8
+		MyRepeatable[] array = clazz.getAnnotationsByType(MyRepeatable.class);
+		assertNotNull(array);
+		List<String> values = stream(array).map(MyRepeatable::value).collect(toList());
+		assertThat(values, is(expectedValuesJava));
+
+		// Spring
+		Set<MyRepeatable> set = getRepeatableAnnotations(clazz, MyRepeatable.class, MyRepeatableContainer.class);
+		assertNotNull(set);
+		values = set.stream().map(MyRepeatable::value).collect(toList());
+		assertThat(values, is(expectedValuesSpring));
+	}
+
+	@Test
 	public void getDeclaredRepeatableAnnotationsDeclaredOnClass() {
 		final List<String> expectedValuesJava = Arrays.asList("A", "B", "C");
 		final List<String> expectedValuesSpring = Arrays.asList("A", "B", "C", "meta1");
@@ -1356,6 +1375,10 @@ public class AnnotationUtilsTests {
 	@MyRepeatableContainer({ @MyRepeatable("Y"), @MyRepeatable("Z") })
 	@MyRepeatableMeta2
 	static class SubMyRepeatableWithAdditionalLocalDeclarationsClass extends MyRepeatableClass {
+	}
+
+	static class SubSubMyRepeatableWithAdditionalLocalDeclarationsClass extends
+			SubMyRepeatableWithAdditionalLocalDeclarationsClass {
 	}
 
 	enum RequestMethod {
