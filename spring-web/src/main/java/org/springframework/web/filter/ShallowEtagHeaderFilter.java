2c32873309417d5bba8f9aa376c30e7690ae2ba1
Brian Clozel <bclozel@pivotal.io>
2015-07-03 21:40:48 +0200

    Fix error message typo in ShallowEtagHeaderFilter
    Issue: SPR-13194

--- spring-web/src/main/java/org/springframework/web/filter/ShallowEtagHeaderFilter.java
+++ spring-web/src/main/java/org/springframework/web/filter/ShallowEtagHeaderFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,7 +89,7 @@ public class ShallowEtagHeaderFilter extends OncePerRequestFilter {
 	private void updateResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		ContentCachingResponseWrapper responseWrapper =
 				WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
-		Assert.notNull(responseWrapper, "ShallowEtagResponseWrapper not found");
+		Assert.notNull(responseWrapper, "ContentCachingResponseWrapper not found");
 
 		HttpServletResponse rawResponse = (HttpServletResponse) responseWrapper.getResponse();
 		int statusCode = responseWrapper.getStatusCode();
