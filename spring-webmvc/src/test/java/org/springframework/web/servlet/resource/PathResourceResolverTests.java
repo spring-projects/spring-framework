106cce58cc32ef5afcbe17cf6bb18e325591a1be
Brian Clozel <bclozel@pivotal.io>
2015-07-17 13:32:38 +0200

    Fix tests
    Issue: SPR-13241

--- spring-webmvc/src/test/java/org/springframework/web/servlet/resource/PathResourceResolverTests.java
+++ spring-webmvc/src/test/java/org/springframework/web/servlet/resource/PathResourceResolverTests.java
@@ -128,11 +128,8 @@ public class PathResourceResolverTests {
 	@Test
 	public void resolvePathRootResource() throws Exception {
 		Resource webjarsLocation = new ClassPathResource("/META-INF/resources/webjars/", PathResourceResolver.class);
-		Resource actual = this.resolver.resolveResource(null, "", Arrays.asList(webjarsLocation), null);
 		String path = this.resolver.resolveUrlPathInternal("", Arrays.asList(webjarsLocation), null);
 
-		assertNotNull(actual);
-		assertTrue(actual.exists() && actual.isReadable());
 		assertNull(path);
 	}
 }
