1c6febc45c78973a830412c062e53f2e3c14d22e
Sebastien Deleuze <sdeleuze@pivotal.io>
2015-10-01 11:14:38 +0200

    Make UriComponentsBuilder.fromOriginHeader() IPv6 compliant
    Issue: SPR-13525

--- spring-web/src/test/java/org/springframework/web/util/WebUtilsTests.java
+++ spring-web/src/test/java/org/springframework/web/util/WebUtilsTests.java
@@ -142,6 +142,14 @@ public class WebUtilsTests {
 		assertFalse(checkSameOrigin("mydomain2.com", -1, "http://mydomain1.com:80/"));
 		assertFalse(checkSameOrigin("mydomain2.com", -1, "http://mydomain1.com/path"));
 		assertFalse(checkSameOrigin("mydomain2.com", -1, "http://mydomain1.com:80/path"));
+
+		// Handling of IPv6 hosts as described in SPR-13525
+		assertTrue(checkSameOrigin("[::1]", -1, "http://[::1]"));
+		assertTrue(checkSameOrigin("[::1]", 8080, "http://[::1]:8080"));
+		assertTrue(checkSameOrigin("[2001:0db8:0000:85a3:0000:0000:ac1f:8001]", -1, "http://[2001:0db8:0000:85a3:0000:0000:ac1f:8001]"));
+		assertTrue(checkSameOrigin("[2001:0db8:0000:85a3:0000:0000:ac1f:8001]", 8080, "http://[2001:0db8:0000:85a3:0000:0000:ac1f:8001]:8080"));
+		assertFalse(checkSameOrigin("[::1]", -1, "http://[::1]:8080"));
+		assertFalse(checkSameOrigin("[::1]", 8080, "http://[2001:0db8:0000:85a3:0000:0000:ac1f:8001]:8080"));
 	}
 
 
