bd472af37e656bb2ec84b8ec3a67486ee79f3506
Arjen Poutsma <apoutsma@pivotal.io>
2015-10-30 09:26:34 +0100

    Updated Copyright notices

--- spring-web-reactive/src/main/java/org/springframework/reactive/web/http/reactor/PublisherReactorServerHttpRequest.java
+++ spring-web-reactive/src/main/java/org/springframework/reactive/web/http/reactor/PublisherReactorServerHttpRequest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2011-2015 Pivotal Software Inc, All Rights Reserved.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
