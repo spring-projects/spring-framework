5f2d34f526f3f2b3c41dbc39a69374e526b6b5aa
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2015-11-17 15:19:40 -0500

    Update Javadoc
    Issue: SPR-13678

--- spring-web/src/main/java/org/springframework/web/util/UriComponentsBuilder.java
+++ spring-web/src/main/java/org/springframework/web/util/UriComponentsBuilder.java
@@ -584,8 +584,9 @@ public class UriComponentsBuilder implements Cloneable {
 	}
 
 	/**
-	 * Append the given path segments to the existing path of this builder.
-	 * Each given path segment may contain URI template variables.
+	 * Append path segments to the existing path. Each path segment may contain
+	 * URI template variables and should not contain any slashes.
+	 * Use {@code path("/")} subsequently to ensure a trailing slash.
 	 * @param pathSegments the URI path segments
 	 * @return this UriComponentsBuilder
 	 */
