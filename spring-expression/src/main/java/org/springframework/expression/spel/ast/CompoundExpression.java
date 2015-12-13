ef1e17fd15e5b32dbfb427ba4cf7c03b4d769070
Juergen Hoeller <jhoeller@pivotal.io>
2015-12-13 01:14:38 +0100

    Polishing

--- spring-expression/src/main/java/org/springframework/expression/spel/ast/CompoundExpression.java
+++ spring-expression/src/main/java/org/springframework/expression/spel/ast/CompoundExpression.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,7 +62,7 @@ public class CompoundExpression extends SpelNodeImpl {
 			}
 			try {
 				state.pushActiveContextObject(result);
-				nextNode = this.children[cc-1];
+				nextNode = this.children[cc - 1];
 				return nextNode.getValueRef(state);
 			}
 			finally {
