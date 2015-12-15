9e03e0e73d53c4a6e077856aa00792493b2d0d4d
Juergen Hoeller <jhoeller@pivotal.io>
2015-12-15 11:53:40 +0100

    Polishing

--- spring-webmvc-portlet/src/main/java/org/springframework/web/portlet/mvc/annotation/AnnotationMethodHandlerExceptionResolver.java
+++ spring-webmvc-portlet/src/main/java/org/springframework/web/portlet/mvc/annotation/AnnotationMethodHandlerExceptionResolver.java
@@ -75,9 +75,11 @@ import org.springframework.web.servlet.View;
  */
 public class AnnotationMethodHandlerExceptionResolver extends AbstractHandlerExceptionResolver {
 
-	// dummy method placeholder
-	private static final Method NO_METHOD_FOUND =
-			ClassUtils.getMethodIfAvailable(System.class, "currentTimeMillis", (Class<?>[]) null);
+	/**
+	 * Arbitrary {@link Method} reference, indicating no method found in the cache.
+	 */
+	private static final Method NO_METHOD_FOUND = ClassUtils.getMethodIfAvailable(System.class, "currentTimeMillis");
+
 
 	private final Map<Class<?>, Map<Class<? extends Throwable>, Method>> exceptionHandlerCache =
 			new ConcurrentHashMap<Class<?>, Map<Class<? extends Throwable>, Method>>(64);
@@ -210,7 +212,7 @@ public class AnnotationMethodHandlerExceptionResolver extends AbstractHandlerExc
 
 	/**
 	 * Uses the {@link ExceptionDepthComparator} to find the best matching method.
-	 * @return the best matching method or {@code null}.
+	 * @return the best matching method, or {@code null} if none found
 	 */
 	private Method getBestMatchingMethod(
 			Map<Class<? extends Throwable>, Method> resolverMethods, Exception thrownException) {
