8a6fa02887e7b96024a94e43569688533ba01ab0
Stephane Maldini <smaldini@pivotal.io>
2016-01-27 17:41:19 +0000

    Sync with RSC move graph to flow

--- spring-web-reactive/src/main/java/org/springframework/http/server/reactive/boot/ReactorHttpServer.java
+++ spring-web-reactive/src/main/java/org/springframework/http/server/reactive/boot/ReactorHttpServer.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright (c) 2011-2016 Pivotal Software Inc, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +16,7 @@
 
 package org.springframework.http.server.reactive.boot;
 
-import reactor.core.graph.Connectable;
+import reactor.core.flow.Loopback;
 import reactor.core.state.Completable;
 import reactor.io.buffer.Buffer;
 import reactor.io.net.ReactiveNet;
@@ -30,7 +30,7 @@ import org.springframework.util.Assert;
  * @author Stephane Maldini
  */
 public class ReactorHttpServer extends HttpServerSupport
-		implements HttpServer, Connectable, Completable {
+		implements HttpServer, Loopback, Completable {
 
 	private ReactorHttpHandlerAdapter reactorHandler;
 
