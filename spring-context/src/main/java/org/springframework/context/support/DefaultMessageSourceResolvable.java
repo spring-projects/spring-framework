5c87afc51d1bd319d83b171fc4b8402fe84b1a37
Juergen Hoeller <jhoeller@pivotal.io>
2016-02-16 22:28:46 +0100

    Polishing

--- spring-context/src/main/java/org/springframework/context/support/DefaultMessageSourceResolvable.java
+++ spring-context/src/main/java/org/springframework/context/support/DefaultMessageSourceResolvable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -127,7 +127,7 @@ public class DefaultMessageSourceResolvable implements MessageSourceResolvable, 
 	protected final String resolvableToString() {
 		StringBuilder result = new StringBuilder();
 		result.append("codes [").append(StringUtils.arrayToDelimitedString(this.codes, ","));
-		result.append("]; arguments [" + StringUtils.arrayToDelimitedString(this.arguments, ","));
+		result.append("]; arguments [").append(StringUtils.arrayToDelimitedString(this.arguments, ","));
 		result.append("]; default message [").append(this.defaultMessage).append(']');
 		return result.toString();
 	}
