a620770470c6ae501a5d54f4f7149745d96cb923
Juergen Hoeller <jhoeller@pivotal.io>
2016-02-19 16:02:25 +0100

    Polishing

--- spring-core/src/main/java/org/springframework/core/convert/support/StringToNumberConverterFactory.java
+++ spring-core/src/main/java/org/springframework/core/convert/support/StringToNumberConverterFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,6 +44,7 @@ final class StringToNumberConverterFactory implements ConverterFactory<String, N
 	public <T extends Number> Converter<String, T> getConverter(Class<T> targetType) {
 		return new StringToNumber<T>(targetType);
 	}
+
 
 	private static final class StringToNumber<T extends Number> implements Converter<String, T> {
 
--- spring-web/src/main/java/org/springframework/http/client/OkHttpClientHttpRequestFactory.java
+++ spring-web/src/main/java/org/springframework/http/client/OkHttpClientHttpRequestFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,7 +54,7 @@ public class OkHttpClientHttpRequestFactory
 	 * @param client the client to use
 	 */
 	public OkHttpClientHttpRequestFactory(OkHttpClient client) {
-		Assert.notNull(client, "'client' must not be null");
+		Assert.notNull(client, "OkHttpClient must not be null");
 		this.client = client;
 		this.defaultClient = false;
 	}
