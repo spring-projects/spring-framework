e086a5d58b48e3d2cdff342b6b6b202af0f43852
Stephane Nicoll <snicoll@pivotal.io>
2016-03-06 09:04:59 +0100

    Polish
    Add period unit in the description so that it shows up in the summary
    description.

--- spring-context/src/main/java/org/springframework/scheduling/annotation/Scheduled.java
+++ spring-context/src/main/java/org/springframework/scheduling/annotation/Scheduled.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -72,28 +72,30 @@ public @interface Scheduled {
 	String zone() default "";
 
 	/**
-	 * Execute the annotated method with a fixed period between the end
-	 * of the last invocation and the start of the next.
+	 * Execute the annotated method with a fixed period in milliseconds between the
+	 * end of the last invocation and the start of the next.
 	 * @return the delay in milliseconds
 	 */
 	long fixedDelay() default -1;
 
 	/**
-	 * Execute the annotated method with a fixed period between the end
-	 * of the last invocation and the start of the next.
+	 * Execute the annotated method with a fixed period in milliseconds between the
+	 * end of the last invocation and the start of the next.
 	 * @return the delay in milliseconds as a String value, e.g. a placeholder
 	 * @since 3.2.2
 	 */
 	String fixedDelayString() default "";
 
 	/**
-	 * Execute the annotated method with a fixed period between invocations.
+	 * Execute the annotated method with a fixed period in milliseconds between
+	 * invocations.
 	 * @return the period in milliseconds
 	 */
 	long fixedRate() default -1;
 
 	/**
-	 * Execute the annotated method with a fixed period between invocations.
+	 * Execute the annotated method with a fixed period in milliseconds between
+	 * invocations.
 	 * @return the period in milliseconds as a String value, e.g. a placeholder
 	 * @since 3.2.2
 	 */
