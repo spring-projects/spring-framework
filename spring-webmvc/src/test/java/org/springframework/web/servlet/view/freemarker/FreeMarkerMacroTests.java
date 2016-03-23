8994498b4477a4cef59dd928b70ed86d99f5f465
Juergen Hoeller <jhoeller@pivotal.io>
2016-03-23 18:35:26 +0100

    Revised newline handling tests pass on Windows now
    Issue: SPR-13503
    (cherry picked from commit 311d4c9)

--- spring-webmvc/src/test/java/org/springframework/web/servlet/view/freemarker/FreeMarkerMacroTests.java
+++ spring-webmvc/src/test/java/org/springframework/web/servlet/view/freemarker/FreeMarkerMacroTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -326,6 +326,7 @@ public class FreeMarkerMacroTests {
 
 		// tokenize output and ignore whitespace
 		String output = response.getContentAsString();
+		output = output.replace("\r\n", "\n");
 		return output.trim();
 	}
 
