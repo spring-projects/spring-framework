9a52c8144313a5093bf6b7153cd89857aaed1899
Juergen Hoeller <jhoeller@pivotal.io>
2016-03-24 15:33:44 +0100

    UriComponentsBuilder allows more efficient (and less restrictive) CORS origin comparison
    Also introduces a specifically narrowed cloneBuilder() method for UriComponentsBuilders.
    
    Issue: SPR-14080

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/MvcUriComponentsBuilder.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/MvcUriComponentsBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -406,7 +406,7 @@ public class MvcUriComponentsBuilder {
 
 	private static UriComponentsBuilder getBaseUrlToUse(UriComponentsBuilder baseUrl) {
 		if (baseUrl != null) {
-			return (UriComponentsBuilder) baseUrl.clone();
+			return baseUrl.cloneBuilder();
 		}
 		else {
 			return ServletUriComponentsBuilder.fromCurrentServletMapping();
