49ad8c90b4f2212b17ab0a77cba190b7edbe16d3
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2016-03-27 17:32:12 +0300

    Added test for null queryString if parameters absent

--- spring-test/src/test/java/org/springframework/test/web/servlet/request/MockHttpServletRequestBuilderTests.java
+++ spring-test/src/test/java/org/springframework/test/web/servlet/request/MockHttpServletRequestBuilderTests.java
@@ -214,6 +214,12 @@ public class MockHttpServletRequestBuilderTests {
 	}
 
 	@Test
+	public void requestQueryStringIsNullIfThereIsNoParameters() throws Exception {
+		MockHttpServletRequest request = this.builder.buildRequest(this.servletContext);
+		assertNull(request.getQueryString());
+	}
+
+	@Test
 	public void requestParameterQueryString() throws Exception {
 		this.builder.param("foo", "1").param("bar", "2").param("baz", "3");
 
