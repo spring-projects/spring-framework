d303ac94dc5028cc069dd5d07c784339094cc859
Juergen Hoeller <jhoeller@pivotal.io>
2016-04-11 12:22:37 +0200

    Polishing

--- spring-web/src/main/java/org/springframework/http/client/Netty4ClientHttpResponse.java
+++ spring-web/src/main/java/org/springframework/http/client/Netty4ClientHttpResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,8 +28,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.util.Assert;
 
 /**
- * {@link org.springframework.http.client.ClientHttpResponse} implementation that uses
- * Netty 4 to execute requests.
+ * {@link org.springframework.http.client.ClientHttpResponse} implementation
+ * that uses Netty 4 to parse responses.
  *
  * @author Arjen Poutsma
  * @since 4.1.2
