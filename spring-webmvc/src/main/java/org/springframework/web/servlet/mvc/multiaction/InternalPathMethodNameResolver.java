9975f02e4d61ff98369aac7ca4c8c403b5119489
youmoo <13003062543@qq.com>
2016-04-12 18:37:41 +0800

    Method comment is wrong

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/multiaction/InternalPathMethodNameResolver.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/multiaction/InternalPathMethodNameResolver.java
@@ -99,7 +99,7 @@ public class InternalPathMethodNameResolver extends AbstractUrlMethodNameResolve
 
 	/**
 	 * Extract the handler method name from the given request URI.
-	 * Delegates to {@code WebUtils.extractViewNameFromUrlPath(String)}.
+	 * Delegates to {@code WebUtils.extractFilenameFromUrlPath(String)}.
 	 * @param uri the request URI (e.g. "/index.html")
 	 * @return the extracted URI filename (e.g. "index")
 	 * @see org.springframework.web.util.WebUtils#extractFilenameFromUrlPath
