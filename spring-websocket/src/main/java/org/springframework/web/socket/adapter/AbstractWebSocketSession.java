aafd46aeb34cbaaaaaf3630a31793204b04ed213
Venil Noronha <venil.noronha@gmail.com>
2016-04-14 18:32:50 +0530

    Remove isOpen assertion when sending WebSocket message
    Issue: SPR-14138

--- spring-websocket/src/main/java/org/springframework/web/socket/adapter/AbstractWebSocketSession.java
+++ spring-websocket/src/main/java/org/springframework/web/socket/adapter/AbstractWebSocketSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -95,7 +95,6 @@ public abstract class AbstractWebSocketSession<T> implements NativeWebSocketSess
 	public final void sendMessage(WebSocketMessage<?> message) throws IOException {
 
 		checkNativeSessionInitialized();
-		Assert.isTrue(isOpen(), "Cannot send message after connection closed.");
 
 		if (logger.isTraceEnabled()) {
 			logger.trace("Sending " + message + ", " + this);
