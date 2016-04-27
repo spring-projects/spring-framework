6fbaa7dbb4a20710ad7eee8232df19c8414e0e72
Stephane Maldini <smaldini@pivotal.io>
2016-04-27 22:08:51 +0100

    compile time issue

--- spring-web-reactive/src/main/java/org/springframework/web/server/session/DefaultWebSessionManager.java
+++ spring-web-reactive/src/main/java/org/springframework/web/server/session/DefaultWebSessionManager.java
@@ -110,7 +110,7 @@ public class DefaultWebSessionManager implements WebSessionManager {
 	protected Mono<WebSession> validateSession(ServerWebExchange exchange, WebSession session) {
 		if (session.isExpired()) {
 			this.sessionIdResolver.setSessionId(exchange, "");
-			return this.sessionStore.removeSession(session.getId()).then(Mono::empty);
+			return this.sessionStore.removeSession(session.getId()).cast(WebSession.class);
 		}
 		else {
 			return Mono.just(session);
