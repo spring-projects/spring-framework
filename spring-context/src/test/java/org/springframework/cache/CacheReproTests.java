fdb31cd715f4a8811fbbe0b541b54ee5f65fa8df
Juergen Hoeller <jhoeller@pivotal.io>
2016-04-29 10:56:26 +0200

    Check actual cache value for unwrapped Optional
    Issue: SPR-14230

--- spring-context/src/test/java/org/springframework/cache/CacheReproTests.java
+++ spring-context/src/test/java/org/springframework/cache/CacheReproTests.java
@@ -137,10 +137,17 @@ public class CacheReproTests {
 	public void spr14230AdaptsToOptional() {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Spr14230Config.class);
 		Spr14230Service bean = context.getBean(Spr14230Service.class);
+		Cache cache = context.getBean(CacheManager.class).getCache("itemCache");
 
 		TestBean tb = new TestBean("tb1");
 		bean.insertItem(tb);
 		assertSame(tb, bean.findById("tb1").get());
+		assertSame(tb, cache.get("tb1").get());
+
+		cache.clear();
+		TestBean tb2 = bean.findById("tb1").get();
+		assertNotSame(tb, tb2);
+		assertSame(tb2, cache.get("tb1").get());
 	}
 
 
