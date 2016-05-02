e45d33f9de595d7348f3b4695960cb26bd9255c0
Juergen Hoeller <jhoeller@pivotal.io>
2016-05-02 12:57:30 +0200

    DefaultMessageListenerContainer immediately invokes stop callback when not running
    Issue: SPR-14233

--- spring-jms/src/main/java/org/springframework/jms/listener/DefaultMessageListenerContainer.java
+++ spring-jms/src/main/java/org/springframework/jms/listener/DefaultMessageListenerContainer.java
@@ -614,6 +614,12 @@ public class DefaultMessageListenerContainer extends AbstractPollingMessageListe
 	@Override
 	public void stop(Runnable callback) throws JmsException {
 		synchronized (this.lifecycleMonitor) {
+			if (!isRunning() || this.stopCallback != null) {
+				// Not started, already stopped, or previous stop attempt in progress
+				// -> return immediately, no stop process to control anymore.
+				callback.run();
+				return;
+			}
 			this.stopCallback = callback;
 		}
 		stop();
