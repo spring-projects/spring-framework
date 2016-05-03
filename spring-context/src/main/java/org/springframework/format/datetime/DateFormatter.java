e1e3384b99af9cb6993476bce0d97579752aa062
Fabian Raetz <fabian.raetz@gmail.com>
2016-05-03 07:33:11 +0200

    The ISO 8601 time zone letter is X instead of Z
    Use it for ISO.TIME as well.

--- spring-context/src/main/java/org/springframework/format/datetime/DateFormatter.java
+++ spring-context/src/main/java/org/springframework/format/datetime/DateFormatter.java
@@ -48,7 +48,7 @@ public class DateFormatter implements Formatter<Date> {
 	static {
 		Map<ISO, String> formats = new HashMap<DateTimeFormat.ISO, String>(4);
 		formats.put(ISO.DATE, "yyyy-MM-dd");
-		formats.put(ISO.TIME, "HH:mm:ss.SSSZ");
+		formats.put(ISO.TIME, "HH:mm:ss.SSSX");
 		formats.put(ISO.DATE_TIME, "yyyy-MM-dd'T'HH:mm:ss.SSSX");
 		ISO_PATTERNS = Collections.unmodifiableMap(formats);
 	}
