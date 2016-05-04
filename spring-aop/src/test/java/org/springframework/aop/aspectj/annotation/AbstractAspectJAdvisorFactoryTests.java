e6e3ca3e96d6272094c9e41354a372dbd74be80f
Juergen Hoeller <jhoeller@pivotal.io>
2016-05-04 09:03:28 +0200

    LazySingletonAspectInstanceFactoryDecorator uses shared singleton mutex
    Issue: SPR-14241

--- spring-aop/src/test/java/org/springframework/aop/aspectj/annotation/AbstractAspectJAdvisorFactoryTests.java
+++ spring-aop/src/test/java/org/springframework/aop/aspectj/annotation/AbstractAspectJAdvisorFactoryTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -691,6 +691,11 @@ public abstract class AbstractAspectJAdvisorFactoryTests {
 		@Override
 		public AspectMetadata getAspectMetadata() {
 			return new AspectMetadata(PerTypeWithinAspect.class, "perTypeWithin");
+		}
+
+		@Override
+		public Object getAspectCreationMutex() {
+			return this;
 		}
 
 		@Override
