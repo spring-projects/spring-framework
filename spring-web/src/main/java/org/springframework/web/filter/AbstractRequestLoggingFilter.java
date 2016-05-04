25cf96f6df4ff5fc97278fcbb0853c21b4b6a45c
Juergen Hoeller <jhoeller@pivotal.io>
2016-05-04 18:17:06 +0200

    AbstractRequestLoggingFilter ignores non-available query string
    Issue: SPR-14244
    (cherry picked from commit 0f2bc3f)

--- spring-web/src/main/java/org/springframework/web/filter/AbstractRequestLoggingFilter.java
+++ spring-web/src/main/java/org/springframework/web/filter/AbstractRequestLoggingFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -258,7 +258,10 @@ public abstract class AbstractRequestLoggingFilter extends OncePerRequestFilter 
 		msg.append(prefix);
 		msg.append("uri=").append(request.getRequestURI());
 		if (isIncludeQueryString()) {
-			msg.append('?').append(request.getQueryString());
+			String queryString = request.getQueryString();
+			if (queryString != null) {
+				msg.append('?').append(queryString);
+			}
 		}
 		if (isIncludeClientInfo()) {
 			String client = request.getRemoteAddr();
