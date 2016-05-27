07b5763324b0887455c560506f51110b8fc2a71a
Juergen Hoeller <jhoeller@pivotal.io>
2016-05-27 18:24:49 +0200

    SpringTilesContainerFactory uses Tiles 3's createDecoratedContainer
    Issue: SPR-14311
    (cherry picked from commit 2d85acc)

--- spring-webmvc/src/main/java/org/springframework/web/servlet/view/tiles3/TilesConfigurer.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/view/tiles3/TilesConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -293,9 +293,8 @@ public class TilesConfigurer implements ServletContextAware, InitializingBean, D
 	private class SpringTilesContainerFactory extends BasicTilesContainerFactory {
 
 		@Override
-		public TilesContainer createContainer(ApplicationContext context) {
-			TilesContainer container = super.createContainer(context);
-			return (useMutableTilesContainer ? new CachingTilesContainer(container) : container);
+		protected TilesContainer createDecoratedContainer(TilesContainer originalContainer, ApplicationContext context) {
+			return (useMutableTilesContainer ? new CachingTilesContainer(originalContainer) : originalContainer);
 		}
 
 		@Override
