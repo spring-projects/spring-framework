d467dc2a052531fb5d6fdecd155b0fc8af2d2097
Juergen Hoeller <jhoeller@pivotal.io>
2016-06-04 00:20:46 +0200

    Avoid double getFlushMode call

--- spring-orm-hibernate5/src/main/java/org/springframework/orm/hibernate5/HibernateTransactionManager.java
+++ spring-orm-hibernate5/src/main/java/org/springframework/orm/hibernate5/HibernateTransactionManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -477,7 +477,7 @@ public class HibernateTransactionManager extends AbstractPlatformTransactionMana
 			if (!definition.isReadOnly() && !txObject.isNewSession()) {
 				// We need AUTO or COMMIT for a non-read-only transaction.
 				FlushMode flushMode = session.getFlushMode();
-				if (session.getFlushMode().equals(FlushMode.MANUAL)) {
+				if (FlushMode.MANUAL.equals(flushMode)) {
 					session.setFlushMode(FlushMode.AUTO);
 					txObject.getSessionHolder().setPreviousFlushMode(flushMode);
 				}
