70017070286e96706040ff92c2b6dab382254f7c
Juergen Hoeller <jhoeller@pivotal.io>
2016-06-07 16:49:23 +0200

    CorsFilter asserts presence of CorsConfigurationSource (cherry picked from commit 31aed61)

--- spring-web/src/main/java/org/springframework/web/cors/CorsConfigurationSource.java
+++ spring-web/src/main/java/org/springframework/web/cors/CorsConfigurationSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,6 +29,7 @@ public interface CorsConfigurationSource {
 
 	/**
 	 * Return a {@link CorsConfiguration} based on the incoming request.
+	 * @return the associated {@link CorsConfiguration}, or {@code null} if none
 	 */
 	CorsConfiguration getCorsConfiguration(HttpServletRequest request);
 
