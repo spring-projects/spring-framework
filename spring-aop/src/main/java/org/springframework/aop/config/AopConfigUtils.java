8cb9d5ebae0d47f00f4e4f71fd405d42441831e3
Juergen Hoeller <jhoeller@pivotal.io>
2016-06-20 13:50:04 +0200

    EnableAspectJAutoProxy features exposeProxy flag (analogous to XML namespace)
    Issue: SPR-10454

--- spring-aop/src/main/java/org/springframework/aop/config/AopConfigUtils.java
+++ spring-aop/src/main/java/org/springframework/aop/config/AopConfigUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -97,7 +97,7 @@ public abstract class AopConfigUtils {
 		}
 	}
 
-	static void forceAutoProxyCreatorToExposeProxy(BeanDefinitionRegistry registry) {
+	public static void forceAutoProxyCreatorToExposeProxy(BeanDefinitionRegistry registry) {
 		if (registry.containsBeanDefinition(AUTO_PROXY_CREATOR_BEAN_NAME)) {
 			BeanDefinition definition = registry.getBeanDefinition(AUTO_PROXY_CREATOR_BEAN_NAME);
 			definition.getPropertyValues().add("exposeProxy", Boolean.TRUE);
