cc47f92473d1cf2490b4bab38f2c5775eb3df57d
jpradhan <jayaram_pradhan@intuit.com>
2016-06-25 22:15:37 +0530

    Fixed #DATAREDIS-527 Added ErrorHandler to the AspectjCachingConfiguration

--- spring-aspects/src/main/java/org/springframework/cache/aspectj/AspectJCachingConfiguration.java
+++ spring-aspects/src/main/java/org/springframework/cache/aspectj/AspectJCachingConfiguration.java
@@ -39,11 +39,17 @@ public class AspectJCachingConfiguration extends AbstractCachingConfiguration {
 	@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 	public AnnotationCacheAspect cacheAspect() {
 		AnnotationCacheAspect cacheAspect = AnnotationCacheAspect.aspectOf();
+        if (this.cacheResolver != null) {
+            cacheAspect.setCacheResolver(this.cacheResolver);
+        }
 		if (this.cacheManager != null) {
 			cacheAspect.setCacheManager(this.cacheManager);
 		}
 		if (this.keyGenerator != null) {
 			cacheAspect.setKeyGenerator(this.keyGenerator);
+		}
+		if (this.errorHandler != null) {
+            cacheAspect.setErrorHandler(this.errorHandler);
 		}
 		return cacheAspect;
 	}
