8efe39070bcbfc928c74d446a4cda746cee96dd3
Caleb Cushing <xenoterracide@gmail.com>
2016-07-18 15:18:18 -0500

    getDeclared
    Having a problem with this method because it's not finding my inner class constructor
    
    here's my test
    
    ```
    	@Test
    	public void testInstantiate() {
    
    		Constructor<MyClassBar> ctor = ClassUtils.getConstructorIfAvailable( MyClassBar.class, MyInterface.class );
    		assertThat( ctor, notNullValue() );
            }
    
    private interface MyInterface {
    
    	}
    
    	private static class MyClassFoo implements MyInterface {
    
    	}
    
    	private static class MyClassBar {
    
    		private final MyInterface myInterface;
    
    		MyClassBar( final MyInterface myInterface )
    		{
    			this.myInterface = myInterface;
    		}
    
    		MyInterface getMyInterface()
    		{
    			return myInterface;
    		}
    	}```
    
    I can work put more work into this if desired.

--- spring-core/src/main/java/org/springframework/util/ClassUtils.java
+++ spring-core/src/main/java/org/springframework/util/ClassUtils.java
@@ -583,7 +583,7 @@ public abstract class ClassUtils {
 	public static <T> Constructor<T> getConstructorIfAvailable(Class<T> clazz, Class<?>... paramTypes) {
 		Assert.notNull(clazz, "Class must not be null");
 		try {
-			return clazz.getConstructor(paramTypes);
+			return clazz.getDeclaredConstructor(paramTypes);
 		}
 		catch (NoSuchMethodException ex) {
 			return null;
