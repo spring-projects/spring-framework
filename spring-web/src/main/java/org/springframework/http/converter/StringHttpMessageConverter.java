0c3c23db814a7ff747539cbaa47cb35e745f0874
niaoge <78493244@qq.com>
2016-07-19 22:48:51 +0800

    StringHttpMessageConverter improve getContentLength

--- spring-web/src/main/java/org/springframework/http/converter/StringHttpMessageConverter.java
+++ spring-web/src/main/java/org/springframework/http/converter/StringHttpMessageConverter.java
@@ -17,7 +17,6 @@
 package org.springframework.http.converter;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
@@ -88,13 +87,7 @@ public class StringHttpMessageConverter extends AbstractHttpMessageConverter<Str
 	@Override
 	protected Long getContentLength(String str, MediaType contentType) {
 		Charset charset = getContentTypeCharset(contentType);
-		try {
-			return (long) str.getBytes(charset.name()).length;
-		}
-		catch (UnsupportedEncodingException ex) {
-			// should not occur
-			throw new IllegalStateException(ex);
-		}
+		return (long) str.getBytes(charset).length;
 	}
 
 	@Override
