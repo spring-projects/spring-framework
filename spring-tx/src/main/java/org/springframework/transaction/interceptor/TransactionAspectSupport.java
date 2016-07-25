e4def2f55d29b9aeac559f23a8b9872c58cdbf65
Juergen Hoeller <jhoeller@pivotal.io>
2016-07-26 00:54:53 +0200

    Aligned ConcurrentMap declaration with 4.3.x branch

--- spring-tx/src/main/java/org/springframework/transaction/interceptor/TransactionAspectSupport.java
+++ spring-tx/src/main/java/org/springframework/transaction/interceptor/TransactionAspectSupport.java
@@ -17,9 +17,9 @@
 package org.springframework.transaction.interceptor;
 
 import java.lang.reflect.Method;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -86,7 +86,7 @@ public abstract class TransactionAspectSupport implements BeanFactoryAware, Init
 			new NamedThreadLocal<>("Current aspect-driven transaction");
 
 
-	private final Map<Object, PlatformTransactionManager> transactionManagerCache =
+	private final ConcurrentMap<Object, PlatformTransactionManager> transactionManagerCache =
 			new ConcurrentHashMap<>();
 
 	/**
