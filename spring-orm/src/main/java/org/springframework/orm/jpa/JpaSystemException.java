9ee1dc79fda2b24e92b1ccb91b0fad33984d1d9e
Juergen Hoeller <jhoeller@pivotal.io>
2016-07-26 01:29:51 +0200

    Deprecate JpaSystemException(PersistenceException) constructor to prepare for removal in Spring 5
    Issue: SPR-13827

--- spring-orm/src/main/java/org/springframework/orm/jpa/JpaSystemException.java
+++ spring-orm/src/main/java/org/springframework/orm/jpa/JpaSystemException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,7 @@ import org.springframework.dao.UncategorizedDataAccessException;
 @SuppressWarnings("serial")
 public class JpaSystemException extends UncategorizedDataAccessException {
 
+	@Deprecated
 	public JpaSystemException(PersistenceException ex) {
 		super(ex.getMessage(), ex);
 	}
