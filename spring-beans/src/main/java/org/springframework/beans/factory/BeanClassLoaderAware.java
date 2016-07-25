bfa48d551d62f68de6c803f67e34be6d9ad37a78
Stephane Nicoll <snicoll@pivotal.io>
2016-07-25 12:34:42 +0200

    Polish contribution
    Closes gh-1112

--- spring-beans/src/main/java/org/springframework/beans/factory/BeanClassLoaderAware.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/BeanClassLoaderAware.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ package org.springframework.beans.factory;
 
 /**
  * Callback that allows a bean to be aware of the bean
- * {@link ClassLoader class loader}; that is, the class loader is used by the
+ * {@link ClassLoader class loader}; that is, the class loader used by the
  * present bean factory to load bean classes.
  *
  * <p>This is mainly intended to be implemented by framework classes which
--- spring-beans/src/main/java/org/springframework/beans/factory/InitializingBean.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/InitializingBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,8 @@ package org.springframework.beans.factory;
  *
  * <p>An alternative to implementing InitializingBean is specifying a custom
  * init-method, for example in an XML bean definition.
- * For a list of all bean lifecycle methods, see the {@link BeanFactory BeanFactory javadocs}.
+ * For a list of all bean lifecycle methods, see the
+ * {@link BeanFactory BeanFactory javadocs}.
  *
  * @author Rod Johnson
  * @see BeanNameAware
