fbf3807af1f7e01462bb6d34ac7dac5e72d3a2d4
Juergen Hoeller <jhoeller@pivotal.io>
2016-07-27 16:21:55 +0200

    Polishing (cherry picked from commit 5db4495)

--- spring-context/src/test/java/org/springframework/context/annotation/AsmCircularImportDetectionTests.java
+++ spring-context/src/test/java/org/springframework/context/annotation/AsmCircularImportDetectionTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,12 +23,8 @@ import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
 
 /**
- * Unit test proving that ASM-based {@link ConfigurationClassParser} correctly detects circular
- * use of the {@link Import @Import} annotation.
- *
- * <p>While this test is the only subclass of {@link AbstractCircularImportDetectionTests},
- * the hierarchy remains in place in case a JDT-based ConfigurationParser implementation
- * needs to be developed.
+ * Unit test proving that ASM-based {@link ConfigurationClassParser} correctly detects
+ * circular use of the {@link Import @Import} annotation.
  *
  * @author Chris Beams
  */
