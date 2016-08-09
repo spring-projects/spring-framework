4627545a3b4bccbaf844a065be000ada4c82968a
Juergen Hoeller <jhoeller@pivotal.io>
2016-08-09 10:44:10 +0200

    Polishing (cherry picked from commit eae079a)

--- spring-test/src/test/java/org/springframework/test/web/client/response/ResponseCreatorsTests.java
+++ spring-test/src/test/java/org/springframework/test/web/client/response/ResponseCreatorsTests.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.test.web.client.response;
 
 import java.net.URI;
