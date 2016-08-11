b2a0b75f7e8771cf19535789ef13ce4dc5b4a516
Juergen Hoeller <jhoeller@pivotal.io>
2016-08-11 22:56:14 +0200

    Avoid stack overflow in case of chained factory-bean references to FactoryBean class
    Issue: SPR-14551
    (cherry picked from commit e23ac03)

--- spring-orm/src/test/java/org/springframework/orm/jpa/AbstractEntityManagerFactoryIntegrationTests.java
+++ spring-orm/src/test/java/org/springframework/orm/jpa/AbstractEntityManagerFactoryIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,7 +90,7 @@ public abstract class AbstractEntityManagerFactoryIntegrationTests extends org.s
 	}
 
 
-	static enum Provider {
+	enum Provider {
 
 		ECLIPSELINK, HIBERNATE, OPENJPA
 	}
