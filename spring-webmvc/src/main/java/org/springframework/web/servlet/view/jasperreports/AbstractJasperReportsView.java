c926ec477a6aabad1b34c42f2d5857ab416d8b0c
Juergen Hoeller <jhoeller@pivotal.io>
2016-08-17 17:40:26 +0200

    Polishing

--- spring-webmvc/src/main/java/org/springframework/web/servlet/view/jasperreports/AbstractJasperReportsView.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/view/jasperreports/AbstractJasperReportsView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -390,7 +390,7 @@ public abstract class AbstractJasperReportsView extends AbstractUrlBasedView {
 			else if (str.length() > 0 && Character.isDigit(str.charAt(0))) {
 				// Looks like a number... let's try.
 				try {
-					return new Integer(str);
+					return Integer.valueOf(str);
 				}
 				catch (NumberFormatException ex) {
 					// OK, then let's keep it as a String value.
