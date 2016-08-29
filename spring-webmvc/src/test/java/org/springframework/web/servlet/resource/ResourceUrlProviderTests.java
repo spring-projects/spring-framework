3b95e0b6e0db3a08f3ca0d857ef1a3b74142500f
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2016-08-29 16:27:24 -0400

    Fix media type regression in resource handling
    Issue: SPR-14577

--- spring-webmvc/src/test/java/org/springframework/web/servlet/resource/ResourceUrlProviderTests.java
+++ spring-webmvc/src/test/java/org/springframework/web/servlet/resource/ResourceUrlProviderTests.java
@@ -58,6 +58,7 @@ public class ResourceUrlProviderTests {
 	public void setUp() throws Exception {
 		this.locations.add(new ClassPathResource("test/", getClass()));
 		this.locations.add(new ClassPathResource("testalternatepath/", getClass()));
+		this.handler.setServletContext(new MockServletContext());
 		this.handler.setLocations(locations);
 		this.handler.afterPropertiesSet();
 		this.handlerMap.put("/resources/**", this.handler);
