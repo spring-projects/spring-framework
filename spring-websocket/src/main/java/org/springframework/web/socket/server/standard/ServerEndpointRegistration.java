acbb2544bf90a85c9d42f9e2c71f750e6499cd53
Juergen Hoeller <jhoeller@pivotal.io>
2016-08-30 23:57:11 +0200

    Polishing (cherry picked from commit 4ef428d)

--- spring-websocket/src/main/java/org/springframework/web/socket/server/standard/ServerEndpointRegistration.java
+++ spring-websocket/src/main/java/org/springframework/web/socket/server/standard/ServerEndpointRegistration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -107,8 +107,7 @@ public class ServerEndpointRegistration extends ServerEndpointConfig.Configurato
 
 	@Override
 	public Class<? extends Endpoint> getEndpointClass() {
-		return (this.endpoint != null) ?
-				this.endpoint.getClass() : ((Class<? extends Endpoint>) this.endpointProvider.getHandlerType());
+		return (this.endpoint != null ? this.endpoint.getClass() : this.endpointProvider.getHandlerType());
 	}
 
 	public Endpoint getEndpoint() {
