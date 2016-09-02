a1ac51256a96360c63e26a556cd84133f840dfb8
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2016-09-02 12:14:09 -0400

    Add MockServerHttpRequest/Response to spring-test
    Issue: SPR-14421

--- spring-test/src/main/java/org/springframework/mock/http/server/reactive/MockServerHttpRequest.java
+++ spring-test/src/main/java/org/springframework/mock/http/server/reactive/MockServerHttpRequest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.mock.http.server.reactive.test;
+package org.springframework.mock.http.server.reactive;
 
 import java.net.URI;
 import java.nio.ByteBuffer;
