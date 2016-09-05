e822e4cbe87b09b901eb2363bc884a635a96a01e
Sam Brannen <sam@sambrannen.com>
2016-09-05 13:39:21 +0200

    Improve exception msg for inactive test ApplicationContext
    This commit improves the exception message thrown when a test's
    ApplicationContext is no longer active by explaining that the cause
    may be due to parallel test execution.
    
    Issue: SPR-5863

--- spring-test/src/main/java/org/springframework/test/context/support/DefaultTestContext.java
+++ spring-test/src/main/java/org/springframework/test/context/support/DefaultTestContext.java
@@ -103,8 +103,13 @@ public class DefaultTestContext implements TestContext {
 		if (context instanceof ConfigurableApplicationContext) {
 			@SuppressWarnings("resource")
 			ConfigurableApplicationContext cac = (ConfigurableApplicationContext) context;
-			Assert.state(cac.isActive(), () -> "The ApplicationContext loaded for [" + mergedContextConfiguration
-					+ "] is not active. Ensure that the context has not been closed programmatically.");
+			Assert.state(cac.isActive(), () ->
+					"The ApplicationContext loaded for [" + mergedContextConfiguration +
+					"] is not active. This may be due to one of the following reasons: " +
+					"1) the context was closed programmatically by user code; " +
+					"2) the context was closed during parallel test execution either " +
+					"according to @DirtiesContext semantics or due to automatic eviction " +
+					"from the ContextCache due to a maximum cache size policy.");
 		}
 		return context;
 	}
