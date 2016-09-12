d8f4d37624fc38a8c28d8e427f205b395e290f15
Arjen Poutsma <apoutsma@pivotal.io>
2016-09-12 19:23:15 +0200

    Fixed bug in reading Flux from Channel
    Fixed bug where the returned Flux from DataBufferUtils.read() would be
    completed prematurely if the channel was not ready to read, but did
    not reach the end of the file either.

--- spring-core/src/main/java/org/springframework/core/io/buffer/DataBufferUtils.java
+++ spring-core/src/main/java/org/springframework/core/io/buffer/DataBufferUtils.java
@@ -164,7 +164,7 @@ public abstract class DataBufferUtils {
 			try {
 				ByteBuffer byteBuffer = ByteBuffer.allocate(chunkSize);
 				int read;
-				if ((read = channel.read(byteBuffer)) > 0) {
+				if ((read = channel.read(byteBuffer)) >= 0) {
 					byteBuffer.flip();
 					boolean release = true;
 					DataBuffer dataBuffer = this.dataBufferFactory.allocateBuffer(read);
