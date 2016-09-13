cfd16f9c943ed978d44409383b330c58e8f89a10
Juergen Hoeller <jhoeller@pivotal.io>
2016-09-13 23:14:33 +0200

    ModelAndViewResolver declares handlerType as Class<?>
    Issue: SPR-7860

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/annotation/ModelAndViewResolver.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/annotation/ModelAndViewResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,39 +23,36 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * SPI for resolving custom return values from a specific handler method. Typically implemented to detect special return
- * types, resolving well-known result values for them.
+ * SPI for resolving custom return values from a specific handler method.
+ * Typically implemented to detect special return types, resolving
+ * well-known result values for them.
  *
  * <p>A typical implementation could look like as follows:
  *
  * <pre class="code">
  * public class MyModelAndViewResolver implements ModelAndViewResolver {
  *
- *   public ModelAndView resolveModelAndView(Method handlerMethod,
- *		   			Class handlerType,
- *		   			Object returnValue,
- *		 			ExtendedModelMap implicitModel,
- *		 			NativeWebRequest webRequest) {
- *     if (returnValue instanceof MySpecialRetVal.class)) {
- *       return new MySpecialRetVal(returnValue);
+ *     public ModelAndView resolveModelAndView(Method handlerMethod, Class handlerType,
+ *             Object returnValue, ExtendedModelMap implicitModel, NativeWebRequest webRequest) {
+ *         if (returnValue instanceof MySpecialRetVal.class)) {
+ *             return new MySpecialRetVal(returnValue);
+ *         }
+ *         return UNRESOLVED;
  *     }
- *     return UNRESOLVED;
- *   }
  * }</pre>
  *
  * @author Arjen Poutsma
- * @see org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter#setCustomModelAndViewResolvers
- * @see org.springframework.web.portlet.mvc.annotation.AnnotationMethodHandlerAdapter#setCustomModelAndViewResolvers
  * @since 3.0
  */
 public interface ModelAndViewResolver {
 
-	/** Marker to be returned when the resolver does not know how to handle the given method parameter. */
+	/**
+	 * Marker to be returned when the resolver does not know how to handle the given method parameter.
+	 */
 	ModelAndView UNRESOLVED = new ModelAndView();
 
-	ModelAndView resolveModelAndView(Method handlerMethod,
-			Class handlerType,
-			Object returnValue,
-			ExtendedModelMap implicitModel,
-			NativeWebRequest webRequest);
+
+	ModelAndView resolveModelAndView(Method handlerMethod, Class<?> handlerType, Object returnValue,
+			ExtendedModelMap implicitModel, NativeWebRequest webRequest);
+
 }
