6577faa2da8b94997eb3de53d4bc575388b25e00
Juergen Hoeller <jhoeller@pivotal.io>
2016-09-26 16:33:29 +0200

    StompEncoder performance improvement plus related polishing
    Issue: SPR-14747
    (cherry picked from commit 6c764f6)

--- spring-messaging/src/main/java/org/springframework/messaging/simp/stomp/StompDecoder.java
+++ spring-messaging/src/main/java/org/springframework/messaging/simp/stomp/StompDecoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,7 +52,6 @@ public class StompDecoder {
 	static final byte[] HEARTBEAT_PAYLOAD = new byte[] {'\n'};
 
 	private static final Log logger = LogFactory.getLog(StompDecoder.class);
-
 
 	private MessageHeaderInitializer headerInitializer;
 
