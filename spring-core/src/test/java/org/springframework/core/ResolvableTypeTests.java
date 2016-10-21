be187babf9d00c5145bd43f9300eadf472a9af87
Juergen Hoeller <jhoeller@pivotal.io>
2016-10-21 12:26:06 +0200

    ResolvableType equals/hashCode does not consider TypeProvider source
    Issue: SPR-14826

--- spring-core/src/test/java/org/springframework/core/ResolvableTypeTests.java
+++ spring-core/src/test/java/org/springframework/core/ResolvableTypeTests.java
@@ -175,6 +175,14 @@ public class ResolvableTypeTests {
 		ResolvableType type = ResolvableType.forField(field);
 		assertThat(type.getType(), equalTo(field.getGenericType()));
 		assertThat(type.resolve(), equalTo((Class) List.class));
+
+		Field field2 = Fields.class.getDeclaredField("otherPrivateField");
+		ResolvableType type2 = ResolvableType.forField(field2);
+		assertThat(type2.getType(), equalTo(field2.getGenericType()));
+		assertThat(type2.resolve(), equalTo((Class) List.class));
+
+		assertEquals(type, type2);
+		assertEquals(type.hashCode(), type2.hashCode());
 	}
 
 	@Test
@@ -1383,6 +1391,9 @@ public class ResolvableTypeTests {
 
 		@SuppressWarnings("unused")
 		private List<String> privateField;
+
+		@SuppressWarnings("unused")
+		private List<String> otherPrivateField;
 
 		public Map<Map<String, Integer>, Map<Byte, Long>> nested;
 
