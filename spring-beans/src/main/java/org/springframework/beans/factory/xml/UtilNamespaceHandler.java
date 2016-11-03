9e67ba83a5b6511627f10e32f578a95f913697d5
Juergen Hoeller <jhoeller@pivotal.io>
2016-11-03 14:33:48 +0100

    Consistent support for early placeholder resolution in properties locations
    Issue: SPR-10502
    (cherry picked from commit 8053fef)

--- spring-beans/src/main/java/org/springframework/beans/factory/xml/UtilNamespaceHandler.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/xml/UtilNamespaceHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -200,6 +200,7 @@ public class UtilNamespaceHandler extends NamespaceHandlerSupport {
 
 			String location = element.getAttribute("location");
 			if (StringUtils.hasLength(location)) {
+				location = parserContext.getReaderContext().getEnvironment().resolvePlaceholders(location);
 				String[] locations = StringUtils.commaDelimitedListToStringArray(location);
 				builder.addPropertyValue("locations", locations);
 			}
