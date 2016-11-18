2e994f7dc38b5520555720dac215064949ccfabf
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-11-18 13:46:21 +0100

    Add RedirectView to Spring Web Reactive
    Main differences with the Spring MVC original implementation:
     - Default redirect HTTP code is 303 See Other since we can assume
       all HTTP clients support HTTP 1.1 in 2016
     - No more http10Compatible property, use statusCode instead
     - By default the redirect is relative to the context path
     - A builder allow to set various properties if needed
     - In UrlBasedViewResolver, a Function<String, RedirectView>
       redirectViewProvider property allows to customize RedirectView
       instances in a flexible way
    
    Issue: SPR-14534

--- spring-web-reactive/src/main/java/org/springframework/web/reactive/result/view/UrlBasedViewResolver.java
+++ spring-web-reactive/src/main/java/org/springframework/web/reactive/result/view/UrlBasedViewResolver.java
@@ -17,6 +17,7 @@
 package org.springframework.web.reactive.result.view;
 
 import java.util.Locale;
+import java.util.function.Function;
 
 import reactor.core.publisher.Mono;
 
@@ -56,6 +57,15 @@ import org.springframework.util.PatternMatchUtils;
  */
 public class UrlBasedViewResolver extends ViewResolverSupport implements ViewResolver, InitializingBean {
 
+	/**
+	 * Prefix for special view names that specify a redirect URL (usually
+	 * to a controller after a form has been submitted and processed).
+	 * Such view names will not be resolved in the configured default
+	 * way but rather be treated as special shortcut.
+	 */
+	public static final String REDIRECT_URL_PREFIX = "redirect:";
+
+
 	private Class<?> viewClass;
 
 	private String prefix = "";
@@ -63,6 +73,8 @@ public class UrlBasedViewResolver extends ViewResolverSupport implements ViewRes
 	private String suffix = "";
 
 	private String[] viewNames;
+
+	private Function<String, RedirectView> redirectViewProvider = url -> new RedirectView(url);
 
 
 	/**
@@ -141,6 +153,13 @@ public class UrlBasedViewResolver extends ViewResolverSupport implements ViewRes
 		return this.viewNames;
 	}
 
+	/**
+	 * URL based {@link RedirectView} provider which can be used to provide, for example,
+	 * redirect views with a custom default status code.
+	 */
+	public void setRedirectViewProvider(Function<String, RedirectView> redirectViewProvider) {
+		this.redirectViewProvider = redirectViewProvider;
+	}
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
@@ -149,13 +168,19 @@ public class UrlBasedViewResolver extends ViewResolverSupport implements ViewRes
 		}
 	}
 
-
 	@Override
 	public Mono<View> resolveViewName(String viewName, Locale locale) {
 		if (!canHandle(viewName, locale)) {
 			return Mono.empty();
 		}
-		AbstractUrlBasedView urlBasedView = createUrlBasedView(viewName);
+		AbstractUrlBasedView urlBasedView;
+		if (viewName.startsWith(REDIRECT_URL_PREFIX)) {
+			String redirectUrl = viewName.substring(REDIRECT_URL_PREFIX.length());
+			urlBasedView = this.redirectViewProvider.apply(redirectUrl);
+		}
+		else {
+			urlBasedView = createUrlBasedView(viewName);
+		}
 		View view = applyLifecycleMethods(viewName, urlBasedView);
 		try {
 			return (urlBasedView.checkResourceExists(locale) ? Mono.just(view) : Mono.empty());
