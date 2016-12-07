827b7eddef8d00141c95b33a054c1fc606ee4624
bedrin <dmitry.bedrin@gmail.com>
2016-12-08 01:29:48 +0300

    SPR-14988: Add consumes() attribute to @GetMapping annotation
    (cherry picked from commit 00e6ca4)

--- spring-webmvc/src/test/java/org/springframework/web/servlet/mvc/method/annotation/RequestMappingHandlerMappingTests.java
+++ spring-webmvc/src/test/java/org/springframework/web/servlet/mvc/method/annotation/RequestMappingHandlerMappingTests.java
@@ -22,6 +22,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
@@ -145,6 +146,17 @@ public class RequestMappingHandlerMappingTests {
 			info.getProducesCondition().getProducibleMediaTypes().iterator().next().toString());
 	}
 
+	/**
+	 * SPR-14988: Add consumes() attribute to @GetMapping annotation
+	 */
+	@Test
+	public void getMappingOverridesConsumesFromTypeLevelAnnotation() throws Exception {
+		RequestMappingInfo requestMappingInfo = assertComposedAnnotationMapping(RequestMethod.GET);
+
+		assertArrayEquals(new MediaType[]{MediaType.ALL},
+				new ArrayList<MediaType>(requestMappingInfo.getConsumesCondition().getConsumableMediaTypes()).toArray());
+	}
+
 	@Test
 	public void getMapping() throws Exception {
 		assertComposedAnnotationMapping(RequestMethod.GET);
@@ -199,6 +211,7 @@ public class RequestMappingHandlerMappingTests {
 
 
 	@Controller
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	static class ComposedAnnotationController {
 
 		@RequestMapping
@@ -209,7 +222,7 @@ public class RequestMappingHandlerMappingTests {
 		public void postJson() {
 		}
 
-		@GetMapping("/get")
+		@GetMapping(value = "/get", consumes = MediaType.ALL_VALUE)
 		public void get() {
 		}
 
