154ef8bf10973286438a91e948ff75c2f7cd923b
Juergen Hoeller <jhoeller@pivotal.io>
2016-12-20 19:57:52 +0100

    Polishing

--- spring-core/src/main/java/org/springframework/core/env/Environment.java
+++ spring-core/src/main/java/org/springframework/core/env/Environment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,8 +77,8 @@ public interface Environment extends PropertyResolver {
 	 * activated by setting {@linkplain AbstractEnvironment#ACTIVE_PROFILES_PROPERTY_NAME
 	 * "spring.profiles.active"} as a system property or by calling
 	 * {@link ConfigurableEnvironment#setActiveProfiles(String...)}.
-	 * <p>If no profiles have explicitly been specified as active, then any {@linkplain
-	 * #getDefaultProfiles() default profiles} will automatically be activated.
+	 * <p>If no profiles have explicitly been specified as active, then any
+	 * {@linkplain #getDefaultProfiles() default profiles} will automatically be activated.
 	 * @see #getDefaultProfiles
 	 * @see ConfigurableEnvironment#setActiveProfiles
 	 * @see AbstractEnvironment#ACTIVE_PROFILES_PROPERTY_NAME
