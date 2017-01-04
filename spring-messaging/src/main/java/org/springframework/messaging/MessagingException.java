9c3e17bc290890d82f6195ff7e2b93d68eee1b0f
Gary Russell <grussell@pivotal.io>
2017-01-04 08:55:19 -0500

    Implement MessagingException.toString()
    Issue: SPR-15091

--- spring-messaging/src/main/java/org/springframework/messaging/MessagingException.java
+++ spring-messaging/src/main/java/org/springframework/messaging/MessagingException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,6 +64,12 @@ public class MessagingException extends NestedRuntimeException {
 
 	public Message<?> getFailedMessage() {
 		return this.failedMessage;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + (this.failedMessage == null ? ""
+				: (", failedMessage=" + this.failedMessage));
 	}
 
 }
