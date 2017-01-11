b04b41b93b1bad9b80044a10c71de691be591d36
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-01-11 08:59:10 +0100

    Add bodyToMono/Flux<Foo>() Kotlin extensions
    Issue: SPR-15128

--- spring-web-reactive/src/main/kotlin/org/springframework/web/reactive/function/client/ClientResponseExtensions.kt
+++ spring-web-reactive/src/main/kotlin/org/springframework/web/reactive/function/client/ClientResponseExtensions.kt
@@ -14,11 +14,29 @@ import kotlin.reflect.KClass
 fun <T : Any> ClientResponse.bodyToMono(type: KClass<T>) : Mono<T> = bodyToMono(type.java)
 
 /**
+ * Extension for [ClientResponse.bodyToMono] providing a `bodyToMono<Foo>()` variant.
+ *
+ * @author Sebastien Deleuze
+ * @since 5.0
+ */
+inline fun <reified T : Any> ClientResponse.bodyToMono() = bodyToMono(T::class.java)
+
+
+/**
  * Extension for [ClientResponse.bodyToFlux] providing a [KClass] based variant.
  *
  * @author Sebastien Deleuze
  * @since 5.0
  */
 fun <T : Any> ClientResponse.bodyToFlux(type: KClass<T>) : Flux<T> = bodyToFlux(type.java)
+
+/**
+ * Extension for [ClientResponse.bodyToFlux] providing a `bodyToFlux<Foo>()` variant.
+ *
+ * @author Sebastien Deleuze
+ * @since 5.0
+ */
+inline fun <reified T : Any> ClientResponse.bodyToFlux() = bodyToFlux(T::class.java)
+
 
 
