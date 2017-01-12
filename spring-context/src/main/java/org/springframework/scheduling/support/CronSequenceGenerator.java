644b0b8bebaff53022bb58c3417980965920d9ef
Alexey Nikolaenko <echoAlexey@users.noreply.github.com>
2017-01-12 15:06:49 +0200

    Fix Javadoc for CronSequenceGenerator
    Closes gh-1294

--- spring-context/src/main/java/org/springframework/scheduling/support/CronSequenceGenerator.java
+++ spring-context/src/main/java/org/springframework/scheduling/support/CronSequenceGenerator.java
@@ -42,7 +42,7 @@ import org.springframework.util.StringUtils;
  * <li>"0 0 * * * *" = the top of every hour of every day.</li>
  * <li>"*&#47;10 * * * * *" = every ten seconds.</li>
  * <li>"0 0 8-10 * * *" = 8, 9 and 10 o'clock of every day.</li>
- * <li>"0 * 6,19 * * *" = 6:00 AM and 7:00 PM every day.</li>
+ * <li>"0 0 6,19 * * *" = 6:00 AM and 7:00 PM every day.</li>
  * <li>"0 0/30 8-10 * * *" = 8:00, 8:30, 9:00, 9:30 and 10 o'clock every day.</li>
  * <li>"0 0 9-17 * * MON-FRI" = on the hour nine-to-five weekdays</li>
  * <li>"0 0 0 25 12 ?" = every Christmas Day at midnight</li>
