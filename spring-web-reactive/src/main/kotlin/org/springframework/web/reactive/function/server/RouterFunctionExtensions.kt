02409f74b912c51a666c5afc664470da432f130c
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-01-13 16:57:47 +0100

    Polish Kotlin routing DSL
    This commit changes RouterFunctionDsl {...} (request)
    to route(request) {...}, remove uneeded methods, add
    missing ones and add tests useful to validate the
    DSL syntax and behavior.
    
    Issue: SPR-15065

--- spring-web-reactive/src/main/kotlin/org/springframework/web/reactive/function/server/RouterFunctionExtensions.kt
+++ spring-web-reactive/src/main/kotlin/org/springframework/web/reactive/function/server/RouterFunctionExtensions.kt
@@ -1,14 +1,16 @@
 package org.springframework.web.reactive.function.server
 
 import org.springframework.core.io.Resource
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
 import reactor.core.publisher.Mono
 
 /**
- * Provide a router DSL for [RouterFunctions] and [RouterFunction] in order to be able to
+ * Provide a routing DSL for [RouterFunctions] and [RouterFunction] in order to be able to
  * write idiomatic Kotlin code as below:
  *
  * * ```kotlin
- * fun route(request: ServerRequest) = RouterFunctionDsl {
+ * fun route(request: ServerRequest) = route(request) {
  * 		accept(TEXT_HTML).apply {
  * 			(GET("/user/") or GET("/users/")) { findAllView() }
  * 			GET("/user/{login}") { findViewById() }
@@ -18,16 +20,19 @@ import reactor.core.publisher.Mono
  * 			POST("/api/user/") { create() }
  * 			POST("/api/user/{login}") { findOne() }
  * 		}
- * 	} (request)
+ * 	}
  * ```
  *
  * @since 5.0
  * @author Sebastien Deleuze
  * @author Yevhenii Melnyk
  */
-class RouterFunctionDsl {
+fun RouterFunction<*>.route(request: ServerRequest, configure: RouterDsl.() -> Unit) =
+		RouterDsl().apply(configure).invoke(request)
 
-	val children = mutableListOf<RouterFunctionDsl>()
+class RouterDsl {
+
+	val children = mutableListOf<RouterDsl>()
 	val routes = mutableListOf<RouterFunction<ServerResponse>>()
 
 	operator fun RequestPredicate.invoke(f: () -> HandlerFunction<ServerResponse>) {
@@ -40,65 +45,62 @@ class RouterFunctionDsl {
 
 	operator fun RequestPredicate.not(): RequestPredicate = this.negate()
 
-	fun GET(pattern: String): RequestPredicate {
-		return RequestPredicates.GET(pattern)
-	}
-
 	fun GET(pattern: String, f: () -> HandlerFunction<ServerResponse>) {
 		routes += RouterFunctions.route(RequestPredicates.GET(pattern), f())
-	}
-
-	fun HEAD(pattern: String): RequestPredicate {
-		return RequestPredicates.HEAD(pattern)
 	}
 
 	fun HEAD(pattern: String, f: () -> HandlerFunction<ServerResponse>) {
 		routes += RouterFunctions.route(RequestPredicates.HEAD(pattern), f())
 	}
 
-	fun POST(pattern: String): RequestPredicate {
-		return RequestPredicates.POST(pattern)
-	}
-
 	fun POST(pattern: String, f: () -> HandlerFunction<ServerResponse>) {
 		routes += RouterFunctions.route(RequestPredicates.POST(pattern), f())
-	}
-
-	fun PUT(pattern: String): RequestPredicate {
-		return RequestPredicates.PUT(pattern)
 	}
 
 	fun PUT(pattern: String, f: () -> HandlerFunction<ServerResponse>) {
 		routes += RouterFunctions.route(RequestPredicates.PUT(pattern), f())
 	}
 
-	fun PATCH(pattern: String): RequestPredicate {
-		return RequestPredicates.PATCH(pattern)
-	}
-
 	fun PATCH(pattern: String, f: () -> HandlerFunction<ServerResponse>) {
 		routes += RouterFunctions.route(RequestPredicates.PATCH(pattern), f())
-	}
-
-	fun DELETE(pattern: String): RequestPredicate {
-		return RequestPredicates.DELETE(pattern)
 	}
 
 	fun DELETE(pattern: String, f: () -> HandlerFunction<ServerResponse>) {
 		routes += RouterFunctions.route(RequestPredicates.DELETE(pattern), f())
 	}
 
-	fun OPTIONS(pattern: String): RequestPredicate {
-		return RequestPredicates.OPTIONS(pattern)
-	}
-
 	fun OPTIONS(pattern: String, f: () -> HandlerFunction<ServerResponse>) {
 		routes += RouterFunctions.route(RequestPredicates.OPTIONS(pattern), f())
+	}
+
+	fun accept(vararg mediaType: MediaType, f: () -> HandlerFunction<ServerResponse>) {
+		routes += RouterFunctions.route(RequestPredicates.accept(*mediaType), f())
+	}
+
+	fun contentType(vararg mediaType: MediaType, f: () -> HandlerFunction<ServerResponse>) {
+		routes += RouterFunctions.route(RequestPredicates.contentType(*mediaType), f())
+	}
+
+	fun headers(headerPredicate: (ServerRequest.Headers)->Boolean, f: () -> HandlerFunction<ServerResponse>) {
+		routes += RouterFunctions.route(RequestPredicates.headers(headerPredicate), f())
+	}
+
+	fun method(httpMethod: HttpMethod, f: () -> HandlerFunction<ServerResponse>) {
+		routes += RouterFunctions.route(RequestPredicates.method(httpMethod), f())
+	}
+
+	fun path(pattern: String, f: () -> HandlerFunction<ServerResponse>) {
+		routes += RouterFunctions.route(RequestPredicates.path(pattern), f())
 	}
 
 	fun resources(path: String, location: Resource) {
 		routes +=  RouterFunctions.resources(path, location)
 	}
+
+	fun resources(lookupFunction: (ServerRequest) -> Mono<Resource>) {
+		routes +=  RouterFunctions.resources(lookupFunction)
+	}
+
 
 	@Suppress("UNCHECKED_CAST")
 	fun router(): RouterFunction<ServerResponse> {
@@ -117,6 +119,5 @@ class RouterFunctionDsl {
 		}
 		return allRoutes
 	}
-}
 
-fun RouterFunctionDsl(configure: RouterFunctionDsl.()->Unit) = RouterFunctionDsl().apply(configure)
+}
