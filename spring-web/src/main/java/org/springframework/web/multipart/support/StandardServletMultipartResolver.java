95330d0e44356f98b591e4c865f6864a2b2be7d9
Juergen Hoeller <jhoeller@pivotal.io>
2017-01-16 21:49:21 +0100

    Polishing

--- spring-web/src/main/java/org/springframework/web/multipart/support/StandardServletMultipartResolver.java
+++ spring-web/src/main/java/org/springframework/web/multipart/support/StandardServletMultipartResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,6 +42,9 @@ import org.springframework.web.multipart.MultipartResolver;
  *
  * @author Juergen Hoeller
  * @since 3.1
+ * @see #setResolveLazily
+ * @see HttpServletRequest#getParts()
+ * @see org.springframework.web.multipart.commons.CommonsMultipartResolver
  */
 public class StandardServletMultipartResolver implements MultipartResolver {
 
@@ -87,7 +90,7 @@ public class StandardServletMultipartResolver implements MultipartResolver {
 				}
 			}
 		}
-		catch (Exception ex) {
+		catch (Throwable ex) {
 			LogFactory.getLog(getClass()).warn("Failed to perform cleanup of multipart items", ex);
 		}
 	}
