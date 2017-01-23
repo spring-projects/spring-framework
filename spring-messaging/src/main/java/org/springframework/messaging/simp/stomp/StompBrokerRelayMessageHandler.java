12973ac702d2262c3ea5c785a332ebc6fce0391a
Juergen Hoeller <jhoeller@pivotal.io>
2017-01-24 00:12:53 +0100

    Polishing

--- spring-messaging/src/main/java/org/springframework/messaging/simp/stomp/StompBrokerRelayMessageHandler.java
+++ spring-messaging/src/main/java/org/springframework/messaging/simp/stomp/StompBrokerRelayMessageHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -973,7 +973,7 @@ public class StompBrokerRelayMessageHandler extends AbstractBrokerMessageHandler
 	private static class VoidCallable implements Callable<Void> {
 
 		@Override
-		public Void call() {
+		public Void call() throws Exception {
 			return null;
 		}
 	}
