c236c7fcf912a1a221de8b57b298780613abee7b
Yevhenii Melnyk <melnyk.yevhenii@gmail.com>
2017-02-12 14:59:31 +0200

    Add a Kotlin RouterFunction implementation
    This commit introduces KotlinRouterFunction - abstract subclass of RouterFunction
    to give opportunity to write request handlers as members of controller class:
    
    class SampleController : KotlinRouterFunction({
        GET("/super") { req ->
            ServerResponse.ok().body(fromObject("super!!!"))
        }
    })
    
    Issue: SPR-15065

---
+++ spring-webflux/src/test/kotlin/org/springframework/web/reactive/function/server/KotlinRouterFunctionTests.kt
@@ -0,0 +1,143 @@
+package org.springframework.web.reactive.function.server
+
+import org.junit.Test
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.HttpHeaders.ACCEPT
+import org.springframework.http.HttpHeaders.CONTENT_TYPE
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpMethod.PATCH
+import org.springframework.http.HttpMethod.POST
+import org.springframework.http.MediaType.*
+import org.springframework.web.reactive.function.server.MockServerRequest.builder
+import org.springframework.web.reactive.function.server.RequestPredicates.*
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+import java.net.URI
+
+class KotlinRouterFunctionTests {
+
+	@Test
+	fun header() {
+		val request = builder().header("bar", "bar").build()
+		StepVerifier.create(FooController().route(request))
+				.expectNextCount(1)
+				.verifyComplete()
+	}
+
+	@Test
+	fun accept() {
+		val request = builder().header(ACCEPT, APPLICATION_ATOM_XML_VALUE).build()
+		StepVerifier.create(FooController().route(request))
+				.expectNextCount(1)
+				.verifyComplete()
+	}
+
+	@Test
+	fun acceptAndPOST() {
+		val request = builder().method(POST).uri(URI("/api/foo/")).header(ACCEPT, APPLICATION_JSON_VALUE).build()
+		StepVerifier.create(FooController().route(request))
+				.expectNextCount(1)
+				.verifyComplete()
+	}
+
+	@Test
+	fun contentType() {
+		val request = builder().header(CONTENT_TYPE, APPLICATION_OCTET_STREAM_VALUE).build()
+		StepVerifier.create(FooController().route(request))
+				.expectNextCount(1)
+				.verifyComplete()
+	}
+
+	@Test
+	fun resourceByPath() {
+		val request = builder().uri(URI("/org/springframework/web/reactive/function/response.txt")).build()
+		StepVerifier.create(FooController().route(request))
+				.expectNextCount(1)
+				.verifyComplete()
+	}
+
+	@Test
+	fun method() {
+		val request = builder().method(PATCH).build()
+		StepVerifier.create(FooController().route(request))
+				.expectNextCount(1)
+				.verifyComplete()
+	}
+
+	@Test
+	fun path() {
+		val request = builder().uri(URI("/baz")).build()
+		StepVerifier.create(FooController().route(request))
+				.expectNextCount(1)
+				.verifyComplete()
+	}
+
+	@Test
+	fun resource() {
+		val request = builder().uri(URI("/response.txt")).build()
+		StepVerifier.create(FooController().route(request))
+				.expectNextCount(1)
+				.verifyComplete()
+	}
+
+	@Test
+	fun noRoute() {
+		val request = builder()
+				.uri(URI("/bar"))
+				.header(ACCEPT, APPLICATION_PDF_VALUE)
+				.header(CONTENT_TYPE, APPLICATION_PDF_VALUE)
+				.build()
+		StepVerifier.create(FooController().route(request))
+				.verifyComplete()
+	}
+
+	class FooController : KotlinRouterFunction({
+
+		(GET("/foo/") or GET("/foos/")) { req ->
+			handle(req)
+		}
+
+		accept(APPLICATION_JSON).apply {
+			POST("/api/foo/", ::handle)
+			PUT("/api/foo/", ::handle)
+			DELETE("/api/foo/", ::handle)
+		}
+
+		accept(APPLICATION_ATOM_XML, ::handle)
+
+		contentType(APPLICATION_OCTET_STREAM) {
+			ok().build()
+		}
+
+		method(HttpMethod.PATCH) { req ->
+			handle(req)
+		}
+
+		headers({ it.accept().contains(APPLICATION_JSON) }).apply {
+			GET("/api/foo/", ::handle)
+		}
+
+		headers({ it.header("bar").isNotEmpty() }, ::handle)
+
+		resources("/org/springframework/web/reactive/function/**",
+				ClassPathResource("/org/springframework/web/reactive/function/response.txt"))
+
+		resources {
+			if (it.path() == "/response.txt") {
+				Mono.just(ClassPathResource("/org/springframework/web/reactive/function/response.txt"))
+			}
+			else {
+				Mono.empty()
+			}
+		}
+
+		path("/baz") { req ->
+			handle(req)
+		}
+
+	})
+}
+
+private fun handle(req: ServerRequest) = ok().build()
+
