de5714bb8927e0b180b94da6e46b4b623ec0245f
stonio <stonio@users.noreply.github.com>
2017-02-21 16:23:40 +0100

    Use String#isEmpty()

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/RequestPartMethodArgumentResolver.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/RequestPartMethodArgumentResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -159,7 +159,7 @@ public class RequestPartMethodArgumentResolver extends AbstractMessageConverterM
 
 	private String getPartName(MethodParameter methodParam, RequestPart requestPart) {
 		String partName = (requestPart != null ? requestPart.name() : "");
-		if (partName.length() == 0) {
+		if (partName.isEmpty()) {
 			partName = methodParam.getParameterName();
 			if (partName == null) {
 				throw new IllegalArgumentException("Request part name for argument type [" +
