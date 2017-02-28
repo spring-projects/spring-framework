d69fb06852a8594db1338dc92799e857e0cc7bc8
Juergen Hoeller <jhoeller@pivotal.io>
2017-02-28 13:08:55 +0100

    Skip transaction/caching metadata retrieval for java.lang.Object methods
    Also retrieves CacheConfig as merged annotation now, aligned with other caching annotations.
    
    Issue: SPR-15296
    (cherry picked from commit d4a1b59)

--- spring-tx/src/main/java/org/springframework/transaction/interceptor/AbstractFallbackTransactionAttributeSource.java
+++ spring-tx/src/main/java/org/springframework/transaction/interceptor/AbstractFallbackTransactionAttributeSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,7 +69,8 @@ public abstract class AbstractFallbackTransactionAttributeSource implements Tran
 	 * <p>As this base class is not marked Serializable, the cache will be recreated
 	 * after serialization - provided that the concrete subclass is Serializable.
 	 */
-	final Map<Object, TransactionAttribute> attributeCache = new ConcurrentHashMap<Object, TransactionAttribute>(1024);
+	private final Map<Object, TransactionAttribute> attributeCache =
+			new ConcurrentHashMap<Object, TransactionAttribute>(1024);
 
 
 	/**
@@ -82,6 +83,10 @@ public abstract class AbstractFallbackTransactionAttributeSource implements Tran
 	 */
 	@Override
 	public TransactionAttribute getTransactionAttribute(Method method, Class<?> targetClass) {
+		if (method.getDeclaringClass() == Object.class) {
+			return null;
+		}
+
 		// First, see if we have a cached value.
 		Object cacheKey = getCacheKey(method, targetClass);
 		Object cached = this.attributeCache.get(cacheKey);
