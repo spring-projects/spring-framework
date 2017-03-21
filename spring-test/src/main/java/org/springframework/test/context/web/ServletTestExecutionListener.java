8289bf0afff3c98f661ef201092700b8d3e65c24
Connor Lin <linbaiye@gmail.com>
2017-03-21 15:58:31 +1100

    Remove a redundant word
    Closes gh-1361

--- spring-test/src/main/java/org/springframework/test/context/web/ServletTestExecutionListener.java
+++ spring-test/src/main/java/org/springframework/test/context/web/ServletTestExecutionListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -96,8 +96,8 @@ public class ServletTestExecutionListener extends AbstractTestExecutionListener 
 			ServletTestExecutionListener.class, "createdByTheTestContextFramework");
 
 	/**
-	 * Attribute name for a {@link TestContext} attribute which indicates that that
-	 * the {@code ServletTestExecutionListener} should be activated. When not set to
+	 * Attribute name for a {@link TestContext} attribute which indicates that the
+	 * {@code ServletTestExecutionListener} should be activated. When not set to
 	 * {@code true}, activation occurs when the {@linkplain TestContext#getTestClass()
 	 * test class} is annotated with {@link WebAppConfiguration @WebAppConfiguration}.
 	 * <p>Permissible values include {@link Boolean#TRUE} and {@link Boolean#FALSE}.
