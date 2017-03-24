2233ec04ff4f9dd7c4cfeabeb4586685701421bf
Juergen Hoeller <jhoeller@pivotal.io>
2017-03-24 11:06:39 +0100

    StandardMultipartFile.transferTo falls back to manual copy
    Issue: SPR-15257
    (cherry picked from commit b73153c)

--- spring-web/src/main/java/org/springframework/web/multipart/support/StandardMultipartHttpServletRequest.java
+++ spring-web/src/main/java/org/springframework/web/multipart/support/StandardMultipartHttpServletRequest.java
@@ -17,6 +17,7 @@
 package org.springframework.web.multipart.support;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
@@ -299,6 +300,15 @@ public class StandardMultipartHttpServletRequest extends AbstractMultipartHttpSe
 		@Override
 		public void transferTo(File dest) throws IOException, IllegalStateException {
 			this.part.write(dest.getPath());
+			if (dest.isAbsolute() && !dest.exists()) {
+				// Servlet 3.0 Part.write is not guaranteed to support absolute file paths:
+				// may translate the given path to a relative location within a temp dir
+				// (e.g. on Jetty whereas Tomcat and Undertow detect absolute paths).
+				// At least we offloaded the file from memory storage; it'll get deleted
+				// from the temp dir eventually in any case. And for our user's purposes,
+				// we can manually copy it to the requested location as a fallback.
+				FileCopyUtils.copy(this.part.getInputStream(), new FileOutputStream(dest));
+			}
 		}
 	}
 
