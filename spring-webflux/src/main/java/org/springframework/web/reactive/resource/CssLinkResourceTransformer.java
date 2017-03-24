2baceac5ff03489628e058c76a35306519e6cbb2
Brian Clozel <bclozel@pivotal.io>
2017-03-24 16:09:14 +0100

    Resolve ResourceUrlProvider from current request
    This commit changes `ResourceTransformerSupport` to look for the
    `ResourceUrlProvider` in the current request if none is configured on
    the resource transformer itself.
    
    Issue: SPR-15369

--- spring-webflux/src/main/java/org/springframework/web/reactive/resource/CssLinkResourceTransformer.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/resource/CssLinkResourceTransformer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,7 +103,7 @@ public class CssLinkResourceTransformer extends ResourceTransformerSupport {
 							.concatMap(segment -> {
 								String segmentContent = segment.getContent(fullContent);
 								if (segment.isLink() && !hasScheme(segmentContent)) {
-									String link = toAbsolutePath(segmentContent, exchange.getRequest());
+									String link = toAbsolutePath(segmentContent, exchange);
 									return resolveUrlPath(link, exchange, newResource, transformerChain)
 											.defaultIfEmpty(segmentContent);
 								}
--- spring-webflux/src/main/java/org/springframework/web/reactive/resource/ResourceTransformerSupport.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/resource/ResourceTransformerSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,11 +89,11 @@ public abstract class ResourceTransformerSupport implements ResourceTransformer 
 	 * The resulting path is also cleaned from sequences like "path/..".
 	 *
 	 * @param path the relative path to transform
-	 * @param request the referer request
+	 * @param exchange the current exchange
 	 * @return the absolute request path for the given resource path
 	 */
-	protected String toAbsolutePath(String path, ServerHttpRequest request) {
-		String requestPath = request.getURI().getPath();
+	protected String toAbsolutePath(String path, ServerWebExchange exchange) {
+		String requestPath = exchange.getRequest().getURI().getPath();
 		String absolutePath = StringUtils.applyRelativePath(requestPath, path);
 		return StringUtils.cleanPath(absolutePath);
 	}
