7a01771a45dfa124289403226487b9cd63d17fdb
Sam Brannen <sam@sambrannen.com>
2017-03-28 19:27:03 +0200

    Polishing

--- spring-beans/src/main/java/org/springframework/beans/factory/config/PropertyPlaceholderConfigurer.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/PropertyPlaceholderConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package org.springframework.beans.factory.config;
 
 import java.util.Properties;
-import java.util.Set;
 
 import org.springframework.beans.BeansException;
 import org.springframework.core.Constants;
