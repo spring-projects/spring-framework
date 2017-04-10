0a9f98f0df4519989f5a70f19c6a0c89a6fdb99c
Rob Winch <rwinch@users.noreply.github.com>
2017-04-10 08:54:43 -0500

    Add test to ensure ForwardedHeaderFilter contextPath preserves encoding
    Add a test to verify that the ForwardedHeaderFilter contextPath override preserves URL encoding
    as it should.
    
    Issue SPR-15422

--- spring-web/src/test/java/org/springframework/web/filter/ForwardedHeaderFilterTests.java
+++ spring-web/src/test/java/org/springframework/web/filter/ForwardedHeaderFilterTests.java
@@ -250,6 +250,17 @@ public class ForwardedHeaderFilterTests {
 	}
 
 	@Test
+	public void contextPathPreserveEncoding() throws Exception {
+		this.request.setContextPath("/app%20");
+		this.request.setRequestURI("/app%20/path/");
+		HttpServletRequest actual = filterAndGetWrappedRequest();
+
+		assertEquals("/app%20", actual.getContextPath());
+		assertEquals("/app%20/path/", actual.getRequestURI());
+		assertEquals("http://localhost/app%20/path/", actual.getRequestURL().toString());
+	}
+
+	@Test
 	public void sendRedirectWithAbsolutePath() throws Exception {
 		this.request.addHeader(X_FORWARDED_PROTO, "https");
 		this.request.addHeader(X_FORWARDED_HOST, "example.com");
