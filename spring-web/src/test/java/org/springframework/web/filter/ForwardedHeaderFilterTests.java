95099fd709685f3d9a35e61273c9f23866db7105
Rob Winch <rwinch@users.noreply.github.com>
2017-04-10 09:03:51 -0500

    Fix ForwardedHeaderFilter preserves semicolon content
    Previously a requestURI that contained ';' would have the value incorrectly stripped out when using
    ForwardedHeaderFilter.
    
    This commit ensures that the ';' is preserved when using ForwardedHeaderFilter.
    
    Issue: SPR-15428

--- spring-web/src/test/java/org/springframework/web/filter/ForwardedHeaderFilterTests.java
+++ spring-web/src/test/java/org/springframework/web/filter/ForwardedHeaderFilterTests.java
@@ -172,6 +172,17 @@ public class ForwardedHeaderFilterTests {
 	}
 
 	@Test
+	public void requestUriPreserveSemicolonContent() throws Exception {
+		this.request.setContextPath("");
+		this.request.setRequestURI("/path;a=b/with/semicolon");
+		HttpServletRequest actual = filterAndGetWrappedRequest();
+
+		assertEquals("", actual.getContextPath());
+		assertEquals("/path;a=b/with/semicolon", actual.getRequestURI());
+		assertEquals("http://localhost/path;a=b/with/semicolon", actual.getRequestURL().toString());
+	}
+
+	@Test
 	public void caseInsensitiveForwardedPrefix() throws Exception {
 		this.request = new MockHttpServletRequest() {
 
