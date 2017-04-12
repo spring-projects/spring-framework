093e573aceda50f4e94d35ceec465a3532094f22
Juergen Hoeller <jhoeller@pivotal.io>
2017-04-12 15:36:18 +0200

    ASM annotation visitor defensively accesses enum constants
    Issue: SPR-15442

--- spring-core/src/main/java/org/springframework/core/type/classreading/AbstractRecursiveAnnotationVisitor.java
+++ spring-core/src/main/java/org/springframework/core/type/classreading/AbstractRecursiveAnnotationVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,6 +80,7 @@ abstract class AbstractRecursiveAnnotationVisitor extends AnnotationVisitor {
 			Class<?> enumType = this.classLoader.loadClass(Type.getType(asmTypeDescriptor).getClassName());
 			Field enumConstant = ReflectionUtils.findField(enumType, attributeValue);
 			if (enumConstant != null) {
+				ReflectionUtils.makeAccessible(enumConstant);
 				valueToUse = enumConstant.get(null);
 			}
 		}
