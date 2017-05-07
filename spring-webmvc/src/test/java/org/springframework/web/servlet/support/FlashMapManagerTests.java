e0a7b074ed95f278b782e159436f053d659e28ab
Martin Švorc <svorcmar@gmail.com>
2017-05-07 20:59:09 +0200

    Use original query string of forwarded request
    Prior to this commit, the AbstractFlashMapManager has used the
    originating URI but the query string of the forwarded request. That
    resulted to FlashMap not being matched even when both originating
    URI and query string matched the FlashMap attributes. The originating
    query string is now used to match the forwarded request.
    
    Issue: SPR-15505

--- spring-webmvc/src/test/java/org/springframework/web/servlet/support/FlashMapManagerTests.java
+++ spring-webmvc/src/test/java/org/springframework/web/servlet/support/FlashMapManagerTests.java
@@ -21,6 +21,7 @@ import static org.junit.Assert.*;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -316,6 +317,25 @@ public class FlashMapManagerTests {
 		assertNotNull(flashMap);
 		assertEquals(1, flashMap.size());
 		assertEquals("value", flashMap.get("key"));
+	}
+
+	@Test // SPR-15505
+	public void retrieveAndUpdateMatchByOriginatingPathAndQueryString() {
+		FlashMap flashMap = new FlashMap();
+		flashMap.put("key", "value");
+		flashMap.setTargetRequestPath("/accounts");
+		flashMap.addTargetRequestParam("a", "b");
+
+		this.flashMapManager.setFlashMaps(Collections.singletonList(flashMap));
+
+		this.request.setAttribute(WebUtils.FORWARD_REQUEST_URI_ATTRIBUTE, "/accounts");
+		this.request.setAttribute(WebUtils.FORWARD_QUERY_STRING_ATTRIBUTE, "a=b");
+		this.request.setRequestURI("/mvc/accounts");
+		this.request.setQueryString("x=y");
+		FlashMap inputFlashMap = this.flashMapManager.retrieveAndUpdate(this.request, this.response);
+
+		assertEquals(flashMap, inputFlashMap);
+		assertEquals("Input FlashMap should have been removed", 0, this.flashMapManager.getFlashMaps().size());
 	}
 
 
