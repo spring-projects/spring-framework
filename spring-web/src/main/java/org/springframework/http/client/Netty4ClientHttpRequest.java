d4d609318dc90def2363daeb169c911e3487bb2b
Brian Clozel <bclozel@pivotal.io>
2017-07-04 12:13:06 +0200

    Include port info in HTTP headers for Netty client
    This commit adds the port information in the "Host" HTTP request header
    for the Netty-based HTTP client.
    
    Issue: SPR-15706
    (cherry-picked from commit baaa84e90)

--- spring-web/src/main/java/org/springframework/http/client/Netty4ClientHttpRequest.java
+++ spring-web/src/main/java/org/springframework/http/client/Netty4ClientHttpRequest.java
@@ -136,7 +136,7 @@ class Netty4ClientHttpRequest extends AbstractAsyncClientHttpRequest implements 
 		FullHttpRequest nettyRequest = new DefaultFullHttpRequest(
 				HttpVersion.HTTP_1_1, nettyMethod, path, this.body.buffer());
 
-		nettyRequest.headers().set(HttpHeaders.HOST, this.uri.getHost());
+		nettyRequest.headers().set(HttpHeaders.HOST, this.uri.getHost() + ":" + getPort(uri));
 		nettyRequest.headers().set(HttpHeaders.CONNECTION, "close");
 		for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
 			nettyRequest.headers().add(entry.getKey(), entry.getValue());
