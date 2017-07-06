5b54f386b2a6b8303c6ad95670f355538a74c9c2
Renato Soppelsa <renato.soppelsa@gmail.com>
2017-07-06 11:35:39 +0200

    add UTF8 to existing Nottingham
    add UTF8 to existing Nottingham: application/problem+json;charset=UTF-8

--- spring-web/src/main/java/org/springframework/http/MediaType.java
+++ spring-web/src/main/java/org/springframework/http/MediaType.java
@@ -102,30 +102,6 @@ public class MediaType extends MimeType implements Serializable {
 	 * A String equivalent of {@link MediaType#APPLICATION_JSON_UTF8}.
 	 */
 	public final static String APPLICATION_JSON_UTF8_VALUE = APPLICATION_JSON_VALUE + ";charset=UTF-8";
-	
-	/**
-	 * Public constant media type for Nottingham Problem {@code application/problem+json}.
-	 * https://tools.ietf.org/html/rfc7807
-	 * @see #APPLICATION_JSON_PROBLEM_UTF8
-	 */
-	public final static MediaType APPLICATION_JSON_PROBLEM;
-
-	/**
-	 * A String equivalent of {@link MediaType#APPLICATION_JSON_PROBLEM}.
-	 * @see #APPLICATION_JSON_PROBLEM_UTF8_VALUE
-	 */
-	public final static String APPLICATION_JSON_PROBLEM_VALUE = "application/problem+json";
-
-	/**
-	 * Public constant media type for Nottingham Problem {@code application/problem+json;charset=UTF-8}.
-	 * https://tools.ietf.org/html/rfc7807
-	 */
-	public final static MediaType APPLICATION_JSON_PROBLEM_UTF8;
-
-	/**
-	 * A String equivalent of {@link MediaType#APPLICATION_JSON_PROBLEM_UTF8}.
-	 */
-	public final static String APPLICATION_JSON_PROBLEM_UTF8_VALUE = APPLICATION_JSON_PROBLEM_VALUE + ";charset=UTF-8";
 
 	/**
 	 * Public constant media type for {@code application/octet-stream}.
@@ -300,6 +276,19 @@ public class MediaType extends MimeType implements Serializable {
 	 * @since 5.0
 	 */
 	public final static String APPLICATION_PROBLEM_JSON_VALUE = "application/problem+json";
+	
+	/**
+	 * Public constant media type for {@code application/problem+json}.
+	 * @since 5.0
+	 * @see <a href="https://tools.ietf.org/html/rfc7807#section-6.1">Problem Details for HTTP APIs, 6.1. application/problem+json</a>
+	 */
+	public final static MediaType APPLICATION_PROBLEM_JSON_UTF8;
+
+	/**
+	 * A String equivalent of {@link MediaType#APPLICATION_PROBLEM_JSON_UTF8}.
+	 * @since 5.0
+	 */
+	public final static String APPLICATION_PROBLEM_JSON_UTF8_VALUE = APPLICATION_PROBLEM_JSON_VALUE + ";charset=UTF-8";
 
 	/**
 	 * Public constant media type for {@code application/problem+xml}.
@@ -326,6 +315,7 @@ public class MediaType extends MimeType implements Serializable {
 		APPLICATION_OCTET_STREAM = valueOf(APPLICATION_OCTET_STREAM_VALUE);
 		APPLICATION_PDF = valueOf(APPLICATION_PDF_VALUE);
 		APPLICATION_PROBLEM_JSON = valueOf(APPLICATION_PROBLEM_JSON_VALUE);
+		APPLICATION_PROBLEM_JSON_UTF8 = valueOf(APPLICATION_PROBLEM_JSON_UTF8_VALUE);
 		APPLICATION_PROBLEM_XML = valueOf(APPLICATION_PROBLEM_XML_VALUE);
 		APPLICATION_RSS_XML = valueOf(APPLICATION_RSS_XML_VALUE);
 		APPLICATION_STREAM_JSON = valueOf(APPLICATION_STREAM_JSON_VALUE);
