625855c00f1a670724bcf3cf524b3a24e0e6f1a0
Arjen Poutsma <apoutsma@pivotal.io>
2017-07-06 11:45:13 +0200

    Add comment indicating path segment combining in UriComponents.normalize
    Issue: SPR-15734

--- spring-web/src/main/java/org/springframework/web/util/UriComponents.java
+++ spring-web/src/main/java/org/springframework/web/util/UriComponents.java
@@ -187,7 +187,9 @@ public abstract class UriComponents implements Serializable {
 	abstract UriComponents expandInternal(UriTemplateVariables uriVariables);
 
 	/**
-	 * Normalize the path removing sequences like "path/..".
+	 * Normalize the path removing sequences like "path/..". Note that calling this method will
+	 * combine all path segments into a full path before doing the actual normalisation, i.e.
+	 * individual path segments will not be normalized individually.
 	 * @see org.springframework.util.StringUtils#cleanPath(String)
 	 */
 	public abstract UriComponents normalize();
