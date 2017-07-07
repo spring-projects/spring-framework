853e6f6e41f4ec5828b43ae283aac5a26aa9445e
chuanqin <chuanqing.qin@nokia.com>
2017-07-08 00:01:15 +0800

    refactor redundancy code in spring-core/src/main/java/org/springframework/util/CommonsLogWriter.java

--- spring-core/src/main/java/org/springframework/util/CommonsLogWriter.java
+++ spring-core/src/main/java/org/springframework/util/CommonsLogWriter.java
@@ -57,13 +57,7 @@ public class CommonsLogWriter extends Writer {
 	public void write(char[] buffer, int offset, int length) {
 		for (int i = 0; i < length; i++) {
 			char ch = buffer[offset + i];
-			if (ch == '\n' && this.buffer.length() > 0) {
-				this.logger.debug(this.buffer.toString());
-				this.buffer.setLength(0);
-			}
-			else {
-				this.buffer.append(ch);
-			}
+			write(ch);
 		}
 	}
 
