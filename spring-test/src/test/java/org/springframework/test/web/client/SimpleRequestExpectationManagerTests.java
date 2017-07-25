45e714ec1316633f336ca591d475b5a7d987358e
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2017-07-25 12:29:22 +0200

    Add test case for SPR-15719

--- spring-test/src/test/java/org/springframework/test/web/client/SimpleRequestExpectationManagerTests.java
+++ spring-test/src/test/java/org/springframework/test/web/client/SimpleRequestExpectationManagerTests.java
@@ -173,6 +173,17 @@ public class SimpleRequestExpectationManagerTests {
 		this.manager.validateRequest(createRequest(GET, "/bar"));
 	}
 
+	@Test  // SPR-15719
+	public void repeatedRequestsInSequentialOrder() throws Exception {
+		this.manager.expectRequest(times(2), requestTo("/foo")).andExpect(method(GET)).andRespond(withSuccess());
+		this.manager.expectRequest(times(2), requestTo("/bar")).andExpect(method(GET)).andRespond(withSuccess());
+
+		this.manager.validateRequest(createRequest(GET, "/foo"));
+		this.manager.validateRequest(createRequest(GET, "/foo"));
+		this.manager.validateRequest(createRequest(GET, "/bar"));
+		this.manager.validateRequest(createRequest(GET, "/bar"));
+	}
+
 
 	private ClientHttpRequest createRequest(HttpMethod method, String url) {
 		try {
