ca0983cd850717a23b714f98dbfbb40aaebec45c
Juergen Hoeller <jhoeller@pivotal.io>
2017-08-01 12:08:57 +0200

    Polishing

--- spring-web/src/main/java/org/springframework/web/util/WebUtils.java
+++ spring-web/src/main/java/org/springframework/web/util/WebUtils.java
@@ -154,16 +154,15 @@ public abstract class WebUtils {
 		String root = servletContext.getRealPath("/");
 		if (root == null) {
 			throw new IllegalStateException(
-				"Cannot set web app root system property when WAR file is not expanded");
+					"Cannot set web app root system property when WAR file is not expanded");
 		}
 		String param = servletContext.getInitParameter(WEB_APP_ROOT_KEY_PARAM);
 		String key = (param != null ? param : DEFAULT_WEB_APP_ROOT_KEY);
 		String oldValue = System.getProperty(key);
 		if (oldValue != null && !StringUtils.pathEquals(oldValue, root)) {
-			throw new IllegalStateException(
-				"Web app root system property already set to different value: '" +
-				key + "' = [" + oldValue + "] instead of [" + root + "] - " +
-				"Choose unique values for the 'webAppRootKey' context-param in your web.xml files!");
+			throw new IllegalStateException("Web app root system property already set to different value: '" +
+					key + "' = [" + oldValue + "] instead of [" + root + "] - " +
+					"Choose unique values for the 'webAppRootKey' context-param in your web.xml files!");
 		}
 		System.setProperty(key, root);
 		servletContext.log("Set web app root system property: '" + key + "' = [" + root + "]");
