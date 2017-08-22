5a51351fa4e582571a442b331fb3f2bec0e63f30
Stephane Nicoll <snicoll@pivotal.io>
2017-08-22 10:26:19 +0200

    Relax test assertion when Jacoco is enabled
    This commit changes the resolveMethod to take a method name as
    instrumentation may add additional methods to the test object.
    
    Issue: SPR-15888

--- spring-webflux/src/test/java/org/springframework/web/reactive/result/method/annotation/ResponseBodyResultHandlerTests.java
+++ spring-webflux/src/test/java/org/springframework/web/reactive/result/method/annotation/ResponseBodyResultHandlerTests.java
@@ -83,7 +83,7 @@ public class ResponseBodyResultHandlerTests {
 		method = on(TestController.class).annotPresent(ResponseBody.class).resolveMethod();
 		testSupports(controller, method);
 
-		method = on(TestController.class).annotNotPresent(ResponseBody.class).resolveMethod();
+		method = on(TestController.class).annotNotPresent(ResponseBody.class).resolveMethod("doWork");
 		HandlerResult handlerResult = getHandlerResult(controller, method);
 		assertFalse(this.resultHandler.supports(handlerResult));
 	}
