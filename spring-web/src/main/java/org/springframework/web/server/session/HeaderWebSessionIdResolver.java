d0e745cd32d6cc2617a3868b797d9f7850746518
Rob Winch <rwinch@users.noreply.github.com>
2017-09-05 22:04:14 -0500

    Polish HeaderWebSessionIdResolver
    - Use constant for default header name
    - make getHeaderName private
    
    Issue: SPR-15917

--- spring-web/src/main/java/org/springframework/web/server/session/HeaderWebSessionIdResolver.java
+++ spring-web/src/main/java/org/springframework/web/server/session/HeaderWebSessionIdResolver.java
@@ -26,32 +26,29 @@ import org.springframework.web.server.ServerWebExchange;
  * Request and response header-based {@link WebSessionIdResolver}.
  * 
  * @author Greg Turnquist
+ * @author Rob Winch
  * @since 5.0
  */
 public class HeaderWebSessionIdResolver implements WebSessionIdResolver {
 
-	private String headerName = "SESSION";
+	/**
+	 * The default header name
+	 */
+	public static final String DEFAULT_HEADER_NAME = "SESSION";
 
+	private String headerName = DEFAULT_HEADER_NAME;
 
 	/**
 	 * Set the name of the session header to use for the session id.
 	 * The name is used to extract the session id from the request headers as
 	 * well to set the session id on the response headers.
-	 * <p>By default set to {@literal "SESSION"}.
+	 * <p>By default set to {@code DEFAULT_HEADER_NAME}
 	 * @param headerName the header name
 	 */
 	public void setHeaderName(String headerName) {
 		Assert.hasText(headerName, "'headerName' must not be empty.");
 		this.headerName = headerName;
 	}
-
-	/**
-	 * Return the configured header name.
-	 */
-	public String getHeaderName() {
-		return this.headerName;
-	}
-
 
 	@Override
 	public List<String> resolveSessionIds(ServerWebExchange exchange) {
@@ -70,4 +67,11 @@ public class HeaderWebSessionIdResolver implements WebSessionIdResolver {
 		this.setSessionId(exchange, "");
 	}
 
+	/**
+	 * Return the configured header name.
+	 * @return the configured header name
+	 */
+	private String getHeaderName() {
+		return this.headerName;
+	}
 }
