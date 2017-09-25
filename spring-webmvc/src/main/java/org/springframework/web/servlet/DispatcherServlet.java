9ff4c0bff8d7d9b223986daaf74bfb98f9939386
Juergen Hoeller <jhoeller@pivotal.io>
2017-09-25 22:45:04 +0200

    DispatcherServlet.getHandlerMappings() accessor declared as final
    Issue: SPR-15934

--- spring-webmvc/src/main/java/org/springframework/web/servlet/DispatcherServlet.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/DispatcherServlet.java
@@ -771,20 +771,6 @@ public class DispatcherServlet extends FrameworkServlet {
 	}
 
 	/**
-	 * Return the configured {@link HandlerMapping} beans that were detected by
-	 * type in the {@link WebApplicationContext} or initialized based on the
-	 * default set of strategies from {@literal DispatcherServlet.properties}.
-	 * <p><strong>Note:</strong> This method may return {@code null} if invoked
-	 * prior to {@link #onRefresh(ApplicationContext)}.
-	 * @return immutable list with the configured mappings or {@code null}
-	 * @since 5.0
-	 */
-	@Nullable
-	public List<HandlerMapping> getHandlerMappings() {
-		return this.handlerMappings != null ? Collections.unmodifiableList(this.handlerMappings) : null;
-	}
-
-	/**
 	 * Return this servlet's ThemeSource, if any; else return {@code null}.
 	 * <p>Default is to return the WebApplicationContext as ThemeSource,
 	 * provided that it implements the ThemeSource interface.
@@ -793,12 +779,7 @@ public class DispatcherServlet extends FrameworkServlet {
 	 */
 	@Nullable
 	public final ThemeSource getThemeSource() {
-		if (getWebApplicationContext() instanceof ThemeSource) {
-			return (ThemeSource) getWebApplicationContext();
-		}
-		else {
-			return null;
-		}
+		return (getWebApplicationContext() instanceof ThemeSource ? (ThemeSource) getWebApplicationContext() : null);
 	}
 
 	/**
@@ -809,6 +790,21 @@ public class DispatcherServlet extends FrameworkServlet {
 	@Nullable
 	public final MultipartResolver getMultipartResolver() {
 		return this.multipartResolver;
+	}
+
+	/**
+	 * Return the configured {@link HandlerMapping} beans that were detected by
+	 * type in the {@link WebApplicationContext} or initialized based on the
+	 * default set of strategies from {@literal DispatcherServlet.properties}.
+	 * <p><strong>Note:</strong> This method may return {@code null} if invoked
+	 * prior to {@link #onRefresh(ApplicationContext)}.
+	 * @return an immutable list with the configured mappings, or {@code null}
+	 * if not initialized yet
+	 * @since 5.0
+	 */
+	@Nullable
+	public final List<HandlerMapping> getHandlerMappings() {
+		return (this.handlerMappings != null ? Collections.unmodifiableList(this.handlerMappings) : null);
 	}
 
 	/**
