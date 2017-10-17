97c63857c4562e252943c55dbc9a1cb652f98381
Juergen Hoeller <jhoeller@pivotal.io>
2017-10-17 11:49:54 +0200

    Allow PropertyEditor conversion as fallback for enums
    Issue: SPR-16082
    
    (cherry picked from commit ef6aef9)

--- spring-webmvc/src/main/java/org/springframework/web/servlet/tags/form/SelectedValueComparator.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/tags/form/SelectedValueComparator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -162,7 +162,8 @@ abstract class SelectedValueComparator {
 		else if (ObjectUtils.getDisplayString(boundValue).equals(candidateDisplayString)) {
 			return true;
 		}
-		else if (editor != null && candidate instanceof String) {
+
+		if (editor != null && candidate instanceof String) {
 			// Try PE-based comparison (PE should *not* be allowed to escape creating thread)
 			String candidateAsString = (String) candidate;
 			Object candidateAsValue;
