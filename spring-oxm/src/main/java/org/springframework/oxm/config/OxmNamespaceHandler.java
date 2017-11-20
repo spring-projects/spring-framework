08c78554b9564e2024e7068b4cce55736b73dbb2
Juergen Hoeller <jhoeller@pivotal.io>
2017-11-20 22:27:47 +0100

    Deprecate Castor support
    Issue: SPR-16219

--- spring-oxm/src/main/java/org/springframework/oxm/config/OxmNamespaceHandler.java
+++ spring-oxm/src/main/java/org/springframework/oxm/config/OxmNamespaceHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
  */
 public class OxmNamespaceHandler extends NamespaceHandlerSupport {
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void init() {
 		registerBeanDefinitionParser("jaxb2-marshaller", new Jaxb2MarshallerBeanDefinitionParser());
