5adaa1030c219f791b114e3278c3f76acc8e2f7e
Arjen Poutsma <apoutsma@pivotal.io>
2018-01-10 17:54:34 +0100

    Javadoc

--- spring-core/src/main/java/org/springframework/core/io/buffer/DataBuffer.java
+++ spring-core/src/main/java/org/springframework/core/io/buffer/DataBuffer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -190,7 +190,8 @@ public interface DataBuffer {
 
 	/**
 	 * Write one or more {@code DataBuffer}s to this buffer, starting at the current
-	 * writing position.
+	 * writing position. It is the responsibility of the caller to
+	 * {@linkplain DataBufferUtils#release(DataBuffer) release} the given data buffers.
 	 * @param buffers the byte buffers to write into this buffer
 	 * @return this buffer
 	 */
