03118542f4e3b95d24959f3ba907b3c0b891b899
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2018-01-12 11:44:42 -0500

    MockHttpInputMessage respects getBody contract
    Issue: SPR-16367

--- spring-test/src/main/java/org/springframework/mock/http/MockHttpInputMessage.java
+++ spring-test/src/main/java/org/springframework/mock/http/MockHttpInputMessage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ public class MockHttpInputMessage implements HttpInputMessage {
 
 
 	public MockHttpInputMessage(byte[] contents) {
-		this.body = (contents != null ? new ByteArrayInputStream(contents) : null);
+		this.body = new ByteArrayInputStream(contents != null ? contents : new byte[0]);
 	}
 
 	public MockHttpInputMessage(InputStream body) {
