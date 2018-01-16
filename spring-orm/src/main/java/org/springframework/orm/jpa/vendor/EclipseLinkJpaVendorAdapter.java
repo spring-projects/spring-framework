28f15057acf8702a1afe5a8b60803a0755c32451
Timo Meinen <timomeinen@gmail.com>
2018-01-16 13:00:13 +0100

    Log SQL parameters in EclipseLink when using showSql
    As of EclipseLink 2.4.0 - Juno this is not sufficient to log
    SQL parameter binding. Additionally,
    eclipselink.logging.parameters must be enabled.
    
    Issue: SPR-16383
    
    (cherry picked from commit 823a16c)

--- spring-orm/src/main/java/org/springframework/orm/jpa/vendor/EclipseLinkJpaVendorAdapter.java
+++ spring-orm/src/main/java/org/springframework/orm/jpa/vendor/EclipseLinkJpaVendorAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -78,6 +78,7 @@ public class EclipseLinkJpaVendorAdapter extends AbstractJpaVendorAdapter {
 		if (isShowSql()) {
 			jpaProperties.put(PersistenceUnitProperties.CATEGORY_LOGGING_LEVEL_ +
 					org.eclipse.persistence.logging.SessionLog.SQL, Level.FINE.toString());
+			jpaProperties.put(PersistenceUnitProperties.LOGGING_PARAMETERS, Boolean.TRUE.toString());
 		}
 
 		return jpaProperties;
