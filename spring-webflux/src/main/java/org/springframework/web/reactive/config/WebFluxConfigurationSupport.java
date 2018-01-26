fbde97f30258f90a08375dbe6a949ef4f3a172db
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2018-01-26 17:06:01 -0500

    Default order 0 for ServerResponseResultHandler
    Issue: SPR-16420

--- spring-webflux/src/main/java/org/springframework/web/reactive/config/WebFluxConfigurationSupport.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/config/WebFluxConfigurationSupport.java
@@ -418,14 +418,10 @@ public class WebFluxConfigurationSupport implements ApplicationContextAware {
 
 	@Bean
 	public ServerResponseResultHandler serverResponseResultHandler() {
-		ViewResolverRegistry registry = getViewResolverRegistry();
-		List<ViewResolver> resolvers = registry.getViewResolvers();
-
+		List<ViewResolver> resolvers = getViewResolverRegistry().getViewResolvers();
 		ServerResponseResultHandler handler = new ServerResponseResultHandler();
 		handler.setMessageWriters(serverCodecConfigurer().getWriters());
 		handler.setViewResolvers(resolvers);
-		handler.setOrder(registry.getOrder() + 1);
-
 		return handler;
 	}
 
