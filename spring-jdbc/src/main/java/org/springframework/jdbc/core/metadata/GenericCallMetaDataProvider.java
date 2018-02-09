fc93f99f539afb812a839e48ef9160b92da913b6
Juergen Hoeller <jhoeller@pivotal.io>
2018-02-09 13:30:31 +0100

    Relaxed procedure existence check on Oracle (for non-exposed synonyms)
    Issue: SPR-16478

--- spring-jdbc/src/main/java/org/springframework/jdbc/core/metadata/GenericCallMetaDataProvider.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/core/metadata/GenericCallMetaDataProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,6 +38,7 @@ import org.springframework.util.StringUtils;
  * This class can be extended to provide database specific behavior.
  *
  * @author Thomas Risberg
+ * @author Juergen Hoeller
  * @since 2.5
  */
 public class GenericCallMetaDataProvider implements CallMetaDataProvider {
@@ -350,6 +351,12 @@ public class GenericCallMetaDataProvider implements CallMetaDataProvider {
 							"Unable to determine the correct call signature for '" + metaDataProcedureName +
 							"' - package name should be specified separately using '.withCatalogName(\"" +
 							packageName + "\")'");
+				}
+				else if ("Oracle".equals(databaseMetaData.getDatabaseProductName())) {
+					if (logger.isDebugEnabled()) {
+						logger.debug("Oracle JDBC driver did not return procedure/function/signature for '" +
+								metaDataProcedureName + "' - assuming a non-exposed synonym");
+					}
 				}
 				else {
 					throw new InvalidDataAccessApiUsageException(
