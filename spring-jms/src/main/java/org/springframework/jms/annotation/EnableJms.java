d60446a4feec189a1637c3d8f69e1502582df5bf
Juergen Hoeller <jhoeller@pivotal.io>
2018-02-14 15:32:23 +0100

    Explicit documentation notes on transacted sessions vs AUTO_ACKNOWLEDGE
    Issue: SPR-16487
    
    (cherry picked from commit 95f7180)

--- spring-jms/src/main/java/org/springframework/jms/annotation/EnableJms.java
+++ spring-jms/src/main/java/org/springframework/jms/annotation/EnableJms.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,6 +41,7 @@ import org.springframework.context.annotation.Import;
  *       DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
  *       factory.setConnectionFactory(connectionFactory());
  *       factory.setDestinationResolver(destinationResolver());
+ *       factory.setSessionTransacted(true);
  *       factory.setConcurrency("5");
  *       return factory;
  *     }
