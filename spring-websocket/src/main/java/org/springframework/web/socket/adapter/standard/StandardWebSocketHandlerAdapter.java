b35274f5a725181f1253426b442cb63b50c58fa6
Juergen Hoeller <jhoeller@pivotal.io>
2018-02-16 10:22:05 +0100

    WebSocket callbacks need to remain inner classes (retaining generics)

--- spring-websocket/src/main/java/org/springframework/web/socket/adapter/standard/StandardWebSocketHandlerAdapter.java
+++ spring-websocket/src/main/java/org/springframework/web/socket/adapter/standard/StandardWebSocketHandlerAdapter.java
@@ -60,21 +60,44 @@ public class StandardWebSocketHandlerAdapter extends Endpoint {
 	public void onOpen(final javax.websocket.Session session, EndpointConfig config) {
 		this.wsSession.initializeNativeSession(session);
 
+		// The following inner classes need to remain since lambdas would not retain their
+		// declared generic types (which need to be seen by the underlying WebSocket engine)
+
 		if (this.handler.supportsPartialMessages()) {
-			session.addMessageHandler((MessageHandler.Partial<String>)
-					(message, isLast) -> handleTextMessage(session, message, isLast));
-			session.addMessageHandler((MessageHandler.Partial<ByteBuffer>)
-					(message, isLast) -> handleBinaryMessage(session, message, isLast));
+			session.addMessageHandler(new MessageHandler.Partial<String>() {
+				@Override
+				public void onMessage(String message, boolean isLast) {
+					handleTextMessage(session, message, isLast);
+				}
+			});
+			session.addMessageHandler(new MessageHandler.Partial<ByteBuffer>() {
+				@Override
+				public void onMessage(ByteBuffer message, boolean isLast) {
+					handleBinaryMessage(session, message, isLast);
+				}
+			});
 		}
 		else {
-			session.addMessageHandler((MessageHandler.Whole<String>)
-					message -> handleTextMessage(session, message, true));
-			session.addMessageHandler((MessageHandler.Whole<ByteBuffer>)
-					message -> handleBinaryMessage(session, message, true));
+			session.addMessageHandler(new MessageHandler.Whole<String>() {
+				@Override
+				public void onMessage(String message) {
+					handleTextMessage(session, message, true);
+				}
+			});
+			session.addMessageHandler(new MessageHandler.Whole<ByteBuffer>() {
+				@Override
+				public void onMessage(ByteBuffer message) {
+					handleBinaryMessage(session, message, true);
+				}
+			});
 		}
 
-		session.addMessageHandler((MessageHandler.Whole<javax.websocket.PongMessage>)
-				message -> handlePongMessage(session, message.getApplicationData()));
+		session.addMessageHandler(new MessageHandler.Whole<javax.websocket.PongMessage>() {
+			@Override
+			public void onMessage(javax.websocket.PongMessage message) {
+				handlePongMessage(session, message.getApplicationData());
+			}
+		});
 
 		try {
 			this.handler.afterConnectionEstablished(this.wsSession);
