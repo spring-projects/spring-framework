36cfdf68b15932ac3970a5e0c81462e42adaf1da
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2018-03-01 15:52:26 -0500

    Correct log level
    Issue: SPR-16528

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/ResponseEntityExceptionHandler.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/ResponseEntityExceptionHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -457,8 +457,8 @@ public abstract class ResponseEntityExceptionHandler {
 			HttpServletRequest request = servletWebRequest.getRequest();
 			HttpServletResponse response = servletWebRequest.getResponse();
 			if (response != null && response.isCommitted()) {
-				if (logger.isErrorEnabled()) {
-					logger.error("Async timeout for " + request.getMethod() + " [" + request.getRequestURI() + "]");
+				if (logger.isDebugEnabled()) {
+					logger.debug("Async timeout for " + request.getMethod() + " [" + request.getRequestURI() + "]");
 				}
 				return null;
 			}
