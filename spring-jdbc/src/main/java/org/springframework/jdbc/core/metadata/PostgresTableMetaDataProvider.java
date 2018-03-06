4a4f2c2f0810416b25165545988690d84b55347c
Juergen Hoeller <jhoeller@pivotal.io>
2018-03-06 13:31:26 +0100

    Drop version check in isGetGeneratedKeysSimulated (for PostgreSQL 10)
    Issue: SPR-16556

--- spring-jdbc/src/main/java/org/springframework/jdbc/core/metadata/PostgresTableMetaDataProvider.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/core/metadata/PostgresTableMetaDataProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,19 +33,10 @@ public class PostgresTableMetaDataProvider extends GenericTableMetaDataProvider 
 		super(databaseMetaData);
 	}
 
+
 	@Override
 	public boolean isGetGeneratedKeysSimulated() {
-		String version = getDatabaseVersion();
-		if (version != null && version.compareTo("8.2.0") >= 0) {
-			return true;
-		}
-		else {
-			if (logger.isWarnEnabled()) {
-				logger.warn("PostgreSQL does not support getGeneratedKeys or INSERT ... RETURNING in version " +
-						version);
-			}
-			return false;
-		}
+		return true;
 	}
 
 	@Override
