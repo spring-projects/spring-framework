fef0c7b15193e61a19302554ec3c421b64af7b7f
Sam Brannen <sam@sambrannen.com>
2018-03-07 13:22:21 +0100

    Throw AssertionError instead of IAE in MockRestRequestMatchers

--- spring-test/src/main/java/org/springframework/test/web/client/match/MockRestRequestMatchers.java
+++ spring-test/src/main/java/org/springframework/test/web/client/match/MockRestRequestMatchers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -147,7 +147,7 @@ public abstract class MockRestRequestMatchers {
 		List<String> values = map.get(name);
 
 		String message = "Expected " + valueType + " <" + name + ">";
-		Assert.notNull(values, message);
+		assertTrue(message + " to exist but was null", values != null);
 
 		assertTrue(message + " to have at least <" + count + "> values but found " + values,
 				count <= values.size());
