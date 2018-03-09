00fea23e6b47829f0be7e603c0ba023c5763796f
Sam Brannen <sam@sambrannen.com>
2018-03-09 14:51:37 +0100

    Introduce test for WebTestClient with RegEx-based JsonPath

--- spring-test/src/test/java/org/springframework/test/web/reactive/server/samples/JsonContentTests.java
+++ spring-test/src/test/java/org/springframework/test/web/reactive/server/samples/JsonContentTests.java
@@ -20,11 +20,13 @@ import java.net.URI;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,6 +64,19 @@ public class JsonContentTests {
 	}
 
 	@Test
+	// See https://stackoverflow.com/questions/49149376/webtestclient-check-that-jsonpath-contains-sub-string
+	public void jsonPathContainsSubstringViaRegularExpression() throws Exception {
+		this.client.get().uri("/persons/John")
+				.accept(MediaType.APPLICATION_JSON_UTF8)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				// The following determines if at least one person is returned with a
+				// name containing "oh", and "John" matches that.
+				.jsonPath("$[?(@.name =~ /.*oh.*/)].name").hasJsonPath();
+	}
+
+	@Test
 	public void postJsonContent() throws Exception {
 		this.client.post().uri("/persons")
 				.contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -78,6 +93,11 @@ public class JsonContentTests {
 		@GetMapping("/persons")
 		Flux<Person> getPersons() {
 			return Flux.just(new Person("Jane"), new Person("Jason"), new Person("John"));
+		}
+
+		@GetMapping("/persons/{name}")
+		Mono<Person> getPerson(@PathVariable String name) {
+			return Mono.just(new Person(name));
 		}
 
 		@PostMapping
