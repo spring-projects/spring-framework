98ad23bef8e2e04143f8f5b201380543a8d8c0c3
Juergen Hoeller <jhoeller@pivotal.io>
2018-03-27 17:04:59 +0200

    Consistent logging of encoded path evaluation failure
    Issue: SPR-16616

--- spring-webmvc/src/main/java/org/springframework/web/servlet/resource/PathResourceResolver.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/resource/PathResourceResolver.java
@@ -284,7 +284,12 @@ public class PathResourceResolver extends AbstractResourceResolver {
 			// Use URLDecoder (vs UriUtils) to preserve potentially decoded UTF-8 chars...
 			try {
 				String decodedPath = URLDecoder.decode(resourcePath, "UTF-8");
-				return (decodedPath.contains("../") || decodedPath.contains("..\\"));
+				if (decodedPath.contains("../") || decodedPath.contains("..\\")) {
+					if (logger.isTraceEnabled()) {
+						logger.trace("Ignoring invalid resource path with escape sequences [" + resourcePath + "]");
+					}
+					return true;
+				}
 			}
 			catch (UnsupportedEncodingException ex) {
 				// Should never happen...
