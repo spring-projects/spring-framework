d1a0b8d53f92ec410d51bdd89aee979107541bd4
Stephane Nicoll <snicoll@pivotal.io>
2018-03-28 11:03:53 +0200

    Clarify the format supported by @PropertySource
    Issue: SPR-16563

--- spring-context/src/main/java/org/springframework/context/annotation/PropertySource.java
+++ spring-context/src/main/java/org/springframework/context/annotation/PropertySource.java
@@ -173,8 +173,9 @@ public @interface PropertySource {
 
 	/**
 	 * Indicate the resource location(s) of the properties file to be loaded.
-	 * For example, {@code "classpath:/com/myco/app.properties"} or
-	 * {@code "file:/path/to/file"}.
+	 * <p>Both traditional and XML-based properties file formats are supported
+	 * &mdash; for example, {@code "classpath:/com/myco/app.properties"}
+	 * or {@code "file:/path/to/file.xml"}.
 	 * <p>Resource location wildcards (e.g. *&#42;/*.properties) are not permitted;
 	 * each location must evaluate to exactly one {@code .properties} resource.
 	 * <p>${...} placeholders will be resolved against any/all property sources already
