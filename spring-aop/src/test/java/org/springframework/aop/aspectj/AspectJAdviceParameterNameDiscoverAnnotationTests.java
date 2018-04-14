c1385f52c2fe4e9deb8a2e68f2e7c80caa49ba94
Juergen Hoeller <jhoeller@pivotal.io>
2018-04-14 21:02:53 +0200

    Polishing
    (cherry picked from commit de4ff4b)

--- spring-aop/src/test/java/org/springframework/aop/aspectj/AspectJAdviceParameterNameDiscoverAnnotationTests.java
+++ spring-aop/src/test/java/org/springframework/aop/aspectj/AspectJAdviceParameterNameDiscoverAnnotationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,6 @@
 
 package org.springframework.aop.aspectj;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.junit.Test;
 
@@ -30,13 +27,7 @@ import org.junit.Test;
  * @author Adrian Colyer
  * @author Chris Beams
  */
-public class AspectJAdviceParameterNameDiscoverAnnotationTests
-		extends AspectJAdviceParameterNameDiscovererTests {
-
-	@Retention(RetentionPolicy.RUNTIME)
-	@interface MyAnnotation {}
-
-	public void pjpAndAnAnnotation(ProceedingJoinPoint pjp, MyAnnotation ann) {}
+public class AspectJAdviceParameterNameDiscoverAnnotationTests extends AspectJAdviceParameterNameDiscovererTests {
 
 	@Test
 	public void testAnnotationBinding() {
@@ -44,5 +35,10 @@ public class AspectJAdviceParameterNameDiscoverAnnotationTests
 				"execution(* *(..)) && @annotation(ann)",
 				new String[] {"thisJoinPoint","ann"});
 	}
+
+
+	public void pjpAndAnAnnotation(ProceedingJoinPoint pjp, MyAnnotation ann) {}
+
+	@interface MyAnnotation {}
 
 }
