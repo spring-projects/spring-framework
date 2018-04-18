fb0e3e935638e866b39429faddec2c187c356384
nkjackzhang <nkzhangjie@gmail.com>
2018-04-18 17:23:28 +0800

    Fix a typo in @Nullable Javadoc

--- spring-core/src/main/java/org/springframework/lang/Nullable.java
+++ spring-core/src/main/java/org/springframework/lang/Nullable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@ import javax.annotation.meta.When;
  * <p>Should be used at parameter, return value, and field level. Methods override should
  * repeat parent {@code @Nullable} annotations unless they behave differently.
  *
- * <p>Can be used in association with {@code NonNullApi} or {@code @NonNullFields} to
+ * <p>Can be used in association with {@code @NonNullApi} or {@code @NonNullFields} to
  * override the default non-nullable semantic to nullable.
  *
  * @author Sebastien Deleuze
