d74a2730ec43764a8682650ca8ce4855e856b149
Juergen Hoeller <jhoeller@pivotal.io>
2018-05-01 23:51:05 +0200

    SimpleClientHttpResponse catches any Exception on close
    Issue: SPR-16773
    
    (cherry picked from commit 21fad8e)

--- spring-web/src/main/java/org/springframework/http/client/SimpleClientHttpResponse.java
+++ spring-web/src/main/java/org/springframework/http/client/SimpleClientHttpResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -96,7 +96,7 @@ final class SimpleClientHttpResponse extends AbstractClientHttpResponse {
 				StreamUtils.drain(this.responseStream);
 				this.responseStream.close();
 			}
-			catch (IOException ex) {
+			catch (Exception ex) {
 				// ignore
 			}
 		}
