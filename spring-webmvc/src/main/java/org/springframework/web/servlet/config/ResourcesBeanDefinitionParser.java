b4f83dbdc3b4d22cb2a8add7ac15bc2e1103b5b4
Juergen Hoeller <jhoeller@pivotal.io>
2018-05-07 14:30:50 +0200

    Polishing

--- spring-webmvc/src/main/java/org/springframework/web/servlet/config/ResourcesBeanDefinitionParser.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/config/ResourcesBeanDefinitionParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -201,7 +201,7 @@ class ResourcesBeanDefinitionParser implements BeanDefinitionParser {
 
 
 	private CacheControl parseCacheControl(Element element) {
-		CacheControl cacheControl = CacheControl.empty();
+		CacheControl cacheControl;
 		if ("true".equals(element.getAttribute("no-cache"))) {
 			cacheControl = CacheControl.noCache();
 		}
@@ -211,6 +211,10 @@ class ResourcesBeanDefinitionParser implements BeanDefinitionParser {
 		else if (element.hasAttribute("max-age")) {
 			cacheControl = CacheControl.maxAge(Long.parseLong(element.getAttribute("max-age")), TimeUnit.SECONDS);
 		}
+		else {
+			cacheControl = CacheControl.empty();
+		}
+
 		if ("true".equals(element.getAttribute("must-revalidate"))) {
 			cacheControl = cacheControl.mustRevalidate();
 		}
