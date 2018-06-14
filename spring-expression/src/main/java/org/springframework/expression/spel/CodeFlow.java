13729364ab26bd2b747343b69c28214e706a3d79
Phillip Webb <pwebb@pivotal.io>
2018-06-13 18:38:06 -0700

    Consistently use only one statement per line
    Ensure that only one statement is used per line.
    
    Issue: SPR-16968

--- spring-expression/src/main/java/org/springframework/expression/spel/CodeFlow.java
+++ spring-expression/src/main/java/org/springframework/expression/spel/CodeFlow.java
@@ -907,16 +907,33 @@ public class CodeFlow implements Opcodes {
 	public static void insertArrayStore(MethodVisitor mv, String arrayElementType) {
 		if (arrayElementType.length()==1) {
 			switch (arrayElementType.charAt(0)) {
-				case 'I': mv.visitInsn(IASTORE); break;
-				case 'J': mv.visitInsn(LASTORE); break;
-				case 'F': mv.visitInsn(FASTORE); break;
-				case 'D': mv.visitInsn(DASTORE); break;
-				case 'B': mv.visitInsn(BASTORE); break;
-				case 'C': mv.visitInsn(CASTORE); break;
-				case 'S': mv.visitInsn(SASTORE); break;
-				case 'Z': mv.visitInsn(BASTORE); break;
+				case 'I':
+					mv.visitInsn(IASTORE);
+					break;
+				case 'J':
+					mv.visitInsn(LASTORE);
+					break;
+				case 'F':
+					mv.visitInsn(FASTORE);
+					break;
+				case 'D':
+					mv.visitInsn(DASTORE);
+					break;
+				case 'B':
+					mv.visitInsn(BASTORE);
+					break;
+				case 'C':
+					mv.visitInsn(CASTORE);
+					break;
+				case 'S':
+					mv.visitInsn(SASTORE);
+					break;
+				case 'Z':
+					mv.visitInsn(BASTORE);
+					break;
 				default:
-					throw new IllegalArgumentException("Unexpected arraytype "+arrayElementType.charAt(0));
+					throw new IllegalArgumentException(
+							"Unexpected arraytype " + arrayElementType.charAt(0));
 			}
 		}
 		else {
