1c628293a2eda10ab07d77f4d2ae832d134c578c
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-08-08 17:04:25 +0200

    Add doc & tests to Jaxb2XmlEncoder for collections
    Issue: SPR-16363

--- spring-web/src/main/java/org/springframework/http/codec/xml/Jaxb2XmlEncoder.java
+++ spring-web/src/main/java/org/springframework/http/codec/xml/Jaxb2XmlEncoder.java
@@ -40,7 +40,11 @@ import org.springframework.util.MimeType;
 import org.springframework.util.MimeTypeUtils;
 
 /**
- * Encode from {@code Object} stream to a byte stream containing XML elements.
+ * Encode from single value to a byte stream containing XML elements.
+ *
+ * <p>{@link javax.xml.bind.annotation.XmlElements @XmlElements} and
+ * {@link javax.xml.bind.annotation.XmlElement @XmlElement} can be used to specify how
+ * collections should be marshalled.
  *
  * @author Sebastien Deleuze
  * @author Arjen Poutsma
