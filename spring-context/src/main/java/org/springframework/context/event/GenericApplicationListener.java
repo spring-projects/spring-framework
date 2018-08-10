c437a0d1c3067e781ae3ae73ba197640402e6555
Juergen Hoeller <jhoeller@pivotal.io>
2018-08-10 15:42:30 +0200

    Declare default methods for supportsSourceType and getOrder
    Issue: SPR-17163

--- spring-context/src/main/java/org/springframework/context/event/GenericApplicationListener.java
+++ spring-context/src/main/java/org/springframework/context/event/GenericApplicationListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,24 +24,40 @@ import org.springframework.lang.Nullable;
 
 /**
  * Extended variant of the standard {@link ApplicationListener} interface,
- * exposing further metadata such as the supported event type.
+ * exposing further metadata such as the supported event and source type.
  *
- * <p>As of Spring Framework 4.2, supersedes {@link SmartApplicationListener} with
- * proper handling of generics-based event.
+ * <p>As of Spring Framework 4.2, this interface supersedes the Class-based
+ * {@link SmartApplicationListener} with full handling of generic event types.
  *
  * @author Stephane Nicoll
  * @since 4.2
+ * @see SmartApplicationListener
+ * @see GenericApplicationListenerAdapter
  */
 public interface GenericApplicationListener extends ApplicationListener<ApplicationEvent>, Ordered {
 
 	/**
 	 * Determine whether this listener actually supports the given event type.
+	 * @param eventType the event type (never {@code null})
 	 */
 	boolean supportsEventType(ResolvableType eventType);
 
 	/**
 	 * Determine whether this listener actually supports the given source type.
+	 * <p>The default implementation always returns {@code true}.
+	 * @param sourceType the source type, or {@code null} if no source
 	 */
-	boolean supportsSourceType(@Nullable Class<?> sourceType);
+	default boolean supportsSourceType(@Nullable Class<?> sourceType) {
+		return true;
+	}
+
+	/**
+	 * Determine this listener's order in a set of listeners for the same event.
+	 * <p>The default implementation returns {@link #LOWEST_PRECEDENCE}.
+	 */
+	@Override
+	default int getOrder() {
+		return LOWEST_PRECEDENCE;
+	}
 
 }
