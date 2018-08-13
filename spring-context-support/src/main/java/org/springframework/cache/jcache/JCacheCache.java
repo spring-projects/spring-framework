777bd0d022cb61d5570ccb6617a4b77616674a1b
Sam Brannen <sam@sambrannen.com>
2018-08-13 12:40:34 +0200

    Fix Checkstyle violation in JCacheCache

--- spring-context-support/src/main/java/org/springframework/cache/jcache/JCacheCache.java
+++ spring-context-support/src/main/java/org/springframework/cache/jcache/JCacheCache.java
@@ -17,7 +17,6 @@
 package org.springframework.cache.jcache;
 
 import java.util.concurrent.Callable;
-
 import javax.cache.Cache;
 import javax.cache.processor.EntryProcessor;
 import javax.cache.processor.EntryProcessorException;
