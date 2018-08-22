6ef0938a9206d0034bf4d565554ec82b9735cd23
Juergen Hoeller <jhoeller@pivotal.io>
2018-08-22 14:12:39 +0200

    Defensive URL cleaning (preserving the original URL if possible)
    Issue: SPR-17198

--- spring-core/src/main/java/org/springframework/core/io/UrlResource.java
+++ spring-core/src/main/java/org/springframework/core/io/UrlResource.java
@@ -141,18 +141,20 @@ public class UrlResource extends AbstractFileResolvingResource {
 	 * Determine a cleaned URL for the given original URL.
 	 * @param originalUrl the original URL
 	 * @param originalPath the original URL path
-	 * @return the cleaned URL
+	 * @return the cleaned URL (possibly the original URL as-is)
 	 * @see org.springframework.util.StringUtils#cleanPath
 	 */
 	private URL getCleanedUrl(URL originalUrl, String originalPath) {
-		try {
-			return new URL(StringUtils.cleanPath(originalPath));
+		String cleanedPath = StringUtils.cleanPath(originalPath);
+		if (!cleanedPath.equals(originalPath)) {
+			try {
+				return new URL(cleanedPath);
+			}
+			catch (MalformedURLException ex) {
+				// Cleaned URL path cannot be converted to URL -> take original URL.
+			}
 		}
-		catch (MalformedURLException ex) {
-			// Cleaned URL path cannot be converted to URL
-			// -> take original URL.
-			return originalUrl;
-		}
+		return originalUrl;
 	}
 
 	/**
