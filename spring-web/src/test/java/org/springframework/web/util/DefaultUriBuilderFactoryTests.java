e4c84ec7576bc2986357b5bf12b646d663ef9730
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2018-11-09 14:13:51 -0500

    Consistent default encoding in DefaultUriBuilderFactory
    Issue: SPR-17465

--- spring-web/src/test/java/org/springframework/web/util/DefaultUriBuilderFactoryTests.java
+++ spring-web/src/test/java/org/springframework/web/util/DefaultUriBuilderFactoryTests.java
@@ -35,7 +35,14 @@ public class DefaultUriBuilderFactoryTests {
 	@Test
 	public void defaultSettings() {
 		DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
-		URI uri = factory.uriString("/foo").pathSegment("{id}").build("a/b");
+		URI uri = factory.uriString("/foo/{id}").build("a/b");
+		assertEquals("/foo/a%2Fb", uri.toString());
+	}
+
+	@Test // SPR-17465
+	public void defaultSettingsWithBuilder() {
+		DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
+		URI uri = factory.builder().path("/foo/{id}").build("a/b");
 		assertEquals("/foo/a%2Fb", uri.toString());
 	}
 
