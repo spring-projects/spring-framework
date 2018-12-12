270099383be3020948967d9615143ad6d30675a7
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2018-12-12 09:40:23 -0500

    Add protected method in StandaloneMockMvcBuilder
    To allow sub-classes to register additional MVC infrastructure.
    
    Issue: SPR-17520

--- spring-test/src/main/java/org/springframework/test/web/servlet/setup/StandaloneMockMvcBuilder.java
+++ spring-test/src/main/java/org/springframework/test/web/servlet/setup/StandaloneMockMvcBuilder.java
@@ -399,6 +399,8 @@ public class StandaloneMockMvcBuilder extends AbstractMockMvcBuilder<StandaloneM
 
 		this.flashMapManager = new SessionFlashMapManager();
 		wac.addBean(DispatcherServlet.FLASH_MAP_MANAGER_BEAN_NAME, this.flashMapManager);
+
+		extendMvcSingletons(sc).forEach(wac::addBean);
 	}
 
 	private List<ViewResolver> initViewResolvers(WebApplicationContext wac) {
@@ -410,6 +412,18 @@ public class StandaloneMockMvcBuilder extends AbstractMockMvcBuilder<StandaloneM
 			}
 		}
 		return this.viewResolvers;
+	}
+
+	/**
+	 * This method could be used from a sub-class to register additional Spring
+	 * MVC infrastructure such as additional {@code HandlerMapping},
+	 * {@code HandlerAdapter}, and others.
+	 * @param servletContext the ServletContext
+	 * @return a map with additional MVC infrastructure object instances
+	 * @since 5.1.4
+	 */
+	protected Map<String, Object> extendMvcSingletons(@Nullable ServletContext servletContext) {
+		return Collections.emptyMap();
 	}
 
 
