4972d85ae01014e98b94c4714c49ca7540d7c3ff
Phillip Webb <pwebb@pivotal.io>
2019-01-31 11:03:57 -0800

    Create a new API for handling merged annotations
    Add new `MergedAnnotations` and `MergedAnnotation` interfaces that
    attempt to provide a uniform way for dealing with merged annotations.
    
    Specifically, the new API provides alternatives for the static methods
    in `AnnotationUtils` and `AnnotatedElementUtils` and supports Spring's
    comprehensive annotation attribute `@AliasFor` features. The interfaces
    also open the possibility of the same API being exposed from the
    `AnnotationMetadata` interface.
    
    Additional utility classes for collecting, filtering and selecting
    annotations have also been added.
    
    Typical usage for the new API would be something like:
    
    	MergedAnnotations.from(Example.class)
    		.stream(MyAnnotation.class)
    		.map(a -> a.getString("value"))
    		.forEach(System.out::println);
    
    Closes gh-21697

---
+++ spring-core/src/test/java/org/springframework/core/annotation/AnnotationFilterTests.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2002-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.core.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+
+import org.springframework.lang.Nullable;
+import org.springframework.util.ObjectUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+/**
+ * Tests for {@link AnnotationFilter}.
+ *
+ * @author Phillip Webb
+ */
+public class AnnotationFilterTests {
+
+	private static final AnnotationFilter FILTER = annotationType -> ObjectUtils.nullSafeEquals(
+			annotationType, TestAnnotation.class.getName());
+
+	@Test
+	public void matchesAnnotationWhenAnnotationIsNullReturnsFalse() {
+		TestAnnotation annotation = null;
+		assertThat(FILTER.matches(annotation)).isFalse();
+	}
+
+	@Test
+	public void matchesAnnotationWhenMatchReturnsTrue() {
+		TestAnnotation annotation = WithTestAnnotation.class.getDeclaredAnnotation(
+				TestAnnotation.class);
+		assertThat(FILTER.matches(annotation)).isTrue();
+	}
+
+	@Test
+	public void matchesAnnotationWhenNoMatchReturnsFalse() {
+		OtherAnnotation annotation = WithOtherAnnotation.class.getDeclaredAnnotation(
+				OtherAnnotation.class);
+		assertThat(FILTER.matches(annotation)).isFalse();
+	}
+
+	@Test
+	public void matchesAnnotationClassWhenAnnotationClassIsNullReturnsFalse() {
+		Class<Annotation> annotationType = null;
+		assertThat(FILTER.matches(annotationType)).isFalse();
+	}
+
+	@Test
+	public void matchesAnnotationClassWhenMatchReturnsTrue() {
+		Class<TestAnnotation> annotationType = TestAnnotation.class;
+		assertThat(FILTER.matches(annotationType)).isTrue();
+	}
+
+	@Test
+	public void matchesAnnotationClassWhenNoMatchReturnsFalse() {
+		Class<OtherAnnotation> annotationType = OtherAnnotation.class;
+		assertThat(FILTER.matches(annotationType)).isFalse();
+	}
+
+	@Test
+	public void plainWhenJavaLangAnnotationReturnsTrue() {
+		assertThat(AnnotationFilter.PLAIN.matches(Retention.class)).isTrue();
+	}
+
+	@Test
+	public void plainWhenSpringLangAnnotationReturnsTrue() {
+		assertThat(AnnotationFilter.PLAIN.matches(Nullable.class)).isTrue();
+	}
+
+	@Test
+	public void plainWhenOtherAnnotationReturnsFalse() {
+		assertThat(AnnotationFilter.PLAIN.matches(TestAnnotation.class)).isFalse();
+	}
+
+	@Test
+	public void javaWhenJavaLangAnnotationReturnsTrue() {
+		assertThat(AnnotationFilter.JAVA.matches(Retention.class)).isTrue();
+	}
+
+	@Test
+	public void javaWhenSpringLangAnnotationReturnsFalse() {
+		assertThat(AnnotationFilter.JAVA.matches(Nullable.class)).isFalse();
+	}
+
+	@Test
+	public void javaWhenOtherAnnotationReturnsFalse() {
+		assertThat(AnnotationFilter.JAVA.matches(TestAnnotation.class)).isFalse();
+	}
+
+	@Test
+	public void noneWhenNonNullReturnsFalse() {
+		assertThat(AnnotationFilter.NONE.matches(Retention.class)).isFalse();
+		assertThat(AnnotationFilter.NONE.matches(Nullable.class)).isFalse();
+		assertThat(AnnotationFilter.NONE.matches(TestAnnotation.class)).isFalse();
+		assertThat(AnnotationFilter.NONE.matches((Annotation) null)).isFalse();
+		assertThat(AnnotationFilter.NONE.matches((Class<Annotation>) null)).isFalse();
+		assertThat(AnnotationFilter.NONE.matches((String) null)).isFalse();
+	}
+
+	@Test
+	public void pacakgesReturnsPackagesAnnotationFilter() {
+		assertThat(AnnotationFilter.packages("com.example")).isInstanceOf(
+				PackagesAnnotationFilter.class);
+	}
+
+	@Test
+	public void mostAppropriateForCollectionWhenAnnotationTypesIsNullThrowsException() {
+		assertThatIllegalArgumentException().isThrownBy(
+				() -> AnnotationFilter.mostAppropriateFor(
+						(Collection<Class<? extends Annotation>>) null)).withMessage(
+								"AnnotationTypes must not be null");
+	}
+
+	@Test
+	public void mostAppropriateForCollectionReturnsPlainWhenPossible() {
+		AnnotationFilter filter = AnnotationFilter.mostAppropriateFor(
+				Arrays.asList(TestAnnotation.class, OtherAnnotation.class));
+		assertThat(filter).isSameAs(AnnotationFilter.PLAIN);
+	}
+
+	@Test
+	public void mostAppropriateForCollectionWhenCantUsePlainReturnsNone() {
+		AnnotationFilter filter = AnnotationFilter.mostAppropriateFor(Arrays.asList(
+				TestAnnotation.class, OtherAnnotation.class, Nullable.class));
+		assertThat(filter).isSameAs(AnnotationFilter.NONE);
+	}
+
+	@Test
+	public void mostAppropriateForArrayWhenAnnotationTypesIsNullThrowsException() {
+		assertThatIllegalArgumentException().isThrownBy(
+				() -> AnnotationFilter.mostAppropriateFor(
+						(Class<? extends Annotation>[]) null)).withMessage(
+								"AnnotationTypes must not be null");
+	}
+
+	@Test
+	public void mostAppropriateForArrayReturnsPlainWhenPossible() {
+		AnnotationFilter filter = AnnotationFilter.mostAppropriateFor(
+				TestAnnotation.class, OtherAnnotation.class);
+		assertThat(filter).isSameAs(AnnotationFilter.PLAIN);
+	}
+
+	@Test
+	public void mostAppropriateForArrayWhenCantUsePlainReturnsNone() {
+		AnnotationFilter filter = AnnotationFilter.mostAppropriateFor(
+				TestAnnotation.class, OtherAnnotation.class, Nullable.class);
+		assertThat(filter).isSameAs(AnnotationFilter.NONE);
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	static @interface TestAnnotation {
+
+	}
+
+	@TestAnnotation
+	static class WithTestAnnotation {
+
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	static @interface OtherAnnotation {
+
+	}
+
+	@OtherAnnotation
+	static class WithOtherAnnotation {
+
+	}
+
+}
