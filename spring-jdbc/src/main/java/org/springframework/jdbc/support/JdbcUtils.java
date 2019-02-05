ac4525ddf8b280759c0d3ba63d3569dda64a2e3a
Juergen Hoeller <jhoeller@pivotal.io>
2019-02-05 16:02:17 +0100

    Detect MariaDB as MySQL (for mariadb-java-client 2.4+ compatibility)
    Closes gh-22344

--- spring-jdbc/src/main/java/org/springframework/jdbc/support/JdbcUtils.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/support/JdbcUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -417,6 +417,9 @@ public abstract class JdbcUtils {
 		String name = source;
 		if (source != null && source.startsWith("DB2")) {
 			name = "DB2";
+		}
+		else if ("MariaDB".equals(source)) {
+			name = "MySQL";
 		}
 		else if ("Sybase SQL Server".equals(source) ||
 				"Adaptive Server Enterprise".equals(source) ||
