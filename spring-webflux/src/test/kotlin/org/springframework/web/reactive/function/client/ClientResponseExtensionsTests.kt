0b9522c84e28f5784bb8d9b5eaf8d9c5cf46bc37
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-02-05 15:42:12 +0100

    Migrate Kotlin Mockito tests to Mockk
    Closes gh-22345

--- spring-webflux/src/test/kotlin/org/springframework/web/reactive/function/client/ClientResponseExtensionsTests.kt
+++ spring-webflux/src/test/kotlin/org/springframework/web/reactive/function/client/ClientResponseExtensionsTests.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,13 +16,9 @@
 
 package org.springframework.web.reactive.function.client
 
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Answers
-import org.mockito.Mock
-import org.mockito.Mockito.times
-import org.mockito.Mockito.verify
-import org.mockito.junit.MockitoJUnitRunner
 import org.springframework.core.ParameterizedTypeReference
 
 /**
@@ -30,34 +26,32 @@ import org.springframework.core.ParameterizedTypeReference
  *
  * @author Sebastien Deleuze
  */
-@RunWith(MockitoJUnitRunner::class)
 class ClientResponseExtensionsTests {
 
-	@Mock(answer = Answers.RETURNS_MOCKS)
-	lateinit var response: ClientResponse
+	val response = mockk<ClientResponse>(relaxed = true)
 
 	@Test
 	fun `bodyToMono with reified type parameters`() {
 		response.bodyToMono<List<Foo>>()
-		verify(response, times(1)).bodyToMono(object : ParameterizedTypeReference<List<Foo>>() {})
+		verify { response.bodyToMono(object : ParameterizedTypeReference<List<Foo>>() {}) }
 	}
 
 	@Test
 	fun `bodyToFlux with reified type parameters`() {
 		response.bodyToFlux<List<Foo>>()
-		verify(response, times(1)).bodyToFlux(object : ParameterizedTypeReference<List<Foo>>() {})
+		verify { response.bodyToFlux(object : ParameterizedTypeReference<List<Foo>>() {}) }
 	}
 
 	@Test
 	fun `toEntity with reified type parameters`() {
 		response.toEntity<List<Foo>>()
-		verify(response, times(1)).toEntity(object : ParameterizedTypeReference<List<Foo>>() {})
+		verify { response.toEntity(object : ParameterizedTypeReference<List<Foo>>() {}) }
 	}
 
 	@Test
 	fun `ResponseSpec#toEntityList with reified type parameters`() {
 		response.toEntityList<List<Foo>>()
-		verify(response, times(1)).toEntityList(object : ParameterizedTypeReference<List<Foo>>() {})
+		verify { response.toEntityList(object : ParameterizedTypeReference<List<Foo>>() {}) }
 	}
 
 	class Foo
