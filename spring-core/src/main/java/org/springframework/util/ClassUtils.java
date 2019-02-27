40da78e9df4132044643c93ba46bb093aa731541
Sam Brannen <sbrannen@pivotal.io>
2019-02-27 16:30:05 +0100

    Polishing

--- spring-core/src/main/java/org/springframework/util/ClassUtils.java
+++ spring-core/src/main/java/org/springframework/util/ClassUtils.java
@@ -772,7 +772,7 @@ public abstract class ClassUtils {
 	 * conflicting method signatures (or a similar constraint is violated)
 	 * @see java.lang.reflect.Proxy#getProxyClass
 	 */
-	@SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")  // on JDK 9
 	public static Class<?> createCompositeInterface(Class<?>[] interfaces, @Nullable ClassLoader classLoader) {
 		Assert.notEmpty(interfaces, "Interfaces must not be empty");
 		return Proxy.getProxyClass(classLoader, interfaces);
