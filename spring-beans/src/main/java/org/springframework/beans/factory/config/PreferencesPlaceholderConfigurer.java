aa89a579530b5a519d26e54829e8ffc2f8b4115c
Juergen Hoeller <jhoeller@pivotal.io>
2019-02-27 17:57:21 +0100

    Polishing

--- spring-beans/src/main/java/org/springframework/beans/factory/config/PreferencesPlaceholderConfigurer.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/PreferencesPlaceholderConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -121,7 +121,7 @@ public class PreferencesPlaceholderConfigurer extends PropertyPlaceholderConfigu
 	@Nullable
 	protected String resolvePlaceholder(@Nullable String path, String key, Preferences preferences) {
 		if (path != null) {
-			 // Do not create the node if it does not exist...
+			// Do not create the node if it does not exist...
 			try {
 				if (preferences.nodeExists(path)) {
 					return preferences.node(path).get(key, null);
