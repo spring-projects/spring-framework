74e9825a654b9699ccea7b481629a8c297f6ef73
Sam Brannen <sbrannen@pivotal.io>
2019-03-01 20:18:04 +0100

    Fix CheckStyle violation

--- spring-tx/src/main/java/org/springframework/transaction/package-info.java
+++ spring-tx/src/main/java/org/springframework/transaction/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Spring's core transaction management APIs (independent of any specific transaction
- * management system); an exception hierarchy for Spring's transaction infrastructure; 
+ * management system); an exception hierarchy for Spring's transaction infrastructure;
  * and transaction manager, definition, and status interfaces.
  */
 @NonNullApi
