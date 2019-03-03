6853d50ddb10a7347b4340f455fbba52c92300e8
Sam Brannen <sbrannen@pivotal.io>
2019-03-03 16:29:49 +0100

    Polishing

--- spring-aop/src/main/java/org/springframework/aop/interceptor/AsyncUncaughtExceptionHandler.java
+++ spring-aop/src/main/java/org/springframework/aop/interceptor/AsyncUncaughtExceptionHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import java.lang.reflect.Method;
  *
  * <p>An asynchronous method usually returns a {@link java.util.concurrent.Future}
  * instance that gives access to the underlying exception. When the method does
- * not provide that return type, this handler can be used to managed such
+ * not provide that return type, this handler can be used to manage such
  * uncaught exceptions.
  *
  * @author Stephane Nicoll
--- spring-context/src/test/java/org/springframework/scheduling/annotation/TestableAsyncUncaughtExceptionHandler.java
+++ spring-context/src/test/java/org/springframework/scheduling/annotation/TestableAsyncUncaughtExceptionHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import static org.junit.Assert.*;
 
 /**
- * A {@link AsyncUncaughtExceptionHandler} implementation used for testing purposes.
+ * An {@link AsyncUncaughtExceptionHandler} implementation used for testing purposes.
  * @author Stephane Nicoll
  */
 class TestableAsyncUncaughtExceptionHandler
