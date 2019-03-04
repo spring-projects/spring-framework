ef91313342a5f6173080575207d3b93fc6185df6
www <rupertwong@163.com>
2019-03-05 00:24:39 +0800

    Use String::isEmpty instead of "".equals(arg) when arg is not null

--- spring-test/src/main/java/org/springframework/test/web/servlet/setup/PatternMappingFilterProxy.java
+++ spring-test/src/main/java/org/springframework/test/web/servlet/setup/PatternMappingFilterProxy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,7 +82,7 @@ final class PatternMappingFilterProxy implements Filter {
 			this.exactMatches.add(urlPattern.substring(0, urlPattern.length() - 2));
 		}
 		else {
-			if ("".equals(urlPattern)) {
+			if (urlPattern.isEmpty()) {
 				urlPattern = "/";
 			}
 			this.exactMatches.add(urlPattern);
