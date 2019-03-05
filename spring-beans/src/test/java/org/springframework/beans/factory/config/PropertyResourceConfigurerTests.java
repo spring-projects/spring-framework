eeed20d8d93e7024a69ca5253410289baf83f17b
Juergen Hoeller <jhoeller@pivotal.io>
2019-03-05 18:25:36 +0100

    Polishing

--- spring-beans/src/test/java/org/springframework/beans/factory/config/PropertyResourceConfigurerTests.java
+++ spring-beans/src/test/java/org/springframework/beans/factory/config/PropertyResourceConfigurerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,6 +59,7 @@ import static org.springframework.tests.TestResourceUtils.*;
  * @since 02.10.2003
  * @see PropertyPlaceholderConfigurerTests
  */
+@SuppressWarnings("deprecation")
 public class PropertyResourceConfigurerTests {
 
 	static {
