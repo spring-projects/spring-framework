a98e3f048183528a0e99c3894e24e03873916166
Lars Grefer <github@larsgrefer.de>
2019-03-08 01:30:55 +0100

    Migrate CacheControl to use java.time.Duration for durations

--- spring-web/src/test/java/org/springframework/http/CacheControlTests.java
+++ spring-web/src/test/java/org/springframework/http/CacheControlTests.java
@@ -21,6 +21,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -41,6 +42,12 @@ public class CacheControlTests {
 	}
 
 	@Test
+	public void maxAge_duration() throws Exception {
+		CacheControl cc = CacheControl.maxAge(Duration.ofHours(1));
+		assertThat(cc.getHeaderValue(), Matchers.equalTo("max-age=3600"));
+	}
+
+	@Test
 	public void maxAgeAndDirectives() throws Exception {
 		CacheControl cc = CacheControl.maxAge(3600, TimeUnit.SECONDS).cachePublic().noTransform();
 		assertThat(cc.getHeaderValue(), Matchers.equalTo("max-age=3600, no-transform, public"));
@@ -49,6 +56,12 @@ public class CacheControlTests {
 	@Test
 	public void maxAgeAndSMaxAge() throws Exception {
 		CacheControl cc = CacheControl.maxAge(1, TimeUnit.HOURS).sMaxAge(30, TimeUnit.MINUTES);
+		assertThat(cc.getHeaderValue(), Matchers.equalTo("max-age=3600, s-maxage=1800"));
+	}
+
+	@Test
+	public void maxAgeAndSMaxAge_duration() throws Exception {
+		CacheControl cc = CacheControl.maxAge(Duration.ofHours(1)).sMaxAge(Duration.ofMinutes(30));
 		assertThat(cc.getHeaderValue(), Matchers.equalTo("max-age=3600, s-maxage=1800"));
 	}
 
@@ -71,8 +84,20 @@ public class CacheControlTests {
 	}
 
 	@Test
+	public void staleIfError_duration() throws Exception {
+		CacheControl cc = CacheControl.maxAge(Duration.ofHours(1)).staleIfError(2, TimeUnit.HOURS);
+		assertThat(cc.getHeaderValue(), Matchers.equalTo("max-age=3600, stale-if-error=7200"));
+	}
+
+	@Test
 	public void staleWhileRevalidate() throws Exception {
 		CacheControl cc = CacheControl.maxAge(1, TimeUnit.HOURS).staleWhileRevalidate(2, TimeUnit.HOURS);
+		assertThat(cc.getHeaderValue(), Matchers.equalTo("max-age=3600, stale-while-revalidate=7200"));
+	}
+
+	@Test
+	public void staleWhileRevalidate_duration() throws Exception {
+		CacheControl cc = CacheControl.maxAge(Duration.ofHours(1)).staleWhileRevalidate(2, TimeUnit.HOURS);
 		assertThat(cc.getHeaderValue(), Matchers.equalTo("max-age=3600, stale-while-revalidate=7200"));
 	}
 
