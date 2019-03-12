b88aad6b39f23bb0dd67b69be9a1104049eadf3c
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2019-03-12 16:17:39 -0400

    Avoid payload conversion if unnecessary
    Closes gh-22386

--- spring-messaging/src/test/java/org/springframework/messaging/converter/MappingJackson2MessageConverterTests.java
+++ spring-messaging/src/test/java/org/springframework/messaging/converter/MappingJackson2MessageConverterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,6 +108,14 @@ public class MappingJackson2MessageConverterTests {
 		assertEquals(Arrays.asList("Foo", "Bar"), actual.get("array"));
 		assertEquals(Boolean.TRUE, actual.get("bool"));
 		assertEquals("AQI=", actual.get("bytes"));
+	}
+
+	@Test // gh-22386
+	public void fromMessageMatchingInstance() {
+		MyBean myBean = new MyBean();
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		Message<?> message = MessageBuilder.withPayload(myBean).build();
+		assertSame(myBean, converter.fromMessage(message, MyBean.class));
 	}
 
 	@Test(expected = MessageConversionException.class)
