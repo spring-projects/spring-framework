7d498ba6812724d12992382e3aae08ff34dde732
Sam Brannen <sbrannen@pivotal.io>
2019-03-20 11:03:54 +0100

    Fix Checkstyle error and polish Javadoc for ResourceHttpMessageReader

--- spring-web/src/main/java/org/springframework/http/codec/ResourceHttpMessageReader.java
+++ spring-web/src/main/java/org/springframework/http/codec/ResourceHttpMessageReader.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.http.codec;
 
 import java.util.Map;
@@ -26,15 +27,14 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.util.StringUtils;
 
 /**
- * Simple around around {@link ResourceDecoder} that extracts the filename from
- * the "Content-Disposition" header, if available, and passes it as the hint
- * {@link ResourceDecoder#FILENAME_HINT}.
+ * {@code HttpMessageReader} that wraps and delegates to a {@link ResourceDecoder}
+ * that extracts the filename from the {@code "Content-Disposition"} header, if
+ * available, and passes it as the {@link ResourceDecoder#FILENAME_HINT}.
  *
  * @author Rossen Stoyanchev
  * @since 5.2
  */
 public class ResourceHttpMessageReader extends DecoderHttpMessageReader<Resource> {
-
 
 	public ResourceHttpMessageReader() {
 		super(new ResourceDecoder());
