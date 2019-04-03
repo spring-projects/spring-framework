9ea02c6dfa66a1c4dd576fb7b92e4ea1ea0bc041
Juergen Hoeller <jhoeller@pivotal.io>
2019-04-03 14:34:11 +0200

    Revised javadoc

--- spring-context/src/main/java/org/springframework/context/annotation/Configuration.java
+++ spring-context/src/main/java/org/springframework/context/annotation/Configuration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -430,7 +430,7 @@ public @interface Configuration {
 	 * is registered as a traditional XML bean definition, the name/id of the bean
 	 * element will take precedence.
 	 * @return the explicit component name, if any (or empty String otherwise)
-	 * @see org.springframework.beans.factory.support.DefaultBeanNameGenerator
+	 * @see AnnotationBeanNameGenerator
 	 */
 	@AliasFor(annotation = Component.class)
 	String value() default "";
