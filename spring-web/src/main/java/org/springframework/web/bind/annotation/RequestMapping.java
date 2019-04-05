d04a640baf0b4958a12bdcf269cbc530c494f07d
Sam Brannen <sbrannen@pivotal.io>
2019-04-05 13:39:07 +0200

    Polish Javadoc for @RequestMapping

--- spring-web/src/main/java/org/springframework/web/bind/annotation/RequestMapping.java
+++ spring-web/src/main/java/org/springframework/web/bind/annotation/RequestMapping.java
@@ -103,11 +103,11 @@ public @interface RequestMapping {
 	String[] value() default {};
 
 	/**
-	 * The path mapping URIs (e.g. {@code "/myPath.do"}).
-	 * <p>Ant-style path patterns are also supported (e.g. {@code "/myPath/*.do"}).
-	 * At the method level, relative paths (e.g. {@code "edit.do"}) are supported
+	 * The path mapping URIs (e.g. {@code "/profile"}).
+	 * <p>Ant-style path patterns are also supported (e.g. {@code "/profile/**"}).
+	 * At the method level, relative paths (e.g. {@code "edit"}) are supported
 	 * within the primary mapping expressed at the type level.
-	 * Path mapping URIs may contain placeholders (e.g. <code>"/${connect}"</code>).
+	 * Path mapping URIs may contain placeholders (e.g. <code>"/${profile_path}"</code>).
 	 * <p><b>Supported at the type level as well as at the method level!</b>
 	 * When used at the type level, all method-level mappings inherit
 	 * this primary mapping, narrowing it for a specific handler method.
