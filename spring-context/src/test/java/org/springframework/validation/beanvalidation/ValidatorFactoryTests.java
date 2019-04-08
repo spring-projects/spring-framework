a1efe3cfe58c6ab59c9b45887fc034d5af17be76
Juergen Hoeller <jhoeller@pivotal.io>
2019-04-08 15:33:04 +0200

    Avoid MessageFormat processing for default Bean Validation messages
    Closes gh-22761

--- spring-context/src/test/java/org/springframework/validation/beanvalidation/ValidatorFactoryTests.java
+++ spring-context/src/test/java/org/springframework/validation/beanvalidation/ValidatorFactoryTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -284,6 +284,8 @@ public class ValidatorFactoryTests {
 		validator.validate(listContainer, errors);
 
 		FieldError fieldError = errors.getFieldError("list[1]");
+		assertNotNull(fieldError);
+		assertEquals("X", fieldError.getRejectedValue());
 		assertEquals("X", errors.getFieldValue("list[1]"));
 	}
 
