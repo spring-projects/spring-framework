85597f29f2a2192bc5e7e0a230ffd523a3e84736
P. Ottlinger <ottlinger@users.noreply.github.com>
2019-04-09 19:03:30 +0200

    Improve @Scheduled Javadoc regarding cron element order
    Closes gh-22777

--- spring-context/src/main/java/org/springframework/scheduling/annotation/Scheduled.java
+++ spring-context/src/main/java/org/springframework/scheduling/annotation/Scheduled.java
@@ -66,9 +66,17 @@ public @interface Scheduled {
 
 	/**
 	 * A cron-like expression, extending the usual UN*X definition to include triggers
-	 * on the second as well as minute, hour, day of month, month and day of week.
+	 * on the second, minute, hour, day of month, month and day of week.
 	 * <p>E.g. {@code "0 * * * * MON-FRI"} means once per minute on weekdays
-	 * (at the top of the minute - the 0th second).
+	 * (at the top of the minute - the 0th second). The order read from left to right is:
+	 * <ul>
+	 * <li>second</li>
+	 * <li>minute</li>
+	 * <li>hour</li>
+	 * <li>day of month</li>
+	 * <li>month</li>
+	 * <li>day of week</li>
+	 * </ul>
 	 * <p>The special value {@link #CRON_DISABLED "-"} indicates a disabled cron trigger,
 	 * primarily meant for externally specified values resolved by a ${...} placeholder.
 	 * @return an expression that can be parsed to a cron schedule
