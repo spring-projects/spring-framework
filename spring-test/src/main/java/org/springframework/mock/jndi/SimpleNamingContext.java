88e3b841644bd860d144acb37c13da3f6a8c2d01
Sam Brannen <sbrannen@pivotal.io>
2019-04-10 15:57:51 +0200

    Deprecate mock JNDI support in the spring-test module
    Closes gh-22779

--- spring-test/src/main/java/org/springframework/mock/jndi/SimpleNamingContext.java
+++ spring-test/src/main/java/org/springframework/mock/jndi/SimpleNamingContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,7 +50,10 @@ import org.springframework.util.StringUtils;
  * @author Juergen Hoeller
  * @see SimpleNamingContextBuilder
  * @see org.springframework.jndi.JndiTemplate#createInitialContext
+ * @deprecated Deprecated as of Spring Framework 5.2 in favor of complete solutions from
+ * third parties such as <a href="https://github.com/h-thurow/Simple-JNDI">Simple-JNDI</a>
  */
+@Deprecated
 public class SimpleNamingContext implements Context {
 
 	private final Log logger = LogFactory.getLog(getClass());
