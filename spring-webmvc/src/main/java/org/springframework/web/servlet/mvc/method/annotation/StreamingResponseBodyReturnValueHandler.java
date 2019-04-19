42fc4a35d59a37131bfe15d029738ab25f358241
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2019-04-18 22:49:23 -0400

    Ensure flush after StreamingResponseBody
    Closes gh-22813

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/StreamingResponseBodyReturnValueHandler.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/StreamingResponseBodyReturnValueHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,6 +108,7 @@ public class StreamingResponseBodyReturnValueHandler implements HandlerMethodRet
 		@Override
 		public Void call() throws Exception {
 			this.streamingBody.writeTo(this.outputStream);
+			this.outputStream.flush();
 			return null;
 		}
 	}
