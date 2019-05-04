f4519b428eb1dfda81319e9dd885a2f8a6f66a58
Phillip Webb <pwebb@pivotal.io>
2019-05-04 08:28:30 -0700

    Don't expose RetentionPolicy.CLASS annotations
    Update ASM based metadata readers so that only RetentionPolicy.RUNTIME
    annotations are exposed. This aligned behavior with the reflection based
    implementation.
    
    Closes gh-22886

--- spring-core/src/main/java/org/springframework/core/type/classreading/AnnotationMetadataReadingVisitor.java
+++ spring-core/src/main/java/org/springframework/core/type/classreading/AnnotationMetadataReadingVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -85,6 +85,9 @@ public class AnnotationMetadataReadingVisitor extends ClassMetadataReadingVisito
 
 	@Override
 	public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
+		if (!visible) {
+			return null;
+		}
 		String className = Type.getType(desc).getClassName();
 		this.annotationSet.add(className);
 		return new AnnotationAttributesReadingVisitor(
