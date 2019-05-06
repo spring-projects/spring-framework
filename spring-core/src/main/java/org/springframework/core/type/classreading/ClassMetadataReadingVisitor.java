4e25363ac570f3732b1aa139c80e6f18498aaa04
Phillip Webb <pwebb@pivotal.io>
2019-05-06 10:37:59 -0700

    Add SimpleAnnotationMeta classes and readers
    Replace the existing ASM based readers with new implementations that
    also support MergedAnnotations. The meta-data classes themselves are
    now immutable, and constructed via separate reader classes.
    
    The `SimpleMetadataReader` class has been updated to return the new
    classes, however the old ones remain since some of them are public
    and might be being used directly.
    
    Closes gh-22884

--- spring-core/src/main/java/org/springframework/core/type/classreading/ClassMetadataReadingVisitor.java
+++ spring-core/src/main/java/org/springframework/core/type/classreading/ClassMetadataReadingVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,6 +43,7 @@ import org.springframework.util.StringUtils;
  * @author Chris Beams
  * @since 2.5
  */
+@Deprecated
 class ClassMetadataReadingVisitor extends ClassVisitor implements ClassMetadata {
 
 	private String className = "";
