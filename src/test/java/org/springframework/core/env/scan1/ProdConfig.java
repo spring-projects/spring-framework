f86affe404a344fda2face7f0f5aad9f36549e41
Phillip Webb <pwebb@pivotal.io>
2019-05-05 23:21:04 -0700

    Extract integration test classes from package-info
    Extract the inner classes from package-info into their own files to
    prevent local build problems and to make them easier to find.

--- src/test/java/org/springframework/core/env/scan1/ProdConfig.java
+++ src/test/java/org/springframework/core/env/scan1/ProdConfig.java
@@ -14,39 +14,20 @@
  * limitations under the License.
  */
 
-/**
- * Mirrors the structure of beans and environment-specific config files
- * in EnvironmentIntegrationTests-context.xml
- */
-package org.springframework.core.env.scan1;
 
-import static org.springframework.core.env.EnvironmentSystemIntegrationTests.Constants.DEV_ENV_NAME;
-import static org.springframework.core.env.EnvironmentSystemIntegrationTests.Constants.PROD_ENV_NAME;
+package org.springframework.core.env.scan1;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Import({DevConfig.class, ProdConfig.class})
-class Config {
-}
-
-@Profile(DEV_ENV_NAME)
-@Configuration
-class DevConfig {
-	@Bean
-	public Object devBean() {
-		return new Object();
-	}
-}
-
-@Profile(PROD_ENV_NAME)
+@Profile(org.springframework.core.env.EnvironmentSystemIntegrationTests.Constants.PROD_ENV_NAME)
 @Configuration
 class ProdConfig {
+
 	@Bean
 	public Object prodBean() {
 		return new Object();
 	}
+
 }
