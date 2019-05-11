233b225b4fcb628e0a15c18f94941e2bdd5c6af2
Sam Brannen <sbrannen@pivotal.io>
2019-05-11 18:03:42 +0200

    Remove JUnit dependency from HeaderResultMatchers
    Prior to this commit, HeaderResultMatchers had a direct dependency on
    org.junit.Assert which forces JUnit 4 to be present on the classpath.
    
    This commit fixes this by introducing assertNotNull() in
    org.springframework.test.util.AssertionErrors and delegating to that
    instead.
    
    Fixes gh-22932

--- spring-test/src/main/java/org/springframework/test/web/servlet/result/HeaderResultMatchers.java
+++ spring-test/src/main/java/org/springframework/test/web/servlet/result/HeaderResultMatchers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,8 +26,8 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 /**
