4c334de85006234654538c44ef87d08814e719d5
Juergen Hoeller <jhoeller@pivotal.io>
2019-05-20 17:17:51 +0200

    Use Map.values() iteration within isMultipart check
    Closes gh-22982

--- spring-web/src/main/java/org/springframework/http/converter/FormHttpMessageConverter.java
+++ spring-web/src/main/java/org/springframework/http/converter/FormHttpMessageConverter.java
@@ -283,8 +283,8 @@ public class FormHttpMessageConverter implements HttpMessageConverter<MultiValue
 		if (contentType != null) {
 			return MediaType.MULTIPART_FORM_DATA.includes(contentType);
 		}
-		for (String name : map.keySet()) {
-			for (Object value : map.get(name)) {
+		for (List<?> values : map.values()) {
+			for (Object value : values) {
 				if (value != null && !(value instanceof String)) {
 					return true;
 				}
