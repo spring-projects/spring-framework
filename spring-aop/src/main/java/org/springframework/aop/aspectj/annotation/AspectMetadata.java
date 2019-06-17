4f3fbe168fa305dc0cedf6bd4bf871135baa4400
Issam El-atif <issam.elatif@gmail.com>
2019-06-17 11:26:49 +0200

    Fix typo

--- spring-aop/src/main/java/org/springframework/aop/aspectj/annotation/AspectMetadata.java
+++ spring-aop/src/main/java/org/springframework/aop/aspectj/annotation/AspectMetadata.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -95,7 +95,7 @@ public class AspectMetadata implements Serializable {
 			throw new IllegalArgumentException("Class '" + aspectClass.getName() + "' is not an @AspectJ aspect");
 		}
 		if (ajType.getDeclarePrecedence().length > 0) {
-			throw new IllegalArgumentException("DeclarePrecendence not presently supported in Spring AOP");
+			throw new IllegalArgumentException("DeclarePrecedence not presently supported in Spring AOP");
 		}
 		this.aspectClass = ajType.getJavaClass();
 		this.ajType = ajType;
