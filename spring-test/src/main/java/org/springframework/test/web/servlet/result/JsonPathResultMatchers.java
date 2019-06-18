89ebdc766cfb08ab021108070648dedecc67a706
Sam Brannen <sbrannen@pivotal.io>
2019-06-18 18:05:59 +0300

    Polishing

--- spring-test/src/main/java/org/springframework/test/web/servlet/result/JsonPathResultMatchers.java
+++ spring-test/src/main/java/org/springframework/test/web/servlet/result/JsonPathResultMatchers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,6 @@ import org.springframework.util.StringUtils;
  * <a href="https://github.com/jayway/JsonPath">JsonPath</a> expressions.
  *
  * <p>An instance of this class is typically accessed via
- * {@link MockMvcResultMatchers#jsonPath(String, Matcher)} or
  * {@link MockMvcResultMatchers#jsonPath(String, Object...)}.
  *
  * @author Rossen Stoyanchev
