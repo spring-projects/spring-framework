235858e4e51a09759a35371f11116dfbdc2e9ab5
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2019-07-13 11:00:43 +0100

    Exposes supported mime types in Jaxb2Decoder
    Closes gh-23278

--- spring-web/src/main/java/org/springframework/http/codec/xml/Jaxb2XmlDecoder.java
+++ spring-web/src/main/java/org/springframework/http/codec/xml/Jaxb2XmlDecoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,6 +80,10 @@ public class Jaxb2XmlDecoder extends AbstractDecoder<Object> {
 
 	public Jaxb2XmlDecoder() {
 		super(MimeTypeUtils.APPLICATION_XML, MimeTypeUtils.TEXT_XML);
+	}
+
+	public Jaxb2XmlDecoder(MimeType... supportedMimeTypes) {
+		super(supportedMimeTypes);
 	}
 
 
