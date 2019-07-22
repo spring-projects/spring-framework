acb3d1cf888125d7663628e766a42adaee8e78df
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2019-07-22 14:59:38 +0100

    Fix typo in test from previous commit
    See gh-23310

--- spring-web/src/test/java/org/springframework/web/util/pattern/PathPatternParserTests.java
+++ spring-web/src/test/java/org/springframework/web/util/pattern/PathPatternParserTests.java
@@ -411,7 +411,7 @@ public class PathPatternParserTests {
 	@Test
 	public void separatorTests() {
 		PathPatternParser parser = new PathPatternParser();
-		parser.setPathOptions(PathContainer.Options.HTTP_PATH);
+		parser.setPathOptions(PathContainer.Options.create('.', false));
 		String rawPattern = "first.second.{last}";
 		PathPattern pattern = parser.parse(rawPattern);
 		assertThat(pattern.computePatternString()).isEqualTo(rawPattern);
