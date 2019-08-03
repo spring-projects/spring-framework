9ad9789a1cbf45a99d28bee47b234ded36a90af3
xjh <32237374+xjhxjhxjhxjh@users.noreply.github.com>
2019-08-03 22:37:23 +0800

    Update CollectionUtilsTests.java

--- spring-core/src/test/java/org/springframework/util/CollectionUtilsTests.java
+++ spring-core/src/test/java/org/springframework/util/CollectionUtilsTests.java
@@ -210,6 +210,18 @@ public class CollectionUtilsTests {
 		assertThat(CollectionUtils.hasUniqueObject(list)).isFalse();
 	}
 
+	@Test
+    public void testHasUniqueObject2() {
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(1);
+        System.out.println(CollectionUtils.hasUniqueObject(list1));
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(2);
+        System.out.println(CollectionUtils.hasUniqueObject(list2));
+    }
 
 	private static final class Instance {
 
