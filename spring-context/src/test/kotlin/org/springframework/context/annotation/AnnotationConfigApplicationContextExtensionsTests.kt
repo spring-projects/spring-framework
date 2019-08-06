23cd261b6b1440d2c45479ccb4f13518636e3eb9
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-08-06 11:28:32 +0200

    Deprecate AnnotationConfigApplicationContext { } Kotlin extension
    Closes gh-23420

--- spring-context/src/test/kotlin/org/springframework/context/annotation/AnnotationConfigApplicationContextExtensionsTests.kt
+++ spring-context/src/test/kotlin/org/springframework/context/annotation/AnnotationConfigApplicationContextExtensionsTests.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,8 +27,9 @@ import org.springframework.context.support.registerBean
  * @author Sebastien Deleuze
  */
 class AnnotationConfigApplicationContextExtensionsTests {
- 
+
 	@Test
+	@Suppress("DEPRECATION")
 	fun `Instantiate AnnotationConfigApplicationContext`() {
 		val applicationContext = AnnotationConfigApplicationContext {
 			registerBean<Foo>()
