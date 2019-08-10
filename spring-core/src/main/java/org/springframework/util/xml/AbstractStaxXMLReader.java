849bbf00b52e4e4ea1442a1b5504455e2b0c66b3
zhuzhuman978 <zhuzhuman@hotmail.com>
2019-08-10 20:03:18 +0800

    Simplify if statements and replace try-finally with try-with-resources
    Closes gh-23445

--- spring-core/src/main/java/org/springframework/util/xml/AbstractStaxXMLReader.java
+++ spring-core/src/main/java/org/springframework/util/xml/AbstractStaxXMLReader.java
@@ -64,22 +64,19 @@ abstract class AbstractStaxXMLReader extends AbstractXMLReader {
 
 	@Override
 	public boolean getFeature(String name) throws SAXNotRecognizedException, SAXNotSupportedException {
-		if (NAMESPACES_FEATURE_NAME.equals(name)) {
-			return this.namespacesFeature;
-		}
-		else if (NAMESPACE_PREFIXES_FEATURE_NAME.equals(name)) {
-			return this.namespacePrefixesFeature;
-		}
-		else if (IS_STANDALONE_FEATURE_NAME.equals(name)) {
-			if (this.isStandalone != null) {
-				return this.isStandalone;
-			}
-			else {
-				throw new SAXNotSupportedException("startDocument() callback not completed yet");
-			}
-		}
-		else {
-			return super.getFeature(name);
+		switch (name) {
+			case NAMESPACES_FEATURE_NAME:
+				return this.namespacesFeature;
+			case NAMESPACE_PREFIXES_FEATURE_NAME:
+				return this.namespacePrefixesFeature;
+			case IS_STANDALONE_FEATURE_NAME:
+				if (this.isStandalone != null) {
+					return this.isStandalone;
+				} else {
+					throw new SAXNotSupportedException("startDocument() callback not completed yet");
+				}
+			default:
+				return super.getFeature(name);
 		}
 	}
 
