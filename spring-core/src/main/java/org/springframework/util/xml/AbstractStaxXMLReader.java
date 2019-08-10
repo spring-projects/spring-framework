6c0e550b866abd7001de54232634d92a37cdb79e
Sam Brannen <sbrannen@pivotal.io>
2019-08-10 14:07:15 +0200

    Polish contribution
    See gh-23445

--- spring-core/src/main/java/org/springframework/util/xml/AbstractStaxXMLReader.java
+++ spring-core/src/main/java/org/springframework/util/xml/AbstractStaxXMLReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -72,7 +72,8 @@ abstract class AbstractStaxXMLReader extends AbstractXMLReader {
 			case IS_STANDALONE_FEATURE_NAME:
 				if (this.isStandalone != null) {
 					return this.isStandalone;
-				} else {
+				}
+				else {
 					throw new SAXNotSupportedException("startDocument() callback not completed yet");
 				}
 			default:
