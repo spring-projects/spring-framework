d759ae772bb1d75f92b33636c73bf24c31b9f0cd
Sam Brannen <sbrannen@pivotal.io>
2019-09-11 13:21:19 +0200

    Rename Spr3896SuiteTests to Spr3896TestSuite
    This commit renames Spr3896SuiteTests to comply with our naming
    convention for test classes that should be executed via the Gradle
    build.
    
    The effect of this commit is that test classes included in that "suite"
    are no longer executed twice in the build. Consequently, Gradle and
    Bamboo will now report the same number of executed tests for the
    spring-test project.

--- spring-test/src/test/java/org/springframework/test/context/junit4/spr3896/Spr3896TestSuite.java
+++ spring-test/src/test/java/org/springframework/test/context/junit4/spr3896/Spr3896TestSuite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,5 +45,5 @@ BeanOverridingDefaultLocationsInheritedTests.class,
 BeanOverridingExplicitLocationsInheritedTests.class
 
 })
-public class Spr3896SuiteTests {
+public class Spr3896TestSuite {
 }
