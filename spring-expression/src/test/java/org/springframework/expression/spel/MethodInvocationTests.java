57f67635fe42bdb0178f6b018c668c7644ae8313
Kant Leung <oolpmoo@163.com>
2019-09-12 13:14:10 +0800

    Optimize code where feasible

--- spring-expression/src/test/java/org/springframework/expression/spel/MethodInvocationTests.java
+++ spring-expression/src/test/java/org/springframework/expression/spel/MethodInvocationTests.java
@@ -277,14 +277,11 @@ public class MethodInvocationTests extends AbstractExpressionTests {
 		final BytesService service = new BytesService();
 		byte[] bytes = new byte[100];
 		StandardEvaluationContext context = new StandardEvaluationContext(bytes);
-		context.setBeanResolver(new BeanResolver() {
-			@Override
-			public Object resolve(EvaluationContext context, String beanName) throws AccessException {
-				if ("service".equals(beanName)) {
-					return service;
-				}
-				return null;
+		context.setBeanResolver((context1, beanName) -> {
+			if ("service".equals(beanName)) {
+				return service;
 			}
+			return null;
 		});
 		Expression expression = parser.parseExpression("@service.handleBytes(#root)");
 		byte[] outBytes = expression.getValue(context, byte[].class);
