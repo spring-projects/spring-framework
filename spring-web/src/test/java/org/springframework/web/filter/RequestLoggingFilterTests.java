ae469f4b31e0209ad16396cf18da6993db68b48d
Napster <napster@npstr.space>
2019-09-14 12:42:12 +0200

    Clean up finals, take advantage of more expressive assertions

--- spring-web/src/test/java/org/springframework/web/filter/RequestLoggingFilterTests.java
+++ spring-web/src/test/java/org/springframework/web/filter/RequestLoggingFilterTests.java
@@ -24,7 +24,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.mock.web.test.MockHttpServletRequest;
@@ -44,31 +43,23 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class RequestLoggingFilterTests {
 
-	private MyRequestLoggingFilter filter = new MyRequestLoggingFilter();
-
-	@BeforeEach
-	public void reset() {
-		filter = new MyRequestLoggingFilter();
-	}
+	private final MyRequestLoggingFilter filter = new MyRequestLoggingFilter();
 
 	@Test
 	public void defaultPrefix() throws Exception {
-		final MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
+		MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
 		FilterChain filterChain = new NoOpFilterChain();
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
-		assertThat(filter.beforeRequestMessage.startsWith(AbstractRequestLoggingFilter.DEFAULT_BEFORE_MESSAGE_PREFIX)).isTrue();
-
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.startsWith(AbstractRequestLoggingFilter.DEFAULT_AFTER_MESSAGE_PREFIX)).isTrue();
+		assertThat(filter.beforeRequestMessage).startsWith(AbstractRequestLoggingFilter.DEFAULT_BEFORE_MESSAGE_PREFIX);
+		assertThat(filter.afterRequestMessage).startsWith(AbstractRequestLoggingFilter.DEFAULT_AFTER_MESSAGE_PREFIX);
 	}
 
 	@Test
 	public void customPrefix() throws Exception {
-		final MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
+		MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
 		filter.setBeforeMessagePrefix("Before prefix: ");
@@ -77,31 +68,25 @@ public class RequestLoggingFilterTests {
 		FilterChain filterChain = new NoOpFilterChain();
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
-		assertThat(filter.beforeRequestMessage.startsWith("Before prefix: ")).isTrue();
-
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.startsWith("After prefix: ")).isTrue();
+		assertThat(filter.beforeRequestMessage).startsWith("Before prefix: ");
+		assertThat(filter.afterRequestMessage).startsWith("After prefix: ");
 	}
 
 	@Test
 	public void defaultSuffix() throws Exception {
-		final MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
+		MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
 		FilterChain filterChain = new NoOpFilterChain();
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
-		assertThat(filter.beforeRequestMessage.endsWith(AbstractRequestLoggingFilter.DEFAULT_BEFORE_MESSAGE_SUFFIX)).isTrue();
-
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.endsWith(AbstractRequestLoggingFilter.DEFAULT_AFTER_MESSAGE_SUFFIX)).isTrue();
+		assertThat(filter.beforeRequestMessage).endsWith(AbstractRequestLoggingFilter.DEFAULT_BEFORE_MESSAGE_SUFFIX);
+		assertThat(filter.afterRequestMessage).endsWith(AbstractRequestLoggingFilter.DEFAULT_AFTER_MESSAGE_SUFFIX);
 	}
 
 	@Test
 	public void customSuffix() throws Exception {
-		final MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
+		MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
 		filter.setBeforeMessageSuffix("}");
@@ -110,16 +95,13 @@ public class RequestLoggingFilterTests {
 		FilterChain filterChain = new NoOpFilterChain();
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
-		assertThat(filter.beforeRequestMessage.endsWith("}")).isTrue();
-
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.endsWith(")")).isTrue();
+		assertThat(filter.beforeRequestMessage).endsWith("}");
+		assertThat(filter.afterRequestMessage).endsWith(")");
 	}
 
 	@Test
 	public void method() throws Exception {
-		final MockHttpServletRequest request = new MockHttpServletRequest("PATCH", "/hotels");
+		MockHttpServletRequest request = new MockHttpServletRequest("PATCH", "/hotels");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
 		filter.setBeforeMessagePrefix("");
@@ -128,16 +110,13 @@ public class RequestLoggingFilterTests {
 		FilterChain filterChain = new NoOpFilterChain();
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
-		assertThat(filter.beforeRequestMessage.startsWith("PATCH")).isTrue();
-
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.startsWith("PATCH")).isTrue();
+		assertThat(filter.beforeRequestMessage).startsWith("PATCH");
+		assertThat(filter.afterRequestMessage).startsWith("PATCH");
 	}
 
 	@Test
 	public void uri() throws Exception {
-		final MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
+		MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
 		request.setQueryString("booking=42");
@@ -145,13 +124,11 @@ public class RequestLoggingFilterTests {
 		FilterChain filterChain = new NoOpFilterChain();
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
-		assertThat(filter.beforeRequestMessage.contains("/hotel")).isTrue();
-		assertThat(filter.beforeRequestMessage.contains("booking=42")).isFalse();
+		assertThat(filter.beforeRequestMessage).contains("/hotel");
+		assertThat(filter.beforeRequestMessage).doesNotContain("booking=42");
 
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.contains("/hotel")).isTrue();
-		assertThat(filter.afterRequestMessage.contains("booking=42")).isFalse();
+		assertThat(filter.afterRequestMessage).contains("/hotel");
+		assertThat(filter.afterRequestMessage).doesNotContain("booking=42");
 	}
 
 	@Test
@@ -166,11 +143,8 @@ public class RequestLoggingFilterTests {
 		FilterChain filterChain = new NoOpFilterChain();
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
-		assertThat(filter.beforeRequestMessage.contains("/hotels?booking=42")).isTrue();
-
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.contains("/hotels?booking=42")).isTrue();
+		assertThat(filter.beforeRequestMessage).contains("/hotels?booking=42");
+		assertThat(filter.afterRequestMessage).contains("/hotels?booking=42");
 	}
 
 	@Test
@@ -183,11 +157,8 @@ public class RequestLoggingFilterTests {
 		FilterChain filterChain = new NoOpFilterChain();
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
-		assertThat(filter.beforeRequestMessage.contains("/hotels]")).isTrue();
-
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.contains("/hotels]")).isTrue();
+		assertThat(filter.beforeRequestMessage).contains("/hotels]");
+		assertThat(filter.afterRequestMessage).contains("/hotels]");
 	}
 
 	@Test
@@ -201,11 +172,8 @@ public class RequestLoggingFilterTests {
 		FilterChain filterChain = new NoOpFilterChain();
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
-		assertThat(filter.beforeRequestMessage.contains("client=4.2.2.2")).isTrue();
-
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.contains("client=4.2.2.2")).isTrue();
+		assertThat(filter.beforeRequestMessage).contains("client=4.2.2.2");
+		assertThat(filter.afterRequestMessage).contains("client=4.2.2.2");
 	}
 
 	@Test
@@ -220,11 +188,8 @@ public class RequestLoggingFilterTests {
 		FilterChain filterChain = new NoOpFilterChain();
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
-		assertThat(filter.beforeRequestMessage.contains("session=42")).isTrue();
-
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.contains("session=42")).isTrue();
+		assertThat(filter.beforeRequestMessage).contains("session=42");
+		assertThat(filter.afterRequestMessage).contains("session=42");
 	}
 
 	@Test
@@ -238,16 +203,13 @@ public class RequestLoggingFilterTests {
 		FilterChain filterChain = new NoOpFilterChain();
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
-		assertThat(filter.beforeRequestMessage.contains("user=Arthur")).isTrue();
-
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.contains("user=Arthur")).isTrue();
+		assertThat(filter.beforeRequestMessage).contains("user=Arthur");
+		assertThat(filter.afterRequestMessage).contains("user=Arthur");
 	}
 
 	@Test
 	public void headers() throws Exception {
-		final MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
+		MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
 		request.setContentType("application/json");
 		request.addHeader("token", "123");
 		MockHttpServletResponse response = new MockHttpServletResponse();
@@ -257,10 +219,7 @@ public class RequestLoggingFilterTests {
 		filter.setHeaderPredicate(name -> !name.equalsIgnoreCase("token"));
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.beforeRequestMessage).isNotNull();
 		assertThat(filter.beforeRequestMessage).isEqualTo("Before request [POST /hotels, headers=[Content-Type:\"application/json\", token:\"masked\"]]");
-
-		assertThat(filter.afterRequestMessage).isNotNull();
 		assertThat(filter.afterRequestMessage).isEqualTo("After request [POST /hotels, headers=[Content-Type:\"application/json\", token:\"masked\"]]");
 	}
 
@@ -268,10 +227,10 @@ public class RequestLoggingFilterTests {
 	public void payloadInputStream() throws Exception {
 		filter.setIncludePayload(true);
 
-		final MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
+		MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
-		final byte[] requestBody = "Hello World".getBytes(StandardCharsets.UTF_8);
+		byte[] requestBody = "Hello World".getBytes(StandardCharsets.UTF_8);
 		request.setContent(requestBody);
 
 		FilterChain filterChain = (filterRequest, filterResponse) -> {
@@ -282,18 +241,17 @@ public class RequestLoggingFilterTests {
 
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.contains("Hello World")).isTrue();
+		assertThat(filter.afterRequestMessage).contains("Hello World");
 	}
 
 	@Test
 	public void payloadReader() throws Exception {
 		filter.setIncludePayload(true);
 
-		final MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
+		MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
-		final String requestBody = "Hello World";
+		String requestBody = "Hello World";
 		request.setContent(requestBody.getBytes(StandardCharsets.UTF_8));
 
 		FilterChain filterChain = (filterRequest, filterResponse) -> {
@@ -304,8 +262,7 @@ public class RequestLoggingFilterTests {
 
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.contains(requestBody)).isTrue();
+		assertThat(filter.afterRequestMessage).contains(requestBody);
 	}
 
 	@Test
@@ -313,10 +270,10 @@ public class RequestLoggingFilterTests {
 		filter.setIncludePayload(true);
 		filter.setMaxPayloadLength(3);
 
-		final MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
+		MockHttpServletRequest request = new MockHttpServletRequest("POST", "/hotels");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
-		final byte[] requestBody = "Hello World".getBytes(StandardCharsets.UTF_8);
+		byte[] requestBody = "Hello World".getBytes(StandardCharsets.UTF_8);
 		request.setContent(requestBody);
 
 		FilterChain filterChain = (filterRequest, filterResponse) -> {
@@ -330,9 +287,8 @@ public class RequestLoggingFilterTests {
 
 		filter.doFilter(request, response, filterChain);
 
-		assertThat(filter.afterRequestMessage).isNotNull();
-		assertThat(filter.afterRequestMessage.contains("Hel")).isTrue();
-		assertThat(filter.afterRequestMessage.contains("Hello World")).isFalse();
+		assertThat(filter.afterRequestMessage).contains("Hel");
+		assertThat(filter.afterRequestMessage).doesNotContain("Hello World");
 	}
 
 
