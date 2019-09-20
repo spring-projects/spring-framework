a48c13ae73618b8ee6c288db9e0cc9c271b5becb
Juergen Hoeller <jhoeller@pivotal.io>
2019-09-20 21:54:18 +0200

    Avoid deprecated usingWhen method in Reactor 3.3
    Closes gh-23562

--- spring-tx/src/main/java/org/springframework/transaction/reactive/TransactionalOperatorImpl.java
+++ spring-tx/src/main/java/org/springframework/transaction/reactive/TransactionalOperatorImpl.java
@@ -80,7 +80,7 @@ final class TransactionalOperatorImpl implements TransactionalOperator {
 			// Need re-wrapping of ReactiveTransaction until we get hold of the exception
 			// through usingWhen.
 			return status.flatMap(it -> Mono.usingWhen(Mono.just(it), ignore -> mono,
-					this.transactionManager::commit, s -> Mono.empty())
+					this.transactionManager::commit, (res, err) -> Mono.empty(), s -> Mono.empty())
 					.onErrorResume(ex -> rollbackOnException(it, ex).then(Mono.error(ex))));
 		})
 		.subscriberContext(TransactionContextManager.getOrCreateContext())
