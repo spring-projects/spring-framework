357beb24bce72b20aed3a774b4eddc42e3f098f2
Juergen Hoeller <jhoeller@pivotal.io>
2019-09-25 22:15:07 +0200

    Polishing

--- spring-aop/src/main/java/org/springframework/aop/support/ComposablePointcut.java
+++ spring-aop/src/main/java/org/springframework/aop/support/ComposablePointcut.java
@@ -202,7 +202,7 @@ public class ComposablePointcut implements Pointcut, Serializable {
 
 	@Override
 	public String toString() {
-		return "ComposablePointcut: " + this.classFilter + ", " + this.methodMatcher;
+		return getClass().getName() + ": " + this.classFilter + ", " + this.methodMatcher;
 	}
 
 }
