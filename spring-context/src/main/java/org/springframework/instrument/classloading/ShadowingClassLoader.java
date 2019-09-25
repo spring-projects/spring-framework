0519a2ff3c745f9898775745aaf83fcd67b2f1b5
Juergen Hoeller <jhoeller@pivotal.io>
2019-09-25 12:11:33 +0200

    Exclude jdk package in ShadowingClassLoader (JDK 11 compatibility)
    Closes gh-23641

--- spring-context/src/main/java/org/springframework/instrument/classloading/ShadowingClassLoader.java
+++ spring-context/src/main/java/org/springframework/instrument/classloading/ShadowingClassLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,7 +48,7 @@ public class ShadowingClassLoader extends DecoratingClassLoader {
 
 	/** Packages that are excluded by default. */
 	public static final String[] DEFAULT_EXCLUDED_PACKAGES =
-			new String[] {"java.", "javax.", "sun.", "oracle.", "com.sun.", "com.ibm.", "COM.ibm.",
+			new String[] {"java.", "javax.", "jdk.", "sun.", "oracle.", "com.sun.", "com.ibm.", "COM.ibm.",
 					"org.w3c.", "org.xml.", "org.dom4j.", "org.eclipse", "org.aspectj.", "net.sf.cglib",
 					"org.springframework.cglib", "org.apache.xerces.", "org.apache.commons.logging."};
 
