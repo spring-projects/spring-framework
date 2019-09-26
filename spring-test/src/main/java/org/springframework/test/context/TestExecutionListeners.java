61862392876a968a1af5e196da1491ac02bfb36e
Sam Brannen <sbrannen@pivotal.io>
2019-09-26 15:02:30 +0200

    Remove obsolete references to specific release versions in testing docs

--- spring-test/src/main/java/org/springframework/test/context/TestExecutionListeners.java
+++ spring-test/src/main/java/org/springframework/test/context/TestExecutionListeners.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,8 +33,8 @@ import org.springframework.core.annotation.AliasFor;
  * <p>Typically, {@code @TestExecutionListeners} will be used in conjunction
  * with {@link ContextConfiguration @ContextConfiguration}.
  *
- * <p>As of Spring Framework 4.0, this annotation may be used as a
- * <em>meta-annotation</em> to create custom <em>composed annotations</em>.
+ * <p>This annotation may be used as a <em>meta-annotation</em> to create custom
+ * <em>composed annotations</em>.
  *
  * @author Sam Brannen
  * @since 2.5
