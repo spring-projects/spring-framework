a9a9c8e7ed834cc3815a538710c362f3f2ff0b58
yanghuai <yang.huai@xyb2b.com>
2019-09-29 07:41:08 +0800

    spring启动实例

---
+++ example/src/main/java/com/example/example/ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+}
---
+++ example/src/test/java/com/example/example/ExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
