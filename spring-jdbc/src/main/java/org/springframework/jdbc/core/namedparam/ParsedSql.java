b253c6271de4976353044baa0df43cd66960e449
simmy <siminture@live.com>
2019-10-08 16:16:54 +0800

    change methods of ParsedSql to public

--- spring-jdbc/src/main/java/org/springframework/jdbc/core/namedparam/ParsedSql.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/core/namedparam/ParsedSql.java
@@ -43,47 +43,50 @@ public class ParsedSql {
 
 	/**
 	 * Create a new instance of the {@link ParsedSql} class.
+	 *
 	 * @param originalSql the SQL statement that is being (or is to be) parsed
 	 */
-	ParsedSql(String originalSql) {
+	public ParsedSql(String originalSql) {
 		this.originalSql = originalSql;
 	}
 
 	/**
 	 * Return the SQL statement that is being parsed.
 	 */
-	String getOriginalSql() {
+	public String getOriginalSql() {
 		return this.originalSql;
 	}
 
 
 	/**
 	 * Add a named parameter parsed from this SQL statement.
+	 *
 	 * @param parameterName the name of the parameter
-	 * @param startIndex the start index in the original SQL String
-	 * @param endIndex the end index in the original SQL String
+	 * @param startIndex    the start index in the original SQL String
+	 * @param endIndex      the end index in the original SQL String
 	 */
-	void addNamedParameter(String parameterName, int startIndex, int endIndex) {
+	public void addNamedParameter(String parameterName, int startIndex, int endIndex) {
 		this.parameterNames.add(parameterName);
-		this.parameterIndexes.add(new int[] {startIndex, endIndex});
+		this.parameterIndexes.add(new int[]{startIndex, endIndex});
 	}
 
 	/**
 	 * Return all of the parameters (bind variables) in the parsed SQL statement.
 	 * Repeated occurrences of the same parameter name are included here.
 	 */
-	List<String> getParameterNames() {
+	public List<String> getParameterNames() {
 		return this.parameterNames;
 	}
 
 	/**
 	 * Return the parameter indexes for the specified parameter.
+	 *
 	 * @param parameterPosition the position of the parameter
-	 * (as index in the parameter names List)
+	 *                          (as index in the parameter names List)
 	 * @return the start index and end index, combined into
 	 * a int array of length 2
 	 */
-	int[] getParameterIndexes(int parameterPosition) {
+	public int[] getParameterIndexes(int parameterPosition) {
 		return this.parameterIndexes.get(parameterPosition);
 	}
 
@@ -99,21 +102,21 @@ public class ParsedSql {
 	 * Return the count of named parameters in the SQL statement.
 	 * Each parameter name counts once; repeated occurrences do not count here.
 	 */
-	int getNamedParameterCount() {
+	public int getNamedParameterCount() {
 		return this.namedParameterCount;
 	}
 
 	/**
 	 * Set the count of all of the unnamed parameters in the SQL statement.
 	 */
-	void setUnnamedParameterCount(int unnamedParameterCount) {
+	public void setUnnamedParameterCount(int unnamedParameterCount) {
 		this.unnamedParameterCount = unnamedParameterCount;
 	}
 
 	/**
 	 * Return the count of all of the unnamed parameters in the SQL statement.
 	 */
-	int getUnnamedParameterCount() {
+	public int getUnnamedParameterCount() {
 		return this.unnamedParameterCount;
 	}
 
@@ -121,7 +124,7 @@ public class ParsedSql {
 	 * Set the total count of all of the parameters in the SQL statement.
 	 * Repeated occurrences of the same parameter name do count here.
 	 */
-	void setTotalParameterCount(int totalParameterCount) {
+	public void setTotalParameterCount(int totalParameterCount) {
 		this.totalParameterCount = totalParameterCount;
 	}
 
@@ -129,7 +132,7 @@ public class ParsedSql {
 	 * Return the total count of all of the parameters in the SQL statement.
 	 * Repeated occurrences of the same parameter name do count here.
 	 */
-	int getTotalParameterCount() {
+	public int getTotalParameterCount() {
 		return this.totalParameterCount;
 	}
 
