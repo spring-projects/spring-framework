9b2087618bba6cd546066e2f27deb166f6870a64
Vedran Pavic <vedran.pavic@gmail.com>
2019-10-09 00:01:14 +0200

    Preserve expires attribute in MockCookie
    At present, MockCookie doesn't preserve expires attribute. This has a
    consequence that a cookie value set using
    MockHttpServletResponse#addHeader containing an expires attribute will
    not match the cookie value obtained from
    MockHttpServletResponse#getHeader, since the expires attribute will get
    calculated based on current time.
    
    This commit enhances MockCookie to preserve the expires attribute.
    
    Closes gh-23769

--- spring-web/src/test/java/org/springframework/mock/web/test/MockHttpServletResponse.java
+++ spring-web/src/test/java/org/springframework/mock/web/test/MockHttpServletResponse.java
@@ -27,6 +27,7 @@ import java.nio.charset.Charset;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -374,9 +375,14 @@ public class MockHttpServletResponse implements HttpServletResponse {
 		if (maxAge >= 0) {
 			buf.append("; Max-Age=").append(maxAge);
 			buf.append("; Expires=");
-			HttpHeaders headers = new HttpHeaders();
-			headers.setExpires(maxAge > 0 ? System.currentTimeMillis() + 1000L * maxAge : 0);
-			buf.append(headers.getFirst(HttpHeaders.EXPIRES));
+			if (cookie instanceof MockCookie && ((MockCookie) cookie).getExpires() != null) {
+				buf.append(((MockCookie) cookie).getExpires().format(DateTimeFormatter.RFC_1123_DATE_TIME));
+			}
+			else {
+				HttpHeaders headers = new HttpHeaders();
+				headers.setExpires(maxAge > 0 ? System.currentTimeMillis() + 1000L * maxAge : 0);
+				buf.append(headers.getFirst(HttpHeaders.EXPIRES));
+			}
 		}
 
 		if (cookie.getSecure()) {
