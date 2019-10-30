c9a6f4282a677e1d5b018b72701899dabf07f75a
monosoul <andrei.nevedomskii@n26.com>
2019-10-30 15:14:13 +0100

    Inverse condition to fix ISO-formatted Instant parsing
    Prior to this commit, InstantFormatter was able to properly serialize
    an Instant that is far in the future (or in the past), but it could not
    properly deserialize it, because in such scenarios an ISO-formatted
    Instant starts with a +/- sign.
    
    This commit fixes this issue, while maintaining the previous contract,
    and also introduces tests for InstantFormatter.
    
    Closes gh-23895

--- spring-context/src/main/java/org/springframework/format/datetime/standard/InstantFormatter.java
+++ spring-context/src/main/java/org/springframework/format/datetime/standard/InstantFormatter.java
@@ -40,13 +40,13 @@ public class InstantFormatter implements Formatter<Instant> {
 
 	@Override
 	public Instant parse(String text, Locale locale) throws ParseException {
-		if (text.length() > 0 && Character.isDigit(text.charAt(0))) {
-			// assuming UTC instant a la "2007-12-03T10:15:30.00Z"
-			return Instant.parse(text);
-		}
-		else {
+		if (text.length() > 0 && Character.isAlphabetic(text.charAt(0))) {
 			// assuming RFC-1123 value a la "Tue, 3 Jun 2008 11:05:30 GMT"
 			return Instant.from(DateTimeFormatter.RFC_1123_DATE_TIME.parse(text));
+		}
+		else {
+			// assuming UTC instant a la "2007-12-03T10:15:30.00Z"
+			return Instant.parse(text);
 		}
 	}
 
