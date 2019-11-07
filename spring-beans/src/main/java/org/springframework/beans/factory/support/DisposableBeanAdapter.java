7aa73792a184a030296de00e0cbac6122496f68d
Juergen Hoeller <jhoeller@pivotal.io>
2019-11-07 22:43:24 +0100

    Use Method.getParameterCount() when full type array is never needed

--- spring-beans/src/main/java/org/springframework/beans/factory/support/DisposableBeanAdapter.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/DisposableBeanAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -309,9 +309,9 @@ class DisposableBeanAdapter implements DisposableBean, Runnable, Serializable {
 	 * assuming a "force" parameter), else logging an error.
 	 */
 	private void invokeCustomDestroyMethod(final Method destroyMethod) {
-		Class<?>[] paramTypes = destroyMethod.getParameterTypes();
-		final Object[] args = new Object[paramTypes.length];
-		if (paramTypes.length == 1) {
+		int paramCount = destroyMethod.getParameterCount();
+		final Object[] args = new Object[paramCount];
+		if (paramCount == 1) {
 			args[0] = Boolean.TRUE;
 		}
 		if (logger.isDebugEnabled()) {
