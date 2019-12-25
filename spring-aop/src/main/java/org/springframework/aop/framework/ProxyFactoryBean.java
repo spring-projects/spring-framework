22db4d4e6584001e81a6931db9668211271c29a9
Pascal Schumacher <pascalschumacher@gmx.net>
2019-12-25 16:20:37 +0100

    Add missing blank to AopConfigException message thrown by ProxyFactoryBean#namedBeanToAdvisor

--- spring-aop/src/main/java/org/springframework/aop/framework/ProxyFactoryBean.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/ProxyFactoryBean.java
@@ -600,7 +600,7 @@ public class ProxyFactoryBean extends ProxyCreatorSupport
 			// We expected this to be an Advisor or Advice,
 			// but it wasn't. This is a configuration error.
 			throw new AopConfigException("Unknown advisor type " + next.getClass() +
-					"; Can only include Advisor or Advice type beans in interceptorNames chain except for last entry," +
+					"; Can only include Advisor or Advice type beans in interceptorNames chain except for last entry, " +
 					"which may also be target or TargetSource", ex);
 		}
 	}
