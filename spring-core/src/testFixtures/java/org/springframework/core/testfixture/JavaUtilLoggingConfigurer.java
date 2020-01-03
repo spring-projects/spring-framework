081b3d304d94bdbdffc663e3a4810f76d6eb1fc4
Sam Brannen <sbrannen@pivotal.io>
2020-01-03 14:34:29 +0100

    Move JavaUtilLoggingConfigurer to spring-core test fixtures
    See gh-23550

--- spring-core/src/testFixtures/java/org/springframework/core/testfixture/JavaUtilLoggingConfigurer.java
+++ spring-core/src/testFixtures/java/org/springframework/core/testfixture/JavaUtilLoggingConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.web.reactive.fixtures;
+package org.springframework.core.testfixture;
 
 import java.io.InputStream;
 import java.util.logging.LogManager;
