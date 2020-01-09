bdb9f9570e3c325999167415e66fc1339f9400ff
Stephane Nicoll <snicoll@pivotal.io>
2020-01-09 17:08:48 +0100

    Polish

--- spring-context/src/test/java/org/springframework/context/event/ApplicationContextEventTests.java
+++ spring-context/src/test/java/org/springframework/context/event/ApplicationContextEventTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -79,13 +79,13 @@ public class ApplicationContextEventTests extends AbstractApplicationEventListen
 	@Test
 	public void multicastGenericEvent() {
 		multicastEvent(true, StringEventListener.class, createGenericTestEvent("test"),
-				getGenericApplicationEventType("stringEvent"));
+				ResolvableType.forClassWithGenerics(GenericTestEvent.class, String.class));
 	}
 
 	@Test
 	public void multicastGenericEventWrongType() {
 		multicastEvent(false, StringEventListener.class, createGenericTestEvent(123L),
-				getGenericApplicationEventType("longEvent"));
+				ResolvableType.forClassWithGenerics(GenericTestEvent.class, Long.class));
 	}
 
 	@Test
