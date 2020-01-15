89b8449999769cbc412cd3301614630bbe98ae34
hyeonisism <54709431+hyeonisism@users.noreply.github.com>
2020-01-15 22:01:38 +0900

    Simplify getParsedSql() method in NamedParameterJdbcTemplate
    Closes gh-24358

--- spring-jdbc/src/main/java/org/springframework/jdbc/core/namedparam/NamedParameterJdbcTemplate.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/core/namedparam/NamedParameterJdbcTemplate.java
@@ -430,12 +430,7 @@ public class NamedParameterJdbcTemplate implements NamedParameterJdbcOperations 
 			return NamedParameterUtils.parseSqlStatement(sql);
 		}
 		synchronized (this.parsedSqlCache) {
-			ParsedSql parsedSql = this.parsedSqlCache.get(sql);
-			if (parsedSql == null) {
-				parsedSql = NamedParameterUtils.parseSqlStatement(sql);
-				this.parsedSqlCache.put(sql, parsedSql);
-			}
-			return parsedSql;
+			return parsedSqlCache.computeIfAbsent(sql, NamedParameterUtils::parseSqlStatement);
 		}
 	}
 
