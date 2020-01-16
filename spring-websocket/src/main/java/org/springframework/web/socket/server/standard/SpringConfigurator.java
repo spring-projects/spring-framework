9d9b8ff0b23e624e4d3acbc8f9a31e1558c459a8
choihyunjin <iamchoihyunjin@gmail.com>
2020-01-17 02:59:25 +0900

    Simplify spring configurator method getBeanNameByType()

--- spring-websocket/src/main/java/org/springframework/web/socket/server/standard/SpringConfigurator.java
+++ spring-websocket/src/main/java/org/springframework/web/socket/server/standard/SpringConfigurator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,11 +103,7 @@ public class SpringConfigurator extends Configurator {
 	private String getBeanNameByType(WebApplicationContext wac, Class<?> endpointClass) {
 		String wacId = wac.getId();
 
-		Map<Class<?>, String> beanNamesByType = cache.get(wacId);
-		if (beanNamesByType == null) {
-			beanNamesByType = new ConcurrentHashMap<>();
-			cache.put(wacId, beanNamesByType);
-		}
+		Map<Class<?>, String> beanNamesByType = cache.computeIfAbsent(wacId, id -> new ConcurrentHashMap<>());
 
 		if (!beanNamesByType.containsKey(endpointClass)) {
 			String[] names = wac.getBeanNamesForType(endpointClass);
