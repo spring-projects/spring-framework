cf45d901d4c8b53ae61e03ac622692debc6ae267
choihyunjin <iamchoihyunjin@gmail.com>
2020-01-17 01:51:41 +0900

    Separate catch code

--- spring-core/src/main/java/org/springframework/core/annotation/TypeMappedAnnotation.java
+++ spring-core/src/main/java/org/springframework/core/annotation/TypeMappedAnnotation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -671,10 +671,10 @@ final class TypeMappedAnnotation<A extends Annotation> extends AbstractMergedAnn
 			return new TypeMappedAnnotation<>(mapping, null, source, rootAttribute,
 					valueExtractor, aggregateIndex);
 		}
+		catch (AnnotationConfigurationException ex) {
+			throw ex;
+		}
 		catch (Exception ex) {
-			if (ex instanceof AnnotationConfigurationException) {
-				throw (AnnotationConfigurationException) ex;
-			}
 			if (logger.isEnabled()) {
 				String type = mapping.getAnnotationType().getName();
 				String item = (mapping.getDistance() == 0 ? "annotation " + type :
