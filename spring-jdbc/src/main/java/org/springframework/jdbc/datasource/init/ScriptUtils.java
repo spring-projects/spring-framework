b4cf88499c36c5120da9596e2785895a3d2a512d
Hyunjin Choi <hyeonisism@gmail.com>
2020-02-04 22:21:59 +0900

    Use try-with-resource in ScriptUtils
    Closes gh-24385

--- spring-jdbc/src/main/java/org/springframework/jdbc/datasource/init/ScriptUtils.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/datasource/init/ScriptUtils.java
@@ -325,12 +325,8 @@ public abstract class ScriptUtils {
 	private static String readScript(EncodedResource resource, @Nullable String[] commentPrefixes,
 			@Nullable String separator, @Nullable String blockCommentEndDelimiter) throws IOException {
 
-		LineNumberReader lnr = new LineNumberReader(resource.getReader());
-		try {
+		try (LineNumberReader lnr = new LineNumberReader(resource.getReader())) {
 			return readScript(lnr, commentPrefixes, separator, blockCommentEndDelimiter);
-		}
-		finally {
-			lnr.close();
 		}
 	}
 
