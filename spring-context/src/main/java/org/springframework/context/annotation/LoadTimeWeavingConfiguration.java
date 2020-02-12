ef1be1723e002d77edbddcb3227b9e447b0511f4
Juergen Hoeller <jhoeller@pivotal.io>
2020-02-12 18:17:20 +0100

    Consistent ROLE_INFRASTRUCTURE declarations for configuration classes
    Closes gh-24509

--- spring-context/src/main/java/org/springframework/context/annotation/LoadTimeWeavingConfiguration.java
+++ spring-context/src/main/java/org/springframework/context/annotation/LoadTimeWeavingConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,6 +40,7 @@ import org.springframework.instrument.classloading.LoadTimeWeaver;
  * @see ConfigurableApplicationContext#LOAD_TIME_WEAVER_BEAN_NAME
  */
 @Configuration
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class LoadTimeWeavingConfiguration implements ImportAware, BeanClassLoaderAware {
 
 	private AnnotationAttributes enableLTW;
