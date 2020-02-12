9dbd411f81ca14bd1a490bc9086a757398329bee
Sam Brannen <sbrannen@pivotal.io>
2020-02-12 11:04:07 +0100

    Suppress warnings in Gradle build

--- spring-context/src/test/java/org/springframework/aop/framework/CglibProxyTests.java
+++ spring-context/src/test/java/org/springframework/aop/framework/CglibProxyTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -387,6 +387,7 @@ public class CglibProxyTests extends AbstractAopProxyTests implements Serializab
 	}
 
 	@Test  // SPR-13328
+	@SuppressWarnings("unchecked")
 	public void testVarargsWithEnumArray() {
 		ProxyFactory proxyFactory = new ProxyFactory(new MyBean());
 		MyBean proxy = (MyBean) proxyFactory.getProxy();
