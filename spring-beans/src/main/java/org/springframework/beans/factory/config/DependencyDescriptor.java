c841ce102b8210c05589f13a827e2caff3c6d88b
陈其苗 <chenqimiao1994@126.com>
2020-02-19 12:10:14 +0800

    Fix javadoc in DependencyDescriptor

--- spring-beans/src/main/java/org/springframework/beans/factory/config/DependencyDescriptor.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/DependencyDescriptor.java
@@ -371,7 +371,7 @@ public class DependencyDescriptor extends InjectionPoint implements Serializable
 
 	/**
 	 * Determine the name of the wrapped parameter/field.
-	 * @return the declared name (never {@code null})
+	 * @return the declared name
 	 */
 	@Nullable
 	public String getDependencyName() {
