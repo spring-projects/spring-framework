6ee399f3024b201a4b7e4bd557d2950c685c1f59
陈其苗 <chenqimiao1994@126.com>
2020-03-04 11:46:50 +0800

    Polish mergeArrayIntoCollection of CollectionUtils

--- spring-core/src/main/java/org/springframework/util/CollectionUtils.java
+++ spring-core/src/main/java/org/springframework/util/CollectionUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,9 +89,7 @@ public abstract class CollectionUtils {
 	@SuppressWarnings("unchecked")
 	public static <E> void mergeArrayIntoCollection(@Nullable Object array, Collection<E> collection) {
 		Object[] arr = ObjectUtils.toObjectArray(array);
-		for (Object elem : arr) {
-			collection.add((E) elem);
-		}
+		collection.addAll(Arrays.asList((E[])arr));
 	}
 
 	/**
