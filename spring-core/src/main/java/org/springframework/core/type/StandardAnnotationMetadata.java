70f0fa87f18cd1c09137a0420e5ff5cb651243ca
陈其苗 <chenqimiao1994@126.com>
2020-03-15 21:36:08 +0800

    Suppress deprecation warnings

--- spring-core/src/main/java/org/springframework/core/type/StandardAnnotationMetadata.java
+++ spring-core/src/main/java/org/springframework/core/type/StandardAnnotationMetadata.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -172,7 +172,7 @@ public class StandardAnnotationMetadata extends StandardClassMetadata implements
 				AnnotatedElementUtils.isAnnotated(method, annotationName);
 	}
 
-
+	@SuppressWarnings("deprecation")
 	static AnnotationMetadata from(Class<?> introspectedClass) {
 		return new StandardAnnotationMetadata(introspectedClass, true);
 	}
