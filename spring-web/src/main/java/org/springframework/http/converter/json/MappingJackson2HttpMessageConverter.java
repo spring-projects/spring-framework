f79b5439a682a0c30f1fd55a6ce74d35eded9672
luohongtu <luohongtuyx@163.com>
2020-03-17 17:02:29 +0800

    The preprocessing method completes the processing of the object

--- spring-web/src/main/java/org/springframework/http/converter/json/MappingJackson2HttpMessageConverter.java
+++ spring-web/src/main/java/org/springframework/http/converter/json/MappingJackson2HttpMessageConverter.java
@@ -62,6 +62,7 @@ public class MappingJackson2HttpMessageConverter extends AbstractJackson2HttpMes
 	/**
 	 * Construct a new {@link MappingJackson2HttpMessageConverter} with a custom {@link ObjectMapper}.
 	 * You can use {@link Jackson2ObjectMapperBuilder} to build it easily.
+	 *
 	 * @see Jackson2ObjectMapperBuilder#json()
 	 */
 	public MappingJackson2HttpMessageConverter(ObjectMapper objectMapper) {
@@ -72,6 +73,7 @@ public class MappingJackson2HttpMessageConverter extends AbstractJackson2HttpMes
 	/**
 	 * Specify a custom prefix to use for this view's JSON output.
 	 * Default is none.
+	 *
 	 * @see #setPrefixJson
 	 */
 	public void setJsonPrefix(String jsonPrefix) {
@@ -83,6 +85,7 @@ public class MappingJackson2HttpMessageConverter extends AbstractJackson2HttpMes
 	 * <p>Prefixing the JSON string in this manner is used to help prevent JSON Hijacking.
 	 * The prefix renders the string syntactically invalid as a script so that it cannot be hijacked.
 	 * This prefix should be stripped before parsing the string as JSON.
+	 *
 	 * @see #setJsonPrefix
 	 */
 	public void setPrefixJson(boolean prefixJson) {
@@ -91,10 +94,11 @@ public class MappingJackson2HttpMessageConverter extends AbstractJackson2HttpMes
 
 
 	@Override
-	protected void writePrefix(JsonGenerator generator, Object object) throws IOException {
+	protected Object writePrefix(JsonGenerator generator, Object object) throws IOException {
 		if (this.jsonPrefix != null) {
 			generator.writeRaw(this.jsonPrefix);
 		}
+		return object;
 	}
 
 }
