2ca93cbe1c761dac8c61290513571f6c403a233c
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2020-03-20 18:02:10 +0000

    Update Javadoc for WebClient onStatus handlers
    Closes gh-24736

--- spring-webflux/src/main/java/org/springframework/web/reactive/function/client/ClientResponse.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/function/client/ClientResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -197,9 +197,9 @@ public interface ClientResponse {
 	Mono<ResponseEntity<Void>> toBodilessEntity();
 
 	/**
-	 * Creates a {@link WebClientResponseException} based on the status code,
-	 * headers, and body of this response as well as the corresponding request.
-	 * @return a {@code Mono} with a {@code WebClientResponseException} based on this response
+	 * Create a {@link WebClientResponseException} that contains the response
+	 * status, headers, body, and the originating request.
+	 * @return a {@code Mono} with the created exception
 	 * @since 5.2
 	 */
 	Mono<WebClientResponseException> createException();
