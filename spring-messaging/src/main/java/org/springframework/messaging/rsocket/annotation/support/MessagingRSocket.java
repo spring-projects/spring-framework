7efb62091d1670e6937493e11328ef33003e802b
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2020-03-20 10:03:56 +0000

    MessagingRSocket correctly handles unconsumed input
    Closes gh-24741

--- spring-messaging/src/main/java/org/springframework/messaging/rsocket/annotation/support/MessagingRSocket.java
+++ spring-messaging/src/main/java/org/springframework/messaging/rsocket/annotation/support/MessagingRSocket.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -174,7 +174,7 @@ class MessagingRSocket extends AbstractRSocket {
 				.doFinally(s -> {
 					// Subscription should have happened by now due to ChannelSendOperator
 					if (!read.get()) {
-						buffers.subscribe(DataBufferUtils::release);
+						firstPayload.release();
 					}
 				})
 				.thenMany(Flux.defer(() -> replyMono.isTerminated() ?
