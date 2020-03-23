e11373fe690f603fc1df55574d9a9a652239a5ee
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2020-03-23 12:21:33 +0000

    Unwrap XMLStreamException from cause
    Closes: gh-24622

--- spring-web/src/main/java/org/springframework/http/codec/xml/Jaxb2XmlDecoder.java
+++ spring-web/src/main/java/org/springframework/http/codec/xml/Jaxb2XmlDecoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -195,6 +195,10 @@ public class Jaxb2XmlDecoder extends AbstractDecoder<Object> {
 			return unmarshal(events, targetType.toClass());
 		}
 		catch (XMLStreamException ex) {
+			throw Exceptions.propagate(ex);
+		}
+		catch (Throwable ex) {
+			ex = (ex.getCause() instanceof XMLStreamException ? ex.getCause() : ex);
 			throw Exceptions.propagate(ex);
 		}
 		finally {
