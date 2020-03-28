1a92f3cfceea3bfbdb8c19a9b8f77170996b745d
martin-tarjanyi <tmartin193@gmail.com>
2020-03-28 22:20:57 +0100

    Re-add response cookie integration test for WebClient

--- spring-webflux/src/test/java/org/springframework/web/reactive/function/client/WebClientIntegrationTests.java
+++ spring-webflux/src/test/java/org/springframework/web/reactive/function/client/WebClientIntegrationTests.java
@@ -58,6 +58,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.HttpComponentsClientHttpConnector;
@@ -1082,6 +1083,42 @@ class WebClientIntegrationTests {
 				.expectComplete().verify(Duration.ofSeconds(3));
 
 		expectRequestCount(2);
+	}
+
+	@ParameterizedWebClientTest
+	void exchangeResponseCookies(ClientHttpConnector connector) {
+		startServer(connector);
+
+		prepareResponse(response -> response
+				.setHeader("Content-Type", "text/plain")
+				.addHeader("Set-Cookie", "testkey1=testvalue1;")
+				.addHeader("Set-Cookie", "testkey2=testvalue2; Max-Age=42; HttpOnly; Secure")
+				.setBody("test"));
+
+		Mono<ClientResponse> result = this.webClient.get()
+				.uri("/test")
+				.exchange();
+
+		StepVerifier.create(result)
+				.consumeNextWith(response -> {
+					assertThat(response.cookies()).containsOnlyKeys("testkey1", "testkey2");
+
+					ResponseCookie cookie1 = response.cookies().get("testkey1").get(0);
+					assertThat(cookie1.getValue()).isEqualTo("testvalue1");
+					assertThat(cookie1.isSecure()).isFalse();
+					assertThat(cookie1.isHttpOnly()).isFalse();
+					assertThat(cookie1.getMaxAge().getSeconds()).isEqualTo(-1);
+
+					ResponseCookie cookie2 = response.cookies().get("testkey2").get(0);
+					assertThat(cookie2.getValue()).isEqualTo("testvalue2");
+					assertThat(cookie2.isSecure()).isTrue();
+					assertThat(cookie2.isHttpOnly()).isTrue();
+					assertThat(cookie2.getMaxAge().getSeconds()).isEqualTo(42);
+				})
+				.expectComplete()
+				.verify(Duration.ofSeconds(3));
+
+		expectRequestCount(1);
 	}
 
 
