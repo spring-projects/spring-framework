f8ff4e42b91ec7098d6979e72b8c88601f476d5b
Qimiao Chen <chenqimiao1994@126.com>
2020-03-30 18:05:29 +0800

    Use try-with-resource to close resources
    Closes gh-24807

--- spring-core/src/main/java/org/springframework/core/io/support/PropertiesLoaderUtils.java
+++ spring-core/src/main/java/org/springframework/core/io/support/PropertiesLoaderUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -182,17 +182,13 @@ public abstract class PropertiesLoaderUtils {
 			URL url = urls.nextElement();
 			URLConnection con = url.openConnection();
 			ResourceUtils.useCachesIfNecessary(con);
-			InputStream is = con.getInputStream();
-			try {
+			try (InputStream is = con.getInputStream()) {
 				if (resourceName.endsWith(XML_FILE_EXTENSION)) {
 					props.loadFromXML(is);
 				}
 				else {
 					props.load(is);
 				}
-			}
-			finally {
-				is.close();
 			}
 		}
 		return props;
