3276f818519f9bde187c4ccb83653c21c2e0e7fb
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2020-05-08 10:34:26 +0100

    Avoid one layer of HttpHeaders wrapping
    See gh-24680

--- spring-web/src/main/java/org/springframework/http/server/reactive/AbstractServerHttpRequest.java
+++ spring-web/src/main/java/org/springframework/http/server/reactive/AbstractServerHttpRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -76,7 +76,7 @@ public abstract class AbstractServerHttpRequest implements ServerHttpRequest {
 	 * @param contextPath the context path for the request
 	 * @param headers the headers for the request
 	 */
-	public AbstractServerHttpRequest(URI uri, @Nullable String contextPath, HttpHeaders headers) {
+	public AbstractServerHttpRequest(URI uri, @Nullable String contextPath, MultiValueMap<String, String> headers) {
 		this.uri = uri;
 		this.path = RequestPath.parse(uri, contextPath);
 		this.headers = HttpHeaders.readOnlyHttpHeaders(headers);
--- spring-web/src/main/java/org/springframework/http/server/reactive/ReactorServerHttpRequest.java
+++ spring-web/src/main/java/org/springframework/http/server/reactive/ReactorServerHttpRequest.java
@@ -33,7 +33,6 @@ import reactor.netty.http.server.HttpServerRequest;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.NettyDataBufferFactory;
 import org.springframework.http.HttpCookie;
-import org.springframework.http.HttpHeaders;
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
 import org.springframework.util.LinkedMultiValueMap;
@@ -59,7 +58,7 @@ class ReactorServerHttpRequest extends AbstractServerHttpRequest {
 	public ReactorServerHttpRequest(HttpServerRequest request, NettyDataBufferFactory bufferFactory)
 			throws URISyntaxException {
 
-		super(initUri(request), "", initHeaders(request));
+		super(initUri(request), "", new NettyHeadersAdapter(request.requestHeaders()));
 		Assert.notNull(bufferFactory, "DataBufferFactory must not be null");
 		this.request = request;
 		this.bufferFactory = bufferFactory;
@@ -125,11 +124,6 @@ class ReactorServerHttpRequest extends AbstractServerHttpRequest {
 			}
 		}
 		return uri;
-	}
-
-	private static HttpHeaders initHeaders(HttpServerRequest channel) {
-		NettyHeadersAdapter headersMap = new NettyHeadersAdapter(channel.requestHeaders());
-		return new HttpHeaders(headersMap);
 	}
 
 
