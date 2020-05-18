4ec02a7e53175159560f97d3a9d376843a399949
Juergen Hoeller <jhoeller@pivotal.io>
2020-05-18 14:54:01 +0200

    Consistent not-null assertions for configured interceptors
    Closes gh-25089

--- spring-messaging/src/main/java/org/springframework/messaging/support/AbstractMessageChannel.java
+++ spring-messaging/src/main/java/org/springframework/messaging/support/AbstractMessageChannel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -93,11 +93,13 @@ public abstract class AbstractMessageChannel implements MessageChannel, Intercep
 
 	@Override
 	public void addInterceptor(ChannelInterceptor interceptor) {
+		Assert.notNull(interceptor, "ChannelInterceptor must not be null");
 		this.interceptors.add(interceptor);
 	}
 
 	@Override
 	public void addInterceptor(int index, ChannelInterceptor interceptor) {
+		Assert.notNull(interceptor, "ChannelInterceptor must not be null");
 		this.interceptors.add(index, interceptor);
 	}
 
