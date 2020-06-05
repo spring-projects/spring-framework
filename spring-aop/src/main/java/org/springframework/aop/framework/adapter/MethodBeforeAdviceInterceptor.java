04c8de4e507183c2bc8fd64811b0998a72aff88c
Sam Brannen <sbrannen@vmware.com>
2020-06-05 14:29:06 +0200

    Polishing

--- spring-aop/src/main/java/org/springframework/aop/framework/adapter/MethodBeforeAdviceInterceptor.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/adapter/MethodBeforeAdviceInterceptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,9 +26,9 @@ import org.springframework.aop.MethodBeforeAdvice;
 import org.springframework.util.Assert;
 
 /**
- * Interceptor to wrap am {@link org.springframework.aop.MethodBeforeAdvice}.
- * Used internally by the AOP framework; application developers should not need
- * to use this class directly.
+ * Interceptor to wrap a {@link MethodBeforeAdvice}.
+ * <p>Used internally by the AOP framework; application developers should not
+ * need to use this class directly.
  *
  * @author Rod Johnson
  * @see AfterReturningAdviceInterceptor
