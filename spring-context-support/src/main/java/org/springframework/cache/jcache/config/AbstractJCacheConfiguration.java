85e3f238636cdefaa603101ca5477416e729e686
Juergen Hoeller <hoellerj@vmware.com>
2020-06-11 23:43:41 +0200

    Consistent abstract declaration of "Abstract" base classes
    Closes gh-25240

--- spring-context-support/src/main/java/org/springframework/cache/jcache/config/AbstractJCacheConfiguration.java
+++ spring-context-support/src/main/java/org/springframework/cache/jcache/config/AbstractJCacheConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ import org.springframework.context.annotation.Role;
  * @see JCacheConfigurer
  */
 @Configuration
-public class AbstractJCacheConfiguration extends AbstractCachingConfiguration {
+public abstract class AbstractJCacheConfiguration extends AbstractCachingConfiguration {
 
 	protected CacheResolver exceptionCacheResolver;
 
--- spring-webmvc/src/main/java/org/springframework/web/servlet/view/AbstractTemplateViewResolver.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/view/AbstractTemplateViewResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ package org.springframework.web.servlet.view;
  * @see org.springframework.web.servlet.view.velocity.VelocityViewResolver
  * @see org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver
  */
-public class AbstractTemplateViewResolver extends UrlBasedViewResolver {
+public abstract class AbstractTemplateViewResolver extends UrlBasedViewResolver {
 
 	private boolean exposeRequestAttributes = false;
 
