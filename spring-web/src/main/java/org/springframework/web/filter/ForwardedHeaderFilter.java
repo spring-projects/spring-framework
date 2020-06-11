bc4945d677da65c176aabc29f936770f47c0bde8
Sam Brannen <sbrannen@vmware.com>
2020-06-11 15:09:05 +0200

    Ensure non-null condition comes 1st in ternary operator
    See gh-25232

--- spring-web/src/main/java/org/springframework/web/filter/ForwardedHeaderFilter.java
+++ spring-web/src/main/java/org/springframework/web/filter/ForwardedHeaderFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -364,7 +364,7 @@ public class ForwardedHeaderFilter extends OncePerRequestFilter {
 
 
 		public String getContextPath() {
-			return (this.forwardedPrefix == null ? this.delegate.get().getContextPath() : this.forwardedPrefix);
+			return (this.forwardedPrefix != null ? this.forwardedPrefix : this.delegate.get().getContextPath());
 		}
 
 		public String getRequestUri() {
