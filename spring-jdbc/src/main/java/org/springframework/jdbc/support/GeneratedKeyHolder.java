cc301011b2edd6c6a02bfe5236482038c4b6473a
Sam Brannen <sbrannen@vmware.com>
2020-06-16 14:17:43 +0200

    Polish JDBC KeyHolder support
    See gh-24655

--- spring-jdbc/src/main/java/org/springframework/jdbc/support/GeneratedKeyHolder.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/support/GeneratedKeyHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,25 +66,25 @@ public class GeneratedKeyHolder implements KeyHolder {
 
 	@Override
 	@Nullable
-	public <T> T getKeyAs(Class<T> keyClass) throws InvalidDataAccessApiUsageException, DataRetrievalFailureException {
+	public <T> T getKeyAs(Class<T> keyType) throws InvalidDataAccessApiUsageException, DataRetrievalFailureException {
 		if (this.keyList.isEmpty()) {
 			return null;
 		}
 		if (this.keyList.size() > 1 || this.keyList.get(0).size() > 1) {
 			throw new InvalidDataAccessApiUsageException(
-					"The getKey method should only be used when a single key is returned.  " +
+					"The getKey method should only be used when a single key is returned. " +
 					"The current key entry contains multiple keys: " + this.keyList);
 		}
 		Iterator<Object> keyIter = this.keyList.get(0).values().iterator();
 		if (keyIter.hasNext()) {
 			Object key = keyIter.next();
-			if (key == null || !(keyClass.isAssignableFrom(key.getClass()))) {
+			if (key == null || !(keyType.isAssignableFrom(key.getClass()))) {
 				throw new DataRetrievalFailureException(
-						"The generated key is not of a supported type. " +
+						"The generated key type is not supported. " +
 						"Unable to cast [" + (key != null ? key.getClass().getName() : null) +
-						"] to [" + keyClass.getName() + "]");
+						"] to [" + keyType.getName() + "].");
 			}
-			return keyClass.cast(key);
+			return keyType.cast(key);
 		}
 		else {
 			throw new DataRetrievalFailureException("Unable to retrieve the generated key. " +
@@ -100,7 +100,7 @@ public class GeneratedKeyHolder implements KeyHolder {
 		}
 		if (this.keyList.size() > 1) {
 			throw new InvalidDataAccessApiUsageException(
-					"The getKeys method should only be used when keys for a single row are returned.  " +
+					"The getKeys method should only be used when keys for a single row are returned. " +
 					"The current key list contains keys for multiple rows: " + this.keyList);
 		}
 		return this.keyList.get(0);
