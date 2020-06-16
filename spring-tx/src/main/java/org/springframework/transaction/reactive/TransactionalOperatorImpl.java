aca28127051200ec4ac4f72fb03c788e7f8bc251
Mark Paluch <mpaluch@pivotal.io>
2020-06-16 11:00:58 +0200

    Rollback reactive transaction on cancel
    This commit introduces a change in reactive transaction semantics for
    cancel signals. Canceling a subscription now rolls back a reactive transaction
    to achieve a deterministic transaction outcome.
    
    Previously, cancel signals committed a transaction which could
    cause partially committed transactions depending on when the cancel happened.

--- spring-tx/src/main/java/org/springframework/transaction/reactive/TransactionalOperatorImpl.java
+++ spring-tx/src/main/java/org/springframework/transaction/reactive/TransactionalOperatorImpl.java
@@ -79,7 +79,7 @@ final class TransactionalOperatorImpl implements TransactionalOperator {
 			// Need re-wrapping of ReactiveTransaction until we get hold of the exception
 			// through usingWhen.
 			return status.flatMap(it -> Mono.usingWhen(Mono.just(it), ignore -> mono,
-					this.transactionManager::commit, (res, err) -> Mono.empty(), this.transactionManager::commit)
+					this.transactionManager::commit, (res, err) -> Mono.empty(), this.transactionManager::rollback)
 					.onErrorResume(ex -> rollbackOnException(it, ex).then(Mono.error(ex))));
 		})
 		.subscriberContext(TransactionContextManager.getOrCreateContext())
@@ -100,7 +100,7 @@ final class TransactionalOperatorImpl implements TransactionalOperator {
 							action::doInTransaction,
 							this.transactionManager::commit,
 							(tx, ex) -> Mono.empty(),
-							this.transactionManager::commit)
+							this.transactionManager::rollback)
 					.onErrorResume(ex ->
 							rollbackOnException(it, ex).then(Mono.error(ex))));
 		})
