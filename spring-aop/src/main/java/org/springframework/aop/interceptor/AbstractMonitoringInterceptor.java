51a5517a4580b97790710b2b05a6799d7535f08c
Sam Brannen <sbrannen@vmware.com>
2020-06-27 14:33:44 +0200

    Polishing

--- spring-aop/src/main/java/org/springframework/aop/interceptor/AbstractMonitoringInterceptor.java
+++ spring-aop/src/main/java/org/springframework/aop/interceptor/AbstractMonitoringInterceptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,8 +103,8 @@ public abstract class AbstractMonitoringInterceptor extends AbstractTraceInterce
 		if (this.logTargetClassInvocation && clazz.isInstance(invocation.getThis())) {
 			clazz = invocation.getThis().getClass();
 		}
-		String clazzName = clazz.getName();
-		return getPrefix() + clazzName + '.' + method.getName() + getSuffix();
+		String className = clazz.getName();
+		return getPrefix() + className + '.' + method.getName() + getSuffix();
 	}
 
 }
