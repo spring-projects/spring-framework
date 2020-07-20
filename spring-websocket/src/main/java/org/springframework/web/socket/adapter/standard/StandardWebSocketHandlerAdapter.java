35adf2b1bc2e7449fcfc991ffdf996cabf45d8d1
Juergen Hoeller <hoellerj@vmware.com>
2020-07-20 16:47:32 +0200

    Defer creating logger in StandardWebSocketHandlerAdapter to instantiation time
    Closes gh-25427
    
    (cherry picked from commit 64f4703445c57adba37477394f2c3217d0af99cb)

--- spring-websocket/src/main/java/org/springframework/web/socket/adapter/standard/StandardWebSocketHandlerAdapter.java
+++ spring-websocket/src/main/java/org/springframework/web/socket/adapter/standard/StandardWebSocketHandlerAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ import org.springframework.web.socket.handler.ExceptionWebSocketHandlerDecorator
  */
 public class StandardWebSocketHandlerAdapter extends Endpoint {
 
-	private static final Log logger = LogFactory.getLog(StandardWebSocketHandlerAdapter.class);
+	private final Log logger = LogFactory.getLog(StandardWebSocketHandlerAdapter.class);
 
 	private final WebSocketHandler handler;
 
