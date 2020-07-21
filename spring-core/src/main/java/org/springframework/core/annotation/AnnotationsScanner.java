e894dbb7a34073408962f7b92899003a60a1e132
yilianhuaixiao <85142297@qq.com>
2020-07-21 14:22:26 +0800

    filter after doWithAggregate

--- spring-core/src/main/java/org/springframework/core/annotation/AnnotationsScanner.java
+++ spring-core/src/main/java/org/springframework/core/annotation/AnnotationsScanner.java
@@ -146,14 +146,14 @@ abstract class AnnotationsScanner {
 			Class<?> root = source;
 			while (source != null && source != Object.class && remaining > 0 &&
 					!hasPlainJavaAnnotationsOnly(source)) {
+				R result = processor.doWithAggregate(context, aggregateIndex);
+				if (result != null) {
+					return result;
+				}
 				if (isFiltered(source, context, classFilter)) {
 					source = source.getSuperclass();
 					aggregateIndex++;
 					continue;
-				}
-				R result = processor.doWithAggregate(context, aggregateIndex);
-				if (result != null) {
-					return result;
 				}
 				Annotation[] declaredAnnotations =
 						getDeclaredAnnotations(context, source, classFilter, true);
