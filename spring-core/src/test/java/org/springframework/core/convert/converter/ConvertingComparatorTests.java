471577a11b786d1518abc2de13cfeb88eba1442b
Sam Brannen <sbrannen@vmware.com>
2020-07-22 11:35:31 +0200

    Polish contribution
    See gh-25454

--- spring-core/src/test/java/org/springframework/core/convert/converter/ConvertingComparatorTests.java
+++ spring-core/src/test/java/org/springframework/core/convert/converter/ConvertingComparatorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package org.springframework.core.convert.converter;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
