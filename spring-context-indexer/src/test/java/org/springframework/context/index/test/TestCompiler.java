63aab62515fd4e97c745d64f1faf01a96c64ec45
matheus0210 <matheus0210@hotmail.com>
2020-07-22 05:10:01 -0300

    Removed unused imports

--- spring-context-indexer/src/test/java/org/springframework/context/index/test/TestCompiler.java
+++ spring-context-indexer/src/test/java/org/springframework/context/index/test/TestCompiler.java
@@ -21,8 +21,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.annotation.processing.Processor;
 import javax.tools.JavaCompiler;
