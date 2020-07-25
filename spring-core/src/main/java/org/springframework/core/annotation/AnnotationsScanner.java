5442d8779aa6ae3d5495255853ef7be8a14ca6da
Sam Brannen <sbrannen@vmware.com>
2020-07-25 12:27:21 +0200

    Polish contribution
    See gh-25429

--- spring-core/src/main/java/org/springframework/core/annotation/AnnotationsScanner.java
+++ spring-core/src/main/java/org/springframework/core/annotation/AnnotationsScanner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -151,6 +151,7 @@ abstract class AnnotationsScanner {
 					return result;
 				}
 				if (isFiltered(source, context, classFilter)) {
+					// Skip the current level in the class hierarchy.
 					source = source.getSuperclass();
 					aggregateIndex++;
 					continue;
