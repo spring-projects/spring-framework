bb17ce5086494c9eea6c131a60694d86929ccd6b
minuk8932 <minuk8932@naver.com>
2020-07-29 18:57:18 +0900

    change String(source) statement

--- spring-core/src/main/java/org/springframework/core/convert/support/StringToCharacterConverter.java
+++ spring-core/src/main/java/org/springframework/core/convert/support/StringToCharacterConverter.java
@@ -28,10 +28,7 @@ final class StringToCharacterConverter implements Converter<String, Character> {
 
 	@Override
 	public Character convert(String source) {
-		if (source.isEmpty()) {
-			return null;
-		}
-		if (source.length() > 1) {
+		if (source.length() != 1) {
 			throw new IllegalArgumentException(
 					"Can only convert a [String] with length of 1 to a [Character]; string value '" + source + "'  has length of " + source.length());
 		}
