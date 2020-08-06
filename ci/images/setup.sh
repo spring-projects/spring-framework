df98d0c6610f901c9552cda282e2622d683b924d
Brian Clozel <bclozel@vmware.com>
2020-08-06 10:17:19 +0200

    Update CI to concourse-release-scripts 0.2.0

--- ci/images/setup.sh
+++ ci/images/setup.sh
@@ -11,7 +11,7 @@ rm -rf /var/lib/apt/lists/*
 
 curl https://raw.githubusercontent.com/spring-io/concourse-java-scripts/v0.0.3/concourse-java.sh > /opt/concourse-java.sh
 
-curl --output /opt/concourse-release-scripts.jar https://repo.spring.io/release/io/spring/concourse/releasescripts/concourse-release-scripts/0.1.0/concourse-release-scripts-0.1.0.jar
+curl --output /opt/concourse-release-scripts.jar https://repo.spring.io/release/io/spring/concourse/releasescripts/concourse-release-scripts/0.2.0/concourse-release-scripts-0.2.0.jar
 
 ###########################################################
 # JAVA
