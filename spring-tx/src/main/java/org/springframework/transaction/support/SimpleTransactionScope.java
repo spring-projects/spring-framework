148dc95eb110c4d97829ee3d60c78fb683b9db79
Sam Brannen <sbrannen@vmware.com>
2020-08-22 23:19:58 +0200

    Fix regressions in SimpleThreadScope and SimpleTransactionScope
    PR gh-25038 introduced regressions in SimpleThreadScope and
    SimpleTransactionScope in Spring Framework 5.2.7. Specifically, if a
    thread-scoped or transaction-scoped bean has a dependency on another
    thread-scoped or transaction-scoped bean, respectively, a
    ConcurrentModificationException will be thrown on Java 11 or higher.
    
    The reason is that Java 11 introduced a check for concurrent
    modification in java.util.HashMap's computeIfAbsent() implementation,
    and such a modification can occur when a thread-scoped bean is being
    created in order to satisfy a dependency of another thread-scoped bean
    that is currently being created.
    
    This commit fixes these regressions by switching from HashMap to
    ConcurrentHashMap for the instance maps in SimpleThreadScope and
    SimpleTransactionScope.
    
    Closes gh-25618

--- spring-tx/src/main/java/org/springframework/transaction/support/SimpleTransactionScope.java
+++ spring-tx/src/main/java/org/springframework/transaction/support/SimpleTransactionScope.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,9 @@
 
 package org.springframework.transaction.support;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.Scope;
@@ -92,7 +92,7 @@ public class SimpleTransactionScope implements Scope {
 	 */
 	static class ScopedObjectsHolder {
 
-		final Map<String, Object> scopedInstances = new HashMap<>();
+		final Map<String, Object> scopedInstances = new ConcurrentHashMap<>();
 
 		final Map<String, Runnable> destructionCallbacks = new LinkedHashMap<>();
 	}
