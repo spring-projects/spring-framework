9f2d8d409970791d574072a28660dcb0e89a0c0a
Juergen Hoeller <hoellerj@vmware.com>
2020-09-01 23:31:00 +0200

    Explicit nullability declarations for getTarget() implementations

--- spring-web/src/main/java/org/springframework/web/bind/support/WebExchangeBindException.java
+++ spring-web/src/main/java/org/springframework/web/bind/support/WebExchangeBindException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -217,6 +217,7 @@ public class WebExchangeBindException extends ServerWebInputException implements
 	}
 
 	@Override
+	@Nullable
 	public Object getTarget() {
 		return this.bindingResult.getTarget();
 	}
