72b2dda17fcdb43af14778597da0c92970143b80
Juergen Hoeller <hoellerj@vmware.com>
2020-10-13 00:20:06 +0200

    Avoid creation of unused logger instance in AbstractMediaTypeExpression
    Closes gh-25901

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/condition/AbstractMediaTypeExpression.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/condition/AbstractMediaTypeExpression.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,6 @@
 
 package org.springframework.web.servlet.mvc.condition;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import org.springframework.http.MediaType;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,8 +29,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @since 3.1
  */
 abstract class AbstractMediaTypeExpression implements MediaTypeExpression, Comparable<AbstractMediaTypeExpression> {
-
-	protected final Log logger = LogFactory.getLog(getClass());
 
 	private final MediaType mediaType;
 
