3d31750acc14e1172fe83075bd1bb08b3e6db0e9
Brian Clozel <bclozel@vmware.com>
2020-11-16 18:20:19 +0100

    Don't swallow logs during promotion job in CI
    Prior to this commit, the promote task in the CI release pipeline would
    write the "concourse-release-scripts" CONSOLE logs to /dev/null; this
    commit ensures that we can read those while promoting artifacts.

--- ci/scripts/promote-version.sh
+++ ci/scripts/promote-version.sh
@@ -6,11 +6,11 @@ CONFIG_DIR=git-repo/ci/config
 version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
 export BUILD_INFO_LOCATION=$(pwd)/artifactory-repo/build-info.json
 
-java -jar /opt/concourse-release-scripts.jar promote $RELEASE_TYPE $BUILD_INFO_LOCATION > /dev/null || { exit 1; }
+java -jar /opt/concourse-release-scripts.jar promote $RELEASE_TYPE $BUILD_INFO_LOCATION || { exit 1; }
 
 java -jar /opt/concourse-release-scripts.jar \
   --spring.config.location=${CONFIG_DIR}/release-scripts.yml \
-  distribute $RELEASE_TYPE $BUILD_INFO_LOCATION > /dev/null || { exit 1; }
+  distribute $RELEASE_TYPE $BUILD_INFO_LOCATION || { exit 1; }
 
 echo "Promotion complete"
 echo $version > version/version
