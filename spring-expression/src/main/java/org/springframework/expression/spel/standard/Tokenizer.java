52d54e5df56b5646bc896d7b381a6295fc7a0789
fengyuanwei <fengyuanwei@wondersgroup.com>
2020-12-03 15:48:38 +0800

    Fix comments to match behaviour in Tokenizer#lexNumericLiteral

--- spring-expression/src/main/java/org/springframework/expression/spel/standard/Tokenizer.java
+++ spring-expression/src/main/java/org/springframework/expression/spel/standard/Tokenizer.java
@@ -322,7 +322,6 @@ class Tokenizer {
 	}
 
 	// REAL_LITERAL :
-	// ('.' (DECIMAL_DIGIT)+ (EXPONENT_PART)? (REAL_TYPE_SUFFIX)?) |
 	// ((DECIMAL_DIGIT)+ '.' (DECIMAL_DIGIT)+ (EXPONENT_PART)? (REAL_TYPE_SUFFIX)?) |
 	// ((DECIMAL_DIGIT)+ (EXPONENT_PART) (REAL_TYPE_SUFFIX)?) |
 	// ((DECIMAL_DIGIT)+ (REAL_TYPE_SUFFIX));
@@ -330,10 +329,10 @@ class Tokenizer {
 	// fragment HEX_DIGIT :
 	// '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|'A'|'B'|'C'|'D'|'E'|'F'|'a'|'b'|'c'|'d'|'e'|'f';
 	//
-	// fragment EXPONENT_PART : 'e' (SIGN)* (DECIMAL_DIGIT)+ | 'E' (SIGN)*
-	// (DECIMAL_DIGIT)+ ;
+	// fragment EXPONENT_PART : 'e' (SIGN)? (DECIMAL_DIGIT)+ | 'E' (SIGN)? (DECIMAL_DIGIT)+ ;
 	// fragment SIGN : '+' | '-' ;
 	// fragment REAL_TYPE_SUFFIX : 'F' | 'f' | 'D' | 'd';
+	//
 	// INTEGER_LITERAL
 	// : (DECIMAL_DIGIT)+ (INTEGER_TYPE_SUFFIX)?;
 
