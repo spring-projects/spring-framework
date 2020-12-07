e87e03c539b13a709a4019369a396949db3ac4e8
Sébastien Deleuze <sdeleuze@vmware.com>
2020-12-08 00:06:19 +0100

    Refine ConfigurationClassPostProcessor behavior in native images
    This commit refines ConfigurationClassPostProcessor behavior in
    native images by skipping configuration classes enhancement
    instead of raising an error.
    
    See spring-projects-experimental/spring-graalvm-native#248 for
    more details.
    
    Closes gh-26236

--- spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassPostProcessor.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassPostProcessor.java
@@ -427,14 +427,10 @@ public class ConfigurationClassPostProcessor implements BeanDefinitionRegistryPo
 				configBeanDefs.put(beanName, (AbstractBeanDefinition) beanDef);
 			}
 		}
-		if (configBeanDefs.isEmpty()) {
+		if (configBeanDefs.isEmpty() || IN_NATIVE_IMAGE) {
 			// nothing to enhance -> return immediately
 			enhanceConfigClasses.end();
 			return;
-		}
-		if (IN_NATIVE_IMAGE) {
-			throw new BeanDefinitionStoreException("@Configuration classes need to be marked as " +
-					"proxyBeanMethods=false. Found: " + configBeanDefs.keySet());
 		}
 
 		ConfigurationClassEnhancer enhancer = new ConfigurationClassEnhancer();
