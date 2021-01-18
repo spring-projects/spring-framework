3c959d3515ded1d021954b6b4ab1aba4e25077c3
Sam Brannen <sbrannen@vmware.com>
2021-01-18 11:26:29 +0100

    Fix grammar in Javadoc

--- spring-aop/src/main/java/org/springframework/aop/target/AbstractPoolingTargetSource.java
+++ spring-aop/src/main/java/org/springframework/aop/target/AbstractPoolingTargetSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -116,7 +116,7 @@ public abstract class AbstractPoolingTargetSource extends AbstractPrototypeBased
 
 
 	/**
-	 * Return an IntroductionAdvisor that providing a mixin
+	 * Return an IntroductionAdvisor that provides a mixin
 	 * exposing statistics about the pool maintained by this object.
 	 */
 	public DefaultIntroductionAdvisor getPoolingConfigMixin() {
