e7fd048ab98cdc9b914a066b6ddfefbb37cd1e7b
Craig Andrews <candrews@integralblue.com>
2021-02-20 00:28:03 -0500

    getResource can throw IllegalArgumentException
    Class.getResource, ClassLoader.getResource, and ClassLoader.getSystemResource will throw IllegalArgumentException if a malformed URL is provided to them.
    
    According to its javadoc, resolveURL should return null if not resolvable, so catch the IllegalArgumentException and return null.

--- spring-core/src/main/java/org/springframework/core/io/ClassPathResource.java
+++ spring-core/src/main/java/org/springframework/core/io/ClassPathResource.java
@@ -148,14 +148,19 @@ public class ClassPathResource extends AbstractFileResolvingResource {
 	 */
 	@Nullable
 	protected URL resolveURL() {
-		if (this.clazz != null) {
-			return this.clazz.getResource(this.path);
+		try {
+			if (this.clazz != null) {
+				return this.clazz.getResource(this.path);
+			}
+			else if (this.classLoader != null) {
+				return this.classLoader.getResource(this.path);
+			}
+			else {
+				return ClassLoader.getSystemResource(this.path);
+			}
 		}
-		else if (this.classLoader != null) {
-			return this.classLoader.getResource(this.path);
-		}
-		else {
-			return ClassLoader.getSystemResource(this.path);
+		catch (IllegalArgumentException ex) {
+			return null;
 		}
 	}
 
