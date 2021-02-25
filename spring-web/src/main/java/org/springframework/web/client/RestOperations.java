8ed7f4703bbe371aa54b7ae5d61cce92828d53b6
Rossen Stoyanchev <rstoyanchev@vmware.com>
2021-02-25 18:29:05 +0000

    Improve Javadoc for responseType on RestTemplate#exchange
    Closes gh-26599

--- spring-web/src/main/java/org/springframework/web/client/RestOperations.java
+++ spring-web/src/main/java/org/springframework/web/client/RestOperations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -499,7 +499,7 @@ public interface RestOperations {
 	 * @param method the HTTP method (GET, POST, etc)
 	 * @param requestEntity the entity (headers and/or body) to write to the request
 	 * may be {@code null})
-	 * @param responseType the type of the return value
+	 * @param responseType the type to convert the response to, or {@code Void.class} for no body
 	 * @param uriVariables the variables to expand in the template
 	 * @return the response as entity
 	 * @since 3.0.2
@@ -515,7 +515,7 @@ public interface RestOperations {
 	 * @param method the HTTP method (GET, POST, etc)
 	 * @param requestEntity the entity (headers and/or body) to write to the request
 	 * (may be {@code null})
-	 * @param responseType the type of the return value
+	 * @param responseType the type to convert the response to, or {@code Void.class} for no body
 	 * @param uriVariables the variables to expand in the template
 	 * @return the response as entity
 	 * @since 3.0.2
@@ -530,7 +530,7 @@ public interface RestOperations {
 	 * @param method the HTTP method (GET, POST, etc)
 	 * @param requestEntity the entity (headers and/or body) to write to the request
 	 * (may be {@code null})
-	 * @param responseType the type of the return value
+	 * @param responseType the type to convert the response to, or {@code Void.class} for no body
 	 * @return the response as entity
 	 * @since 3.0.2
 	 */
@@ -552,7 +552,7 @@ public interface RestOperations {
 	 * @param method the HTTP method (GET, POST, etc)
 	 * @param requestEntity the entity (headers and/or body) to write to the
 	 * request (may be {@code null})
-	 * @param responseType the type of the return value
+	 * @param responseType the type to convert the response to, or {@code Void.class} for no body
 	 * @param uriVariables the variables to expand in the template
 	 * @return the response as entity
 	 * @since 3.2
@@ -575,7 +575,7 @@ public interface RestOperations {
 	 * @param method the HTTP method (GET, POST, etc)
 	 * @param requestEntity the entity (headers and/or body) to write to the request
 	 * (may be {@code null})
-	 * @param responseType the type of the return value
+	 * @param responseType the type to convert the response to, or {@code Void.class} for no body
 	 * @param uriVariables the variables to expand in the template
 	 * @return the response as entity
 	 * @since 3.2
@@ -598,7 +598,7 @@ public interface RestOperations {
 	 * @param method the HTTP method (GET, POST, etc)
 	 * @param requestEntity the entity (headers and/or body) to write to the request
 	 * (may be {@code null})
-	 * @param responseType the type of the return value
+	 * @param responseType the type to convert the response to, or {@code Void.class} for no body
 	 * @return the response as entity
 	 * @since 3.2
 	 */
@@ -618,7 +618,7 @@ public interface RestOperations {
 	 * ResponseEntity&lt;MyResponse&gt; response = template.exchange(request, MyResponse.class);
 	 * </pre>
 	 * @param requestEntity the entity to write to the request
-	 * @param responseType the type of the return value
+	 * @param responseType the type to convert the response to, or {@code Void.class} for no body
 	 * @return the response as entity
 	 * @since 4.1
 	 */
@@ -640,7 +640,7 @@ public interface RestOperations {
 	 * ResponseEntity&lt;List&lt;MyResponse&gt;&gt; response = template.exchange(request, myBean);
 	 * </pre>
 	 * @param requestEntity the entity to write to the request
-	 * @param responseType the type of the return value
+	 * @param responseType the type to convert the response to, or {@code Void.class} for no body
 	 * @return the response as entity
 	 * @since 4.1
 	 */
