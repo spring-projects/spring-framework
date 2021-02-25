b5147a034c05b85b9619b107584f0837aab86961
Juergen Hoeller <hoellerj@vmware.com>
2021-02-25 18:08:04 +0100

    Polishing
    See gh-26574
    See gh-26575

--- spring-beans/src/main/java/org/springframework/beans/propertyeditors/PathEditor.java
+++ spring-beans/src/main/java/org/springframework/beans/propertyeditors/PathEditor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -98,6 +98,7 @@ public class PathEditor extends PropertyEditorSupport {
 			setValue(null);
 		}
 		else if (!resource.isFile() && !resource.exists() && nioPathCandidate) {
+			// Prefer getFile().toPath() below for non-existent file handles
 			setValue(Paths.get(text).normalize());
 		}
 		else {
--- spring-core/src/main/java/org/springframework/core/io/ClassPathResource.java
+++ spring-core/src/main/java/org/springframework/core/io/ClassPathResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -160,6 +160,8 @@ public class ClassPathResource extends AbstractFileResolvingResource {
 			}
 		}
 		catch (IllegalArgumentException ex) {
+			// Should not happen according to the JDK's contract:
+			// see https://github.com/openjdk/jdk/pull/2662
 			return null;
 		}
 	}
