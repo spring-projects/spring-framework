48688b7b0455efc74b2998288242e01ba78f3f73
Juergen Hoeller <hoellerj@vmware.com>
2021-03-08 23:45:47 +0100

    Pass getListenerId call on to delegate
    See gh-26638

--- spring-context/src/main/java/org/springframework/context/event/SourceFilteringListener.java
+++ spring-context/src/main/java/org/springframework/context/event/SourceFilteringListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -72,6 +72,11 @@ public class SourceFilteringListener implements GenericApplicationListener, Smar
 		if (event.getSource() == this.source) {
 			onApplicationEventInternal(event);
 		}
+	}
+
+	@Override
+	public String getListenerId() {
+		return (this.delegate != null ? this.delegate.getListenerId() : "");
 	}
 
 	@Override
