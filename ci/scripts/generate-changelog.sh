f6d3d9d6e6ea38599b98da907ecd536487d07565
Stephane Nicoll <snicoll@vmware.com>
2021-03-10 13:59:30 +0100

    Polish CI pipeline

--- ci/scripts/generate-changelog.sh
+++ ci/scripts/generate-changelog.sh
@@ -4,7 +4,6 @@ set -e
 CONFIG_DIR=git-repo/ci/config
 version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
 
-
 java -jar /github-changelog-generator.jar \
   --spring.config.location=${CONFIG_DIR}/changelog-generator.yml \
   ${version} generated-changelog/changelog.md
