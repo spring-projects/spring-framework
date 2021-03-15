459860a5166abed4e090cd2154a26b75c50101a4
Rebwon <msolo021015@naver.com>
2021-03-15 21:31:53 +0900

    polish

--- spring-test/src/test/java/org/springframework/mock/web/MockFilterChainTests.java
+++ spring-test/src/test/java/org/springframework/mock/web/MockFilterChainTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,7 +55,7 @@ class MockFilterChainTests {
 	@Test
 	void constructorNullServlet() {
 		assertThatIllegalArgumentException().isThrownBy(() ->
-				new MockFilterChain((Servlet) null));
+				new MockFilterChain(null));
 	}
 
 	@Test
