9744817c8cae3ef6280ad02ee07525dd854aa1fd
Rebwon <msolo021015@naver.com>
2021-03-16 11:39:35 +0900

    Polish TestContextAnnotationsUtils.java

--- spring-test/src/test/java/org/springframework/test/context/TestContextAnnotationUtilsTests.java
+++ spring-test/src/test/java/org/springframework/test/context/TestContextAnnotationUtilsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -557,10 +557,10 @@ class TestContextAnnotationUtilsTests {
 	@Target(ElementType.TYPE)
 	@interface MetaConfig {
 
-		static class DevConfig {
+		class DevConfig {
 		}
 
-		static class ProductionConfig {
+		class ProductionConfig {
 		}
 
 
@@ -607,11 +607,11 @@ class TestContextAnnotationUtilsTests {
 	}
 
 	@MetaConfig
-	class MetaConfigWithDefaultAttributesTestCase {
+	static class MetaConfigWithDefaultAttributesTestCase {
 	}
 
 	@MetaConfig(classes = TestContextAnnotationUtilsTests.class)
-	class MetaConfigWithOverriddenAttributesTestCase {
+	static class MetaConfigWithOverriddenAttributesTestCase {
 	}
 
 	// -------------------------------------------------------------------------
