a08593b44bada4f53797f991d89a21582eee4217
Rossen Stoyanchev <rstoyanchev@vmware.com>
2021-04-12 19:53:08 +0100

    Correct matching of static resources with parsed patterns
    Closes gh-26775

--- spring-webflux/src/main/java/org/springframework/web/reactive/resource/PathResourceResolver.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/resource/PathResourceResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -110,7 +110,7 @@ public class PathResourceResolver extends AbstractResourceResolver {
 	 */
 	protected Mono<Resource> getResource(String resourcePath, Resource location) {
 		try {
-			if (location instanceof ClassPathResource) {
+			if (!(location instanceof UrlResource)) {
 				resourcePath = UriUtils.decode(resourcePath, StandardCharsets.UTF_8);
 			}
 			Resource resource = location.createRelative(resourcePath);
