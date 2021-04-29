36c829386b72ee730497b8b0833044bcd39be5db
Tasuku Nakagawa <38446259+T45K@users.noreply.github.com>
2021-04-30 00:59:26 +0900

    Use an import instead of FQCN in StaxEventXMLReader
    Closes gh-26875

--- spring-core/src/main/java/org/springframework/util/xml/StaxEventXMLReader.java
+++ spring-core/src/main/java/org/springframework/util/xml/StaxEventXMLReader.java
@@ -292,7 +292,7 @@ class StaxEventXMLReader extends AbstractStaxXMLReader {
 
 	private void handleDtd(DTD dtd) throws SAXException {
 		if (getLexicalHandler() != null) {
-			javax.xml.stream.Location location = dtd.getLocation();
+			Location location = dtd.getLocation();
 			getLexicalHandler().startDTD(null, location.getPublicId(), location.getSystemId());
 		}
 		if (getLexicalHandler() != null) {
