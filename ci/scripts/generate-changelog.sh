30655555bdab0d6a402387e70e044c92f6b28ed9
Stephane Nicoll <snicoll@vmware.com>
2021-05-11 16:20:28 +0200

    Remove .RELEASE suffix before querying GitHub
    This commit removes the `.RELEASE` suffix of the released version, if
    necessary. Such commit is not present on GitHub and the changelog
    generation process fails to find the milestone to use to generate the
    changelog.
    
    Closes gh-26796

--- ci/scripts/generate-changelog.sh
+++ ci/scripts/generate-changelog.sh
@@ -4,9 +4,14 @@ set -e
 CONFIG_DIR=git-repo/ci/config
 version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
 
+milestone=${version}
+if [[ $RELEASE_TYPE = "RELEASE" ]]; then
+	milestone=${version%.RELEASE}
+fi
+
 java -jar /github-changelog-generator.jar \
   --spring.config.location=${CONFIG_DIR}/changelog-generator.yml \
-  ${version} generated-changelog/changelog.md
+  ${milestone} generated-changelog/changelog.md
 
 echo ${version} > generated-changelog/version
 echo v${version} > generated-changelog/tag
