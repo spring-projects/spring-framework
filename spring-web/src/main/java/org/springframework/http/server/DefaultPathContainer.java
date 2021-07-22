460947651ae4c58d31386a0a14396c273b287119
Rossen Stoyanchev <rstoyanchev@vmware.com>
2021-07-22 21:53:14 +0100

    Minor refactoring in DefaultPathContainer
    Closes gh-27204

--- spring-web/src/main/java/org/springframework/http/server/DefaultPathContainer.java
+++ spring-web/src/main/java/org/springframework/http/server/DefaultPathContainer.java
@@ -165,10 +165,10 @@ final class DefaultPathContainer implements PathContainer {
 			int index = input.indexOf('=');
 			if (index != -1) {
 				String name = input.substring(0, index);
-				String value = input.substring(index + 1);
-				for (String v : StringUtils.commaDelimitedListToStringArray(value)) {
-					name = StringUtils.uriDecode(name, charset);
-					if (StringUtils.hasText(name)) {
+				name = StringUtils.uriDecode(name, charset);
+				if (StringUtils.hasText(name)) {
+					String value = input.substring(index + 1);
+					for (String v : StringUtils.commaDelimitedListToStringArray(value)) {
 						output.add(name, StringUtils.uriDecode(v, charset));
 					}
 				}
