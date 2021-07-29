881fa889fcdb1870e12beb7010408a316b30dc62
Sam Brannen <sbrannen@vmware.com>
2021-07-29 19:18:18 +0200

    Apply global ResultHandlers before ResultMatchers in MockMvc
    Prior to this commit, MockMvc applied global ResultMatchers before
    global ResultHandlers. This lead to unexpected scenarios where a
    failing matcher would prevent a handler from being applied.
    
    One concrete use case is `alwaysDo(print(System.err))` which should
    print out MockMvc results for debugging purposes. However, if MockMvc is
    configured with something like `alwaysExpect(content().string("?"))`
    and the expectation fails, the user will never see the expected debug
    output to help diagnose the problem.
    
    This commit addresses this issue by applying global ResultHandlers
    before ResultMatchers in MockMvc.
    
    Closes gh-27225

--- spring-test/src/main/java/org/springframework/test/web/servlet/MockMvc.java
+++ spring-test/src/main/java/org/springframework/test/web/servlet/MockMvc.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -217,11 +217,11 @@ public final class MockMvc {
 	}
 
 	private void applyDefaultResultActions(MvcResult mvcResult) throws Exception {
-		for (ResultMatcher matcher : this.defaultResultMatchers) {
-			matcher.match(mvcResult);
-		}
 		for (ResultHandler handler : this.defaultResultHandlers) {
 			handler.handle(mvcResult);
+		}
+		for (ResultMatcher matcher : this.defaultResultMatchers) {
+			matcher.match(mvcResult);
 		}
 	}
 
