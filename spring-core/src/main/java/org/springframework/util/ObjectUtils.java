f1b35f15931e1d0e74df3feb9d74ddecedc694f8
Mateusz Swiatkowski <6883643+matvs@users.noreply.github.com>
2021-07-29 11:03:26 +0200

    Fix reference to Optional.isPresent() in ObjectUtils.isEmpty()
    Closes gh-27223

--- spring-core/src/main/java/org/springframework/util/ObjectUtils.java
+++ spring-core/src/main/java/org/springframework/util/ObjectUtils.java
@@ -114,7 +114,7 @@ public abstract class ObjectUtils {
 	 * Determine whether the given object is empty.
 	 * <p>This method supports the following object types.
 	 * <ul>
-	 * <li>{@code Optional}: considered empty if {@link Optional#empty()}</li>
+	 * <li>{@code Optional}: considered empty if not {@link Optional#isPresent()}</li>
 	 * <li>{@code Array}: considered empty if its length is zero</li>
 	 * <li>{@link CharSequence}: considered empty if its length is zero</li>
 	 * <li>{@link Collection}: delegates to {@link Collection#isEmpty()}</li>
