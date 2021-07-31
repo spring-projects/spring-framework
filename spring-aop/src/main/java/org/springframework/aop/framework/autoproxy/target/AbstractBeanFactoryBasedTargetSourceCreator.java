ce94f69e525282b418f801e9d734070ad5cbfc45
Sam Brannen <sbrannen@vmware.com>
2021-07-31 16:30:39 +0200

    Polish contribution
    See gh-27234

--- spring-aop/src/main/java/org/springframework/aop/framework/autoproxy/target/AbstractBeanFactoryBasedTargetSourceCreator.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/autoproxy/target/AbstractBeanFactoryBasedTargetSourceCreator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -125,7 +125,8 @@ public abstract class AbstractBeanFactoryBasedTargetSourceCreator
 	 */
 	protected DefaultListableBeanFactory getInternalBeanFactoryForBean(String beanName) {
 		synchronized (this.internalBeanFactories) {
-			return this.internalBeanFactories.computeIfAbsent(beanName, k -> buildInternalBeanFactory(this.beanFactory));
+			return this.internalBeanFactories.computeIfAbsent(beanName,
+					name -> buildInternalBeanFactory(this.beanFactory));
 		}
 	}
 
