c0e479460e09b7065a21ebfbd95cec7d213ca62e
Brian Clozel <bclozel@vmware.com>
2021-09-15 12:37:10 +0200

    Switch CI pipeline to a JDK17 baseline
    This commit introduces a new `spring-framework-6.0.x` CI pipeline with
    JDK 17 baseline.
    
    Note that Kotlin still uses a JDK11 baseline for now, this will be
    addressed in gh-27413.
    
    Closes gh-27409

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -2,15 +2,9 @@
 set -e
 
 case "$1" in
-	java8)
-		 echo "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u302b08.tar.gz"
+	java17)
+		 echo "https://github.com/adoptium/temurin17-binaries/releases/download/jdk17-2021-09-15-08-15-beta/OpenJDK17-jdk_x64_linux_hotspot_2021-09-15-08-15.tar.gz"
 	;;
-	java11)
-		 echo "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.12%2B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.12_7.tar.gz"
-	;;
-  java16)
-		 echo "https://github.com/adoptium/temurin16-binaries/releases/download/jdk-16.0.2%2B7/OpenJDK16U-jdk_x64_linux_hotspot_16.0.2_7.tar.gz"
-  ;;
   *)
 		echo $"Unknown java version"
 		exit 1
