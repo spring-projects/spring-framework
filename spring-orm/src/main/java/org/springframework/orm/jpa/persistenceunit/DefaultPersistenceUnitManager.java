715f300fa11e4c20a7aaa9f9ac8439fc4072b00b
Juergen Hoeller <hoellerj@vmware.com>
2021-10-12 15:15:51 +0200

    Avoid expensive isReadable() check during classpath scan
    Closes gh-25741
    See gh-21372

--- spring-orm/src/main/java/org/springframework/orm/jpa/persistenceunit/DefaultPersistenceUnitManager.java
+++ spring-orm/src/main/java/org/springframework/orm/jpa/persistenceunit/DefaultPersistenceUnitManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package org.springframework.orm.jpa.persistenceunit;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -585,7 +586,7 @@ public class DefaultPersistenceUnitManager
 			Resource[] resources = this.resourcePatternResolver.getResources(pattern);
 			MetadataReaderFactory readerFactory = new CachingMetadataReaderFactory(this.resourcePatternResolver);
 			for (Resource resource : resources) {
-				if (resource.isReadable()) {
+				try {
 					MetadataReader reader = readerFactory.getMetadataReader(resource);
 					String className = reader.getClassMetadata().getClassName();
 					if (matchesFilter(reader, readerFactory)) {
@@ -601,6 +602,9 @@ public class DefaultPersistenceUnitManager
 						scannedUnit.addManagedPackage(
 								className.substring(0, className.length() - PACKAGE_INFO_SUFFIX.length()));
 					}
+				}
+				catch (FileNotFoundException ex) {
+					// Ignore non-readable resource
 				}
 			}
 		}
