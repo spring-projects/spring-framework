971f665eb1e8cd9e8a0c788a8f5612f815894082
Sam Brannen <sbrannen@vmware.com>
2021-10-14 19:53:40 +0200

    Apply "instanceof pattern matching" in spring-jdbc
    This commit also applies additional clean-up tasks such as the following.
    
    - final fields
    
    This has only been applied to `src/main/java`.

--- spring-jdbc/src/main/java/org/springframework/jdbc/core/BatchUpdateUtils.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/core/BatchUpdateUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,8 +62,7 @@ public abstract class BatchUpdateUtils {
 		int colIndex = 0;
 		for (Object value : values) {
 			colIndex++;
-			if (value instanceof SqlParameterValue) {
-				SqlParameterValue paramValue = (SqlParameterValue) value;
+			if (value instanceof SqlParameterValue paramValue) {
 				StatementCreatorUtils.setParameterValue(ps, colIndex, paramValue, paramValue.getValue());
 			}
 			else {
