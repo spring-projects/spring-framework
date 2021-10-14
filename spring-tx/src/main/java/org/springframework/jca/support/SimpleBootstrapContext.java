e9cf645b86c39c66759492435e6d44b70736cee5
Sam Brannen <sbrannen@vmware.com>
2021-10-14 22:18:01 +0200

    Apply "instanceof pattern matching" in spring-tx
    This commit also applies additional clean-up tasks such as the following.
    
    - final fields
    
    This has only been applied to `src/main/java`.

--- spring-tx/src/main/java/org/springframework/jca/support/SimpleBootstrapContext.java
+++ spring-tx/src/main/java/org/springframework/jca/support/SimpleBootstrapContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,7 @@ import org.springframework.util.Assert;
 public class SimpleBootstrapContext implements BootstrapContext {
 
 	@Nullable
-	private WorkManager workManager;
+	private final WorkManager workManager;
 
 	@Nullable
 	private XATerminator xaTerminator;
--- spring-tx/src/main/java/org/springframework/transaction/annotation/AnnotationTransactionAttributeSource.java
+++ spring-tx/src/main/java/org/springframework/transaction/annotation/AnnotationTransactionAttributeSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -194,10 +194,9 @@ public class AnnotationTransactionAttributeSource extends AbstractFallbackTransa
 		if (this == other) {
 			return true;
 		}
-		if (!(other instanceof AnnotationTransactionAttributeSource)) {
+		if (!(other instanceof AnnotationTransactionAttributeSource otherTas)) {
 			return false;
 		}
-		AnnotationTransactionAttributeSource otherTas = (AnnotationTransactionAttributeSource) other;
 		return (this.annotationParsers.equals(otherTas.annotationParsers) &&
 				this.publicMethodsOnly == otherTas.publicMethodsOnly);
 	}
