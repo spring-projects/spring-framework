65bf5f7c81fb35f3291a548e9d326a65e331e5eb
Brian Clozel <bclozel@vmware.com>
2021-10-25 09:58:04 +0200

    Add JDK18 variant to CI pipeline
    This commit adds a new JDK 18 variant to the CI build image and
    configures a JDK18 build (sources compiled with JDK17, tests compiled
    and run with JDK18) to the CI pipeline.
    
    Closes gh-27607

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -2,10 +2,13 @@
 set -e
 
 case "$1" in
-	java17)
-		 echo "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17%2B35/OpenJDK17-jdk_x64_linux_hotspot_17_35.tar.gz"
-	;;
+  java17)
+    echo "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17%2B35/OpenJDK17-jdk_x64_linux_hotspot_17_35.tar.gz"
+  ;;
+  java18)
+    echo "https://github.com/adoptium/temurin18-binaries/releases/download/jdk-2021-10-22-05-05-beta/OpenJDK-jdk_x64_linux_hotspot_2021-10-21-23-30.tar.gz"
+  ;;
   *)
-		echo $"Unknown java version"
-		exit 1
+    echo $"Unknown java version"
+    exit 1
 esac
