60f75603f1e4d84d579b730de3ad984556f47adb
Vedran Pavic <vpavic@duck.com>
2021-10-25 23:06:50 +0200

    Update LocaleContextResolver to implement LocaleResolver
    This commit updates LocaleContextResolver to implement LocaleResolver using default methods, which simplifies AbstractLocaleContextResolver and aligns it more closely with AbstractLocaleResolver.

--- spring-webmvc/src/main/java/org/springframework/web/servlet/i18n/AbstractLocaleContextResolver.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/i18n/AbstractLocaleContextResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,22 +16,14 @@
 
 package org.springframework.web.servlet.i18n;
 
-import java.util.Locale;
 import java.util.TimeZone;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import org.springframework.context.i18n.SimpleLocaleContext;
 import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.LocaleContextResolver;
 
 /**
  * Abstract base class for {@link LocaleContextResolver} implementations.
  * Provides support for a default locale and a default time zone.
- *
- * <p>Also provides pre-implemented versions of {@link #resolveLocale} and {@link #setLocale},
- * delegating to {@link #resolveLocaleContext} and {@link #setLocaleContext}.
  *
  * @author Juergen Hoeller
  * @since 4.0
@@ -57,18 +49,6 @@ public abstract class AbstractLocaleContextResolver extends AbstractLocaleResolv
 	@Nullable
 	public TimeZone getDefaultTimeZone() {
 		return this.defaultTimeZone;
-	}
-
-
-	@Override
-	public Locale resolveLocale(HttpServletRequest request) {
-		Locale locale = resolveLocaleContext(request).getLocale();
-		return (locale != null ? locale : request.getLocale());
-	}
-
-	@Override
-	public void setLocale(HttpServletRequest request, @Nullable HttpServletResponse response, @Nullable Locale locale) {
-		setLocaleContext(request, response, (locale != null ? new SimpleLocaleContext(locale) : null));
 	}
 
 }
