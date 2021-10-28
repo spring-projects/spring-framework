475e1996dfc6dc77725401599d4952eb135b29eb
yujinchoi-94 <yjchoi4120@gmail.com>
2021-10-28 15:45:18 +0900

    Remove unused import statement

--- spring-core/src/main/java/org/springframework/core/io/support/SpringFactoriesLoader.java
+++ spring-core/src/main/java/org/springframework/core/io/support/SpringFactoriesLoader.java
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
