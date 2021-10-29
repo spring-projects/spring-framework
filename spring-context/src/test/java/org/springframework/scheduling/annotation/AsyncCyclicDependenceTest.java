165a6091e285778455d42efe45d85aec7b61aced
baker.yuan <1750762853@qq.com>
2021-10-29 23:13:25 +0800

    add async test

---
+++ spring-context/src/test/java/org/springframework/scheduling/annotation/AsyncCyclicDependenceTest.java
@@ -0,0 +1,60 @@
+package org.springframework.scheduling.annotation;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.Advised;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AsyncCyclicDependenceTest {
+
+	@Test
+	public void testAsyncCyclicDependenceTest() throws ExecutionException, InterruptedException {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.register(AsyncConfig.class, AsyncService.class, AopAspect.class);
+		ctx.refresh();
+		AsyncService asyncService = ctx.getBean(AsyncService.class);
+		assertThat(asyncService instanceof Advised).isEqualTo(true);
+		Future<String> asyncFuture = asyncService.async();
+		assertThat(asyncFuture.get().equals(Thread.currentThread().getName())).isEqualTo(false);
+		ctx.close();
+	}
+
+	@EnableAsync
+	@EnableAspectJAutoProxy
+	@Configuration
+	static class AsyncConfig {
+	}
+
+	static class AsyncService {
+		@Autowired
+		private AsyncService asyncService;
+
+		@Async
+		public Future<String> async() {
+			// System.out.println("async => " + Thread.currentThread().getName());
+			return new AsyncResult<>(Thread.currentThread().getName());
+		}
+	}
+
+	@Aspect
+	static class AopAspect {
+		@Around("execution(* org.springframework.scheduling.annotation.AsyncCyclicDependenceTest.AsyncService.*(..))")
+		public Object logAspect(ProceedingJoinPoint joinPoint) throws Throwable {
+			Object[] args = joinPoint.getArgs();
+			Object result = joinPoint.proceed(args);
+			// System.out.println("result => " + result);
+			return result;
+		}
+	}
+
+}
