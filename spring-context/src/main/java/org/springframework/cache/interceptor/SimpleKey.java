9d7bd9520c409642e96a9a82e64745d90001bae8
asgh <asgh@users.noreply.github.com>
2021-11-10 20:12:21 -0500

    Use Arrays.deepToString for toString in SimpleKey
    See gh-27670

--- spring-context/src/main/java/org/springframework/cache/interceptor/SimpleKey.java
+++ spring-context/src/main/java/org/springframework/cache/interceptor/SimpleKey.java
@@ -23,7 +23,6 @@ import java.util.Arrays;
 
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
 
 /**
  * A simple key as returned from the {@link SimpleKeyGenerator}.
@@ -73,7 +72,7 @@ public class SimpleKey implements Serializable {
 
 	@Override
 	public String toString() {
-		return getClass().getSimpleName() + " [" + StringUtils.arrayToCommaDelimitedString(this.params) + "]";
+		return getClass().getSimpleName() + " " + Arrays.deepToString(this.params);
 	}
 
 	private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
