7021eb5bb1703a851028b0a9cae013e31dc04ebd
liuzhifei <2679431923@qq.com>
2021-11-18 20:13:13 +0800

    Apply "instanceof pattern matching" in additional locations
    Closes gh-27696

--- spring-websocket/src/main/java/org/springframework/web/socket/AbstractWebSocketMessage.java
+++ spring-websocket/src/main/java/org/springframework/web/socket/AbstractWebSocketMessage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -79,10 +79,9 @@ public abstract class AbstractWebSocketMessage<T> implements WebSocketMessage<T>
 		if (this == other) {
 			return true;
 		}
-		if (!(other instanceof AbstractWebSocketMessage)) {
+		if (!(other instanceof AbstractWebSocketMessage<?> otherMessage)) {
 			return false;
 		}
-		AbstractWebSocketMessage<?> otherMessage = (AbstractWebSocketMessage<?>) other;
 		return ObjectUtils.nullSafeEquals(this.payload, otherMessage.payload);
 	}
 
