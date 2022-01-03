97625e365803526658d47a26cc6f010259154be1
Sam Brannen <sbrannen@vmware.com>
2022-01-03 16:01:01 +0100

    Additional changes because HttpMethod changed from enum to class
    See gh-27697

--- spring-test/src/test/java/org/springframework/mock/http/server/reactive/MockServerHttpRequestTests.java
+++ spring-test/src/test/java/org/springframework/mock/http/server/reactive/MockServerHttpRequestTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -70,6 +70,7 @@ class MockServerHttpRequestTests {
 		assertThat(ex.getMessage()).contains("HTTP method is required.");
 	}
 
+	@SuppressWarnings("deprecation")
 	static Stream<Executable> httpMethodNotNullOrEmpty() {
 		String uriTemplate = "/foo bar?a=b";
 		return Stream.of(
@@ -80,4 +81,5 @@ class MockServerHttpRequestTests {
 				() -> MockServerHttpRequest.method("   ", uriTemplate).build()
 		);
 	}
+
 }
