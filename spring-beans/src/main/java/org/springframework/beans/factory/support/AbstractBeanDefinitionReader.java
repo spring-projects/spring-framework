c1606c5a5e46c2f60e747413fe57758fa6c8a7ac
Sam Brannen <sbrannen@vmware.com>
2022-01-04 12:39:28 +0100

    Polishing

--- spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanDefinitionReader.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanDefinitionReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,6 +103,11 @@ public abstract class AbstractBeanDefinitionReader implements BeanDefinitionRead
 	}
 
 
+	/**
+	 * Return the bean factory to register the bean definitions with.
+	 * <p>The factory is exposed through the BeanDefinitionRegistry interface,
+	 * encapsulating the methods that are relevant for bean definition handling.
+	 */
 	public final BeanDefinitionRegistry getBeanFactory() {
 		return this.registry;
 	}
