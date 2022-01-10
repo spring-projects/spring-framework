2a80b64d405815d3b612d2fc844e7beffa713160
kuanghc1 <kuanghc1@lenovo.com>
2022-01-10 11:44:21 +0800

    Add tests for StringUtils matchesCharacter() method
    Closes gh-27909

--- spring-core/src/test/java/org/springframework/util/StringUtilsTests.java
+++ spring-core/src/test/java/org/springframework/util/StringUtilsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -150,6 +150,14 @@ class StringUtilsTests {
 		assertThat(StringUtils.trimTrailingCharacter(" a ", ' ')).isEqualTo(" a");
 		assertThat(StringUtils.trimTrailingCharacter(" a b ", ' ')).isEqualTo(" a b");
 		assertThat(StringUtils.trimTrailingCharacter(" a b  c ", ' ')).isEqualTo(" a b  c");
+	}
+
+	@Test
+	void matchesCharacter() {
+		assertThat(StringUtils.matchesCharacter(null, '/')).isFalse();
+		assertThat(StringUtils.matchesCharacter("/a", '/')).isFalse();
+		assertThat(StringUtils.matchesCharacter("a", '/')).isFalse();
+		assertThat(StringUtils.matchesCharacter("/", '/')).isTrue();
 	}
 
 	@Test
