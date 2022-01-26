260da3c2699476a7bfd6715652be514b9ce44612
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2022-01-25 18:06:49 -0800

    Use SpringProperties in native image detection
    Prior to this change, native detection was performed at static class
    initialization and the result was kept in a static final variable. The
    arrangement made it difficult to write tests that need to simulate a
    native environment on JVM.
    
    This commit changes to use `SpringProperties` to retrieve the target
    system property when the method is called.  This allows tests to set an
    arbitrary value to `SpringProperties` to control the native detection
    result.
    
    Signed-off-by: Tadaya Tsuyukubo <tadaya@ttddyy.net>

--- spring-core/src/main/java/org/springframework/core/NativeDetector.java
+++ spring-core/src/main/java/org/springframework/core/NativeDetector.java
@@ -27,13 +27,11 @@ package org.springframework.core;
  */
 public abstract class NativeDetector {
 
-	// See https://github.com/oracle/graal/blob/master/sdk/src/org.graalvm.nativeimage/src/org/graalvm/nativeimage/ImageInfo.java
-	private static final boolean imageCode = (System.getProperty("org.graalvm.nativeimage.imagecode") != null);
-
 	/**
 	 * Returns {@code true} if invoked in the context of image building or during image runtime, else {@code false}.
 	 */
 	public static boolean inNativeImage() {
-		return imageCode;
+		// See https://github.com/oracle/graal/blob/master/sdk/src/org.graalvm.nativeimage/src/org/graalvm/nativeimage/ImageInfo.java
+		return SpringProperties.getProperty("org.graalvm.nativeimage.imagecode") != null;
 	}
 }
---
+++ spring-core/src/test/java/org/springframework/core/NativeDetectorTests.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2002-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test for {@link NativeDetector}.
+ *
+ * @author Tadaya Tsuyukubo
+ */
+class NativeDetectorTests {
+
+	@Test
+	void inNativeImage() {
+		assertThat(NativeDetector.inNativeImage()).isFalse();
+		try {
+			SpringProperties.setProperty("org.graalvm.nativeimage.imagecode", "some-value");
+			assertThat(NativeDetector.inNativeImage()).isTrue();
+		}
+		finally {
+			SpringProperties.setProperty("org.graalvm.nativeimage.imagecode", null);
+		}
+	}
+
+}
