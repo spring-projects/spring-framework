e32f94bf8cbf5280d04434b0c5777fba73ab3e18
Sam Brannen <sbrannen@vmware.com>
2022-01-26 20:21:13 +0100

    Polish GenericWebApplicationContext and AnnotationConfigWebApplicationContext

--- spring-web/src/main/java/org/springframework/web/context/support/AnnotationConfigWebApplicationContext.java
+++ spring-web/src/main/java/org/springframework/web/context/support/AnnotationConfigWebApplicationContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,12 +36,13 @@ import org.springframework.web.context.ContextLoader;
 /**
  * {@link org.springframework.web.context.WebApplicationContext WebApplicationContext}
  * implementation which accepts <em>component classes</em> as input &mdash; in particular
- * {@link org.springframework.context.annotation.Configuration @Configuration}-annotated
+ * {@link org.springframework.context.annotation.Configuration @Configuration}
  * classes, but also plain {@link org.springframework.stereotype.Component @Component}
- * classes and JSR-330 compliant classes using {@code javax.inject} annotations.
+ * classes as well as JSR-330 compliant classes using {@code javax.inject} annotations.
  *
  * <p>Allows for registering classes one by one (specifying class names as config
- * location) as well as for classpath scanning (specifying base packages as config location).
+ * locations) as well as via classpath scanning (specifying base packages as config
+ * locations).
  *
  * <p>This is essentially the equivalent of
  * {@link org.springframework.context.annotation.AnnotationConfigApplicationContext
