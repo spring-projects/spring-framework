f4f5cee76cc9b9b5a7ba8efbfd37fd4b55dfe8a1
Sam Brannen <sbrannen@vmware.com>
2022-01-26 12:29:30 +0100

    Polish AOP namespace tests

--- spring-aop/src/test/java/org/springframework/aop/config/AopNamespaceHandlerPointcutErrorTests.java
+++ spring-aop/src/test/java/org/springframework/aop/config/AopNamespaceHandlerPointcutErrorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,10 +30,10 @@ import static org.springframework.core.testfixture.io.ResourceTestUtils.qualifie
  * @author Mark Fisher
  * @author Chris Beams
  */
-public class AopNamespaceHandlerPointcutErrorTests {
+class AopNamespaceHandlerPointcutErrorTests {
 
 	@Test
-	public void testDuplicatePointcutConfig() {
+	void duplicatePointcutConfig() {
 		DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
 		assertThatExceptionOfType(BeanDefinitionStoreException.class).isThrownBy(() ->
 				new XmlBeanDefinitionReader(bf).loadBeanDefinitions(
@@ -42,7 +42,7 @@ public class AopNamespaceHandlerPointcutErrorTests {
 	}
 
 	@Test
-	public void testMissingPointcutConfig() {
+	void missingPointcutConfig() {
 		DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
 		assertThatExceptionOfType(BeanDefinitionStoreException.class).isThrownBy(() ->
 				new XmlBeanDefinitionReader(bf).loadBeanDefinitions(
