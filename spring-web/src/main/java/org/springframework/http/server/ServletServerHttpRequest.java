4effca35b500fda26f1f7d8322bc622468e5e93a
rstoyanchev <rstoyanchev@vmware.com>
2022-01-31 13:51:40 +0100

    Ignore Content-Type that is invalid (not concrete)
    Closes gh-27957

--- spring-web/src/main/java/org/springframework/http/server/ServletServerHttpRequest.java
+++ spring-web/src/main/java/org/springframework/http/server/ServletServerHttpRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -158,7 +158,9 @@ public class ServletServerHttpRequest implements ServerHttpRequest {
 					String requestContentType = this.servletRequest.getContentType();
 					if (StringUtils.hasLength(requestContentType)) {
 						contentType = MediaType.parseMediaType(requestContentType);
-						this.headers.setContentType(contentType);
+						if (contentType.isConcrete()) {
+							this.headers.setContentType(contentType);
+						}
 					}
 				}
 				if (contentType != null && contentType.getCharset() == null) {
