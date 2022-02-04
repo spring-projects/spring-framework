5d7a632965aff526cb6bbd0380b445f102819dca
Sam Brannen <sbrannen@vmware.com>
2022-02-04 19:41:46 +0100

    Ensure Spring AOP generates JDK dynamic proxies for lambdas
    Prior to this commit, if AOP proxy generation was configured with
    proxyTargetClass=true (which is the default behavior in recent versions
    of Spring Boot), beans implemented as lambda expressions or method
    references could not be proxied with CGLIB on Java 16 or higher without
    specifying `--add-opens java.base/java.lang=ALL-UNNAMED`.
    
    This commit addresses this shortcoming by ensuring that beans
    implemented as lambda expressions or method references are always
    proxied using a JDK dynamic proxy even if proxyTargetClass=true.
    
    Closes gh-27971

--- spring-aop/src/main/java/org/springframework/aop/framework/AopProxyUtils.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/AopProxyUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,6 +44,7 @@ import org.springframework.util.ReflectionUtils;
  *
  * @author Rod Johnson
  * @author Juergen Hoeller
+ * @author Sam Brannen
  * @see org.springframework.aop.support.AopUtils
  */
 public abstract class AopProxyUtils {
@@ -133,7 +134,7 @@ public abstract class AopProxyUtils {
 				if (targetClass.isInterface()) {
 					advised.setInterfaces(targetClass);
 				}
-				else if (Proxy.isProxyClass(targetClass)) {
+				else if (Proxy.isProxyClass(targetClass) || isLambda(targetClass)) {
 					advised.setInterfaces(targetClass.getInterfaces());
 				}
 				specifiedInterfaces = advised.getProxiedInterfaces();
@@ -242,6 +243,20 @@ public abstract class AopProxyUtils {
 			}
 		}
 		return arguments;
+	}
+
+	/**
+	 * Determine if the supplied {@link Class} is a JVM-generated implementation
+	 * class for a lambda expression or method reference.
+	 * <p>This method makes a best-effort attempt at determining this, based on
+	 * checks that work on modern, main stream JVMs.
+	 * @param clazz the class to check
+	 * @return {@code true} if the class is a lambda implementation class
+	 * @since 5.2.16
+	 */
+	static boolean isLambda(Class<?> clazz) {
+		return (clazz.isSynthetic() && (clazz.getSuperclass() == Object.class) &&
+				(clazz.getInterfaces().length > 0) && clazz.getName().contains("$$Lambda"));
 	}
 
 }
