6c42bcfaec45128f7b0807676288552912a1b234
Brian Clozel <bclozel@vmware.com>
2022-02-11 11:39:32 +0100

    Upgrade to Kotlin 1.6.20-M1
    This commit upgrades the Kotlin dependencies to 1.6.20-M1 and configures
    the build to generate Java 17 bytecode for Kotlin classes.
    
    Closes gh-27814

--- spring-webflux/src/test/kotlin/org/springframework/web/reactive/result/method/annotation/CoroutinesIntegrationTests.kt
+++ spring-webflux/src/test/kotlin/org/springframework/web/reactive/result/method/annotation/CoroutinesIntegrationTests.kt
@@ -17,6 +17,7 @@
 package org.springframework.web.reactive.result.method.annotation
 
 import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
@@ -115,6 +116,7 @@ class CoroutinesIntegrationTests : AbstractRequestMappingIntegrationTests() {
 	@ComponentScan(resourcePattern = "**/CoroutinesIntegrationTests*")
 	open class WebConfig
 
+	@OptIn(DelicateCoroutinesApi::class)
 	@RestController
 	class CoroutinesController {
 
