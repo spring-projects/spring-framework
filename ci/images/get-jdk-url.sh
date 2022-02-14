2f78abd56e39eb95c32fd211ca738359f8eec3e5
Brian Clozel <bclozel@vmware.com>
2022-02-14 10:29:45 +0100

    Upgrade CI pipeline
    This commit upgrades the CI pipeline with the following:
    
    * replace JDK16 with JDK17 as build variant
    * upgrade all JDK versions
    * replace docker-image with registry-image resource for CI image

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -6,10 +6,13 @@ case "$1" in
 		 echo "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u322-b06/OpenJDK8U-jdk_x64_linux_hotspot_8u322b06.tar.gz"
 	;;
 	java11)
-		 echo "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.14%2B9/OpenJDK11U-jdk_x64_linux_hotspot_11.0.14_9.tar.gz"
+		 echo "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.14.1%2B1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.14.1_1.tar.gz"
 	;;
-  java16)
-		 echo "https://github.com/adoptium/temurin16-binaries/releases/download/jdk-16.0.2%2B7/OpenJDK16U-jdk_x64_linux_hotspot_16.0.2_7.tar.gz"
+  java17)
+		 echo "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jdk_x64_linux_hotspot_17.0.2_8.tar.gz"
+  ;;
+  java18)
+		 echo "https://github.com/adoptium/temurin18-binaries/releases/download/jdk18-2022-02-12-08-06-beta/OpenJDK18-jdk_x64_linux_hotspot_2022-02-12-08-06.tar.gz"
   ;;
   *)
 		echo $"Unknown java version"
