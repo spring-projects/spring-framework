e3ceb9b23dacbed3db0811b7535cbe07ccda58cb
Sam Brannen <sbrannen@vmware.com>
2022-02-16 12:01:14 +0100

    Polish @Target declarations for stereotype annotations

--- spring-context/src/main/java/org/springframework/stereotype/Controller.java
+++ spring-context/src/main/java/org/springframework/stereotype/Controller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,7 +39,7 @@ import org.springframework.core.annotation.AliasFor;
  * @see org.springframework.web.bind.annotation.RequestMapping
  * @see org.springframework.context.annotation.ClassPathBeanDefinitionScanner
  */
-@Target({ElementType.TYPE})
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Component
