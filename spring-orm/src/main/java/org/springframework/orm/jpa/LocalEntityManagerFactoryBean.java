0ad58f64df2e77d42a62838c25dea19e89a4c002
ShinDongHun1 <huipulci1@naver.com>
2022-03-17 23:42:16 +0900

    Polish LocalEntityManagerFactoryBean

--- spring-orm/src/main/java/org/springframework/orm/jpa/LocalEntityManagerFactoryBean.java
+++ spring-orm/src/main/java/org/springframework/orm/jpa/LocalEntityManagerFactoryBean.java
@@ -77,20 +77,21 @@ public class LocalEntityManagerFactoryBean extends AbstractEntityManagerFactoryB
 			logger.debug("Building JPA EntityManagerFactory for persistence unit '" + getPersistenceUnitName() + "'");
 		}
 		PersistenceProvider provider = getPersistenceProvider();
-		if (provider != null) {
-			// Create EntityManagerFactory directly through PersistenceProvider.
-			EntityManagerFactory emf = provider.createEntityManagerFactory(getPersistenceUnitName(), getJpaPropertyMap());
-			if (emf == null) {
-				throw new IllegalStateException(
-						"PersistenceProvider [" + provider + "] did not return an EntityManagerFactory for name '" +
-						getPersistenceUnitName() + "'");
-			}
-			return emf;
-		}
-		else {
+
+		if (provider == null) {
 			// Let JPA perform its standard PersistenceProvider autodetection.
 			return Persistence.createEntityManagerFactory(getPersistenceUnitName(), getJpaPropertyMap());
 		}
+
+		// Create EntityManagerFactory directly through PersistenceProvider.
+		EntityManagerFactory emf = provider.createEntityManagerFactory(getPersistenceUnitName(), getJpaPropertyMap());
+		if (emf == null) {
+			throw new IllegalStateException(
+					"PersistenceProvider [" + provider + "] did not return an EntityManagerFactory for name '" +
+							getPersistenceUnitName() + "'");
+		}
+		return emf;
+
 	}
 
 }
