e90225336a57fa269ec40d6e238e4a27541b56fc
Sam Brannen <sbrannen@vmware.com>
2022-03-27 17:01:26 +0200

    Use JPA from Jakarta

--- spring-test/src/test/java/org/springframework/test/context/junit/jupiter/orm/domain/JpaPersonRepository.java
+++ spring-test/src/test/java/org/springframework/test/context/junit/jupiter/orm/domain/JpaPersonRepository.java
@@ -16,8 +16,8 @@
 
 package org.springframework.test.context.junit.jupiter.orm.domain;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
