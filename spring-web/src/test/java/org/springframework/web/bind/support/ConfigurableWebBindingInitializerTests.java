047a98dab78b99d218a6df1e5591dde9a81321b3
Tubby <git@tubby.nl>
2022-03-31 15:09:18 +0200

    satisfying all :spring-web:checkstyleTest tests

--- spring-web/src/test/java/org/springframework/web/bind/support/ConfigurableWebBindingInitializerTests.java
+++ spring-web/src/test/java/org/springframework/web/bind/support/ConfigurableWebBindingInitializerTests.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2002-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.web.bind.support;
 
 import org.junit.jupiter.api.Test;
 
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.beans.testfixture.beans.TestBean;
+import org.springframework.web.bind.WebDataBinder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,7 +28,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Michaël van de Giessen
  */
-public class ConfigurableWebBindingInitializerTest {
+public class ConfigurableWebBindingInitializerTests {
 	private static final String[] ALLOWED = new String[]{"allowed"};
 	private static final String[] DISALLOWED =  new String[]{"disallowed"};
 	private WebDataBinder binder = new WebDataBinder(new TestBean());
@@ -37,8 +53,6 @@ public class ConfigurableWebBindingInitializerTest {
 
 		// if we dont set allowed / disallowed (null = default)
 		ConfigurableWebBindingInitializer sut = new ConfigurableWebBindingInitializer();
-		sut.setAllowedFields(null);
-		sut.setDisallowedFields(null);
 		sut.initBinder(this.binder);
 
 		// we expect original values to be retained on the initialising WebDataBinder
