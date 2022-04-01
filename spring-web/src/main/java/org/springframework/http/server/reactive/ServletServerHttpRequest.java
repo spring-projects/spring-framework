d518a7d8c81e2cf7484942c37836219b3669240d
rstoyanchev <rstoyanchev@vmware.com>
2022-04-01 17:36:14 +0100

    AbstractListenerReadPublisher continues after 0 bytes
    If we read 0 bytes, e.g. chunked encoding markup read but not the
    actual data within it, don't stop reading since the server may or
    may not consider it necessary to call onDataAvailable again.
    Instead, we keep on reading, and although isReady likely returns
    false on the next iteration, it eliminates ambiguity and ensures
    the server will call onDataAvailable when more data arrives.
    
    Closes gh-28241

--- spring-web/src/main/java/org/springframework/http/server/reactive/ServletServerHttpRequest.java
+++ spring-web/src/main/java/org/springframework/http/server/reactive/ServletServerHttpRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -236,10 +236,10 @@ class ServletServerHttpRequest extends AbstractServerHttpRequest {
 	/**
 	 * Read from the request body InputStream and return a DataBuffer.
 	 * Invoked only when {@link ServletInputStream#isReady()} returns "true".
-	 * @return a DataBuffer with data read, or {@link #EOF_BUFFER} if the input
-	 * stream returned -1, or null if 0 bytes were read.
+	 * @return a DataBuffer with data read, or
+	 * {@link AbstractListenerReadPublisher#EMPTY_BUFFER} if 0 bytes were read,
+	 * or {@link #EOF_BUFFER} if the input stream returned -1.
 	 */
-	@Nullable
 	DataBuffer readFromInputStream() throws IOException {
 		int read = this.request.getInputStream().read(this.buffer);
 		logBytesRead(read);
@@ -254,7 +254,7 @@ class ServletServerHttpRequest extends AbstractServerHttpRequest {
 			return EOF_BUFFER;
 		}
 
-		return null;
+		return AbstractListenerReadPublisher.EMPTY_BUFFER;
 	}
 
 	protected final void logBytesRead(int read) {
