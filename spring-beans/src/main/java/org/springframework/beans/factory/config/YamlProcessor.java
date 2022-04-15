188d0768ff92f29c3257aaa7f2c4d7f46f781468
Michal Domagala <outsider404@gmail.com>
2022-04-15 21:46:28 +0200

    Update YamlProcessor.java

--- spring-beans/src/main/java/org/springframework/beans/factory/config/YamlProcessor.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/YamlProcessor.java
@@ -25,6 +25,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.snakeyaml.engine.v2.api.Load;
