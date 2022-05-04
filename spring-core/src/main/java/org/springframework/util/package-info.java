cf30327740aae7700ad26c80bc8d73d667eeb6d4
neals <shandeai520@gmail.com>
2022-05-03 20:22:09 -0700

    Remove Log4J configurer from package-info.java in spring-core
    Closes gh-28411

--- spring-core/src/main/java/org/springframework/util/package-info.java
+++ spring-core/src/main/java/org/springframework/util/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Miscellaneous utility classes, such as String manipulation utilities,
- * a Log4J configurer, and a state holder for paged lists of objects.
+ * and a state holder for paged lists of objects.
  */
 @NonNullApi
 @NonNullFields
