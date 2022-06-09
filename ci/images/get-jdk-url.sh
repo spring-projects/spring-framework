8a30bc299344bd3231bce4c597186b884d2544b8
Stephane Nicoll <snicoll@vmware.com>
2022-06-09 08:14:54 +0200

    Upgrade Java 18 version in CI image

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -12,7 +12,7 @@ case "$1" in
 		echo "https://github.com/bell-sw/Liberica/releases/download/17.0.3.1+2/bellsoft-jdk17.0.3.1+2-linux-amd64.tar.gz"
 	;;
 	java18)
-		echo "https://github.com/adoptium/temurin18-binaries/releases/download/jdk-18.0.1%2B10/OpenJDK18U-jdk_x64_linux_hotspot_18.0.1_10.tar.gz"
+		echo "https://github.com/bell-sw/Liberica/releases/download/18.0.1.1+2/bellsoft-jdk18.0.1.1+2-linux-amd64.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
