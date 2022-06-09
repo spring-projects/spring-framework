4d9dc61f5d90a54d573c37a7b4f86a8e3b8e309a
Stephane Nicoll <snicoll@vmware.com>
2022-06-09 08:13:12 +0200

    Polish

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -8,13 +8,13 @@ case "$1" in
 	java11)
 		 echo "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.15%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.15_10.tar.gz"
 	;;
-  java17)
-		 echo "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.3%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.3_7.tar.gz"
-  ;;
-  java18)
-		 echo "https://github.com/adoptium/temurin18-binaries/releases/download/jdk-18.0.1%2B10/OpenJDK18U-jdk_x64_linux_hotspot_18.0.1_10.tar.gz"
-  ;;
-  *)
+	java17)
+		echo "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.3%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.3_7.tar.gz"
+	;;
+	java18)
+		echo "https://github.com/adoptium/temurin18-binaries/releases/download/jdk-18.0.1%2B10/OpenJDK18U-jdk_x64_linux_hotspot_18.0.1_10.tar.gz"
+	;;
+	*)
 		echo $"Unknown java version"
 		exit 1
 esac
