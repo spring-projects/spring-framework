2afe560e41c52c51a36314de3884416a9b1ed432
Vikey Chen <vikeytk@gmail.com>
2022-06-17 17:45:00 +0800

    Replace forEach with putAll
    See gh-28646

--- spring-webflux/src/main/java/org/springframework/web/reactive/socket/client/UndertowWebSocketClient.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/socket/client/UndertowWebSocketClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -243,7 +243,7 @@ public class UndertowWebSocketClient implements WebSocketClient {
 
 		@Override
 		public void beforeRequest(Map<String, List<String>> headers) {
-			this.requestHeaders.forEach(headers::put);
+			headers.putAll(this.requestHeaders);
 			if (this.delegate != null) {
 				this.delegate.beforeRequest(headers);
 			}
@@ -251,7 +251,7 @@ public class UndertowWebSocketClient implements WebSocketClient {
 
 		@Override
 		public void afterRequest(Map<String, List<String>> headers) {
-			headers.forEach(this.responseHeaders::put);
+			this.responseHeaders.putAll(headers);
 			if (this.delegate != null) {
 				this.delegate.afterRequest(headers);
 			}
