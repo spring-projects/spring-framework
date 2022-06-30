9ea45697ac5f99bd8f62a8391bdec4bbe35efab1
Sam Brannen <sbrannen@vmware.com>
2022-06-30 16:51:43 +0200

    Support cookie comments in MockHttpServletResponse and MockCookie
    Prior to this commit, if a cookie was added to MockHttpServletResponse,
    the comment attribute was not included in the generated Set-Cookie
    header. In addition, MockCookie.parse(String) did not support the
    Comment attribute.
    
    This commit addresses both of these issues.
    
    Closes gh-28730

--- spring-test/src/main/java/org/springframework/mock/web/MockHttpServletResponse.java
+++ spring-test/src/main/java/org/springframework/mock/web/MockHttpServletResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -449,6 +449,9 @@ public class MockHttpServletResponse implements HttpServletResponse {
 			if (StringUtils.hasText(mockCookie.getSameSite())) {
 				buf.append("; SameSite=").append(mockCookie.getSameSite());
 			}
+		}
+		if (StringUtils.hasText(cookie.getComment())) {
+			buf.append("; Comment=").append(cookie.getComment());
 		}
 		return buf.toString();
 	}
