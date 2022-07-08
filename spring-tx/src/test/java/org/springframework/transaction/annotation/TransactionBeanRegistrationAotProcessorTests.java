49fd7c99ae6483b4878145bf79ae1fe93de74f7d
Sébastien Deleuze <sdeleuze@vmware.com>
2022-07-08 17:30:11 +0200

    Fix TransactionBeanRegistrationAotProcessorTests
    See gh-28717

--- spring-tx/src/test/java/org/springframework/transaction/annotation/TransactionBeanRegistrationAotProcessorTests.java
+++ spring-tx/src/test/java/org/springframework/transaction/annotation/TransactionBeanRegistrationAotProcessorTests.java
@@ -170,7 +170,6 @@ public class TransactionBeanRegistrationAotProcessorTests {
 
 	static class TransactionalOnInterfaceMethodBean implements TransactionalOnMethodInterface {
 
-		@Transactional
 		public void transactional() {
 		}
 	}
