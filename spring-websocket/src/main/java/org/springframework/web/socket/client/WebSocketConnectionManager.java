cd10171f989b3ef0375a90efa01dfce059689a80
npriebe <Nicolas.Priebe1@yahoo.de>
2022-07-11 15:20:13 +0200

    Make WebSocketConnectionManager#isConnected public
    See gh-28785

--- spring-websocket/src/main/java/org/springframework/web/socket/client/WebSocketConnectionManager.java
+++ spring-websocket/src/main/java/org/springframework/web/socket/client/WebSocketConnectionManager.java
@@ -160,7 +160,7 @@ public class WebSocketConnectionManager extends ConnectionManagerSupport {
 	}
 
 	@Override
-	protected boolean isConnected() {
+	public boolean isConnected() {
 		return (this.webSocketSession != null && this.webSocketSession.isOpen());
 	}
 
