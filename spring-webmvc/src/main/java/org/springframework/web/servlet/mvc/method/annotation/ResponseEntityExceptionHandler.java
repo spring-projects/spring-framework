1c03aaaddc6ab1034d6455ffc8d82fe9d05c54fb
Sam Brannen <sbrannen@vmware.com>
2022-07-12 13:29:02 +0200

    Fix Javadoc in ResponseEntityExceptionHandler

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/ResponseEntityExceptionHandler.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/ResponseEntityExceptionHandler.java
@@ -59,7 +59,7 @@ import org.springframework.web.util.WebUtils;
  * individual methods that handle a specific exception, override
  * {@link #handleExceptionInternal} to override common handling of all exceptions,
  * or {@link #createResponseEntity} to intercept the final step of creating the
- * @link ResponseEntity} from the selected HTTP status code, headers, and body.
+ * {@link ResponseEntity} from the selected HTTP status code, headers, and body.
  *
  * @author Rossen Stoyanchev
  * @since 3.2
