d660f5f62f7ba98ca510b2e7f07b36712495214a
Sam Brannen <sbrannen@vmware.com>
2022-07-13 12:22:03 +0200

    Fix Javadoc in ResponseEntityExceptionHandler

--- spring-webflux/src/main/java/org/springframework/web/reactive/result/method/annotation/ResponseEntityExceptionHandler.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/result/method/annotation/ResponseEntityExceptionHandler.java
@@ -50,7 +50,7 @@ import org.springframework.web.server.UnsupportedMediaTypeStatusException;
  * individual methods that handle a specific exception, override
  * {@link #handleExceptionInternal} to override common handling of all exceptions,
  * or {@link #createResponseEntity} to intercept the final step of creating the
- * @link ResponseEntity} from the selected HTTP status code, headers, and body.
+ * {@link ResponseEntity} from the selected HTTP status code, headers, and body.
  *
  * @author Rossen Stoyanchev
  * @since 6.0
