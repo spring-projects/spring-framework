b4e8e6397a399b7a657513c226e0f2a5fa2b0f13
Marc Wrobel <marc.wrobel@gmail.com>
2022-07-26 15:45:12 +0200

    Fix links in javadoc and documentations
    - fix broken links (using their new URL, an alternative URL or a Wayback Machine link),
    - use HTTPS where possible,
    - remove https://issuetracker.springsource.com/browse/EBR-349 : this link is dead and is also mentioned in https://jira.spring.io/browse/SPR-8093,
    - cleanup nohttp allowlist.lines.

--- spring-web/src/main/java/org/springframework/web/util/JavaScriptUtils.java
+++ spring-web/src/main/java/org/springframework/web/util/JavaScriptUtils.java
@@ -21,7 +21,7 @@ package org.springframework.web.util;
  * Escapes based on the JavaScript 1.5 recommendation.
  *
  * <p>Reference:
- * <a href="https://developer.mozilla.org/en-US/docs/JavaScript/Guide/Values,_variables,_and_literals#String_literals">
+ * <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#string_literals">
  * JavaScript Guide</a> on Mozilla Developer Network.
  *
  * @author Juergen Hoeller
