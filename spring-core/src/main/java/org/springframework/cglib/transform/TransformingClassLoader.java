7fa5b8dcb59957c634b8d706f28c03456c62c5c6
Juergen Hoeller <hoellerj@vmware.com>
2022-08-12 11:38:43 +0200

    Turn CGLIB patch into full CGLIB fork in spring-core
    Closes gh-28955

---
+++ spring-core/src/main/java/org/springframework/cglib/transform/TransformingClassLoader.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2003 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cglib.transform;
+
+import java.util.*;
+import org.springframework.cglib.core.ClassGenerator;
+import org.springframework.asm.*;
+
+public class TransformingClassLoader extends AbstractClassLoader {
+    private ClassTransformerFactory t;
+    
+    public TransformingClassLoader(ClassLoader parent, ClassFilter filter, ClassTransformerFactory t) {
+        super(parent, parent, filter);
+        this.t = t;
+    }
+
+    protected ClassGenerator getGenerator(ClassReader r) {
+        ClassTransformer t2 = t.newInstance();
+        return new TransformingClassGenerator(super.getGenerator(r), t2);
+    }
+}
