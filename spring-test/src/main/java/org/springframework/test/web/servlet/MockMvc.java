4264d0becd0ef614d52dd6384ef8e47cb88fb584
Sam Brannen <sbrannen@vmware.com>
2022-08-19 16:02:10 +0200

    Fix example in MockMvc Javadoc

--- spring-test/src/main/java/org/springframework/test/web/servlet/MockMvc.java
+++ spring-test/src/main/java/org/springframework/test/web/servlet/MockMvc.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,7 +57,7 @@ import org.springframework.web.servlet.DispatcherServlet;
  *
  * mockMvc.perform(get("/form"))
  *     .andExpect(status().isOk())
- *     .andExpect(content().mimeType("text/html"))
+ *     .andExpect(content().contentType("text/html"))
  *     .andExpect(forwardedUrl("/WEB-INF/layouts/main.jsp"));
  * </pre>
  *
