3c22404bce604c815954745dbd311bb24a2b44cd
Brian Clozel <bclozel@vmware.com>
2022-08-31 15:19:31 +0200

    Fix RuntimeHintsAgent instrumentation for method invocation
    Prior to this commit, the `RuntimeHintsAgent` would instrument
    `Method.invoke` in a way that fails when invoked methods are not public.
    
    This commit ensures that before delegating the invocation call, the
    instrumentation makes the method accessible before delegating the call.
    
    Fixes gh-29046

--- spring-core-test/src/main/java/org/springframework/aot/agent/InstrumentedBridgeMethods.java
+++ spring-core-test/src/main/java/org/springframework/aot/agent/InstrumentedBridgeMethods.java
@@ -337,13 +337,21 @@ public abstract class InstrumentedBridgeMethods {
 
 	public static Object methodinvoke(Method method, Object object, Object... arguments) throws InvocationTargetException, IllegalAccessException {
 		Object result = null;
+		boolean accessibilityChanged = false;
 		try {
+			if (!Modifier.isPublic(method.getModifiers())) {
+				method.setAccessible(true);
+				accessibilityChanged = true;
+			}
 			result = method.invoke(object, arguments);
 		}
 		finally {
 			RecordedInvocation invocation = RecordedInvocation.of(InstrumentedMethod.METHOD_INVOKE)
 					.onInstance(method).withArguments(object, arguments).returnValue(result).build();
 			RecordedInvocationsPublisher.publish(invocation);
+			if (accessibilityChanged) {
+				method.setAccessible(false);
+			}
 		}
 		return result;
 	}
