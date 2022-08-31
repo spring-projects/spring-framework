78d32367929db8582b60fbeffea422bda63e89a9
Kevin Yue <yuezk001@gmail.com>
2022-08-31 10:57:16 -0400

    fix: commit the response in `sendRedirect()` method

--- spring-web/src/main/java/org/springframework/web/filter/RelativeRedirectResponseWrapper.java
+++ spring-web/src/main/java/org/springframework/web/filter/RelativeRedirectResponseWrapper.java
@@ -16,6 +16,8 @@
 
 package org.springframework.web.filter;
 
+import java.io.IOException;
+
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpServletResponseWrapper;
 
@@ -44,9 +46,10 @@ final class RelativeRedirectResponseWrapper extends HttpServletResponseWrapper {
 
 
 	@Override
-	public void sendRedirect(String location) {
+	public void sendRedirect(String location) throws IOException {
 		setStatus(this.redirectStatus.value());
 		setHeader(HttpHeaders.LOCATION, location);
+		flushBuffer();
 	}
 
 
