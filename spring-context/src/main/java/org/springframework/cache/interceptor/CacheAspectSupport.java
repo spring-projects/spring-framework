dae7899fbac29c74ad5615fbf1b61cadc6d17e18
Mengshuai <ms1.0@qq.com>
2022-09-02 15:16:56 +0800

    Update CacheAspectSupport.java
    There is no need to save the rejected results to a list.

--- spring-context/src/main/java/org/springframework/cache/interceptor/CacheAspectSupport.java
+++ spring-context/src/main/java/org/springframework/cache/interceptor/CacheAspectSupport.java
@@ -469,19 +469,20 @@ public abstract class CacheAspectSupport extends AbstractCacheInvoker
 	private boolean hasCachePut(CacheOperationContexts contexts) {
 		// Evaluate the conditions *without* the result object because we don't have it yet...
 		Collection<CacheOperationContext> cachePutContexts = contexts.get(CachePutOperation.class);
-		Collection<CacheOperationContext> excluded = new ArrayList<>();
+
 		for (CacheOperationContext context : cachePutContexts) {
 			try {
-				if (!context.isConditionPassing(CacheOperationExpressionEvaluator.RESULT_UNAVAILABLE)) {
-					excluded.add(context);
+				if (context.isConditionPassing(CacheOperationExpressionEvaluator.RESULT_UNAVAILABLE)) {
+					return true;
 				}
 			}
 			catch (VariableNotAvailableException ex) {
 				// Ignoring failure due to missing result, consider the cache put has to proceed
+				return true;
 			}
 		}
 		// Check if all puts have been excluded by condition
-		return (cachePutContexts.size() != excluded.size());
+		return false;
 	}
 
 	private void processCacheEvicts(
