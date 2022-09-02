2b45dccb8012a0334f7830ca976b16e1a204f9ab
Vladyslav Zolotarov <zolotaryov.v.g@gmail.com>
2022-09-02 22:02:39 +0300

    Remove date

--- spring-tx/src/main/java/org/springframework/transaction/TransactionCommitException.java
+++ spring-tx/src/main/java/org/springframework/transaction/TransactionCommitException.java
@@ -5,7 +5,6 @@ package org.springframework.transaction;
  * Exception when reactive transaction manager fails on commit
  *
  * @author Vladyslav Zolotarov
- * @since 12.05.2022
  */
 @SuppressWarnings("serial")
 public class TransactionCommitException extends TransactionException {
