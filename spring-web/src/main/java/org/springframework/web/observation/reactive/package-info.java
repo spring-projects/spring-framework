82e47db28f08ccaf390ea878f899c746547837c5
Brian Clozel <bclozel@vmware.com>
2022-09-12 11:36:48 +0200

    Instrument reactive servers for Observability
    This commit introduces a `HttpRequestsObservationWebFilter` which
    instruments web frameworks using Spring's reactive `ServerHttpRequest`
    and `ServerHttpResponse` interfaces.
    
    This replaces Spring Boot's `MetricsWebFilter`.
    
    See gh-28880

---
+++ spring-web/src/main/java/org/springframework/web/observation/reactive/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Instrumentation for {@link io.micrometer.observation.Observation observing} reactive web applications.
+ */
+@NonNullApi
+@NonNullFields
+package org.springframework.web.observation.reactive;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
