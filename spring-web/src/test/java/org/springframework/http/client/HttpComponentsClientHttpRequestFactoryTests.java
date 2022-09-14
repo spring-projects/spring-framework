990a34074ac609e259149c993d6cc308ad9a7a88
Arjen Poutsma <poutsmaa@vmware.com>
2022-09-14 15:36:08 +0200

    Upgrade RestTemplate to HttpClient 5
    This commit upgrades the HttpComponentClientHttpRequestFactory and
    related types from HttpClient version 4.5 to 5.
    
    Closes gh-28925

--- spring-web/src/test/java/org/springframework/http/client/HttpComponentsClientHttpRequestFactoryTests.java
+++ spring-web/src/test/java/org/springframework/http/client/HttpComponentsClientHttpRequestFactoryTests.java
@@ -18,18 +18,18 @@ package org.springframework.http.client;
 
 import java.net.URI;
 
-import org.apache.http.HttpEntityEnclosingRequest;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.Configurable;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.config.Configurable;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.protocol.HttpClientContext;
+import org.apache.hc.core5.util.Timeout;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.http.HttpMethod;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -59,7 +59,6 @@ public class HttpComponentsClientHttpRequestFactoryTests extends AbstractHttpReq
 		try (HttpComponentsClientHttpRequestFactory hrf = new HttpComponentsClientHttpRequestFactory(httpClient)) {
 			hrf.setConnectTimeout(1234);
 			hrf.setConnectionRequestTimeout(4321);
-			hrf.setReadTimeout(4567);
 
 			URI uri = new URI(baseUrl + "/status/ok");
 			request = (HttpComponentsClientHttpRequest)
@@ -69,15 +68,14 @@ public class HttpComponentsClientHttpRequestFactoryTests extends AbstractHttpReq
 			assertThat(config).as("Request config should be set").isNotNull();
 			assertThat(config).as("Wrong request config type " + config.getClass().getName()).isInstanceOf(RequestConfig.class);
 			RequestConfig requestConfig = (RequestConfig) config;
-			assertThat(requestConfig.getConnectTimeout()).as("Wrong custom connection timeout").isEqualTo(1234);
-			assertThat(requestConfig.getConnectionRequestTimeout()).as("Wrong custom connection request timeout").isEqualTo(4321);
-			assertThat(requestConfig.getSocketTimeout()).as("Wrong custom socket timeout").isEqualTo(4567);
+			assertThat(requestConfig.getConnectTimeout()).as("Wrong custom connection timeout").isEqualTo(Timeout.of(1234, MILLISECONDS));
+			assertThat(requestConfig.getConnectionRequestTimeout()).as("Wrong custom connection request timeout").isEqualTo(Timeout.of(4321, MILLISECONDS));
 		}
 	}
 
 	@Test
 	public void defaultSettingsOfHttpClientMergedOnExecutorCustomization() throws Exception {
-		RequestConfig defaultConfig = RequestConfig.custom().setConnectTimeout(1234).build();
+		RequestConfig defaultConfig = RequestConfig.custom().setConnectTimeout(1234, MILLISECONDS).build();
 		CloseableHttpClient client = mock(CloseableHttpClient.class,
 				withSettings().extraInterfaces(Configurable.class));
 		Configurable configurable = (Configurable) client;
@@ -89,15 +87,17 @@ public class HttpComponentsClientHttpRequestFactoryTests extends AbstractHttpReq
 		hrf.setConnectionRequestTimeout(4567);
 		RequestConfig requestConfig = retrieveRequestConfig(hrf);
 		assertThat(requestConfig).isNotNull();
-		assertThat(requestConfig.getConnectionRequestTimeout()).isEqualTo(4567);
+		assertThat(requestConfig.getConnectionRequestTimeout()).isEqualTo(Timeout.of(4567, MILLISECONDS));
 		// Default connection timeout merged
-		assertThat(requestConfig.getConnectTimeout()).isEqualTo(1234);
+		assertThat(requestConfig.getConnectTimeout()).isEqualTo(Timeout.of(1234, MILLISECONDS));
 	}
 
 	@Test
 	public void localSettingsOverrideClientDefaultSettings() throws Exception {
 		RequestConfig defaultConfig = RequestConfig.custom()
-				.setConnectTimeout(1234).setConnectionRequestTimeout(6789).build();
+				.setConnectTimeout(1234, MILLISECONDS)
+				.setConnectionRequestTimeout(6789, MILLISECONDS)
+				.build();
 		CloseableHttpClient client = mock(CloseableHttpClient.class,
 				withSettings().extraInterfaces(Configurable.class));
 		Configurable configurable = (Configurable) client;
@@ -107,15 +107,15 @@ public class HttpComponentsClientHttpRequestFactoryTests extends AbstractHttpReq
 		hrf.setConnectTimeout(5000);
 
 		RequestConfig requestConfig = retrieveRequestConfig(hrf);
-		assertThat(requestConfig.getConnectTimeout()).isEqualTo(5000);
-		assertThat(requestConfig.getConnectionRequestTimeout()).isEqualTo(6789);
-		assertThat(requestConfig.getSocketTimeout()).isEqualTo(-1);
+		assertThat(requestConfig.getConnectTimeout()).isEqualTo(Timeout.of(5000, MILLISECONDS));
+		assertThat(requestConfig.getConnectionRequestTimeout()).isEqualTo(Timeout.of(6789, MILLISECONDS));
 	}
 
 	@Test
 	public void mergeBasedOnCurrentHttpClient() throws Exception {
 		RequestConfig defaultConfig = RequestConfig.custom()
-				.setSocketTimeout(1234).build();
+				.setConnectionRequestTimeout(1234, MILLISECONDS)
+				.build();
 		final CloseableHttpClient client = mock(CloseableHttpClient.class,
 				withSettings().extraInterfaces(Configurable.class));
 		Configurable configurable = (Configurable) client;
@@ -127,22 +127,20 @@ public class HttpComponentsClientHttpRequestFactoryTests extends AbstractHttpReq
 				return client;
 			}
 		};
-		hrf.setReadTimeout(5000);
+		hrf.setConnectionRequestTimeout(5000);
 
 		RequestConfig requestConfig = retrieveRequestConfig(hrf);
-		assertThat(requestConfig.getConnectTimeout()).isEqualTo(-1);
-		assertThat(requestConfig.getConnectionRequestTimeout()).isEqualTo(-1);
-		assertThat(requestConfig.getSocketTimeout()).isEqualTo(5000);
+		assertThat(requestConfig.getConnectionRequestTimeout()).isEqualTo(Timeout.of(5000, MILLISECONDS));
+		assertThat(requestConfig.getConnectTimeout()).isEqualTo(RequestConfig.DEFAULT.getConnectTimeout());
 
 		// Update the Http client so that it returns an updated config
 		RequestConfig updatedDefaultConfig = RequestConfig.custom()
-				.setConnectTimeout(1234).build();
+				.setConnectTimeout(1234, MILLISECONDS).build();
 		given(configurable.getConfig()).willReturn(updatedDefaultConfig);
-		hrf.setReadTimeout(7000);
+		hrf.setConnectionRequestTimeout(7000);
 		RequestConfig requestConfig2 = retrieveRequestConfig(hrf);
-		assertThat(requestConfig2.getConnectTimeout()).isEqualTo(1234);
-		assertThat(requestConfig2.getConnectionRequestTimeout()).isEqualTo(-1);
-		assertThat(requestConfig2.getSocketTimeout()).isEqualTo(7000);
+		assertThat(requestConfig2.getConnectTimeout()).isEqualTo(Timeout.of(1234, MILLISECONDS));
+		assertThat(requestConfig2.getConnectionRequestTimeout()).isEqualTo(Timeout.of(7000, MILLISECONDS));
 	}
 
 	private RequestConfig retrieveRequestConfig(HttpComponentsClientHttpRequestFactory factory) throws Exception {
@@ -150,26 +148,6 @@ public class HttpComponentsClientHttpRequestFactoryTests extends AbstractHttpReq
 		HttpComponentsClientHttpRequest request = (HttpComponentsClientHttpRequest)
 				factory.createRequest(uri, HttpMethod.GET);
 		return (RequestConfig) request.getHttpContext().getAttribute(HttpClientContext.REQUEST_CONFIG);
-	}
-
-	@Test
-	public void createHttpUriRequest() throws Exception {
-		URI uri = new URI("https://example.com");
-		testRequestBodyAllowed(uri, HttpMethod.GET, false);
-		testRequestBodyAllowed(uri, HttpMethod.HEAD, false);
-		testRequestBodyAllowed(uri, HttpMethod.OPTIONS, false);
-		testRequestBodyAllowed(uri, HttpMethod.TRACE, false);
-		testRequestBodyAllowed(uri, HttpMethod.PUT, true);
-		testRequestBodyAllowed(uri, HttpMethod.POST, true);
-		testRequestBodyAllowed(uri, HttpMethod.PATCH, true);
-		testRequestBodyAllowed(uri, HttpMethod.DELETE, true);
-
-	}
-
-	private void testRequestBodyAllowed(URI uri, HttpMethod method, boolean allowed) {
-		HttpUriRequest request = ((HttpComponentsClientHttpRequestFactory) this.factory).createHttpUriRequest(method, uri);
-		Object actual = request instanceof HttpEntityEnclosingRequest;
-		assertThat(actual).isEqualTo(allowed);
 	}
 
 }
--- spring-web/src/test/java/org/springframework/http/server/reactive/ServerHttpsRequestIntegrationTests.java
+++ spring-web/src/test/java/org/springframework/http/server/reactive/ServerHttpsRequestIntegrationTests.java
@@ -18,12 +18,14 @@ package org.springframework.http.server.reactive;
 
 import java.net.URI;
 
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.TrustSelfSignedStrategy;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -67,8 +69,11 @@ class ServerHttpsRequestIntegrationTests {
 		builder.loadTrustMaterial(new TrustSelfSignedStrategy());
 		SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
 				builder.build(), NoopHostnameVerifier.INSTANCE);
-		CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(
-				socketFactory).build();
+		PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+				.setSSLSocketFactory(socketFactory)
+				.build();
+		CloseableHttpClient httpclient = HttpClients.custom().
+				setConnectionManager(connectionManager).build();
 		HttpComponentsClientHttpRequestFactory requestFactory =
 				new HttpComponentsClientHttpRequestFactory(httpclient);
 		this.restTemplate = new RestTemplate(requestFactory);
