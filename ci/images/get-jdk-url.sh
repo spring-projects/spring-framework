ee1afb864c8d637d3ce2b3d89170a9daad49e8b1
Brian Clozel <bclozel@vmware.com>
2022-10-02 16:23:22 +0200

    Add Java 19 to CI image

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -8,6 +8,9 @@ case "$1" in
 	java18)
 		echo "https://github.com/bell-sw/Liberica/releases/download/18.0.2.1+1/bellsoft-jdk18.0.2.1+1-linux-amd64.tar.gz"
 	;;
+	java19)
+		 echo "https://github.com/bell-sw/Liberica/releases/download/19+37/bellsoft-jdk19+37-linux-amd64.tar.gz"
+	;;
 	*)
 		echo $"Unknown java version"
 		exit 1
