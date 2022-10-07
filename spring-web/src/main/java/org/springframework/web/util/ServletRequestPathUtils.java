16ea4692bab551800b9ba994ac08099e8acfd6cd
rstoyanchev <rstoyanchev@vmware.com>
2022-10-07 18:14:23 +0100

    Drop Servlet 4 classpath check

--- spring-web/src/main/java/org/springframework/web/util/ServletRequestPathUtils.java
+++ spring-web/src/main/java/org/springframework/web/util/ServletRequestPathUtils.java
@@ -249,15 +249,15 @@ public abstract class ServletRequestPathUtils {
 			if (requestUri == null) {
 				requestUri = request.getRequestURI();
 			}
-			if (UrlPathHelper.servlet4Present) {
-				String servletPathPrefix = Servlet4Delegate.getServletPathPrefix(request);
-				if (StringUtils.hasText(servletPathPrefix)) {
-					if (servletPathPrefix.endsWith("/")) {
-						servletPathPrefix = servletPathPrefix.substring(0, servletPathPrefix.length() - 1);
-					}
-					return new ServletRequestPath(requestUri, request.getContextPath(), servletPathPrefix);
+
+			String servletPathPrefix = Servlet4Delegate.getServletPathPrefix(request);
+			if (StringUtils.hasText(servletPathPrefix)) {
+				if (servletPathPrefix.endsWith("/")) {
+					servletPathPrefix = servletPathPrefix.substring(0, servletPathPrefix.length() - 1);
 				}
+				return new ServletRequestPath(requestUri, request.getContextPath(), servletPathPrefix);
 			}
+
 			return RequestPath.parse(requestUri, request.getContextPath());
 		}
 	}
