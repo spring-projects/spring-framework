a11222f681090ecb5557907e08023c34be3884f2
Juergen Hoeller <hoellerj@vmware.com>
2022-11-25 17:58:59 +0100

    Polishing

--- spring-web/src/main/java/org/springframework/http/server/reactive/JettyHeadersAdapter.java
+++ spring-web/src/main/java/org/springframework/http/server/reactive/JettyHeadersAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -170,7 +170,6 @@ class JettyHeadersAdapter implements MultiValueMap<String, String> {
 			public Iterator<Entry<String, List<String>>> iterator() {
 				return new EntryIterator();
 			}
-
 			@Override
 			public int size() {
 				return headers.size();
