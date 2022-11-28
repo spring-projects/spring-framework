eee50d3b8ab353f41060f33f55e9a5165c8337ca
Sam Brannen <sam@sambrannen.com>
2022-11-28 17:06:22 +0100

    Polishing

--- spring-websocket/src/main/java/org/springframework/web/socket/sockjs/transport/handler/HtmlFileTransportHandler.java
+++ spring-websocket/src/main/java/org/springframework/web/socket/sockjs/transport/handler/HtmlFileTransportHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,7 @@ import org.springframework.web.util.JavaScriptUtils;
 
 /**
  * An HTTP {@link TransportHandler} that uses a famous browser
- * {@code document.domain technique}. See <a href=
+ * {@code document.domain} technique. See <a href=
  * "https://stackoverflow.com/questions/1481251/what-does-document-domain-document-domain-do">
  * stackoverflow.com/questions/1481251/what-does-document-domain-document-domain-do</a>
  * for details.
