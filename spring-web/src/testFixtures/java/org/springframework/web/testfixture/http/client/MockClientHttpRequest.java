2b6f3caff4f3f9eb784c583c5521987fe6c66ceb
Sam Brannen <sbrannen@vmware.com>
2022-12-09 15:57:35 -0500

    Remove dead code in MockClientHttpRequest

--- spring-web/src/testFixtures/java/org/springframework/web/testfixture/http/client/MockClientHttpRequest.java
+++ spring-web/src/testFixtures/java/org/springframework/web/testfixture/http/client/MockClientHttpRequest.java
@@ -141,13 +141,9 @@ public class MockClientHttpRequest extends MockHttpOutputMessage implements Clie
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(this.httpMethod);
-		sb.append(' ').append(this.uri);
+		sb.append(this.httpMethod).append(' ').append(this.uri);
 		if (!getHeaders().isEmpty()) {
 			sb.append(", headers: ").append(getHeaders());
-		}
-		if (sb.length() == 0) {
-			sb.append("Not yet initialized");
 		}
 		return sb.toString();
 	}
