4cbd211a57508fead4cdc0b2c8017d401e1512b2
Zen Huifer <huifer97@163.com>
2022-12-23 13:09:25 +0800

    Add SimpleTypeConverterTest

--- spring-beans/src/test/java/org/springframework/beans/SimpleTypeConverterTest.java
+++ spring-beans/src/test/java/org/springframework/beans/SimpleTypeConverterTest.java
@@ -1,16 +1,34 @@
 package org.springframework.beans;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.beans.PropertyEditorSupport;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SimpleTypeConverterTest {
+
 	@Test
-	void stringToDate(){
+	void stringToDate() {
 		SimpleTypeConverter converter = new SimpleTypeConverter();
+		converter.registerCustomEditor(Date.class, new SampleDatePropertyEditor());
 		Date date = converter.convertIfNecessary("2022-10-13 00:00:00", Date.class);
-		System.out.println();
+		Assertions.assertEquals(date, new Date(2022, 10, 13, 0, 0, 0));
+	}
+
+
+	class SampleDatePropertyEditor extends PropertyEditorSupport {
+
+		@Override
+		public void setAsText(String text) throws IllegalArgumentException {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			try {
+				setValue(sdf.parse(text));
+			} catch (ParseException e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 
 }
