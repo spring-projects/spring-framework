62cf2f0a4f6de789a3acec7778cce7eeea6c849e
Johnny Lim <izeye@naver.com>
2023-01-03 18:57:02 +0900

    Fix wrong asserted code in SQLExceptionSubclassTranslatorTests (#29748)
    There was a typo in the test, covering the wrong SQLState code.
    
    Polishes a644245.
    Relates to gh-29699.

--- spring-jdbc/src/test/java/org/springframework/jdbc/support/SQLExceptionSubclassTranslatorTests.java
+++ spring-jdbc/src/test/java/org/springframework/jdbc/support/SQLExceptionSubclassTranslatorTests.java
@@ -59,7 +59,7 @@ public class SQLExceptionSubclassTranslatorTests {
 		doTest(new SQLIntegrityConstraintViolationException("", "23505", 0), DuplicateKeyException.class);
 		doTest(new SQLIntegrityConstraintViolationException("", "23000", 1), DuplicateKeyException.class);
 		doTest(new SQLIntegrityConstraintViolationException("", "23000", 1062), DuplicateKeyException.class);
-		doTest(new SQLIntegrityConstraintViolationException("", "23505", 2627), DuplicateKeyException.class);
+		doTest(new SQLIntegrityConstraintViolationException("", "23000", 2627), DuplicateKeyException.class);
 		doTest(new SQLInvalidAuthorizationSpecException("", "", 0), PermissionDeniedDataAccessException.class);
 		doTest(new SQLNonTransientConnectionException("", "", 0), DataAccessResourceFailureException.class);
 		doTest(new SQLRecoverableException("", "", 0), RecoverableDataAccessException.class);
