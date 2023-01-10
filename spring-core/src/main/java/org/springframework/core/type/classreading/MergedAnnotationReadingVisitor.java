139ca63959c1b8058fd9522d631619540e0804f4
Vasiliy Kudriavtsev <vbkudriavtsev@gmail.com>
2023-01-10 15:25:44 +0100

    fix code style

--- spring-core/src/main/java/org/springframework/core/type/classreading/MergedAnnotationReadingVisitor.java
+++ spring-core/src/main/java/org/springframework/core/type/classreading/MergedAnnotationReadingVisitor.java
@@ -18,7 +18,11 @@ package org.springframework.core.type.classreading;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 import org.springframework.asm.AnnotationVisitor;
