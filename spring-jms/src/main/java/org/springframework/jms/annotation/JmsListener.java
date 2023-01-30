6bd7a7321e78878d5888930ab39c71162d045f16
Sam Brannen <sam@sambrannen.com>
2023-01-30 15:36:45 +0100

    Revise generated default name for @JmsListener subscription
    The previous commit changed the generated default name for a JMS
    subscription to <FQCN>#<method name> -- for example:
    
    - org.example.MyListener#myListenerMethod
    
    However, the JMS spec does not guarantee that '#' is a supported
    character. This commit therefore changes '#' to '.' as the separator
    between the class name and method name -- for example:
    
    - org.example.MyListener.myListenerMethod
    
    This commit also introduces tests and documentation for these changes.
    
    See gh-29902

--- spring-jms/src/main/java/org/springframework/jms/annotation/JmsListener.java
+++ spring-jms/src/main/java/org/springframework/jms/annotation/JmsListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -76,6 +76,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
  * <em>composed annotations</em> with attribute overrides.
  *
  * @author Stephane Nicoll
+ * @author Sam Brannen
  * @since 4.1
  * @see EnableJms
  * @see JmsListenerAnnotationBeanPostProcessor
@@ -110,6 +111,12 @@ public @interface JmsListener {
 
 	/**
 	 * The name for the durable subscription, if any.
+	 * <p>As of Spring Framework 5.3.26, if an explicit subscription name is not
+	 * specified, a default subscription name will be generated based on the fully
+	 * qualified name of the annotated listener method &mdash; for example,
+	 * {@code "org.example.jms.ProductListener.processRequest"} for a
+	 * {@code processRequest(...)} listener method in the
+	 * {@code org.example.jms.ProductListener} class.
 	 */
 	String subscription() default "";
 
