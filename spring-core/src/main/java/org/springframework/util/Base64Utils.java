0d1d2a881a70bb93d721901ca735f44e81321fe4
Karos <mail@wzl1.top>
2023-02-09 01:57:45 +0800

    Extension Base64Util utility class

--- spring-core/src/main/java/org/springframework/util/Base64Utils.java
+++ spring-core/src/main/java/org/springframework/util/Base64Utils.java
@@ -31,6 +31,23 @@ import java.util.Base64;
  */
 @Deprecated(since = "6.0.5", forRemoval = true)
 public abstract class Base64Utils {
+	//Base64 code regular judgment expression
+	private static final String BASE64_PATTERN = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$";
+
+	/**
+	 * To determine whether a string is a base64 expression, you can use the Base64 header
+	 * @param base64str
+	 * @return Returns a result indicating whether the current base64str meets the base64 code
+	 */
+	public static boolean isBase64(String base64str){
+		if (base64str==null||base64str.length()==0){
+			return false;
+		}
+		if (base64str.length()>=11&"data:image/".equals(base64str.substring(0,11))){
+			base64str=base64str.substring(base64str.indexOf(',')+1);
+		}
+		return base64str.matches(BASE64_PATTERN);
+	}
 
 	/**
 	 * Base64-encode the given byte array.
