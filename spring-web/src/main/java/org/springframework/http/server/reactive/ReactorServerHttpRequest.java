af8de1f6f7a33ac90e15f7ca14ea2ebfed41d5be
Violeta Georgieva <violetag@vmware.com>
2023-03-02 11:03:39 +0200

    Prefer request hostName and hostPort in ReactorServerHttpRequest
    - Prefer request hostName and hostPort in ReactorServerHttpRequest#resolveBaseUrl
    - The request hostName and hostPort are derived from the Host/X-Forwarded-*/Forwarded header associated with this request.
    - Do not add the port when it is the default one
    
    Closes gh-30033

--- spring-web/src/main/java/org/springframework/http/server/reactive/ReactorServerHttpRequest.java
+++ spring-web/src/main/java/org/springframework/http/server/reactive/ReactorServerHttpRequest.java
@@ -79,36 +79,10 @@ class ReactorServerHttpRequest extends AbstractServerHttpRequest {
 
 	private static URI resolveBaseUrl(HttpServerRequest request) throws URISyntaxException {
 		String scheme = getScheme(request);
-
-		InetSocketAddress hostAddress = request.hostAddress();
-		if (hostAddress != null) {
-			return new URI(scheme, null, hostAddress.getHostString(), hostAddress.getPort(), null, null, null);
-		}
-
-		String header = request.requestHeaders().get(HttpHeaderNames.HOST);
-		if (header != null) {
-			final int portIndex;
-			if (header.startsWith("[")) {
-				portIndex = header.indexOf(':', header.indexOf(']'));
-			}
-			else {
-				portIndex = header.indexOf(':');
-			}
-			if (portIndex != -1) {
-				try {
-					return new URI(scheme, null, header.substring(0, portIndex),
-							Integer.parseInt(header, portIndex + 1, header.length(), 10), null, null, null);
-				}
-				catch (NumberFormatException ex) {
-					throw new URISyntaxException(header, "Unable to parse port", portIndex);
-				}
-			}
-			else {
-				return new URI(scheme, header, null, null);
-			}
-		}
-
-		throw new IllegalStateException("Neither local hostAddress nor HOST header available");
+		int port = request.hostPort();
+		return (scheme.equals("http") && (port != 80)) || (scheme.equals("https") && (port != 443)) ?
+				new URI(scheme, null, request.hostName(), port, null, null, null) :
+				new URI(scheme, request.hostName(), null, null);
 	}
 
 	private static String getScheme(HttpServerRequest request) {
