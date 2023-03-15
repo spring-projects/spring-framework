6317569b1c6b90f51ff5cd69fb7c49826152e3f0
James Yuzawa <jtyuzawa@gmail.com>
2023-03-15 09:44:22 -0400

    Use iterators in BodyExtractor and BodyInserter
    they use less memory and cpu

--- spring-webflux/src/main/java/org/springframework/web/reactive/function/BodyInserters.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/function/BodyInserters.java
@@ -373,12 +373,12 @@ public abstract class BodyInserters {
 			publisher = Mono.just(body);
 		}
 		MediaType mediaType = outputMessage.getHeaders().getContentType();
-		return context.messageWriters().stream()
-				.filter(messageWriter -> messageWriter.canWrite(bodyType, mediaType))
-				.findFirst()
-				.map(BodyInserters::cast)
-				.map(writer -> write(publisher, bodyType, mediaType, outputMessage, context, writer))
-				.orElseGet(() -> Mono.error(unsupportedError(bodyType, context, mediaType)));
+		for (HttpMessageWriter<?> messageWriter : context.messageWriters()) {
+			if (messageWriter.canWrite(bodyType, mediaType)) {
+				return write(publisher, bodyType, mediaType, outputMessage, context, cast(messageWriter));
+			}
+		}
+		return Mono.error(unsupportedError(bodyType, context, mediaType));
 	}
 
 	private static UnsupportedMediaTypeException unsupportedError(ResolvableType bodyType,
@@ -406,12 +406,13 @@ public abstract class BodyInserters {
 	private static <T> HttpMessageWriter<T> findWriter(
 			BodyInserter.Context context, ResolvableType elementType, @Nullable MediaType mediaType) {
 
-		return context.messageWriters().stream()
-				.filter(messageWriter -> messageWriter.canWrite(elementType, mediaType))
-				.findFirst()
-				.map(BodyInserters::<T>cast)
-				.orElseThrow(() -> new IllegalStateException(
-						"No HttpMessageWriter for \"" + mediaType + "\" and \"" + elementType + "\""));
+		for (HttpMessageWriter<?> messageWriter : context.messageWriters()) {
+			if (messageWriter.canWrite(elementType, mediaType)) {
+				return cast(messageWriter);
+			}
+		}
+		throw new IllegalStateException(
+						"No HttpMessageWriter for \"" + mediaType + "\" and \"" + elementType + "\"");
 	}
 
 	@SuppressWarnings("unchecked")
