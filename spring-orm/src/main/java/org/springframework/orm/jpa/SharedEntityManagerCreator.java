881d2598eda8ec9a22e20edb6aff931d6c9f0399
Giuseppe <g.milicia@outlook.com>
2023-03-22 10:21:03 +0000

    Closes gh-30160

--- spring-orm/src/main/java/org/springframework/orm/jpa/SharedEntityManagerCreator.java
+++ spring-orm/src/main/java/org/springframework/orm/jpa/SharedEntityManagerCreator.java
@@ -430,7 +430,7 @@ public abstract class SharedEntityManagerCreator {
 									entry.setValue(storedProc.getOutputParameterValue(key.toString()));
 								}
 							}
-							catch (IllegalArgumentException ex) {
+							catch (RuntimeException ex) {
 								entry.setValue(ex);
 							}
 						}
