e931fdc5c258e66d06c8dd19abe516b6730d85b3
Sébastien Deleuze <sdeleuze@vmware.com>
2023-04-03 08:59:08 +0200

    Make HttpComponentsHeadersAdapter#getFirst nullable
    Backport of gh-30267
    
    Closes gh-30269

--- spring-web/src/main/java/org/springframework/http/client/reactive/HttpComponentsHeadersAdapter.java
+++ spring-web/src/main/java/org/springframework/http/client/reactive/HttpComponentsHeadersAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,6 +53,7 @@ class HttpComponentsHeadersAdapter implements MultiValueMap<String, String> {
 
 
 	@Override
+	@Nullable
 	public String getFirst(String key) {
 		Header header = this.message.getFirstHeader(key);
 		return (header != null ? header.getValue() : null);
