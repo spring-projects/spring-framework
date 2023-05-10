6622a3c7d6c475a5e205f49fe63eff0f569a4780
Juergen Hoeller <hoellerj@vmware.com>
2023-05-10 18:50:17 +0200

    Consistent package-info declarations for CGLIB fork
    Closes gh-30461

--- spring-core/src/main/java/org/springframework/cglib/package-info.java
+++ spring-core/src/main/java/org/springframework/cglib/package-info.java
@@ -6,8 +6,5 @@
  * <p>This repackaging technique avoids any potential conflicts with
  * dependencies on CGLIB at the application level or from third-party
  * libraries and frameworks.
- *
- * <p>As this repackaging happens at the class file level, sources
- * and javadocs are not available here.
  */
 package org.springframework.cglib;
