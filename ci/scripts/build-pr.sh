b4866e51b296bd79d251c860fbb9b934128995a2
Brian Clozel <bclozel@vmware.com>
2023-05-12 11:33:17 +0200

    Configure toolchain locations when building PRs
    This commit updates the script that builds PRs to add locations to the
    installed JDK within the CI image. Without this change, PRs fail to
    build because of the recently introduced JDK21 toolchain requirement in
    the Spring Framework build.
    
    Closes gh-30472

--- ci/scripts/build-pr.sh
+++ ci/scripts/build-pr.sh
@@ -4,5 +4,6 @@ set -e
 source $(dirname $0)/common.sh
 
 pushd git-repo > /dev/null
-./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --max-workers=4 check
+./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false -Porg.gradle.java.installations.fromEnv=JDK17,JDK21 \
+  --no-daemon --max-workers=4 check
 popd > /dev/null
