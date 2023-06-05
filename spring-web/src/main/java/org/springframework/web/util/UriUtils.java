8d6b0eb191f3f005a3c800b92e88a3c1a713b2ba
Valery Yatsynovich <valery_yatsynovich@epam.com>
2023-06-05 18:34:03 +0300

    Fix typo in UriUtils Javadoc
    Closes gh-30598

--- spring-web/src/main/java/org/springframework/web/util/UriUtils.java
+++ spring-web/src/main/java/org/springframework/web/util/UriUtils.java
@@ -307,7 +307,7 @@ public abstract class UriUtils {
 	 * meaning, anywhere within a URI, as defined in
 	 * <a href="https://tools.ietf.org/html/rfc3986">RFC 3986</a>.
 	 * This is useful to ensure that the given String will be preserved as-is
-	 * and will not have any o impact on the structure or meaning of the URI.
+	 * and will not have any impact on the structure or meaning of the URI.
 	 * @param source the String to be encoded
 	 * @param charset the character encoding to encode to
 	 * @return the encoded String
