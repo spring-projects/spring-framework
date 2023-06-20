db19f6395deb3e38f89ee2dc2e929aa0af7cac2c
rstoyanchev <rstoyanchev@vmware.com>
2023-06-20 17:15:32 +0100

    Polishing
    Closes gh-29768

--- spring-messaging/src/test/java/org/springframework/messaging/converter/MessageConverterTests.java
+++ spring-messaging/src/test/java/org/springframework/messaging/converter/MessageConverterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -138,10 +138,11 @@ public class MessageConverterTests {
 		assertThat(message.getHeaders().get(MessageHeaders.CONTENT_TYPE)).isEqualTo(MimeTypeUtils.TEXT_PLAIN);
 	}
 
-	@Test
+	@Test // gh-29768
 	public void toMessageDefaultContentType() {
 		DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
 		resolver.setDefaultMimeType(MimeTypeUtils.TEXT_PLAIN);
+
 		TestMessageConverter converter = new TestMessageConverter();
 		converter.setContentTypeResolver(resolver);
 		converter.setStrictContentTypeMatch(true);
@@ -167,16 +168,12 @@ public class MessageConverterTests {
 		}
 
 		@Override
-		protected Object convertFromInternal(
-				Message<?> message, Class<?> targetClass, @Nullable Object conversionHint) {
-
+		protected Object convertFromInternal(Message<?> message, Class<?> targetClass, @Nullable Object hint) {
 			return "success-from";
 		}
 
 		@Override
-		protected Object convertToInternal(
-				Object payload, @Nullable MessageHeaders headers, @Nullable Object conversionHint) {
-
+		protected Object convertToInternal(Object payload, @Nullable MessageHeaders headers, @Nullable Object hint) {
 			return "success-to";
 		}
 	}
