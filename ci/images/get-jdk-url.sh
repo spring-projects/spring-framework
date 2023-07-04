14857ae0da2b714cb6a630daf54a43d5ecd79313
Brian Clozel <bclozel@vmware.com>
2023-07-04 15:23:51 +0200

    Upgrade to JDK 21ea29 in CI build

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -9,7 +9,7 @@ case "$1" in
 		echo "https://github.com/bell-sw/Liberica/releases/download/20.0.1+10/bellsoft-jdk20.0.1+10-linux-amd64.tar.gz"
 	;;
   java21)
-    echo "https://download.java.net/java/early_access/jdk21/27/GPL/openjdk-21-ea+27_linux-x64_bin.tar.gz"
+    echo "https://download.java.net/java/early_access/jdk21/29/GPL/openjdk-21-ea+29_linux-x64_bin.tar.gz"
   ;;
 	*)
 		echo $"Unknown java version"
