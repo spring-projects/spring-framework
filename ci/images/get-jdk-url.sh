ca01ee05f7928258694a0dc490f83143d959cd31
Brian Clozel <bclozel@vmware.com>
2023-07-12 08:22:34 +0200

    Update JDK and OS versions in CI image

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,10 +3,10 @@ set -e
 
 case "$1" in
 	java8)
-		 echo "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u322-b06/OpenJDK8U-jdk_x64_linux_hotspot_8u322b06.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/8u372+7/bellsoft-jdk8u372+7-linux-amd64.tar.gz"
 	;;
 	java11)
-		 echo "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.14.1%2B1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.14.1_1.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/11.0.19%2B7/bellsoft-jdk11.0.19+7-linux-amd64.tar.gz"
 	;;
   *)
 		echo $"Unknown java version"
