0611192dacc995891fd99904d2c4273fe9212d28
Sam Brannen <sam@sambrannen.com>
2023-07-15 13:35:02 +0200

    Enable KotlinScriptTemplateTests in Spring MVC on Java 20
    See gh-29249
    Closes gh-30896

--- spring-webmvc/src/test/java/org/springframework/web/servlet/view/script/KotlinScriptTemplateTests.java
+++ spring-webmvc/src/test/java/org/springframework/web/servlet/view/script/KotlinScriptTemplateTests.java
@@ -35,7 +35,7 @@ import org.springframework.web.testfixture.servlet.MockHttpServletResponse;
 import org.springframework.web.testfixture.servlet.MockServletContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.condition.JRE.JAVA_20;
+import static org.junit.jupiter.api.condition.JRE.JAVA_21;
 import static org.mockito.Mockito.mock;
 
 /**
@@ -44,7 +44,7 @@ import static org.mockito.Mockito.mock;
  * @author Sebastien Deleuze
  * @author Sam Brannen
  */
-@DisabledForJreRange(min = JAVA_20, disabledReason = "Kotlin doesn't support Java 20+ yet")
+@DisabledForJreRange(min = JAVA_21, disabledReason = "Kotlin doesn't support Java 21+ yet")
 class KotlinScriptTemplateTests {
 
 	private WebApplicationContext webAppContext = mock();
