a28ec3a0a87e640cc7c9e7e89cae0e232db38c81
Sam Brannen <sam@sambrannen.com>
2023-07-15 10:15:29 +0200

    Make DefaultGenerationContext(<classes>,<files>,<hints>) constructor protected
    See gh-30861
    Closes gh-30895

--- spring-core/src/main/java/org/springframework/aot/generate/DefaultGenerationContext.java
+++ spring-core/src/main/java/org/springframework/aot/generate/DefaultGenerationContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -78,7 +78,7 @@ public class DefaultGenerationContext implements GenerationContext {
 	 * @param generatedFiles the generated files
 	 * @param runtimeHints the runtime hints
 	 */
-	DefaultGenerationContext(GeneratedClasses generatedClasses,
+	protected DefaultGenerationContext(GeneratedClasses generatedClasses,
 			GeneratedFiles generatedFiles, RuntimeHints runtimeHints) {
 
 		Assert.notNull(generatedClasses, "'generatedClasses' must not be null");
