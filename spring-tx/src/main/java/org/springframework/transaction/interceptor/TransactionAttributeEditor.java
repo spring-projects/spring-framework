635cd29599dcde37e5f8eedfd9f72539998fb578
Sam Brannen <sam@sambrannen.com>
2023-07-16 13:50:26 +0200

    Remove cross reference to Constants utility
    See gh-30851

--- spring-tx/src/main/java/org/springframework/transaction/interceptor/TransactionAttributeEditor.java
+++ spring-tx/src/main/java/org/springframework/transaction/interceptor/TransactionAttributeEditor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,6 @@ import org.springframework.util.StringUtils;
  * @author Juergen Hoeller
  * @since 24.04.2003
  * @see org.springframework.transaction.TransactionDefinition
- * @see org.springframework.core.Constants
  */
 public class TransactionAttributeEditor extends PropertyEditorSupport {
 
