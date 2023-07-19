c88c9620b3df450de681d0fc3556d13d07266ae0
Brian Clozel <bclozel@vmware.com>
2023-07-19 09:51:28 +0200

    Upgrade to Java 21 EA 31 in CI build

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -5,12 +5,9 @@ case "$1" in
 	java17)
 		echo "https://github.com/bell-sw/Liberica/releases/download/17.0.7+7/bellsoft-jdk17.0.7+7-linux-amd64.tar.gz"
 	;;
-	java20)
-		echo "https://github.com/bell-sw/Liberica/releases/download/20.0.1+10/bellsoft-jdk20.0.1+10-linux-amd64.tar.gz"
+	java21)
+		echo "https://download.java.net/java/early_access/jdk21/31/GPL/openjdk-21-ea+31_linux-x64_bin.tar.gz"
 	;;
-  java21)
-    echo "https://download.java.net/java/early_access/jdk21/29/GPL/openjdk-21-ea+29_linux-x64_bin.tar.gz"
-  ;;
 	*)
 		echo $"Unknown java version"
 		exit 1
