ad80b94e14591651e75ab1ff312b566865ef8155
rstoyanchev <rstoyanchev@vmware.com>
2023-08-03 15:11:08 +0300

    Allow BindParam on a field
    See gh-30947

--- spring-web/src/main/java/org/springframework/web/bind/annotation/BindParam.java
+++ spring-web/src/main/java/org/springframework/web/bind/annotation/BindParam.java
@@ -31,7 +31,7 @@ import java.lang.annotation.Target;
  * @since 6.1
  * @see org.springframework.web.bind.WebDataBinder#construct
  */
-@Target(ElementType.PARAMETER)
+@Target({ElementType.PARAMETER, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface BindParam {
