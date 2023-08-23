b2a86cc42d8baadfb9a20933798ac694b4fb6b59
Sébastien Deleuze <sdeleuze@vmware.com>
2023-08-23 18:53:38 +0200

    Polishing
    See gh-24311

--- spring-core/src/test/kotlin/org/springframework/core/convert/support/DefaultConversionServiceKotlinTests.kt
+++ spring-core/src/test/kotlin/org/springframework/core/convert/support/DefaultConversionServiceKotlinTests.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.core.convert.support
 
 import org.assertj.core.api.Assertions.assertThat
@@ -20,9 +36,8 @@ class DefaultConversionServiceKotlinTests {
 	@Test
 	fun stringToRegex() {
 		val pattern = "\\w+"
-		assertThat(conversionService.convert(pattern, Regex::class.java)).isInstanceOfSatisfying(
-			Regex::class.java
-		) { regex -> assertThat(regex.pattern).isEqualTo(pattern) }
+		assertThat(conversionService.convert(pattern, Regex::class.java))
+			.isInstanceOfSatisfying(Regex::class.java) { assertThat(it.pattern).isEqualTo(pattern) }
 	}
 
 	@Test
