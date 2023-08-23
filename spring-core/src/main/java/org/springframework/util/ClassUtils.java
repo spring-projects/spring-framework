906a9f7982c7f880313664a47f682ac379ae508b
Juergen Hoeller <hoellerj@vmware.com>
2023-08-23 18:52:55 +0200

    Polishing

--- spring-core/src/main/java/org/springframework/util/ClassUtils.java
+++ spring-core/src/main/java/org/springframework/util/ClassUtils.java
@@ -1427,7 +1427,7 @@ public abstract class ClassUtils {
 		Assert.notNull(methodName, "Method name must not be null");
 		try {
 			Method method = clazz.getMethod(methodName, args);
-			return Modifier.isStatic(method.getModifiers()) ? method : null;
+			return (Modifier.isStatic(method.getModifiers()) ? method : null);
 		}
 		catch (NoSuchMethodException ex) {
 			return null;
