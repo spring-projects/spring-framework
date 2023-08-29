64ff37f42c18183bffb1654ed220084fc5861a46
Brian Clozel <bclozel@vmware.com>
2023-08-29 11:28:47 +0200

    Update JDK21 release candidate download URL

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -6,7 +6,7 @@ case "$1" in
 		echo "https://download.bell-sw.com/java/17.0.8.1+1/bellsoft-jdk17.0.8.1+1-linux-amd64.tar.gz"
 	;;
 	java21)
-		echo "https://download.java.net/java/early_access/jdk21/35/GPL/openjdk-21-ea+35_linux-x64_bin.tar.gz"
+		echo "https://download.java.net/java/GA/jdk21/fd2272bbf8e04c3dbaee13770090416c/35/GPL/openjdk-21_linux-x64_bin.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
