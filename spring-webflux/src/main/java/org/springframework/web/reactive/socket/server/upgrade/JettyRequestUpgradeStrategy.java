5dacf688b2e971156898a75d62d520431c46128d
Sam Brannen <sam@sambrannen.com>
2023-09-13 16:01:11 +0200

    Polishing

--- spring-webflux/src/main/java/org/springframework/web/reactive/socket/server/upgrade/JettyRequestUpgradeStrategy.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/socket/server/upgrade/JettyRequestUpgradeStrategy.java
@@ -59,7 +59,7 @@ public class JettyRequestUpgradeStrategy implements RequestUpgradeStrategy {
 	/**
 	 * Add a callback to configure WebSocket server parameters on
 	 * {@link JettyWebSocketServerContainer}.
-	 * @since 6.1.0
+	 * @since 6.1
 	 */
 	public void addWebSocketConfigurer(Consumer<Configurable> webSocketConfigurer) {
 		this.webSocketConfigurer = (this.webSocketConfigurer != null ?
