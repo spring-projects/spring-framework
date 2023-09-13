e63e3a6d28b9bcf01c144894ea9e1e6ec929c219
Sam Brannen <sam@sambrannen.com>
2023-09-13 16:13:09 +0200

    Delete obsolete constants

--- spring-core/src/main/java/org/springframework/util/ObjectUtils.java
+++ spring-core/src/main/java/org/springframework/util/ObjectUtils.java
@@ -49,9 +49,6 @@ import org.springframework.lang.Nullable;
  */
 public abstract class ObjectUtils {
 
-	private static final int INITIAL_HASH = 7;
-	private static final int MULTIPLIER = 31;
-
 	private static final String EMPTY_STRING = "";
 	private static final String NULL_STRING = "null";
 	private static final String ARRAY_START = "{";
