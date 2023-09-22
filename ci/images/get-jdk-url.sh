1afb6f1861f898f06a4e029a8a5fc6ccc8216d61
Brian Clozel <bclozel@vmware.com>
2023-09-22 09:02:12 +0200

    Upgrade to Java 21 in CI image

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -6,7 +6,7 @@ case "$1" in
 		echo "https://download.bell-sw.com/java/17.0.8.1+1/bellsoft-jdk17.0.8.1+1-linux-amd64.tar.gz"
 	;;
 	java21)
-		echo "https://download.java.net/java/GA/jdk21/fd2272bbf8e04c3dbaee13770090416c/35/GPL/openjdk-21_linux-x64_bin.tar.gz"
+		echo "https://download.bell-sw.com/java/21+37/bellsoft-jdk21+37-linux-amd64.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
