101d156d58877181e0e9563f0a8489d81b847244
ali dandach <alidandach1995@gmail.com>
2023-09-24 23:53:04 +0300

    refactor: Use Objects.equals() for null-safe string comparison
    Replaced direct string reference comparisons with Objects.equals() to ensure consistent content comparison, improve readability, and provide null safety.

--- buildSrc/src/main/java/org/springframework/build/optional/OptionalDependenciesPlugin.java
+++ buildSrc/src/main/java/org/springframework/build/optional/OptionalDependenciesPlugin.java
@@ -20,7 +20,6 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.plugins.JavaBasePlugin;
-import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.tasks.SourceSetContainer;
 
