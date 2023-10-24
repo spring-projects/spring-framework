9aa707ec4b04fe5a27d93143a42bec8cd30c7666
Stéphane Nicoll <snicoll@vmware.com>
2023-10-24 10:49:41 +0200

    Polish "Return consistent collection type for matrix variables"
    See gh-31483

--- spring-webmvc/src/test/java/org/springframework/web/servlet/mvc/method/annotation/MatrixVariablesMapMethodArgumentResolverTests.java
+++ spring-webmvc/src/test/java/org/springframework/web/servlet/mvc/method/annotation/MatrixVariablesMapMethodArgumentResolverTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -163,8 +164,8 @@ public class MatrixVariablesMapMethodArgumentResolverTests {
 
 		Object result = this.resolver.resolveArgument(param, this.mavContainer, this.webRequest, null);
 
-		//noinspection unchecked
-		assertThat(result).isInstanceOfSatisfying(MultiValueMap.class, map -> assertThat(map).isEmpty());
+		assertThat(result).isInstanceOf(MultiValueMap.class)
+				.asInstanceOf(InstanceOfAssertFactories.MAP).isEmpty();
 	}
 
 	@Test
