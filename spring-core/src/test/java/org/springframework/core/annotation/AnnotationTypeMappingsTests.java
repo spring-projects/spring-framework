6a7a0bddb749cbc8d716d4aee3d10ff88e4793eb
lorenzsimon <lorenz.simon.mail@gmail.com>
2023-10-29 12:36:33 +0100

    Restore support for recursive annotations in Kotlin
    This commit reinstates support for recursive annotations in Kotlin.
    
    See gh-28012
    See gh-28618
    See gh-31400
    Closes gh-31518

--- spring-core/src/test/java/org/springframework/core/annotation/AnnotationTypeMappingsTests.java
+++ spring-core/src/test/java/org/springframework/core/annotation/AnnotationTypeMappingsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,7 +80,7 @@ class AnnotationTypeMappingsTests {
 	@Test
 	void forAnnotationTypeWhenRepeatableMetaAnnotationIsFiltered() {
 		AnnotationTypeMappings mappings = AnnotationTypeMappings.forAnnotationType(WithRepeatedMetaAnnotations.class,
-				Repeating.class.getName()::equals);
+				RepeatableContainers.standardRepeatables(), Repeating.class.getName()::equals);
 		assertThat(getAll(mappings)).flatExtracting(AnnotationTypeMapping::getAnnotationType)
 				.containsExactly(WithRepeatedMetaAnnotations.class);
 	}
