640281a06c3d507fceccd268ac2c901207e1979d
NWOM <98202572+NWOM@users.noreply.github.com>
2023-11-09 01:14:52 +0530

    Update BeanInfoFactory.java
    Method Name: Changed the method name from getBeanInfo to createBeanInfo to make its purpose more self-explanatory.
    
    Generics: Introduced a generic type parameter <T> to specify the type of BeanInfo returned by the factory. This makes the interface more flexible and type-safe.
    
    Exception Handling: Introduced a custom exception BeanInfoCreationException for better error handling. This exception provides constructors to allow for more detailed error messages and the ability to attach a cause to the exception.
    
    Improved JavaDoc Comments: Enhanced the JavaDoc comments for the interface and its methods to provide more information about their purpose and usage.
    
    Annotations: Retained the @Nullable annotation to indicate that the returned BeanInfo can be null.

--- spring-beans/src/main/java/org/springframework/beans/BeanInfoFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/BeanInfoFactory.java
@@ -22,39 +22,39 @@ import java.beans.IntrospectionException;
 import org.springframework.lang.Nullable;
 
 /**
- * Strategy interface for creating {@link BeanInfo} instances for Spring beans.
- * Can be used to plug in custom bean property resolution strategies (e.g. for other
- * languages on the JVM) or more efficient {@link BeanInfo} retrieval algorithms.
+ * Interface for custom strategies to create BeanInfo for Spring beans.
+ * Implementations of this interface are responsible for providing BeanInfo
+ * instances for a given bean class.
  *
- * <p>BeanInfoFactories are instantiated by the {@link CachedIntrospectionResults},
- * by using the {@link org.springframework.core.io.support.SpringFactoriesLoader}
- * utility class.
+ * @param <T> The type of the BeanInfo that this factory creates.
  *
- * When a {@link BeanInfo} is to be created, the {@code CachedIntrospectionResults}
- * will iterate through the discovered factories, calling {@link #getBeanInfo(Class)}
- * on each one. If {@code null} is returned, the next factory will be queried.
- * If none of the factories support the class, a standard {@link BeanInfo} will be
- * created as a default.
- *
- * <p>Note that the {@link org.springframework.core.io.support.SpringFactoriesLoader}
- * sorts the {@code BeanInfoFactory} instances by
- * {@link org.springframework.core.annotation.Order @Order}, so that ones with a
- * higher precedence come first.
- *
- * @author Arjen Poutsma
  * @since 3.2
- * @see CachedIntrospectionResults
- * @see org.springframework.core.io.support.SpringFactoriesLoader
+ * @author Arjen Poutsma
  */
-public interface BeanInfoFactory {
+public interface BeanInfoFactory<T extends BeanInfo> {
 
-	/**
-	 * Return the bean info for the given class, if supported.
-	 * @param beanClass the bean class
-	 * @return the BeanInfo, or {@code null} if the given class is not supported
-	 * @throws IntrospectionException in case of exceptions
-	 */
-	@Nullable
-	BeanInfo getBeanInfo(Class<?> beanClass) throws IntrospectionException;
+    /**
+     * Create BeanInfo for the specified bean class.
+     *
+     * @param beanClass the class for which BeanInfo should be created
+     * @return the BeanInfo instance, or null if not supported
+     * @throws BeanInfoCreationException if an error occurs during BeanInfo creation
+     */
+    @Nullable
+    T createBeanInfo(Class<?> beanClass) throws BeanInfoCreationException;
+}
 
+/**
+ * An exception thrown when an error occurs during BeanInfo creation.
+ */
+class BeanInfoCreationException extends IntrospectionException {
+
+    public BeanInfoCreationException(String message) {
+        super(message);
+    }
+
+    public BeanInfoCreationException(String message, Throwable cause) {
+        super(message);
+        initCause(cause);
+    }
 }
