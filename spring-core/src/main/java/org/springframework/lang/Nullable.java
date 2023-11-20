8c9481d4c4e9e69606c6d8f31608e13c812b1169
Sam Brannen <sam@sambrannen.com>
2023-11-20 11:19:13 +0100

    Polishing

--- spring-core/src/main/java/org/springframework/lang/Nullable.java
+++ spring-core/src/main/java/org/springframework/lang/Nullable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,14 +27,15 @@ import javax.annotation.meta.TypeQualifierNickname;
 import javax.annotation.meta.When;
 
 /**
- * A common Spring annotation to declare that annotated elements can be {@code null} under
- * some circumstance.
+ * A common Spring annotation to declare that annotated elements can be {@code null}
+ * under certain circumstances.
  *
  * <p>Leverages JSR-305 meta-annotations to indicate nullability in Java to common
  * tools with JSR-305 support and used by Kotlin to infer nullability of Spring API.
  *
- * <p>Should be used at parameter, return value, and field level. Methods override should
- * repeat parent {@code @Nullable} annotations unless they behave differently.
+ * <p>Should be used at the parameter, return value, and field level. Method
+ * overrides should repeat parent {@code @Nullable} annotations unless they behave
+ * differently.
  *
  * <p>Can be used in association with {@code @NonNullApi} or {@code @NonNullFields} to
  * override the default non-nullable semantic to nullable.
