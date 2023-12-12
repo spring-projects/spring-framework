240a75f313d2b122834f4e6432bc19d5bf6817d6
Juergen Hoeller <hoellerj@vmware.com>
2023-12-12 12:40:04 +0100

    Polishing

--- spring-context/src/main/java/org/springframework/context/annotation/ImportRuntimeHints.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ImportRuntimeHints.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,6 @@ import org.springframework.aot.hint.RuntimeHintsRegistrar;
  *     public MyService myService() {
  *         return new MyService();
  *     }
- *
  * }</pre>
  *
  * <p>If the configuration class above is processed, {@code MyHints} will be
