8de0fadd0972f1e4632a23695d0e35f900b616fd
Brian Clozel <brian.clozel@broadcom.com>
2023-12-12 14:47:50 +0100

    Upgrade CI to JDK 22 early access 27

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -9,7 +9,7 @@ case "$1" in
 		echo "https://download.bell-sw.com/java/21.0.1+12/bellsoft-jdk21.0.1+12-linux-amd64.tar.gz"
 	;;
 	java22)
-		echo "https://download.java.net/java/early_access/jdk22/19/GPL/openjdk-22-ea+19_linux-x64_bin.tar.gz"
+		echo "https://download.java.net/java/early_access/jdk22/27/GPL/openjdk-22-ea+27_linux-x64_bin.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
