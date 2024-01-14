e1236a8672822f3d88f6658b98e56b178eff996d
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2024-01-14 16:00:50 +0100

    Polishing

--- spring-core/src/main/java/org/springframework/core/SimpleAliasRegistry.java
+++ spring-core/src/main/java/org/springframework/core/SimpleAliasRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,7 @@ import org.springframework.util.StringValueResolver;
  *
  * @author Juergen Hoeller
  * @author Qimiao Chen
+ * @author Sam Brannen
  * @since 2.5.2
  */
 public class SimpleAliasRegistry implements AliasRegistry {
