b9d366d203810658931d85bf8463ca7730b2daea
Brian Clozel <brian.clozel@broadcom.com>
2024-01-17 21:29:50 +0100

    Upgrade CI to JDK 17.0.10+13 21.0.2+14 and 22-ea+31

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,13 +3,13 @@ set -e
 
 case "$1" in
 	java17)
-		echo "https://download.bell-sw.com/java/17.0.9+11/bellsoft-jdk17.0.9+11-linux-amd64.tar.gz"
+		echo "https://github.com/bell-sw/Liberica/releases/download/17.0.10%2B13/bellsoft-jdk17.0.10+13-linux-amd64.tar.gz"
 	;;
 	java21)
-		echo "https://download.bell-sw.com/java/21.0.1+12/bellsoft-jdk21.0.1+12-linux-amd64.tar.gz"
+		echo "https://github.com/bell-sw/Liberica/releases/download/21.0.2%2B14/bellsoft-jdk21.0.2+14-linux-amd64.tar.gz"
 	;;
 	java22)
-		echo "https://download.java.net/java/early_access/jdk22/28/GPL/openjdk-22-ea+28_linux-x64_bin.tar.gz"
+		echo "https://download.java.net/java/early_access/jdk22/31/GPL/openjdk-22-ea+31_linux-x64_bin.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
