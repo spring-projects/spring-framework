562fa4e1adec735803b8abb281214dbbd192552e
Brian Clozel <brian.clozel@broadcom.com>
2024-01-17 21:17:46 +0100

    Upgrade CI image to JDK 8u402+7 and 17.0.10+13

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,10 +3,10 @@ set -e
 
 case "$1" in
 	java8)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/8u372+7/bellsoft-jdk8u372+7-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/8u402%2B7/bellsoft-jdk8u402+7-linux-amd64.tar.gz"
 	;;
 	java17)
-		echo "https://github.com/bell-sw/Liberica/releases/download/17.0.7+7/bellsoft-jdk17.0.7+7-linux-amd64.tar.gz"
+		echo "https://github.com/bell-sw/Liberica/releases/download/17.0.10%2B13/bellsoft-jdk17.0.10+13-linux-amd64.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
