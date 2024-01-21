5856d2e54efe9209e415b0547174cd754473d6cc
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2024-01-21 16:27:48 +0100

    Polish contribution
    See gh-32069

--- spring-test/src/main/java/org/springframework/test/annotation/DirtiesContext.java
+++ spring-test/src/main/java/org/springframework/test/annotation/DirtiesContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -142,7 +142,7 @@ public @interface DirtiesContext {
 		 * <em>dirty</em> after the corresponding test method.
 		 */
 		AFTER_METHOD
-  }
+	}
 
 
 	/**
@@ -179,7 +179,7 @@ public @interface DirtiesContext {
 		 * <em>dirty</em> after the test class.
 		 */
 		AFTER_CLASS
-  }
+	}
 
 
 	/**
@@ -213,6 +213,6 @@ public @interface DirtiesContext {
 		 * current test.
 		 */
 		CURRENT_LEVEL
-  }
+	}
 
 }
