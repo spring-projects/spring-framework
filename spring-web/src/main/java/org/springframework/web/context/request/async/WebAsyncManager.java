e2a5cfb4591c2460d7d1ff26047a3ced0e6e2020
Juergen Hoeller <juergen.hoeller@broadcom.com>
2024-01-24 11:43:36 +0100

    Consistent nullability for concurrent result
    (cherry picked from commit b92877990da57c36eee035095c9ae72f5c0a7537)

--- spring-web/src/main/java/org/springframework/web/context/request/async/WebAsyncManager.java
+++ spring-web/src/main/java/org/springframework/web/context/request/async/WebAsyncManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -159,6 +159,7 @@ public final class WebAsyncManager {
 	 * concurrent handling raised one.
 	 * @see #clearConcurrentResult()
 	 */
+	@Nullable
 	public Object getConcurrentResult() {
 		return this.concurrentResult;
 	}
