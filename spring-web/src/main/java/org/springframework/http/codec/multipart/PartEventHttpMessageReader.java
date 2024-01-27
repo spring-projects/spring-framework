4b4778d5694402bea70ab72ad4c7dc5a4520374b
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-01-27 12:21:37 +0100

    Polish "Simplify use of Reactor's cast operator"
    See gh-32141

--- spring-web/src/main/java/org/springframework/http/codec/multipart/PartEventHttpMessageReader.java
+++ spring-web/src/main/java/org/springframework/http/codec/multipart/PartEventHttpMessageReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -167,8 +167,8 @@ public class PartEventHttpMessageReader extends LoggingCodecSupport implements H
 									Assert.state(headersToken != null, "Signal should be headers token");
 
 									HttpHeaders headers = headersToken.headers();
-									Flux<MultipartParser.BodyToken> bodyTokens =
-											flux.ofType(MultipartParser.BodyToken.class);
+									Flux<MultipartParser.BodyToken> bodyTokens = flux.ofType(
+											MultipartParser.BodyToken.class);
 									return createEvents(headers, bodyTokens);
 								}
 								else {
