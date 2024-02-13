a3323de5da924215c8ae9710e4fc684d4f9af2e1
Napster <napster@npstr.space>
2024-02-13 16:59:42 +0100

    Handle trailing semicolon when parsing Accept-Language header

--- spring-web/src/test/java/org/springframework/http/HttpHeadersTests.java
+++ spring-web/src/test/java/org/springframework/http/HttpHeadersTests.java
@@ -489,6 +489,19 @@ class HttpHeadersTests {
 		assertThat(headers.getAcceptLanguageAsLocales()).element(0).isEqualTo(Locale.FRANCE);
 	}
 
+	@Test
+	void acceptLanguageTrailingSemicolon() {
+		String headerValue = "en-us,en;";
+		headers.set(HttpHeaders.ACCEPT_LANGUAGE, headerValue);
+		assertThat(headers.getFirst(HttpHeaders.ACCEPT_LANGUAGE)).isEqualTo(headerValue);
+
+		List<Locale.LanguageRange> expectedRanges = Arrays.asList(
+				new Locale.LanguageRange("en-us"),
+				new Locale.LanguageRange("en")
+		);
+		assertThat(headers.getAcceptLanguage()).isEqualTo(expectedRanges);
+	}
+
 	@Test // SPR-15603
 	void acceptLanguageWithEmptyValue() {
 		this.headers.set(HttpHeaders.ACCEPT_LANGUAGE, "");
