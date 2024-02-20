85c9279431e15f8f3d30e1dd946f76549e0d3390
Juergen Hoeller <juergen.hoeller@broadcom.com>
2024-02-20 15:42:07 +0100

    Consistent default error handling/logging for all scheduled tasks
    Closes gh-32298

--- spring-context/src/main/java/org/springframework/scheduling/concurrent/SimpleAsyncTaskScheduler.java
+++ spring-context/src/main/java/org/springframework/scheduling/concurrent/SimpleAsyncTaskScheduler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -184,7 +184,7 @@ public class SimpleAsyncTaskScheduler extends SimpleAsyncTaskExecutor implements
 	}
 
 	private Runnable scheduledTask(Runnable task) {
-		return () -> execute(task);
+		return () -> execute(new DelegatingErrorHandlingRunnable(task, TaskUtils.LOG_AND_PROPAGATE_ERROR_HANDLER));
 	}
 
 	private Runnable taskOnSchedulerThread(Runnable task) {
