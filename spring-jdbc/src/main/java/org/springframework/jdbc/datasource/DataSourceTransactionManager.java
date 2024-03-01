861ef88d9f4f0c03471985bc9059ca771b85cf7d
Juergen Hoeller <juergen.hoeller@broadcom.com>
2024-03-01 16:48:51 +0100

    Expose savepoint callbacks on TransactionSynchronization
    Closes gh-30509

--- spring-jdbc/src/main/java/org/springframework/jdbc/datasource/DataSourceTransactionManager.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/datasource/DataSourceTransactionManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -478,9 +478,7 @@ public class DataSourceTransactionManager extends AbstractPlatformTransactionMan
 
 		@Override
 		public void flush() {
-			if (TransactionSynchronizationManager.isSynchronizationActive()) {
-				TransactionSynchronizationUtils.triggerFlush();
-			}
+			TransactionSynchronizationUtils.triggerFlush();
 		}
 	}
 
