5dfec09edd62be011bbdbfd4e225f89b1e750311
Kasper Bisgaard <kasper.bisgaard@live.com>
2024-03-13 10:20:27 +0100

    Allow UriTemplate to be built with an empty template
    Closes gh-32438

--- spring-web/src/test/java/org/springframework/web/util/UriTemplateTests.java
+++ spring-web/src/test/java/org/springframework/web/util/UriTemplateTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 /**
  * @author Arjen Poutsma
@@ -34,6 +35,16 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
  * @author Rossen Stoyanchev
  */
 public class UriTemplateTests {
+
+	@Test
+	void emptyPathDoesNotThrowException() {
+		assertThatNoException().isThrownBy(() -> new UriTemplate(""));
+	}
+
+	@Test
+	void nullPathThrowsException() {
+		assertThatIllegalArgumentException().isThrownBy(() -> new UriTemplate(null));
+	}
 
 	@Test
 	public void getVariableNames() throws Exception {
