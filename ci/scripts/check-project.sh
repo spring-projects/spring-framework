17650e074137e95bac57c3de4c778d3e0b47d7d6
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-03-15 09:30:15 +0100

    Move CI to GitHub Actions
    Closes gh-32449

--- ci/scripts/check-project.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-source $(dirname $0)/common.sh
-
-pushd git-repo > /dev/null
-./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false -Porg.gradle.java.installations.fromEnv=JDK17 \
-  -PmainToolchain=${MAIN_TOOLCHAIN} -PtestToolchain=${TEST_TOOLCHAIN} --no-daemon --max-workers=4 check
-popd > /dev/null
