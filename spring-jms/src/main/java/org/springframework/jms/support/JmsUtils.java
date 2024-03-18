0628b479f15dfb3a7d44bde7b86dfec5d9430c11
Juergen Hoeller <juergen.hoeller@broadcom.com>
2024-03-18 16:03:13 +0100

    Propagate JMS IllegalStateException from commit/rollbackIfNecessary
    Closes gh-32473
    
    (cherry picked from commit cd7ba1835cfeb37c0a28f1eba97a9956697b1623)

--- spring-jms/src/main/java/org/springframework/jms/support/JmsUtils.java
+++ spring-jms/src/main/java/org/springframework/jms/support/JmsUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -217,7 +217,7 @@ public abstract class JmsUtils {
 		try {
 			session.commit();
 		}
-		catch (javax.jms.TransactionInProgressException | javax.jms.IllegalStateException ex) {
+		catch (javax.jms.TransactionInProgressException ex) {
 			// Ignore -> can only happen in case of a JTA transaction.
 		}
 	}
@@ -232,7 +232,7 @@ public abstract class JmsUtils {
 		try {
 			session.rollback();
 		}
-		catch (javax.jms.TransactionInProgressException | javax.jms.IllegalStateException ex) {
+		catch (javax.jms.TransactionInProgressException ex) {
 			// Ignore -> can only happen in case of a JTA transaction.
 		}
 	}
