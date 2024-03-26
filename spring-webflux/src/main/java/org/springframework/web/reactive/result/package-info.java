3ebaa37919fd6ce3972669ca351a0ea2bcf2defd
Łukasz Jernaś <lukasz.jernas@allegro.com>
2024-03-26 09:35:37 +0100

    Fix some package summaries being cut off due to incorrect sentence detection.
    Javadoc doesn't seem to like having `e.g. ` as it thinks, the sentence ends there
    which is usually incorrect and results in broken descriptions.
    Reword the doc strings slightly to avoid problematic parts.

--- spring-webflux/src/main/java/org/springframework/web/reactive/result/package-info.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/result/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Support for various programming model styles including the invocation of
- * different types of handlers, e.g. annotated controller vs simple WebHandler,
- * including the handling of handler result values, e.g. @ResponseBody, view
+ * different types of handlers like an annotated controller or a simple WebHandler.
+ * Includes the handling of handler result values, e.g. @ResponseBody, view
  * resolution, and so on.
  */
 @NonNullApi
