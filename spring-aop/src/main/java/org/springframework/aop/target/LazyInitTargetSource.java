4a7c24d90f25bae4df97f06e8fc39ed1b479a3c5
Sébastien Deleuze <sebastien.deleuze@broadcom.com>
2024-04-05 14:11:33 +0200

    Refine null-safety
    See gh-32475

--- spring-aop/src/main/java/org/springframework/aop/target/LazyInitTargetSource.java
+++ spring-aop/src/main/java/org/springframework/aop/target/LazyInitTargetSource.java
@@ -65,7 +65,6 @@ public class LazyInitTargetSource extends AbstractBeanFactoryBasedTargetSource {
 
 
 	@Override
-	@Nullable
 	public synchronized Object getTarget() throws BeansException {
 		if (this.target == null) {
 			this.target = getBeanFactory().getBean(getTargetBeanName());
