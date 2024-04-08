3a54fd12d5d4454506b3da5ca758a8ed7ee23e44
Hyeon Sung Jang <gorae930301@gmail.com>
2024-04-08 13:23:52 +0900

    Improved readability

--- spring-aop/src/main/java/org/springframework/aop/aspectj/AspectJProxyUtils.java
+++ spring-aop/src/main/java/org/springframework/aop/aspectj/AspectJProxyUtils.java
@@ -46,22 +46,22 @@ public abstract class AspectJProxyUtils {
 	 */
 	public static boolean makeAdvisorChainAspectJCapableIfNecessary(List<Advisor> advisors) {
 		// Don't add advisors to an empty list; may indicate that proxying is just not required
-		if (!advisors.isEmpty()) {
-			boolean foundAspectJAdvice = false;
-			for (Advisor advisor : advisors) {
-				// Be careful not to get the Advice without a guard, as this might eagerly
-				// instantiate a non-singleton AspectJ aspect...
-				if (isAspectJAdvice(advisor)) {
-					foundAspectJAdvice = true;
-					break;
-				}
-			}
-			if (foundAspectJAdvice && !advisors.contains(ExposeInvocationInterceptor.ADVISOR)) {
-				advisors.add(0, ExposeInvocationInterceptor.ADVISOR);
-				return true;
+		if (advisors.isEmpty()) {
+			return false;
+		}
+		boolean foundAspectJAdvice = false;
+		for (Advisor advisor : advisors) {
+			// Be careful not to get the Advice without a guard, as this might eagerly
+			// instantiate a non-singleton AspectJ aspect...
+			if (isAspectJAdvice(advisor)) {
+				foundAspectJAdvice = true;
+				break;
 			}
 		}
-		return false;
+		if (foundAspectJAdvice && !advisors.contains(ExposeInvocationInterceptor.ADVISOR)) {
+			advisors.add(0, ExposeInvocationInterceptor.ADVISOR);
+			return true;
+		}
 	}
 
 	/**
