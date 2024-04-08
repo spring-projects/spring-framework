a54a487ccca1e014bb29c6b1a7ad7f06c66b2a1a
Hyeon Sung Jang <gorae930301@gmail.com>
2024-04-08 16:24:41 +0900

    Improved readability

--- spring-aop/src/main/java/org/springframework/aop/config/SpringConfiguredBeanDefinitionParser.java
+++ spring-aop/src/main/java/org/springframework/aop/config/SpringConfiguredBeanDefinitionParser.java
@@ -54,15 +54,15 @@ class SpringConfiguredBeanDefinitionParser implements BeanDefinitionParser {
 	@Override
 	@Nullable
 	public BeanDefinition parse(Element element, ParserContext parserContext) {
-		if (!parserContext.getRegistry().containsBeanDefinition(BEAN_CONFIGURER_ASPECT_BEAN_NAME)) {
-			RootBeanDefinition def = new RootBeanDefinition();
-			def.setBeanClassName(BEAN_CONFIGURER_ASPECT_CLASS_NAME);
-			def.setFactoryMethodName("aspectOf");
-			def.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
-			def.setSource(parserContext.extractSource(element));
-			parserContext.registerBeanComponent(new BeanComponentDefinition(def, BEAN_CONFIGURER_ASPECT_BEAN_NAME));
+		if (parserContext.getRegistry().containsBeanDefinition(BEAN_CONFIGURER_ASPECT_BEAN_NAME)) {
+			return null;
 		}
-		return null;
+		RootBeanDefinition def = new RootBeanDefinition();
+		def.setBeanClassName(BEAN_CONFIGURER_ASPECT_CLASS_NAME);
+		def.setFactoryMethodName("aspectOf");
+		def.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
+		def.setSource(parserContext.extractSource(element));
+		parserContext.registerBeanComponent(new BeanComponentDefinition(def, BEAN_CONFIGURER_ASPECT_BEAN_NAME));
 	}
 
 }
