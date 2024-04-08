9cd64bd7a37bb95ee200216beb97982c42cc6c1a
Hyeon Sung Jang <gorae930301@gmail.com>
2024-04-08 14:14:14 +0900

    Improved readability

--- spring-aop/src/main/java/org/springframework/aop/aspectj/annotation/BeanFactoryAspectInstanceFactory.java
+++ spring-aop/src/main/java/org/springframework/aop/aspectj/annotation/BeanFactoryAspectInstanceFactory.java
@@ -109,10 +109,8 @@ public class BeanFactoryAspectInstanceFactory implements MetadataAwareAspectInst
 			// Rely on singleton semantics provided by the factory -> no local lock.
 			return null;
 		}
-		else {
-			// No singleton guarantees from the factory -> let's lock locally.
-			return this;
-		}
+		// No singleton guarantees from the factory -> let's lock locally.
+		return this;
 	}
 
 	/**
@@ -128,13 +126,13 @@ public class BeanFactoryAspectInstanceFactory implements MetadataAwareAspectInst
 	@Override
 	public int getOrder() {
 		Class<?> type = this.beanFactory.getType(this.name);
-		if (type != null) {
-			if (Ordered.class.isAssignableFrom(type) && this.beanFactory.isSingleton(this.name)) {
-				return ((Ordered) this.beanFactory.getBean(this.name)).getOrder();
-			}
-			return OrderUtils.getOrder(type, Ordered.LOWEST_PRECEDENCE);
+		if (type == null) {
+			return Ordered.LOWEST_PRECEDENCE;
 		}
-		return Ordered.LOWEST_PRECEDENCE;
+		if (Ordered.class.isAssignableFrom(type) && this.beanFactory.isSingleton(this.name)) {
+			return ((Ordered) this.beanFactory.getBean(this.name)).getOrder();
+		}
+		return OrderUtils.getOrder(type, Ordered.LOWEST_PRECEDENCE);
 	}
 
 
