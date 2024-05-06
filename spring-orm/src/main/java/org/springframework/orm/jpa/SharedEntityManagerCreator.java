59a125d06f89ec4f0efe0628fe3a70ab143a5360
Juergen Hoeller <juergen.hoeller@broadcom.com>
2024-05-06 20:09:43 +0200

    Unwrap raw target Query instance in case of proxy mismatch
    Closes gh-32766

--- spring-orm/src/main/java/org/springframework/orm/jpa/SharedEntityManagerCreator.java
+++ spring-orm/src/main/java/org/springframework/orm/jpa/SharedEntityManagerCreator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -393,6 +393,9 @@ public abstract class SharedEntityManagerCreator {
 					}
 					else if (targetClass.isInstance(proxy)) {
 						return proxy;
+					}
+					else {
+						return this.target.unwrap(targetClass);
 					}
 				}
 				case "getOutputParameterValue" -> {
