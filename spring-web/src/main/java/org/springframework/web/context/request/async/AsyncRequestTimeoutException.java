a5a1ef6b30cd7e1115e953ea4b9dabbead02f28e
rstoyanchev <rossen.stoyanchev@broadcom.com>
2024-05-13 11:14:02 +0100

    Use instance field for ProblemDetail in ErrorResponse's
    Closes gh-32644

--- spring-web/src/main/java/org/springframework/web/context/request/async/AsyncRequestTimeoutException.java
+++ spring-web/src/main/java/org/springframework/web/context/request/async/AsyncRequestTimeoutException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,6 +37,9 @@ import org.springframework.web.ErrorResponse;
 @SuppressWarnings("serial")
 public class AsyncRequestTimeoutException extends RuntimeException implements ErrorResponse {
 
+	private final ProblemDetail body = ProblemDetail.forStatus(getStatusCode());
+
+
 	@Override
 	public HttpStatusCode getStatusCode() {
 		return HttpStatus.SERVICE_UNAVAILABLE;
@@ -44,7 +47,7 @@ public class AsyncRequestTimeoutException extends RuntimeException implements Er
 
 	@Override
 	public ProblemDetail getBody() {
-		return ProblemDetail.forStatus(getStatusCode());
+		return this.body;
 	}
 
 }
