spring-projects/spring-framework

commit d185bb1d97e8f2c8eaf4c3bca7b63d128e18e394

Support @⁠TestBean factory methods defined in interfaces
Prior to this commit, @⁠TestBean factory methods were required to be
defined in the test class or one of its superclasses. However, users
may wish to define common factory methods in interfaces that can be
shared easily across multiple test classes simply by implementing the
necessary interface(s).

This commit therefore switches from ReflectionUtils to
MethodIntrospector to find @⁠TestBean factory methods in implemented
interfaces within the type hierarchy of the test class.

Closes gh-32943

spring-test/src/main/java/org/springframework/test/context/bean/override/convention/TestBeanOverrideProcessor.java
@@ -28,6 +28,7 @@ import java.util.Objects;
 import java.util.Set;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.core.MethodIntrospector;
 import org.springframework.core.ResolvableType;
 import org.springframework.lang.Nullable;
 import org.springframework.test.context.TestContextAnnotationUtils;
@@ -88,24 +89,23 @@ class TestBeanOverrideProcessor implements BeanOverrideProcessor {
 				supportedNames.contains(method.getName()) &&
 				methodReturnType.isAssignableFrom(method.getReturnType()));
 
-		List<Method> methods = findMethods(clazz, methodFilter);
+		Set<Method> methods = findMethods(clazz, methodFilter);
 		if (methods.isEmpty() && TestContextAnnotationUtils.searchEnclosingClass(clazz)) {
 			methods = findMethods(clazz.getEnclosingClass(), methodFilter);
 		}
 
-		Assert.state(!methods.isEmpty(), () -> """
+		int methodCount = methods.size();
+		Assert.state(methodCount > 0, () -> """
 				Failed to find a static test bean factory method in %s with return type %s \
 				whose name matches one of the supported candidates %s""".formatted(
 						clazz.getName(), methodReturnType.getName(), supportedNames));
 
-		long nameCount = methods.stream().map(Method::getName).distinct().count();
-		int methodCount = methods.size();
-		Assert.state(nameCount == 1, () -> """
+		Assert.state(methodCount == 1, () -> """
 				Found %d competing static test bean factory methods in %s with return type %s \
 				whose name matches one of the supported candidates %s""".formatted(
 					methodCount, clazz.getName(), methodReturnType.getName(), supportedNames));
 
-		return methods.get(0);
+		return methods.iterator().next();
 	}
 
 	@Override
@@ -138,10 +138,8 @@ class TestBeanOverrideProcessor implements BeanOverrideProcessor {
 	}
 
 
-	private static List<Method> findMethods(Class<?> clazz, MethodFilter methodFilter) {
-		List<Method> methods = new ArrayList<>();
-		ReflectionUtils.doWithMethods(clazz, methods::add, methodFilter);
-		return methods;
+	private static Set<Method> findMethods(Class<?> clazz, MethodFilter methodFilter) {
+		return MethodIntrospector.selectMethods(clazz, methodFilter);
 	}
 
 

spring-test/src/test/java/org/springframework/test/context/bean/override/convention/TestBeanFactory.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.test.context.bean.override.convention;
+
+/**
+ * Shared {@link TestBean @TestBean} factory methods.
+ *
+ * @author Sam Brannen
+ * @since 6.2
+ */
+interface TestBeanFactory {
+
+	public static String createTestMessage() {
+		return "test";
+	}
+
+}

spring-test/src/test/java/org/springframework/test/context/bean/override/convention/TestBeanInterfaceIntegrationTests.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.test.context.bean.override.convention;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * {@link TestBean @TestBean} integration tests for test bean factory methods
+ * defined in implemented interfaces.
+ *
+ * @author Sam Brannen
+ * @since 6.2
+ */
+@SpringJUnitConfig
+public class TestBeanInterfaceIntegrationTests implements TestBeanFactory {
+
+	@TestBean(methodName = "createTestMessage")
+	String message;
+
+
+	@Test
+	void test() {
+		assertThat(message).isEqualTo("test");
+	}
+
+
+	@Configuration
+	static class Config {
+
+		@Bean
+		String message() {
+			return "prod";
+		}
+	}
+
+}

commit cf3171dae021e0e43f638574a5e417b8394a828c

Find unique @⁠TestBean factory methods in class hierarchy
I accidentally introduced a regression in commit d185bb1d97 by no
longer checking the number of unique method names found when searching
for @⁠TestBean factory methods.

This commit reintroduces that check and introduces a proper unit test
in TestBeanOverrideProcessorTests.

It turns out that we already had an integration test that was intended
to check for this scenario; however, that test actually did not test
this scenario due to a copy-and-paste error. Thus, this commit also
updates TestBeanInheritanceIntegrationTests so that
fieldInSupertypeWithPrioritizedFactoryMethodInSubtype() tests what it's
supposed to.

spring-test/src/main/java/org/springframework/test/context/bean/override/convention/TestBeanOverrideProcessor.java
@@ -92,16 +92,16 @@ class TestBeanOverrideProcessor implements BeanOverrideProcessor {
 
 		Set<Method> methods = findMethods(clazz, methodFilter);
 
-		int methodCount = methods.size();
-		Assert.state(methodCount > 0, () -> """
+		Assert.state(!methods.isEmpty(), () -> """
 				Failed to find a static test bean factory method in %s with return type %s \
 				whose name matches one of the supported candidates %s""".formatted(
 						clazz.getName(), methodReturnType.getName(), supportedNames));
 
-		Assert.state(methodCount == 1, () -> """
+		long uniqueMethodNameCount = methods.stream().map(Method::getName).distinct().count();
+		Assert.state(uniqueMethodNameCount == 1, () -> """
 				Found %d competing static test bean factory methods in %s with return type %s \
 				whose name matches one of the supported candidates %s""".formatted(
-					methodCount, clazz.getName(), methodReturnType.getName(), supportedNames));
+						uniqueMethodNameCount, clazz.getName(), methodReturnType.getName(), supportedNames));
 
 		return methods.iterator().next();
 	}

spring-test/src/test/java/org/springframework/test/context/bean/override/convention/TestBeanInheritanceIntegrationTests.java
@@ -61,6 +61,11 @@ public class TestBeanInheritanceIntegrationTests {
 			return new FakePojo("someBeanOverride");
 		}
 
+		// Hides otherBeanTestOverride() defined in AbstractTestBeanIntegrationTestCase.
+		static Pojo otherBeanTestOverride() {
+			return new FakePojo("otherBean in subclass");
+		}
+
 		@Test
 		void fieldInSubtypeWithFactoryMethodInSupertype() {
 			assertThat(ctx.getBean("pojo")).as("applicationContext").hasToString("in superclass");
@@ -75,8 +80,8 @@ public class TestBeanInheritanceIntegrationTests {
 
 		@Test
 		void fieldInSupertypeWithPrioritizedFactoryMethodInSubtype() {
-			assertThat(ctx.getBean("someBean")).as("applicationContext").hasToString("someBeanOverride");
-			assertThat(this.someBean.getValue()).as("injection point").isEqualTo("someBeanOverride");
+			assertThat(ctx.getBean("otherBean")).as("applicationContext").hasToString("otherBean in subclass");
+			assertThat(super.otherBean.getValue()).as("injection point").isEqualTo("otherBean in subclass");
 		}
 
 		@Test

spring-test/src/test/java/org/springframework/test/context/bean/override/convention/TestBeanOverrideProcessorTests.java
@@ -26,6 +26,7 @@ import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.lang.NonNull;
 import org.springframework.test.context.bean.override.convention.TestBeanOverrideProcessor.TestBeanOverrideMetadata;
 import org.springframework.test.context.bean.override.example.ExampleService;
+import org.springframework.util.ReflectionUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -49,6 +50,16 @@ class TestBeanOverrideProcessorTests {
 		Method method = findTestBeanFactoryMethod(clazz, returnType, "example1", "example2", "example3");
 
 		assertThat(method.getName()).isEqualTo("example2");
+	}
+
+	@Test
+	void findTestBeanFactoryMethodFindsLocalMethodWhenSubclassMethodHidesSuperclassMethod() {
+		Class<?> clazz = SubTestCase.class;
+		Class<?> returnType = String.class;
+
+		Method method = findTestBeanFactoryMethod(clazz, returnType, "factory");
+
+		assertThat(method).isEqualTo(ReflectionUtils.findMethod(clazz, "factory"));
 	}
 
 	@Test
@@ -170,6 +181,24 @@ class TestBeanOverrideProcessorTests {
 		public ExampleService b;
 
 		static ExampleService explicit2() {
+			return null;
+		}
+	}
+
+	static class BaseTestCase {
+
+		@TestBean(methodName = "factory")
+		public String field;
+
+		static String factory() {
+			return null;
+		}
+	}
+
+	static class SubTestCase extends BaseTestCase {
+
+		// Hides factory() in superclass.
+		static String factory() {
 			return null;
 		}
 	}