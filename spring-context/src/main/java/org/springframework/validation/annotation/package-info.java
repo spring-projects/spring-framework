9ddea7a24eed6972e6169b0ffde2242fdb0771cb
Johnny Lim <izeye@naver.com>
2024-06-13 00:40:30 +0900

    Fix package description for org.springframework.validation.annotation
    See gh-32532

--- spring-context/src/main/java/org/springframework/validation/annotation/package-info.java
+++ spring-context/src/main/java/org/springframework/validation/annotation/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Support classes for annotation-based constraint evaluation,
- * e.g. using a JSR-303 Bean Validation provider.
+ * for example, using a JSR-303 Bean Validation provider.
  *
  * <p>Provides an extended variant of JSR-303's {@code @Valid},
  * supporting the specification of validation groups.
