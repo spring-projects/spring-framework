000b563e83c827a0c8961f8cb873ef984272c598
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2024-06-22 15:48:45 +0200

    Use consistent formatting for view name in AbstractView

--- spring-webmvc/src/main/java/org/springframework/web/servlet/view/AbstractView.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/view/AbstractView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -495,7 +495,7 @@ public abstract class AbstractView extends WebApplicationObjectSupport implement
 	}
 
 	protected String formatViewName() {
-		return (getBeanName() != null ? "name '" + getBeanName() + "'" : "[" + getClass().getSimpleName() + "]");
+		return (getBeanName() != null ? "name [" + getBeanName() + "]" : "[" + getClass().getSimpleName() + "]");
 	}
 
 }
