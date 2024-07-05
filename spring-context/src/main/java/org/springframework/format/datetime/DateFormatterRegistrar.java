83f7996c79970aab5d9fa5b63c22e7f45f30b6b6
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2024-07-05 14:21:41 +0200

    Polishing

--- spring-context/src/main/java/org/springframework/format/datetime/DateFormatterRegistrar.java
+++ spring-context/src/main/java/org/springframework/format/datetime/DateFormatterRegistrar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ import org.springframework.util.Assert;
 /**
  * Configures basic date formatting for use with Spring, primarily for
  * {@link org.springframework.format.annotation.DateTimeFormat} declarations.
- * Applies to fields of type {@link Date}, {@link Calendar} and {@code long}.
+ * Applies to fields of type {@link Date}, {@link Calendar}, and {@code long}.
  *
  * <p>Designed for direct instantiation but also exposes the static
  * {@link #addDateConverters(ConverterRegistry)} utility method for
