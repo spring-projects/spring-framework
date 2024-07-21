af8dc4446989f337c1c903d80ce97b15e85a434b
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-07-21 11:18:27 +0200

    Polish "Stop referring to STRUCT and ARRAY as they are deprecated"
    See gh-33248

---
+++ framework-docs/src/main/kotlin/org/springframework/docs/dataaccess/jdbc/jdbccomplextypes/SqlTypeValueFactory.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.docs.dataaccess.jdbc.jdbccomplextypes
+
+import oracle.jdbc.driver.OracleConnection
+import org.springframework.jdbc.core.SqlTypeValue
+import org.springframework.jdbc.core.support.AbstractSqlTypeValue
+import java.sql.Connection
+import java.sql.Date
+import java.text.SimpleDateFormat
+
+@Suppress("unused")
+class SqlTypeValueFactory {
+
+	fun createStructSample(): AbstractSqlTypeValue {
+		// tag::struct[]
+		val testItem = TestItem(123L, "A test item",
+			SimpleDateFormat("yyyy-M-d").parse("2010-12-31"))
+
+		val value = object : AbstractSqlTypeValue() {
+			override fun createTypeValue(connection: Connection, sqlType: Int, typeName: String?): Any {
+				val item = arrayOf<Any>(testItem.id, testItem.description,
+					Date(testItem.expirationDate.time))
+				return connection.createStruct(typeName, item)
+			}
+		}
+		// end::struct[]
+		return value
+	}
+
+	fun createOracleArray() : SqlTypeValue {
+		// tag::oracle-array[]
+		val ids = arrayOf(1L, 2L)
+		val value: SqlTypeValue = object : AbstractSqlTypeValue() {
+			override fun createTypeValue(conn: Connection, sqlType: Int, typeName: String?): Any {
+				return conn.unwrap(OracleConnection::class.java).createOracleArray(typeName, ids)
+			}
+		}
+		// end::oracle-array[]
+		return value
+	}
+
+}
