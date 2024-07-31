a2cae4c8f47e8d86cb588e6db91cb70bbf53bdb3
zinzo <zinzoddari@gmail.com>
2024-08-01 00:12:35 +0900

    Improve test coverage of StringUtils
    This commit adds tests for `trimArrayElements()` and
    `delimitedListToStringArray()` in StringUtils.
    
    See gh-33298

--- spring-core/src/test/java/org/springframework/util/StringUtilsTests.java
+++ spring-core/src/test/java/org/springframework/util/StringUtilsTests.java
@@ -783,4 +783,29 @@ class StringUtilsTests {
 		assertThat(StringUtils.truncate(text, 10)).isEqualTo(truncated);
 	}
 
+	@Test
+	void trimArrayElements() {
+		assertThat(StringUtils.trimArrayElements(null)).isNull();
+		assertThat(StringUtils.trimArrayElements(new String[]{})).isEmpty();
+		assertThat(StringUtils.trimArrayElements(new String[]{"", " ", "  ", "   "})).containsExactly("", "", "", "");
+		assertThat(StringUtils.trimArrayElements(new String[]{"\n", "\t ", "\n\t"})).containsExactly("", "", "");
+		assertThat(StringUtils.trimArrayElements(new String[]{"a", "b", "c"})).containsExactly("a", "b", "c");
+		assertThat(StringUtils.trimArrayElements(new String[]{"  a  ", "  b b ", "  cc  "})).containsExactly("a", "b b", "cc");
+		assertThat(StringUtils.trimArrayElements(new String[]{"  a  ", "b", "  c  "})).containsExactly("a", "b", "c");
+		assertThat(StringUtils.trimArrayElements(new String[]{null, "  a  ", null})).containsExactly(null, "a", null);
+	}
+
+	@Test
+	void delimitedListToStringArray() {
+		assertThat(StringUtils.delimitedListToStringArray(null, ",", null)).isEmpty();
+		assertThat(StringUtils.delimitedListToStringArray("a,b,c", null, null)).containsExactly("a,b,c");
+		assertThat(StringUtils.delimitedListToStringArray("abc", "", null)).containsExactly("a", "b", "c");
+		assertThat(StringUtils.delimitedListToStringArray("a,b,c", ",", null)).containsExactly("a", "b", "c");
+		assertThat(StringUtils.delimitedListToStringArray("a,b,c", ",", "a")).containsExactly("", "b", "c");
+		assertThat(StringUtils.delimitedListToStringArray("a,b,c,", ",", null)).containsExactly("a", "b", "c", "");
+		assertThat(StringUtils.delimitedListToStringArray(",a,b,c", ",", null)).containsExactly("", "a", "b", "c");
+		assertThat(StringUtils.delimitedListToStringArray("abc", ",", null)).containsExactly("abc");
+		assertThat(StringUtils.delimitedListToStringArray("a,b,c", ",", ",")).containsExactly("a", "b", "c");
+		assertThat(StringUtils.delimitedListToStringArray("a,,b,,c", ",,", null)).containsExactly("a", "b", "c");
+	}
 }
