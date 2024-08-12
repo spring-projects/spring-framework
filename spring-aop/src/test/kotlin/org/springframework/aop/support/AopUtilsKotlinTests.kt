1911ca728d8827d9a558c0ea6ecca8354434e9ff
Sébastien Deleuze <sebastien.deleuze@broadcom.com>
2024-08-12 17:27:41 +0200

    Support invoking bridged suspending functions in AopUtils
    Closes gh-33045

--- spring-aop/src/test/kotlin/org/springframework/aop/support/AopUtilsKotlinTests.kt
+++ spring-aop/src/test/kotlin/org/springframework/aop/support/AopUtilsKotlinTests.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,19 +34,48 @@ class AopUtilsKotlinTests {
     @Test
     fun `Invoking suspending function should return Mono`() {
         val value = "foo"
-        val method = ReflectionUtils.findMethod(AopUtilsKotlinTests::class.java, "suspendingFunction",
-			String::class.java, Continuation::class.java)!!
+        val method = ReflectionUtils.findMethod(WithoutInterface::class.java, "handle",
+			String::class. java, Continuation::class.java)!!
         val continuation = Continuation<Any>(CoroutineName("test")) { }
-		val result = AopUtils.invokeJoinpointUsingReflection(this, method, arrayOf(value, continuation))
+		val result = AopUtils.invokeJoinpointUsingReflection(WithoutInterface(), method, arrayOf(value, continuation))
         assertThat(result).isInstanceOfSatisfying(Mono::class.java) {
             assertThat(it.block()).isEqualTo(value)
         }
     }
+
+	@Test
+	fun `Invoking suspending function on bridged method should return Mono`() {
+		val value = "foo"
+		val bridgedMethod = ReflectionUtils.findMethod(WithInterface::class.java, "handle", Object::class.java, Continuation::class.java)!!
+		val continuation = Continuation<Any>(CoroutineName("test")) { }
+		val result = AopUtils.invokeJoinpointUsingReflection(WithInterface(), bridgedMethod, arrayOf(value, continuation))
+		assertThat(result).isInstanceOfSatisfying(Mono::class.java) {
+			assertThat(it.block()).isEqualTo(value)
+		}
+	}
 
     @Suppress("unused")
     suspend fun suspendingFunction(value: String): String {
         delay(1)
         return value
     }
+
+	class WithoutInterface {
+		suspend fun handle(value: String): String {
+			delay(1)
+			return value
+		}
+	}
+
+	interface ProxyInterface<T> {
+		suspend fun handle(value: T): T
+	}
+
+	class WithInterface : ProxyInterface<String> {
+		override suspend fun handle(value: String): String {
+			delay(1)
+			return value
+		}
+	}
 
 }
