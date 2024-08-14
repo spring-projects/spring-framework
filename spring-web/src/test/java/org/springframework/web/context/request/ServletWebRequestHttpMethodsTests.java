8d16a50907c11f7e6b407d878a26e84eba08a533
rstoyanchev <rossen.stoyanchev@broadcom.com>
2024-08-14 07:29:50 +0300

    Efficient ETag parsing
    Closes gh-33372

--- spring-web/src/test/java/org/springframework/web/context/request/ServletWebRequestHttpMethodsTests.java
+++ spring-web/src/test/java/org/springframework/web/context/request/ServletWebRequestHttpMethodsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -163,8 +163,8 @@ class ServletWebRequestHttpMethodsTests {
 		assertOkWithETag(etag);
 	}
 
+	// gh-19127
 	@SafeHttpMethodsTest
-		// SPR-14559
 	void ifNoneMatchShouldNotFailForUnquotedETag(String method) {
 		setUpRequest(method);
 		String etag = "\"etagvalue\"";
