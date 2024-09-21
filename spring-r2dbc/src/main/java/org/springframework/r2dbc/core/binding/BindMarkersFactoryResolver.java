c85050eb43c37f084ae5bd31a708d0e2825ea43c
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2024-09-21 10:39:33 +0700

    Consistently use CharSequence.isEmpty() for emptiness checks
    Closes gh-33577

--- spring-r2dbc/src/main/java/org/springframework/r2dbc/core/binding/BindMarkersFactoryResolver.java
+++ spring-r2dbc/src/main/java/org/springframework/r2dbc/core/binding/BindMarkersFactoryResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -153,7 +153,7 @@ public final class BindMarkersFactoryResolver {
 					builder.append(ch);
 				}
 			}
-			if (builder.length() == 0) {
+			if (builder.isEmpty()) {
 				return "";
 			}
 			return "_" + builder.toString();
