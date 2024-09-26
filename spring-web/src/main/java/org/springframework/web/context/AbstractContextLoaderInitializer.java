5326640f796e924d3baeda65d28c83b0d2d98593
Juergen Hoeller <juergen.hoeller@broadcom.com>
2024-09-26 18:31:02 +0200

    Initialize application context with initializer-given ServletContext
    Closes gh-22319

--- spring-web/src/main/java/org/springframework/web/context/AbstractContextLoaderInitializer.java
+++ spring-web/src/main/java/org/springframework/web/context/AbstractContextLoaderInitializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,7 +58,7 @@ public abstract class AbstractContextLoaderInitializer implements WebApplication
 	protected void registerContextLoaderListener(ServletContext servletContext) {
 		WebApplicationContext rootAppContext = createRootApplicationContext();
 		if (rootAppContext != null) {
-			ContextLoaderListener listener = new ContextLoaderListener(rootAppContext);
+			ContextLoaderListener listener = new ContextLoaderListener(rootAppContext, servletContext);
 			listener.setContextInitializers(getRootApplicationContextInitializers());
 			servletContext.addListener(listener);
 		}
