07d281c6d824c6806680b416729908c31943f8c0
Juergen Hoeller <juergen.hoeller@broadcom.com>
2024-09-26 18:31:36 +0200

    Expose public constant for spring.aot.processing system property
    Closes gh-33388

--- spring-context/src/main/java/org/springframework/context/aot/AbstractAotProcessor.java
+++ spring-context/src/main/java/org/springframework/context/aot/AbstractAotProcessor.java
@@ -47,11 +47,11 @@ import org.springframework.util.FileSystemUtils;
 public abstract class AbstractAotProcessor<T> {
 
 	/**
-	 * The name of a system property that is made available when the processor
-	 * runs.
+	 * The name of a system property that is made available when the processor runs.
+	 * @since 6.2
 	 * @see #doProcess()
 	 */
-	private static final String AOT_PROCESSING = "spring.aot.processing";
+	public static final String AOT_PROCESSING = "spring.aot.processing";
 
 	private final Settings settings;
 
@@ -125,6 +125,7 @@ public abstract class AbstractAotProcessor<T> {
 		writer.write(hints);
 	}
 
+
 	/**
 	 * Common settings for AOT processors.
 	 */
@@ -140,7 +141,6 @@ public abstract class AbstractAotProcessor<T> {
 
 		private final String artifactId;
 
-
 		private Settings(Path sourceOutput, Path resourceOutput, Path classOutput, String groupId, String artifactId) {
 			this.sourceOutput = sourceOutput;
 			this.resourceOutput = resourceOutput;
@@ -149,14 +149,12 @@ public abstract class AbstractAotProcessor<T> {
 			this.artifactId = artifactId;
 		}
 
-
 		/**
 		 * Create a new {@link Builder} for {@link Settings}.
 		 */
 		public static Builder builder() {
 			return new Builder();
 		}
-
 
 		/**
 		 * Get the output directory for generated sources.
@@ -214,11 +212,9 @@ public abstract class AbstractAotProcessor<T> {
 			@Nullable
 			private String artifactId;
 
-
 			private Builder() {
 				// internal constructor
 			}
-
 
 			/**
 			 * Set the output directory for generated sources.
