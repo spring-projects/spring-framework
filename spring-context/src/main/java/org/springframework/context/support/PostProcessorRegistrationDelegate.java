6da32b4631c83f64d4bb86a0d231428dd1cf5de7
Asi Bross <11426802+asibross@users.noreply.github.com>
2024-09-30 11:01:03 -0700

    Adapt Javadoc note about log level of BeanPostProcessorChecker
    See gh-33617

--- spring-context/src/main/java/org/springframework/context/support/PostProcessorRegistrationDelegate.java
+++ spring-context/src/main/java/org/springframework/context/support/PostProcessorRegistrationDelegate.java
@@ -226,7 +226,7 @@ final class PostProcessorRegistrationDelegate {
 
 		String[] postProcessorNames = beanFactory.getBeanNamesForType(BeanPostProcessor.class, true, false);
 
-		// Register BeanPostProcessorChecker that logs an info message when
+		// Register BeanPostProcessorChecker that logs a warn message when
 		// a bean is created during BeanPostProcessor instantiation, i.e. when
 		// a bean is not eligible for getting processed by all BeanPostProcessors.
 		int beanProcessorTargetCount = beanFactory.getBeanPostProcessorCount() + 1 + postProcessorNames.length;
