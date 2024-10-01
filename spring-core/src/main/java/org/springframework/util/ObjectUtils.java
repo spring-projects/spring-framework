de82e9f8368e42f7e1ad4138ef8229188c044489
nomoreFt <hyunwoo.kim1108@gmail.com>
2024-10-01 18:39:58 +0900

    Improve ObjectUtils#nullSafeEquals method for readability and performance

--- spring-core/src/main/java/org/springframework/util/ObjectUtils.java
+++ spring-core/src/main/java/org/springframework/util/ObjectUtils.java
@@ -339,13 +339,10 @@ public abstract class ObjectUtils {
 		if (o1 == null || o2 == null) {
 			return false;
 		}
-		if (o1.equals(o2)) {
-			return true;
-		}
 		if (o1.getClass().isArray() && o2.getClass().isArray()) {
 			return arrayEquals(o1, o2);
 		}
-		return false;
+		return o1.equals(o2);
 	}
 
 	/**
