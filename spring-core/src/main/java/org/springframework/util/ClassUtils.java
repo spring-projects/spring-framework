0d126aca7e03557f728be07e111b975719ab3c1e
SungbinYang <ysb05222@naver.com>
2024-10-04 15:06:33 +0900

    Refactor: Replace hardcoded path separator with PATH_SEPARATOR constant
    Replaced the hardcoded '/' path separator with the `PATH_SEPARATOR` constant to improve code readability and maintainability. This change ensures consistency across the codebase when handling path separators.

--- spring-core/src/main/java/org/springframework/util/ClassUtils.java
+++ spring-core/src/main/java/org/springframework/util/ClassUtils.java
@@ -64,6 +64,7 @@ import org.springframework.lang.Nullable;
  * @author Rob Harrop
  * @author Sam Brannen
  * @author Sebastien Deleuze
+ * @author Sungbin Yang
  * @since 1.1
  * @see TypeUtils
  * @see ReflectionUtils
@@ -694,7 +695,7 @@ public abstract class ClassUtils {
 	public static String addResourcePathToPackagePath(Class<?> clazz, String resourceName) {
 		Assert.notNull(resourceName, "Resource name must not be null");
 		if (!resourceName.startsWith("/")) {
-			return classPackageAsResourcePath(clazz) + '/' + resourceName;
+			return classPackageAsResourcePath(clazz) + PATH_SEPARATOR + resourceName;
 		}
 		return classPackageAsResourcePath(clazz) + resourceName;
 	}
