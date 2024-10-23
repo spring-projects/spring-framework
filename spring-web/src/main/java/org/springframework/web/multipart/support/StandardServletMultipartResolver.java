4a81f2c904ef72ec80a043cb9f9b1d9ab2a390c5
Brian Clozel <brian.clozel@broadcom.com>
2024-10-23 09:24:30 +0200

    Remove HTTP parts workaround for Resin
    A workaround was added for the Resin Servlet container in gh-13937.
    This avoids attempting to delete parts that are not named, because the
    `part.delete()` call would fail for non-file entries. This can be
    problematic for files that are unnamed as they might not be removed by
    the Framework.
    
    This commit removes this workaround as Resin is not supported anymore.
    
    Fixes gh-33511

--- spring-web/src/main/java/org/springframework/web/multipart/support/StandardServletMultipartResolver.java
+++ spring-web/src/main/java/org/springframework/web/multipart/support/StandardServletMultipartResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -116,13 +116,10 @@ public class StandardServletMultipartResolver implements MultipartResolver {
 	public void cleanupMultipart(MultipartHttpServletRequest request) {
 		if (!(request instanceof AbstractMultipartHttpServletRequest abstractMultipartHttpServletRequest) ||
 				abstractMultipartHttpServletRequest.isResolved()) {
-			// To be on the safe side: explicitly delete the parts,
-			// but only actual file parts (for Resin compatibility)
+
 			try {
 				for (Part part : request.getParts()) {
-					if (request.getFile(part.getName()) != null) {
-						part.delete();
-					}
+					part.delete();
 				}
 			}
 			catch (Throwable ex) {
