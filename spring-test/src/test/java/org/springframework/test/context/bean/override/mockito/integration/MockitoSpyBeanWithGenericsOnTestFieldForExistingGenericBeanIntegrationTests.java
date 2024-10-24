0221471265d3ea50beeb23a9aa5f49096afb5380
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2024-10-24 15:45:28 +0200

    Verify @⁠Mockito[Spy]Bean can mock/spy parameterized types
    See gh-33742

---
+++ spring-test/src/test/java/org/springframework/test/context/bean/override/mockito/integration/MockitoSpyBeanWithGenericsOnTestFieldForExistingGenericBeanIntegrationTests.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.test.context.bean.override.mockito.integration;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.example.ExampleGenericService;
+import org.springframework.test.context.bean.override.example.ExampleGenericServiceCaller;
+import org.springframework.test.context.bean.override.example.IntegerExampleGenericService;
+import org.springframework.test.context.bean.override.example.StringExampleGenericService;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.then;
+
+/**
+ * Tests that {@link MockitoSpyBean @MockitoSpyBean} on a field with generics can
+ * be used to replace an existing bean with matching generics.
+ *
+ * @author Phillip Webb
+ * @author Sam Brannen
+ * @since 6.2
+ * @see MockitoBeanWithGenericsOnTestFieldForNewBeanIntegrationTests
+ */
+@SpringJUnitConfig
+class MockitoSpyBeanWithGenericsOnTestFieldForExistingGenericBeanIntegrationTests {
+
+	@MockitoSpyBean
+	ExampleGenericService<String> service;
+
+	@Autowired
+	ExampleGenericServiceCaller caller;
+
+
+	@Test
+	void testSpying() {
+		assertThat(caller.sayGreeting()).isEqualTo("I say Enigma 123");
+		then(service).should().greeting();
+	}
+
+
+	@Configuration(proxyBeanMethods = false)
+	@Import({ ExampleGenericServiceCaller.class, IntegerExampleGenericService.class })
+	static class SpyBeanOnTestFieldForExistingBeanConfig {
+
+		@Bean
+		ExampleGenericService<String> simpleExampleStringGenericService() {
+			// In order to trigger the issue, we need a method signature that returns the
+			// generic type instead of the actual implementation class.
+			return new StringExampleGenericService("Enigma");
+		}
+
+	}
+
+}
