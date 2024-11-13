0a5bd89129182ed03c6e912c8c078744e210e782
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2024-11-13 12:31:27 +0100

    Align JettyByteBufferIterator implementations
    Both are now static nested classes.

--- spring-webflux/src/main/java/org/springframework/web/reactive/socket/adapter/JettyWebSocketHandlerAdapter.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/socket/adapter/JettyWebSocketHandlerAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -322,7 +322,16 @@ public class JettyWebSocketHandlerAdapter {
 		}
 
 
-		private record JettyByteBufferIterator(ByteBufferIterator delegate) implements ByteBufferIterator {
+		private static class JettyByteBufferIterator implements ByteBufferIterator {
+
+			private final ByteBufferIterator delegate;
+
+
+			JettyByteBufferIterator(ByteBufferIterator delegate) {
+				Assert.notNull(delegate, "Delegate must not be null");
+
+				this.delegate = delegate;
+			}
 
 			@Override
 			public void close() {
