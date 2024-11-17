033463b5e5741b67809503bcd91c5949dee460be
KNU-K <knukang334@gmail.com>
2024-11-17 21:50:27 +0900

    fix

--- spring-core/src/main/java/org/springframework/util/FastByteArrayOutputStream.java
+++ spring-core/src/main/java/org/springframework/util/FastByteArrayOutputStream.java
@@ -503,9 +503,11 @@ public class FastByteArrayOutputStream extends OutputStream {
 			if (this.currentBuffer == null) {
 				// This stream doesn't have any data in it...
 				return;
-			}else if(len == 0){
+			}
+			else if(len == 0){
 				return;
-			}else if (len < 0) {
+			}
+			else if (len < 0) {
 				throw new IllegalArgumentException("len must be 0 or greater: " + len);
 			}
 			else {
