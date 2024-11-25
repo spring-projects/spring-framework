8eb24458f82cf3c1f25bcf52a451317f1fc99950
Bao.Ngo <bao.ngo@aeris.net>
2024-11-25 21:08:43 +0700

    Use proper return type in AsyncExecutionInterceptor
    See gh-33957

---
+++ spring-aop/src/test/java/org/springframework/aop/interceptor/AsyncExecutionInterceptorTests.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.aop.interceptor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import org.springframework.core.task.AsyncTaskExecutor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+
+/**
+ * Tests for {@link AsyncExecutionInterceptor}.
+ *
+ * @author Bao Ngo
+ * @since 7.0
+ */
+class AsyncExecutionInterceptorTests {
+
+	@Test
+	public void testInvokeOnInterface() throws Throwable {
+		AsyncExecutionInterceptor interceptor = spy( new AsyncExecutionInterceptor( null ) );
+		Impl impl = new Impl();
+		ArgumentCaptor<Class<?>> classArgumentCaptor = ArgumentCaptor.forClass( Class.class );
+		MethodInvocation mi = mock();
+		given( mi.getThis() ).willReturn( impl );
+		given( mi.getMethod() ).willReturn( I.class.getMethod( "doSomeThing" ) );
+		interceptor.invoke( mi );
+		verify( interceptor ).doSubmit(
+				any( Callable.class ),
+				any( AsyncTaskExecutor.class ),
+				classArgumentCaptor.capture()
+		);
+		assertThat( classArgumentCaptor.getValue() ).isEqualTo( Future.class );
+	}
+
+
+	private interface I<O> {
+		O doSomeThing();
+	}
+
+	private static final class Impl implements I<Future<Void>> {
+		@Override
+		public Future<Void> doSomeThing() {
+			return CompletableFuture.runAsync( () -> {
+			} );
+		}
+	}
+}
