spring-projects/spring-framework

commit 6c2cba5d8ad29657a5164dd1ad369064f39660cf

Polish bean override internals

spring-test/src/main/java/org/springframework/test/context/bean/override/mockito/MockitoBeans.java
@@ -17,16 +17,18 @@
 package org.springframework.test.context.bean.override.mockito;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+
+import org.mockito.Mockito;
 
 /**
  * Beans created using Mockito.
  *
  * @author Andy Wilkinson
+ * @author Sam Brannen
  * @since 6.2
  */
-class MockitoBeans implements Iterable<Object> {
+class MockitoBeans {
 
 	private final List<Object> beans = new ArrayList<>();
 
@@ -35,9 +37,8 @@ class MockitoBeans implements Iterable<Object> {
 		this.beans.add(bean);
 	}
 
-	@Override
-	public Iterator<Object> iterator() {
-		return this.beans.iterator();
+	void resetAll() {
+		this.beans.forEach(Mockito::reset);
 	}
 
 }

spring-test/src/main/java/org/springframework/test/context/bean/override/mockito/MockitoResetTestExecutionListener.java
@@ -87,12 +87,7 @@ public class MockitoResetTestExecutionListener extends AbstractTestExecutionList
 			}
 		}
 		try {
-			MockitoBeans mockedBeans = beanFactory.getBean(MockitoBeans.class);
-			for (Object mockedBean : mockedBeans) {
-				if (reset.equals(MockReset.get(mockedBean))) {
-					Mockito.reset(mockedBean);
-				}
-			}
+			beanFactory.getBean(MockitoBeans.class).resetAll();
 		}
 		catch (NoSuchBeanDefinitionException ex) {
 			// Continue

spring-test/src/main/java/org/springframework/test/context/bean/override/mockito/MockitoBeanOverrideMetadata.java
@@ -35,7 +35,6 @@ import org.springframework.test.context.bean.override.BeanOverrideStrategy;
 import org.springframework.test.context.bean.override.OverrideMetadata;
 import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
-import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 /**
@@ -43,13 +42,14 @@ import org.springframework.util.StringUtils;
  *
  * @author Phillip Webb
  * @author Stephane Nicoll
+ * @author Sam Brannen
  * @since 6.2
  */
 class MockitoBeanOverrideMetadata extends MockitoOverrideMetadata {
 
 	private final Set<Class<?>> extraInterfaces;
 
-	private final Answers answer;
+	private final Answers answers;
 
 	private final boolean serializable;
 
@@ -59,21 +59,22 @@ class MockitoBeanOverrideMetadata extends MockitoOverrideMetadata {
 				annotation.reset(), annotation.extraInterfaces(), annotation.answers(), annotation.serializable());
 	}
 
-	MockitoBeanOverrideMetadata(Field field, ResolvableType typeToMock, @Nullable String beanName, MockReset reset,
-			Class<?>[] extraInterfaces, @Nullable Answers answer, boolean serializable) {
+	private MockitoBeanOverrideMetadata(Field field, ResolvableType typeToMock, @Nullable String beanName, MockReset reset,
+			Class<?>[] extraInterfaces, @Nullable Answers answers, boolean serializable) {
 
 		super(field, typeToMock, beanName, BeanOverrideStrategy.REPLACE_OR_CREATE_DEFINITION, reset, false);
 		Assert.notNull(typeToMock, "'typeToMock' must not be null");
 		this.extraInterfaces = asClassSet(extraInterfaces);
-		this.answer = (answer != null) ? answer : Answers.RETURNS_DEFAULTS;
+		this.answers = (answers != null ? answers : Answers.RETURNS_DEFAULTS);
 		this.serializable = serializable;
 	}
 
-	private static Set<Class<?>> asClassSet(@Nullable Class<?>[] classes) {
-		Set<Class<?>> classSet = new LinkedHashSet<>();
-		if (classes != null) {
-			classSet.addAll(Arrays.asList(classes));
+
+	private static Set<Class<?>> asClassSet(Class<?>[] classes) {
+		if (classes.length == 0) {
+			return Collections.emptySet();
 		}
+		Set<Class<?>> classSet = new LinkedHashSet<>(Arrays.asList(classes));
 		return Collections.unmodifiableSet(classSet);
 	}
 
@@ -90,12 +91,12 @@ class MockitoBeanOverrideMetadata extends MockitoOverrideMetadata {
 	 * Return the {@link Answers}.
 	 * @return the answers mode
 	 */
-	Answers getAnswer() {
-		return this.answer;
+	Answers getAnswers() {
+		return this.answers;
 	}
 
 	/**
-	 * Return if the mock is serializable.
+	 * Determine if the mock is serializable.
 	 * @return {@code true} if the mock is serializable
 	 */
 	boolean isSerializable() {
@@ -108,7 +109,7 @@ class MockitoBeanOverrideMetadata extends MockitoOverrideMetadata {
 	}
 
 	@SuppressWarnings("unchecked")
-	<T> T createMock(String name) {
+	private <T> T createMock(String name) {
 		MockSettings settings = MockReset.withSettings(getReset());
 		if (StringUtils.hasLength(name)) {
 			settings.name(name);
@@ -116,7 +117,7 @@ class MockitoBeanOverrideMetadata extends MockitoOverrideMetadata {
 		if (!this.extraInterfaces.isEmpty()) {
 			settings.extraInterfaces(ClassUtils.toClassArray(this.extraInterfaces));
 		}
-		settings.defaultAnswer(this.answer);
+		settings.defaultAnswer(this.answers);
 		if (this.serializable) {
 			settings.serializable();
 		}
@@ -132,27 +133,26 @@ class MockitoBeanOverrideMetadata extends MockitoOverrideMetadata {
 		if (other == null || other.getClass() != getClass()) {
 			return false;
 		}
-		MockitoBeanOverrideMetadata that = (MockitoBeanOverrideMetadata) other;
-		boolean result = super.equals(that);
-		result = result && ObjectUtils.nullSafeEquals(this.extraInterfaces, that.extraInterfaces);
-		result = result && ObjectUtils.nullSafeEquals(this.answer, that.answer);
-		result = result && this.serializable == that.serializable;
-		return result;
+		return (other instanceof MockitoBeanOverrideMetadata that && super.equals(that) &&
+				(this.serializable == that.serializable) && (this.answers == that.answers) &&
+				Objects.equals(this.extraInterfaces, that.extraInterfaces));
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(this.extraInterfaces, this.answer, this.serializable) + super.hashCode();
+		return super.hashCode() + Objects.hash(this.extraInterfaces, this.answers, this.serializable);
 	}
 
 	@Override
 	public String toString() {
 		return new ToStringCreator(this)
+				.append("field", getField())
 				.append("beanType", getBeanType())
 				.append("beanName", getBeanName())
+				.append("strategy", getStrategy())
 				.append("reset", getReset())
 				.append("extraInterfaces", getExtraInterfaces())
-				.append("answer", getAnswer())
+				.append("answers", getAnswers())
 				.append("serializable", isSerializable())
 				.toString();
 	}

spring-test/src/main/java/org/springframework/test/context/bean/override/mockito/MockitoOverrideMetadata.java
@@ -22,16 +22,17 @@ import java.util.Objects;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.SingletonBeanRegistry;
 import org.springframework.core.ResolvableType;
+import org.springframework.core.style.ToStringCreator;
 import org.springframework.lang.Nullable;
 import org.springframework.test.context.bean.override.BeanOverrideStrategy;
 import org.springframework.test.context.bean.override.OverrideMetadata;
-import org.springframework.util.ObjectUtils;
 
 /**
  * Base {@link OverrideMetadata} implementation for Mockito.
  *
  * @author Phillip Webb
  * @author Stephane Nicoll
+ * @author Sam Brannen
  * @since 6.2
  */
 abstract class MockitoOverrideMetadata extends OverrideMetadata {
@@ -45,7 +46,7 @@ abstract class MockitoOverrideMetadata extends OverrideMetadata {
 			BeanOverrideStrategy strategy, @Nullable MockReset reset, boolean proxyTargetAware) {
 
 		super(field, beanType, beanName, strategy);
-		this.reset = (reset != null) ? reset : MockReset.AFTER;
+		this.reset = (reset != null ? reset : MockReset.AFTER);
 		this.proxyTargetAware = proxyTargetAware;
 	}
 
@@ -73,10 +74,9 @@ abstract class MockitoOverrideMetadata extends OverrideMetadata {
 			tracker = (MockitoBeans) trackingBeanRegistry.getSingleton(MockitoBeans.class.getName());
 		}
 		catch (NoSuchBeanDefinitionException ignored) {
-
 		}
 		if (tracker == null) {
-			tracker= new MockitoBeans();
+			tracker = new MockitoBeans();
 			trackingBeanRegistry.registerSingleton(MockitoBeans.class.getName(), tracker);
 		}
 		tracker.add(mock);
@@ -87,19 +87,25 @@ abstract class MockitoOverrideMetadata extends OverrideMetadata {
 		if (other == this) {
 			return true;
 		}
-		if (other == null || !getClass().isAssignableFrom(other.getClass())) {
-			return false;
-		}
-		MockitoOverrideMetadata that = (MockitoOverrideMetadata) other;
-		boolean result = super.equals(that);
-		result = result && ObjectUtils.nullSafeEquals(this.reset, that.reset);
-		result = result && ObjectUtils.nullSafeEquals(this.proxyTargetAware, that.proxyTargetAware);
-		return result;
+		return (other instanceof MockitoOverrideMetadata that && super.equals(that) &&
+				(this.reset == that.reset) && (this.proxyTargetAware == that.proxyTargetAware));
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(this.reset, this.proxyTargetAware) + super.hashCode();
+		return super.hashCode() + Objects.hash(this.reset, this.proxyTargetAware);
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringCreator(this)
+				.append("field", getField())
+				.append("beanType", getBeanType())
+				.append("beanName", getBeanName())
+				.append("strategy", getStrategy())
+				.append("reset", getReset())
+				.append("proxyTargetAware", isProxyTargetAware())
+				.toString();
 	}
 
 }

spring-test/src/main/java/org/springframework/test/context/bean/override/mockito/MockitoSpyBeanOverrideMetadata.java
@@ -27,7 +27,6 @@ import org.mockito.listeners.VerificationStartedListener;
 
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.core.ResolvableType;
-import org.springframework.core.style.ToStringCreator;
 import org.springframework.lang.Nullable;
 import org.springframework.test.context.bean.override.BeanOverrideStrategy;
 import org.springframework.test.context.bean.override.OverrideMetadata;
@@ -68,8 +67,7 @@ class MockitoSpyBeanOverrideMetadata extends MockitoOverrideMetadata {
 	}
 
 	@SuppressWarnings("unchecked")
-	<T> T createSpy(String name, Object instance) {
-		Assert.notNull(instance, "Instance must not be null");
+	private <T> T createSpy(String name, Object instance) {
 		Class<?> resolvedTypeToOverride = getBeanType().resolve();
 		Assert.notNull(resolvedTypeToOverride, "Failed to resolve type to override");
 		Assert.isInstanceOf(resolvedTypeToOverride, instance);
@@ -94,15 +92,6 @@ class MockitoSpyBeanOverrideMetadata extends MockitoOverrideMetadata {
 			toSpy = instance.getClass();
 		}
 		return (T) Mockito.mock(toSpy, settings);
-	}
-
-	@Override
-	public String toString() {
-		return new ToStringCreator(this)
-				.append("beanName", getBeanName())
-				.append("beanType", getBeanType())
-				.append("reset", getReset())
-				.toString();
 	}
 
 

spring-test/src/test/java/org/springframework/test/context/bean/override/mockito/MockitoBeanForByNameLookupIntegrationTests.java
@@ -56,22 +56,14 @@ public class MockitoBeanForByNameLookupIntegrationTests {
 
 
 	@Test
-	void fieldHasOverride(ApplicationContext ctx) {
+	void fieldAndRenamedFieldHaveSameOverride(ApplicationContext ctx) {
 		assertThat(ctx.getBean("field"))
 				.isInstanceOf(ExampleService.class)
 				.satisfies(o -> assertThat(Mockito.mockingDetails(o).isMock()).as("isMock").isTrue())
-				.isSameAs(this.field);
-
-		assertThat(this.field.greeting()).as("mocked greeting").isNull();
-	}
-
-	@Test
-	void renamedFieldHasOverride(ApplicationContext ctx) {
-		assertThat(ctx.getBean("field"))
-				.isInstanceOf(ExampleService.class)
-				.satisfies(o -> assertThat(Mockito.mockingDetails(o).isMock()).as("isMock").isTrue())
+				.isSameAs(this.field)
 				.isSameAs(this.renamed1);
 
+		assertThat(this.field.greeting()).as("mocked greeting").isNull();
 		assertThat(this.renamed1.greeting()).as("mocked greeting").isNull();
 	}
 
@@ -91,18 +83,11 @@ public class MockitoBeanForByNameLookupIntegrationTests {
 	public class MockitoBeanNestedTests {
 
 		@Test
-		void fieldHasOverride(ApplicationContext ctx) {
+		void fieldAndRenamedFieldHaveSameOverride(ApplicationContext ctx) {
 			assertThat(ctx.getBean("nestedField"))
 					.isInstanceOf(ExampleService.class)
 					.satisfies(o -> assertThat(Mockito.mockingDetails(o).isMock()).as("isMock").isTrue())
-					.isSameAs(nestedField);
-		}
-
-		@Test
-		void renamedFieldHasOverride(ApplicationContext ctx) {
-			assertThat(ctx.getBean("nestedField"))
-					.isInstanceOf(ExampleService.class)
-					.satisfies(o -> assertThat(Mockito.mockingDetails(o).isMock()).as("isMock").isTrue())
+					.isSameAs(nestedField)
 					.isSameAs(renamed2);
 		}
 

commit 0088b9c7f8abad2ec558b583087a2f6d96689c0b

Honor MockReset strategy for @⁠MockitoBean and @⁠MockitoSpyBean
Commit 6c2cba5d8a introduced a regression by inadvertently removing the
MockReset strategy comparison when resetting @⁠MockitoBean and
@⁠MockitoSpyBean mocks.

This commit reinstates the MockReset strategy check and introduces
tests for this feature.

Closes gh-33941

spring-test/src/main/java/org/springframework/test/context/bean/override/mockito/MockReset.java
@@ -99,9 +99,10 @@ public enum MockReset {
 	}
 
 	/**
-	 * Get the {@link MockReset} associated with the given mock.
-	 * @param mock the source mock
-	 * @return the reset type (never {@code null})
+	 * Get the {@link MockReset} strategy associated with the given mock.
+	 * @param mock the mock
+	 * @return the reset strategy for the given mock, or {@link MockReset#NONE}
+	 * if no strategy is associated with the given mock
 	 */
 	static MockReset get(Object mock) {
 		MockingDetails mockingDetails = Mockito.mockingDetails(mock);

spring-test/src/main/java/org/springframework/test/context/bean/override/mockito/MockitoBeans.java
@@ -37,8 +37,18 @@ class MockitoBeans {
 		this.beans.add(bean);
 	}
 
-	void resetAll() {
-		this.beans.forEach(Mockito::reset);
+	/**
+	 * Reset all Mockito beans configured with the supplied {@link MockReset} strategy.
+	 * <p>No mocks will be reset if the supplied strategy is {@link MockReset#NONE}.
+	 */
+	void resetAll(MockReset reset) {
+		if (reset != MockReset.NONE) {
+			for (Object bean : this.beans) {
+				if (reset == MockReset.get(bean)) {
+					Mockito.reset(bean);
+				}
+			}
+		}
 	}
 
 }

spring-test/src/main/java/org/springframework/test/context/bean/override/mockito/MockitoResetTestExecutionListener.java
@@ -116,7 +116,7 @@ public class MockitoResetTestExecutionListener extends AbstractTestExecutionList
 			}
 		}
 		try {
-			beanFactory.getBean(MockitoBeans.class).resetAll();
+			beanFactory.getBean(MockitoBeans.class).resetAll(reset);
 		}
 		catch (NoSuchBeanDefinitionException ex) {
 			// Continue

spring-test/src/test/java/org/springframework/test/context/bean/override/mockito/MockResetStrategiesIntegrationTests.java
@@ -1,0 +1,133 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.test.context.bean.override.mockito;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import org.springframework.test.context.bean.override.mockito.MockResetStrategiesIntegrationTests.MockVerificationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+/**
+ * Integration tests for {@link MockitoBean @MockitoBean} fields with different
+ * {@link MockReset} strategies.
+ *
+ * @author Sam Brannen
+ * @since 6.2.1
+ * @see MockitoResetTestExecutionListenerWithoutMockitoAnnotationsIntegrationTests
+ * @see MockitoResetTestExecutionListenerWithMockitoBeanIntegrationTests
+ */
+// The MockVerificationExtension MUST be registered before the SpringExtension.
+@ExtendWith(MockVerificationExtension.class)
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class MockResetStrategiesIntegrationTests {
+
+	static PuzzleService puzzleServiceNoneStaticReference;
+	static PuzzleService puzzleServiceBeforeStaticReference;
+	static PuzzleService puzzleServiceAfterStaticReference;
+
+
+	@MockitoBean(name = "puzzleServiceNone", reset = MockReset.NONE)
+	PuzzleService puzzleServiceNone;
+
+	@MockitoBean(name = "puzzleServiceBefore", reset = MockReset.BEFORE)
+	PuzzleService puzzleServiceBefore;
+
+	@MockitoBean(name = "puzzleServiceAfter", reset = MockReset.AFTER)
+	PuzzleService puzzleServiceAfter;
+
+
+	@AfterEach
+	void trackStaticReferences() {
+		puzzleServiceNoneStaticReference = this.puzzleServiceNone;
+		puzzleServiceBeforeStaticReference = this.puzzleServiceBefore;
+		puzzleServiceAfterStaticReference = this.puzzleServiceAfter;
+	}
+
+	@AfterAll
+	static void releaseStaticReferences() {
+		puzzleServiceNoneStaticReference = null;
+		puzzleServiceBeforeStaticReference = null;
+		puzzleServiceAfterStaticReference = null;
+	}
+
+
+	@Test
+	void test001(TestInfo testInfo) {
+		assertThat(puzzleServiceNone.getAnswer()).isNull();
+		assertThat(puzzleServiceBefore.getAnswer()).isNull();
+		assertThat(puzzleServiceAfter.getAnswer()).isNull();
+
+		stubAndTestMocks(testInfo);
+	}
+
+	@Test
+	void test002(TestInfo testInfo) {
+		// Should not have been reset.
+		assertThat(puzzleServiceNone.getAnswer()).isEqualTo("none - test001");
+
+		// Should have been reset.
+		assertThat(puzzleServiceBefore.getAnswer()).isNull();
+		assertThat(puzzleServiceAfter.getAnswer()).isNull();
+
+		stubAndTestMocks(testInfo);
+	}
+
+	private void stubAndTestMocks(TestInfo testInfo) {
+		String name = testInfo.getTestMethod().get().getName();
+		given(puzzleServiceNone.getAnswer()).willReturn("none - " + name);
+		assertThat(puzzleServiceNone.getAnswer()).isEqualTo("none - " + name);
+
+		given(puzzleServiceBefore.getAnswer()).willReturn("before - " + name);
+		assertThat(puzzleServiceBefore.getAnswer()).isEqualTo("before - " + name);
+
+		given(puzzleServiceAfter.getAnswer()).willReturn("after - " + name);
+		assertThat(puzzleServiceAfter.getAnswer()).isEqualTo("after - " + name);
+	}
+
+	interface PuzzleService {
+
+		String getAnswer();
+	}
+
+	static class MockVerificationExtension implements AfterEachCallback {
+
+		@Override
+		public void afterEach(ExtensionContext context) throws Exception {
+			String name = context.getRequiredTestMethod().getName();
+
+			// Should not have been reset.
+			assertThat(puzzleServiceNoneStaticReference.getAnswer()).as("puzzleServiceNone").isEqualTo("none - " + name);
+			assertThat(puzzleServiceBeforeStaticReference.getAnswer()).as("puzzleServiceBefore").isEqualTo("before - " + name);
+
+			// Should have been reset.
+			assertThat(puzzleServiceAfterStaticReference.getAnswer()).as("puzzleServiceAfter").isNull();
+		}
+	}
+
+}

spring-test/src/test/java/org/springframework/test/context/bean/override/mockito/MockitoResetTestExecutionListenerWithMockitoBeanIntegrationTests.java
@@ -28,6 +28,7 @@ import static org.mockito.BDDMockito.given;
  * @author Sam Brannen
  * @since 6.2
  * @see MockitoResetTestExecutionListenerWithoutMockitoAnnotationsIntegrationTests
+ * @see MockResetStrategiesIntegrationTests
  */
 class MockitoResetTestExecutionListenerWithMockitoBeanIntegrationTests
 		extends MockitoResetTestExecutionListenerWithoutMockitoAnnotationsIntegrationTests {

spring-test/src/test/java/org/springframework/test/context/bean/override/mockito/MockitoResetTestExecutionListenerWithoutMockitoAnnotationsIntegrationTests.java
@@ -42,6 +42,7 @@ import static org.mockito.Mockito.mock;
  * @author Sam Brannen
  * @since 6.2
  * @see MockitoResetTestExecutionListenerWithMockitoBeanIntegrationTests
+ * @see MockResetStrategiesIntegrationTests
  */
 @SpringJUnitConfig
 @TestMethodOrder(MethodOrderer.MethodName.class)

spring-test/src/test/java/org/springframework/test/context/bean/override/mockito/integration/MockitoBeanUsedDuringApplicationContextRefreshIntegrationTests.java
@@ -1,0 +1,68 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.test.context.bean.override.mockito.integration;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.integration.MockitoBeanUsedDuringApplicationContextRefreshIntegrationTests.ContextRefreshedEventListener;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+
+/**
+ * Integration tests for {@link MockitoBean @MockitoBean} used during
+ * {@code ApplicationContext} refresh.
+ *
+ * @author Sam Brannen
+ * @author Yanming Zhou
+ * @since 6.2.1
+ */
+@SpringJUnitConfig(ContextRefreshedEventListener.class)
+class MockitoBeanUsedDuringApplicationContextRefreshIntegrationTests {
+
+	@MockitoBean
+	ContextRefreshedEventProcessor eventProcessor;
+
+
+	@Test
+	void test() {
+		// Ensure that the mock was invoked during ApplicationContext refresh
+		// and has not been reset in the interim.
+		then(eventProcessor).should().process(any(ContextRefreshedEvent.class));
+	}
+
+
+	interface ContextRefreshedEventProcessor {
+		void process(ContextRefreshedEvent event);
+	}
+
+	// MUST be annotated with @Component, due to EventListenerMethodProcessor.isSpringContainerClass().
+	@Component
+	record ContextRefreshedEventListener(ContextRefreshedEventProcessor contextRefreshedEventProcessor) {
+
+		@EventListener
+		void onApplicationEvent(ContextRefreshedEvent event) {
+			this.contextRefreshedEventProcessor.process(event);
+		}
+	}
+
+}