63af572ce803e9f1d206195060619077239bc1a4
Juergen Hoeller <juergen.hoeller@broadcom.com>
2024-12-11 17:34:53 +0100

    Upgrade to Jackson 2.18.2, RxJava 3.1.10, Checkstyle 10.20.2

--- buildSrc/src/main/java/org/springframework/build/CheckstyleConventions.java
+++ buildSrc/src/main/java/org/springframework/build/CheckstyleConventions.java
@@ -50,7 +50,7 @@ public class CheckstyleConventions {
 			project.getPlugins().apply(CheckstylePlugin.class);
 			project.getTasks().withType(Checkstyle.class).forEach(checkstyle -> checkstyle.getMaxHeapSize().set("1g"));
 			CheckstyleExtension checkstyle = project.getExtensions().getByType(CheckstyleExtension.class);
-			checkstyle.setToolVersion("10.20.1");
+			checkstyle.setToolVersion("10.20.2");
 			checkstyle.getConfigDirectory().set(project.getRootProject().file("src/checkstyle"));
 			String version = SpringJavaFormatPlugin.class.getPackage().getImplementationVersion();
 			DependencySet checkstyleDependencies = project.getConfigurations().getByName("checkstyle").getDependencies();
