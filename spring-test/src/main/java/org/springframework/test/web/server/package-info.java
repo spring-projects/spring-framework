590a16276c543c5ef6d79191236be839ac8e6439
Rob Worsnop <rworsnop@gmail.com>
2024-12-16 22:41:01 -0500

    Add RestTestClient
    Fixes gh-31275
    
    Signed-off-by: Rob Worsnop <rworsnop@gmail.com>

---
+++ spring-test/src/main/java/org/springframework/test/web/server/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Support for testing Spring Web server endpoints via
+ * {@link org.springframework.test.web.server.RestTestClient}.
+ */
+@NullMarked
+package org.springframework.test.web.server;
+
+import org.jspecify.annotations.NullMarked;
---
+++ spring-test/src/test/java/org/springframework/test/web/server/samples/bind/RouterFunctionTests.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2002-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.test.web.server.samples.bind;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.test.web.server.RestTestClient;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+/**
+ * Sample tests demonstrating "mock" server tests binding to a RouterFunction.
+ *
+ * @author Rob Worsnop
+ */
+class RouterFunctionTests {
+
+	private RestTestClient testClient;
+
+
+	@BeforeEach
+	void setUp() throws Exception {
+
+		RouterFunction<?> route = route(GET("/test"), request ->
+				ServerResponse.ok().body("It works!"));
+
+		this.testClient = RestTestClient.bindToRouterFunction(route).build();
+	}
+
+	@Test
+	void test() throws Exception {
+		this.testClient.get().uri("/test")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).isEqualTo("It works!");
+	}
+
+}
