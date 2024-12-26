c245e20e3f7438c0ab1579414d5d45fb268448f7
Sébastien Deleuze <sebastien.deleuze@broadcom.com>
2024-12-26 14:18:48 +0100

    Refine null-safety in the spring-beans module
    See gh-34152

--- spring-beans/src/main/java/org/springframework/beans/factory/config/FieldRetrievingFactoryBean.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/FieldRetrievingFactoryBean.java
@@ -158,7 +158,7 @@ public class FieldRetrievingFactoryBean
 
 
 	@Override
-	@SuppressWarnings("NullAway")
+	@SuppressWarnings("NullAway") // Lazy initialization
 	public void afterPropertiesSet() throws ClassNotFoundException, NoSuchFieldException {
 		if (this.targetClass != null && this.targetObject != null) {
 			throw new IllegalArgumentException("Specify either targetClass or targetObject, not both");
