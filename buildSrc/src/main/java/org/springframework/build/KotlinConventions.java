19f647ab8ce1c17778ab5493b5e9ed2c6ecd1312
Sébastien Deleuze <sebastien.deleuze@broadcom.com>
2025-01-13 21:02:28 +0100

    Set -Xjdk-release=17 for Kotlin build
    Needed due to https://youtrack.jetbrains.com/issue/KT-49746.
    
    See gh-34220

--- buildSrc/src/main/java/org/springframework/build/KotlinConventions.java
+++ buildSrc/src/main/java/org/springframework/build/KotlinConventions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,7 +39,12 @@ public class KotlinConventions {
 			options.getJvmTarget().set(JvmTarget.JVM_17);
 			options.getJavaParameters().set(true);
 			options.getAllWarningsAsErrors().set(true);
-			options.getFreeCompilerArgs().addAll("-Xsuppress-version-warnings", "-Xjsr305=strict", "-opt-in=kotlin.RequiresOptIn");
+			options.getFreeCompilerArgs().addAll(
+					"-Xsuppress-version-warnings",
+					"-Xjsr305=strict", // For dependencies using JSR 305
+					"-opt-in=kotlin.RequiresOptIn",
+					"-Xjdk-release=17" // Needed due to https://youtrack.jetbrains.com/issue/KT-49746
+			);
 		});
 	}
 
