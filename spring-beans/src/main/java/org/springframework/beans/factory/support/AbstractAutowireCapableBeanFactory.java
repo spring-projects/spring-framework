8771b9ea21016efe9969b21ac8b937f7fc2d1ceb
Juergen Hoeller <juergen.hoeller@broadcom.com>
2025-01-13 13:04:42 +0100

    Defensively acquire singleton lock for FactoryBean type check
    Closes gh-34247

--- spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -991,7 +991,11 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 	 */
 	@Nullable
 	private FactoryBean<?> getSingletonFactoryBeanForTypeCheck(String beanName, RootBeanDefinition mbd) {
-		this.singletonLock.lock();
+		boolean locked = this.singletonLock.tryLock();
+		if (!locked) {
+			return null;
+		}
+
 		try {
 			BeanWrapper bw = this.factoryBeanInstanceCache.get(beanName);
 			if (bw != null) {
