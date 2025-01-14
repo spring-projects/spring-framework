5a68d1f029362f0b9ee0b48766660571de55cba6
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2025-01-14 11:29:45 +0100

    Remove @⁠FunctionalInterface declaration from BeanOverrideProcessor
    Closes gh-34259

--- spring-test/src/main/java/org/springframework/test/context/bean/override/BeanOverrideProcessor.java
+++ spring-test/src/main/java/org/springframework/test/context/bean/override/BeanOverrideProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,6 @@ import java.lang.reflect.Field;
  * @author Sam Brannen
  * @since 6.2
  */
-@FunctionalInterface
 public interface BeanOverrideProcessor {
 
 	/**
