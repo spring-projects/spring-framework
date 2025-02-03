1cea1fe962cc95544fce7be8fd2f56419e89aaaf
rstoyanchev <rossen.stoyanchev@broadcom.com>
2025-02-03 15:15:21 +0000

    Polishing contribution
    Closes gh-34066

--- spring-webflux/src/main/java/org/springframework/web/reactive/function/server/support/ServerResponseResultHandler.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/function/server/support/ServerResponseResultHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,26 +47,21 @@ public class ServerResponseResultHandler implements HandlerResultHandler, Initia
 
 	private int order = 0;
 
-	/**
-	 * Return the configured {@link HttpMessageWriter}'s.
-	 */
-	public List<HttpMessageWriter<?>> getMessageWriters() {
-		return this.messageWriters;
-	}
 
 	/**
 	 * Configure HTTP message writers to serialize the request body with.
-	 * <p>By default this is set to {@link ServerCodecConfigurer}'s default writers.
+	 * <p>By default, this is set to {@link ServerCodecConfigurer}'s default writers.
 	 */
 	public void setMessageWriters(List<HttpMessageWriter<?>> configurer) {
 		this.messageWriters = configurer;
 	}
 
 	/**
-	 * Return the configured {@link ViewResolver}'s.
+	 * Return the configured {@link HttpMessageWriter}'s.
+	 * @since 6.2.3
 	 */
-	public List<ViewResolver> getViewResolvers() {
-		return this.viewResolvers;
+	public List<HttpMessageWriter<?>> getMessageWriters() {
+		return this.messageWriters;
 	}
 
 	/**
@@ -77,8 +72,16 @@ public class ServerResponseResultHandler implements HandlerResultHandler, Initia
 	}
 
 	/**
+	 * Return the configured {@link ViewResolver}'s.
+	 * @since 6.2.3
+	 */
+	public List<ViewResolver> getViewResolvers() {
+		return this.viewResolvers;
+	}
+
+	/**
 	 * Set the order for this result handler relative to others.
-	 * <p>By default set to 0. It is generally safe to place it early in the
+	 * <p>By default, set to 0. It is generally safe to place it early in the
 	 * order as it looks for a concrete return type.
 	 */
 	public void setOrder(int order) {
