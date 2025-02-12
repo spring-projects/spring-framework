440a259b717e2d9f8bb3f70ad82793a75b9f5046
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2025-02-12 16:46:41 +0100

    Clean up warnings in Gradle build

--- spring-context/src/test/java/org/springframework/aop/framework/JdkDynamicProxyTests.java
+++ spring-context/src/test/java/org/springframework/aop/framework/JdkDynamicProxyTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -141,6 +141,7 @@ class JdkDynamicProxyTests extends AbstractAopProxyTests {
 	}
 
 	@Test  // SPR-13328
+	@SuppressWarnings("unchecked")
 	void varargsWithEnumArray() {
 		ProxyFactory proxyFactory = new ProxyFactory(new VarargTestBean());
 		VarargTestInterface proxy = (VarargTestInterface) proxyFactory.getProxy();
--- spring-webflux/src/test/java/org/springframework/web/reactive/result/method/annotation/ResponseBodyResultHandlerTests.java
+++ spring-webflux/src/test/java/org/springframework/web/reactive/result/method/annotation/ResponseBodyResultHandlerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -218,12 +218,12 @@ class ResponseBodyResultHandlerTests {
 	}
 
 
+	@SuppressWarnings("serial")
 	private static class MyProblemDetail extends ProblemDetail {
 
 		public MyProblemDetail(HttpStatus status) {
 			super(status.value());
 		}
-
 	}
 
 }
