2472126475c70c0d5e454ed5734661bdbbe670f3
Juergen Hoeller <juergen.hoeller@broadcom.com>
2025-03-05 22:45:22 +0100

    Add fallback to pre-6.2.3 behavior for unresolvable generics
    Closes gh-34535

--- spring-core/src/main/java/org/springframework/core/convert/support/GenericConversionService.java
+++ spring-core/src/main/java/org/springframework/core/convert/support/GenericConversionService.java
@@ -348,6 +348,13 @@ public class GenericConversionService implements ConfigurableConversionService {
 					conditionalConverter.matches(sourceType, targetType);
 		}
 
+		public boolean matchesFallback(TypeDescriptor sourceType, TypeDescriptor targetType) {
+			return (this.typeInfo.getTargetType() == targetType.getObjectType() &&
+					this.targetType.hasUnresolvableGenerics() &&
+					(!(this.converter instanceof ConditionalConverter conditionalConverter) ||
+							conditionalConverter.matches(sourceType, targetType)));
+		}
+
 		@Override
 		@Nullable
 		public Object convert(@Nullable Object source, TypeDescriptor sourceType, TypeDescriptor targetType) {
@@ -622,9 +629,17 @@ public class GenericConversionService implements ConfigurableConversionService {
 
 		@Nullable
 		public GenericConverter getConverter(TypeDescriptor sourceType, TypeDescriptor targetType) {
+			// Look for proper match among all converters (taking full generics into account)
 			for (GenericConverter converter : this.converters) {
 				if (!(converter instanceof ConditionalGenericConverter genericConverter) ||
 						genericConverter.matches(sourceType, targetType)) {
+					return converter;
+				}
+			}
+			// Fallback to pre-6.2.3 behavior: accept Class match for unresolvable generics
+			for (GenericConverter converter : this.converters) {
+				if (converter instanceof ConverterAdapter converterAdapter &&
+						converterAdapter.matchesFallback(sourceType, targetType)) {
 					return converter;
 				}
 			}
