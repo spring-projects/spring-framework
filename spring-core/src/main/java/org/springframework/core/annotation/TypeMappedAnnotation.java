2d88f18bf10400f2675f693d195eaf6430d30ce2
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2025-03-06 15:52:55 +0100

    Update Javadoc

--- spring-core/src/main/java/org/springframework/core/annotation/TypeMappedAnnotation.java
+++ spring-core/src/main/java/org/springframework/core/annotation/TypeMappedAnnotation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ import org.springframework.util.ClassUtils;
  * applying the mapping and mirroring rules of an {@link AnnotationTypeMapping}.
  *
  * <p>Root attribute values are extracted from a source object using a supplied
- * {@code BiFunction}. This allows various different annotation models to be
+ * {@link ValueExtractor}. This allows various different annotation models to be
  * supported by the same class. For example, the attributes source might be an
  * actual {@link Annotation} instance where methods on the annotation instance
  * are {@linkplain AnnotationUtils#invokeAnnotationMethod(Method, Object) invoked}
