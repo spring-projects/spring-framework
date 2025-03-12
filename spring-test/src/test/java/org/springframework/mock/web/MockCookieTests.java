spring-projects/spring-framework

commit 7fc493719962d2ea910866ade9b273b7bac87756

Add Partitioned cookie attribute support for servers
This commit adds support for the "Partitioned" cookie attribute in
WebFlux servers and the related testing infrastructure.
Note, Undertow does not support this feature at the moment.

Closes gh-31454

spring-web/src/testFixtures/java/org/springframework/web/testfixture/servlet/MockCookie.java
@@ -99,6 +99,24 @@ public class MockCookie extends Cookie {
 	}
 
 	/**
+	 * Set the "Partitioned" attribute for this cookie.
+	 * @since 6.2
+	 * @see <a href="https://datatracker.ietf.org/doc/html/draft-cutler-httpbis-partitioned-cookies#section-2.1">The Partitioned attribute spec</a>
+	 */
+	public void setPartitioned(boolean partitioned) {
+		setAttribute("Partitioned", "");
+	}
+
+	/**
+	 * Return whether the "Partitioned" attribute is set for this cookie.
+	 * @since 6.2
+	 * @see <a href="https://datatracker.ietf.org/doc/html/draft-cutler-httpbis-partitioned-cookies#section-2.1">The Partitioned attribute spec</a>
+	 */
+	public boolean isPartitioned() {
+		return getAttribute("Partitioned") != null;
+	}
+
+	/**
 	 * Factory method that parses the value of the supplied "Set-Cookie" header.
 	 * @param setCookieHeader the "Set-Cookie" value; never {@code null} or empty
 	 * @return the created cookie
@@ -145,6 +163,9 @@ public class MockCookie extends Cookie {
 			}
 			else if (StringUtils.startsWithIgnoreCase(attribute, "Comment")) {
 				cookie.setComment(extractAttributeValue(attribute, setCookieHeader));
+			}
+			else {
+				cookie.setAttribute(attribute, extractAttributeValue(attribute, setCookieHeader));
 			}
 		}
 		return cookie;

spring-test/src/main/java/org/springframework/mock/web/MockCookie.java
@@ -99,6 +99,24 @@ public class MockCookie extends Cookie {
 	}
 
 	/**
+	 * Set the "Partitioned" attribute for this cookie.
+	 * @since 6.2
+	 * @see <a href="https://datatracker.ietf.org/doc/html/draft-cutler-httpbis-partitioned-cookies#section-2.1">The Partitioned attribute spec</a>
+	 */
+	public void setPartitioned(boolean partitioned) {
+		setAttribute("Partitioned", "");
+	}
+
+	/**
+	 * Return whether the "Partitioned" attribute is set for this cookie.
+	 * @since 6.2
+	 * @see <a href="https://datatracker.ietf.org/doc/html/draft-cutler-httpbis-partitioned-cookies#section-2.1">The Partitioned attribute spec</a>
+	 */
+	public boolean isPartitioned() {
+		return getAttribute("Partitioned") != null;
+	}
+
+	/**
 	 * Factory method that parses the value of the supplied "Set-Cookie" header.
 	 * @param setCookieHeader the "Set-Cookie" value; never {@code null} or empty
 	 * @return the created cookie
@@ -146,6 +164,9 @@ public class MockCookie extends Cookie {
 			else if (StringUtils.startsWithIgnoreCase(attribute, "Comment")) {
 				cookie.setComment(extractAttributeValue(attribute, setCookieHeader));
 			}
+			else if (!attribute.isEmpty()) {
+				cookie.setAttribute(attribute, extractOptionalAttributeValue(attribute, setCookieHeader));
+			}
 		}
 		return cookie;
 	}
@@ -155,6 +176,11 @@ public class MockCookie extends Cookie {
 		Assert.isTrue(nameAndValue.length == 2,
 				() -> "No value in attribute '" + nameAndValue[0] + "' for Set-Cookie header '" + header + "'");
 		return nameAndValue[1];
+	}
+
+	private static String extractOptionalAttributeValue(String attribute, String header) {
+		String[] nameAndValue = attribute.split("=");
+		return nameAndValue.length == 2 ? nameAndValue[1] : "";
 	}
 
 	@Override
@@ -176,6 +202,7 @@ public class MockCookie extends Cookie {
 				.append("Comment", getComment())
 				.append("Secure", getSecure())
 				.append("HttpOnly", isHttpOnly())
+				.append("Partitioned", isPartitioned())
 				.append(SAME_SITE, getSameSite())
 				.append("Max-Age", getMaxAge())
 				.append(EXPIRES, getAttribute(EXPIRES))

spring-test/src/test/java/org/springframework/mock/web/MockCookieTests.java
@@ -71,7 +71,7 @@ class MockCookieTests {
 	@Test
 	void parseHeaderWithAttributes() {
 		MockCookie cookie = MockCookie.parse("SESSION=123; Domain=example.com; Max-Age=60; " +
-				"Expires=Tue, 8 Oct 2019 19:50:00 GMT; Path=/; Secure; HttpOnly; SameSite=Lax");
+				"Expires=Tue, 8 Oct 2019 19:50:00 GMT; Path=/; Secure; HttpOnly; Partitioned; SameSite=Lax");
 
 		assertCookie(cookie, "SESSION", "123");
 		assertThat(cookie.getDomain()).isEqualTo("example.com");
@@ -79,6 +79,7 @@ class MockCookieTests {
 		assertThat(cookie.getPath()).isEqualTo("/");
 		assertThat(cookie.getSecure()).isTrue();
 		assertThat(cookie.isHttpOnly()).isTrue();
+		assertThat(cookie.isPartitioned()).isTrue();
 		assertThat(cookie.getExpires()).isEqualTo(ZonedDateTime.parse("Tue, 8 Oct 2019 19:50:00 GMT",
 				DateTimeFormatter.RFC_1123_DATE_TIME));
 		assertThat(cookie.getSameSite()).isEqualTo("Lax");
@@ -201,6 +202,14 @@ class MockCookieTests {
 	void setInvalidAttributeExpiresShouldThrow() {
 		MockCookie cookie = new MockCookie("SESSION", "123");
 		assertThatThrownBy(() -> cookie.setAttribute("expires", "12345")).isInstanceOf(DateTimeParseException.class);
+	}
+
+	@Test
+	void setPartitioned() {
+		MockCookie cookie = new MockCookie("SESSION", "123");
+		cookie.setAttribute("Partitioned", "");
+
+		assertThat(cookie.isPartitioned()).isTrue();
 	}
 
 }

spring-test/src/main/java/org/springframework/mock/web/MockHttpServletResponse.java
@@ -481,6 +481,9 @@ public class MockHttpServletResponse implements HttpServletResponse {
 		if (cookie.isHttpOnly()) {
 			buf.append("; HttpOnly");
 		}
+		if (cookie.getAttribute("Partitioned") != null) {
+			buf.append("; Partitioned");
+		}
 		if (cookie instanceof MockCookie mockCookie) {
 			if (StringUtils.hasText(mockCookie.getSameSite())) {
 				buf.append("; SameSite=").append(mockCookie.getSameSite());

spring-test/src/main/java/org/springframework/test/web/reactive/server/CookieAssertions.java
@@ -198,6 +198,19 @@ public class CookieAssertions {
 	}
 
 	/**
+	 * Assert a cookie's "Partitioned" attribute.
+	 * @since 6.2
+	 */
+	public WebTestClient.ResponseSpec partitioned(String name, boolean expected) {
+		boolean isPartitioned = getCookie(name).isPartitioned();
+		this.exchangeResult.assertWithDiagnostics(() -> {
+			String message = getMessage(name) + " isPartitioned";
+			assertEquals(message, expected, isPartitioned);
+		});
+		return this.responseSpec;
+	}
+
+	/**
 	 * Assert a cookie's "SameSite" attribute.
 	 */
 	public WebTestClient.ResponseSpec sameSite(String name, String expected) {

spring-test/src/main/java/org/springframework/test/web/servlet/client/MockMvcHttpConnector.java
@@ -209,6 +209,7 @@ public class MockMvcHttpConnector implements ClientHttpConnector {
 							.path(cookie.getPath())
 							.secure(cookie.getSecure())
 							.httpOnly(cookie.isHttpOnly())
+							.partitioned(cookie.getAttribute("Partitioned") != null)
 							.sameSite(cookie.getAttribute("samesite"))
 							.build();
 			clientResponse.getCookies().add(httpCookie.getName(), httpCookie);

spring-test/src/main/java/org/springframework/test/web/servlet/result/CookieResultMatchers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -226,6 +226,17 @@ public class CookieResultMatchers {
 		return result -> {
 			Cookie cookie = getCookie(result, name);
 			assertEquals("Response cookie '" + name + "' httpOnly", httpOnly, cookie.isHttpOnly());
+		};
+	}
+
+	/**
+	 * Assert whether the cookie is partitioned.
+	 * @since 6.2
+	 */
+	public ResultMatcher partitioned(String name, boolean partitioned) {
+		return result -> {
+			Cookie cookie = getCookie(result, name);
+			assertEquals("Response cookie '" + name + "' partitioned", partitioned, cookie.getAttribute("Partitioned") != null);
 		};
 	}
 

spring-test/src/main/kotlin/org/springframework/test/web/servlet/result/CookieResultMatchersDsl.kt
@@ -158,6 +158,14 @@ class CookieResultMatchersDsl internal constructor (private val actions: ResultA
 	}
 
 	/**
+	 * @see CookieResultMatchers.partitioned
+	 * @since 6.2
+	 */
+	fun partitioned(name: String, partitioned: Boolean) {
+		actions.andExpect(matchers.partitioned(name, partitioned))
+	}
+
+	/**
 	 * @see CookieResultMatchers.attribute
 	 * @since 6.0.8
 	 */

commit 020f556841a60071add53144365358f4f1fd5d7b

Support custom attribute with a value in MockCookie.parse()
Prior to this commit, MockCookie.parse() failed with an
IllegalArgumentException when attempting to parse a custom attribute
with a value, such as "Version=1". This is a regression that was
inadvertently introduced in 7fc493719962d2ea910866ade9b273b7bac87756
when adding support for the "Partitioned" attribute which does not
support a value.

This commit addresses this regression by parsing both the name and the
value from an optional, custom attribute.

See gh-31454
Closes gh-34575

spring-web/src/testFixtures/java/org/springframework/web/testfixture/servlet/MockCookie.java
@@ -178,7 +178,8 @@ public class MockCookie extends Cookie {
 				cookie.setComment(extractAttributeValue(attribute, setCookieHeader));
 			}
 			else if (!attribute.isEmpty()) {
-				cookie.setAttribute(attribute, extractOptionalAttributeValue(attribute, setCookieHeader));
+				String[] nameAndValue = extractOptionalAttributeNameAndValue(attribute, setCookieHeader);
+				cookie.setAttribute(nameAndValue[0], nameAndValue[1]);
 			}
 		}
 		return cookie;
@@ -191,9 +192,9 @@ public class MockCookie extends Cookie {
 		return nameAndValue[1];
 	}
 
-	private static String extractOptionalAttributeValue(String attribute, String header) {
+	private static String[] extractOptionalAttributeNameAndValue(String attribute, String header) {
 		String[] nameAndValue = attribute.split("=");
-		return nameAndValue.length == 2 ? nameAndValue[1] : "";
+		return (nameAndValue.length == 2 ? nameAndValue : new String[] {attribute, ""});
 	}
 
 	@Override

spring-test/src/main/java/org/springframework/mock/web/MockCookie.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -178,7 +178,8 @@ public class MockCookie extends Cookie {
 				cookie.setComment(extractAttributeValue(attribute, setCookieHeader));
 			}
 			else if (!attribute.isEmpty()) {
-				cookie.setAttribute(attribute, extractOptionalAttributeValue(attribute, setCookieHeader));
+				String[] nameAndValue = extractOptionalAttributeNameAndValue(attribute, setCookieHeader);
+				cookie.setAttribute(nameAndValue[0], nameAndValue[1]);
 			}
 		}
 		return cookie;
@@ -191,9 +192,9 @@ public class MockCookie extends Cookie {
 		return nameAndValue[1];
 	}
 
-	private static String extractOptionalAttributeValue(String attribute, String header) {
+	private static String[] extractOptionalAttributeNameAndValue(String attribute, String header) {
 		String[] nameAndValue = attribute.split("=");
-		return nameAndValue.length == 2 ? nameAndValue[1] : "";
+		return (nameAndValue.length == 2 ? nameAndValue : new String[] {attribute, ""});
 	}
 
 	@Override

spring-test/src/test/java/org/springframework/mock/web/MockCookieTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,8 +68,8 @@ class MockCookieTests {
 		assertCookie(cookie, "SESSION", "123");
 	}
 
-	@SuppressWarnings("removal")
 	@Test
+	@SuppressWarnings("removal")
 	void parseHeaderWithAttributes() {
 		MockCookie cookie = MockCookie.parse("SESSION=123; Domain=example.com; Max-Age=60; " +
 				"Expires=Tue, 8 Oct 2019 19:50:00 GMT; Path=/; Secure; HttpOnly; Partitioned; SameSite=Lax");
@@ -85,6 +85,19 @@ class MockCookieTests {
 				DateTimeFormatter.RFC_1123_DATE_TIME));
 		assertThat(cookie.getSameSite()).isEqualTo("Lax");
 		assertThat(cookie.getComment()).isNull();
+	}
+
+	@Test  // gh-34575
+	void parseHeaderWithOptionalAttributes() {
+		MockCookie cookie = MockCookie.parse("SESSION=123; HttpOnly; Version=1; Partitioned; Secure");
+
+		assertCookie(cookie, "SESSION", "123");
+		assertThat(cookie.isHttpOnly()).isTrue();
+		assertThat(cookie.getSecure()).isTrue();
+		assertThat(cookie.isPartitioned()).isTrue();
+		assertThat(cookie.getAttribute("Partitioned")).isEmpty();
+		assertThat(cookie.getAttribute("Version")).isEqualTo("1");
+		assertThat(cookie.getAttribute("BOGUS")).isNull();
 	}
 
 	@ParameterizedTest
@@ -209,10 +222,13 @@ class MockCookieTests {
 	void setPartitioned() {
 		MockCookie cookie = new MockCookie("SESSION", "123");
 		assertThat(cookie.isPartitioned()).isFalse();
+		assertThat(cookie.getAttribute("Partitioned")).isNull();
 		cookie.setPartitioned(true);
 		assertThat(cookie.isPartitioned()).isTrue();
+		assertThat(cookie.getAttribute("Partitioned")).isEmpty();
 		cookie.setPartitioned(false);
 		assertThat(cookie.isPartitioned()).isFalse();
+		assertThat(cookie.getAttribute("Partitioned")).isNull();
 	}
 
 }