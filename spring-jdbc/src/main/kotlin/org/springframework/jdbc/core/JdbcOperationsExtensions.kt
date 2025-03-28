bb471febc241849e58f7f103b36b596511d8cf10
Yanming Zhou <zhouyanming@gmail.com>
2025-03-28 16:45:19 +0800

    Stop calling deprecated JdbcOperations methods
    Signed-off-by: Yanming Zhou <zhouyanming@gmail.com>

--- spring-jdbc/src/main/kotlin/org/springframework/jdbc/core/JdbcOperationsExtensions.kt
+++ spring-jdbc/src/main/kotlin/org/springframework/jdbc/core/JdbcOperationsExtensions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,10 +54,8 @@ inline fun <reified T> JdbcOperations.queryForObject(sql: String, args: Array<ou
  * @author Mario Arias
  * @since 5.0
  */
-@Suppress("DEPRECATION")
-// TODO Replace by the vararg variant in Spring Framework 6
 inline fun <reified T> JdbcOperations.queryForObject(sql: String, args: Array<out Any>): T? =
-		queryForObject(sql, args, T::class.java as Class<*>) as T
+		queryForObject(sql, T::class.java as Class<*>, args) as T
 
 /**
  * Extension for [JdbcOperations.queryForList] providing a `queryForList<Foo>("...")` variant.
@@ -88,10 +86,8 @@ inline fun <reified T : Any> JdbcOperations.queryForList(sql: String, args: Arra
  * @author Mario Arias
  * @since 5.0
  */
-@Suppress("DEPRECATION")
-// TODO Replace by the vararg variant in Spring Framework 6
 inline fun <reified T : Any> JdbcOperations.queryForList(sql: String, args: Array<out Any>): List<T> =
-		queryForList(sql, args, T::class.java)
+		queryForList(sql, T::class.java, args)
 
 
 /**
