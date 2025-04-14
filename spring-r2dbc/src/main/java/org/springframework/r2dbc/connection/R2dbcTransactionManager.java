7095f4cb664f7fcf033dde84325eda83d67ece70
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2025-04-14 11:25:40 +0200

    Use proper casing for parameter and variable names

--- spring-r2dbc/src/main/java/org/springframework/r2dbc/connection/R2dbcTransactionManager.java
+++ spring-r2dbc/src/main/java/org/springframework/r2dbc/connection/R2dbcTransactionManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -296,7 +296,7 @@ public class R2dbcTransactionManager extends AbstractReactiveTransactionManager 
 	}
 
 	@Override
-	protected Mono<Void> doCommit(TransactionSynchronizationManager TransactionSynchronizationManager,
+	protected Mono<Void> doCommit(TransactionSynchronizationManager synchronizationManager,
 			GenericReactiveTransaction status) {
 
 		ConnectionFactoryTransactionObject txObject = (ConnectionFactoryTransactionObject) status.getTransaction();
@@ -308,7 +308,7 @@ public class R2dbcTransactionManager extends AbstractReactiveTransactionManager 
 	}
 
 	@Override
-	protected Mono<Void> doRollback(TransactionSynchronizationManager TransactionSynchronizationManager,
+	protected Mono<Void> doRollback(TransactionSynchronizationManager synchronizationManager,
 			GenericReactiveTransaction status) {
 
 		ConnectionFactoryTransactionObject txObject = (ConnectionFactoryTransactionObject) status.getTransaction();
