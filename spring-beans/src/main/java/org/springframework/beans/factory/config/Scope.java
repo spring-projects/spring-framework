1b072a888022310e23d970d61da451a16d672997
Yanming Zhou <zhouyanming@gmail.com>
2025-04-15 16:51:18 +0800

    Mark applicable Scope's methods as default
    Signed-off-by: Yanming Zhou <zhouyanming@gmail.com>

--- spring-beans/src/main/java/org/springframework/beans/factory/config/Scope.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/Scope.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -130,7 +130,9 @@ public interface Scope {
 	 * @return the corresponding object, or {@code null} if none found
 	 * @throws IllegalStateException if the underlying scope is not currently active
 	 */
-	@Nullable Object resolveContextualObject(String key);
+	default @Nullable Object resolveContextualObject(String key) {
+		return null;
+	}
 
 	/**
 	 * Return the <em>conversation ID</em> for the current underlying scope, if any.
@@ -147,6 +149,8 @@ public interface Scope {
 	 * conversation ID for the current scope
 	 * @throws IllegalStateException if the underlying scope is not currently active
 	 */
-	@Nullable String getConversationId();
+	default @Nullable String getConversationId() {
+		return null;
+	}
 
 }
