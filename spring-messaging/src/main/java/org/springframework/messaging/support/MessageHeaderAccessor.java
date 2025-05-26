bbf61c74ab889788492e5b24acdd20042705fcd3
Mengqi Xu <2663479778@qq.com>
2025-05-26 23:44:05 +0800

    Fix REPLY_CHANNEL header check in MessageHeaderAccessor
    Prior to this commit, the verifyType() method in MessageHeaderAccessor
    checked if REPLY_CHANNEL ended with the given header name which does
    not make much sense and is inconsistent with the ERROR_CHANNEL check.
    
    This commit therefore checks if the REPLY_CHANNEL is equal to the given
    header name, analogous to the ERROR_CHANNEL check.
    
    See gh-34881
    Closes gh-34949
    
    Signed-off-by: Mengqi Xu <2663479778@qq.com>

--- spring-messaging/src/main/java/org/springframework/messaging/support/MessageHeaderAccessor.java
+++ spring-messaging/src/main/java/org/springframework/messaging/support/MessageHeaderAccessor.java
@@ -332,7 +332,7 @@ public class MessageHeaderAccessor {
 	protected void verifyType(@Nullable String headerName, @Nullable Object headerValue) {
 		if (headerName != null && headerValue != null) {
 			if (MessageHeaders.ERROR_CHANNEL.equals(headerName) ||
-					MessageHeaders.REPLY_CHANNEL.endsWith(headerName)) {
+					MessageHeaders.REPLY_CHANNEL.equals(headerName)) {
 				if (!(headerValue instanceof MessageChannel || headerValue instanceof String)) {
 					throw new IllegalArgumentException(
 							"'" + headerName + "' header value must be a MessageChannel or String");
