0d07df968ea1e649cd6b9a5558e6a80156d338dd
Vincent Potucek <vpotucek@me.com>
2025-06-01 10:39:52 +0200

    spotless

--- buildSrc/src/main/java/org/springframework/build/SpotlessConventions.java
+++ buildSrc/src/main/java/org/springframework/build/SpotlessConventions.java
@@ -16,10 +16,6 @@
 
 package org.springframework.build;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.util.List;
-
 import io.spring.javaformat.gradle.SpringJavaFormatPlugin;
 import io.spring.nohttp.gradle.NoHttpExtension;
 import io.spring.nohttp.gradle.NoHttpPlugin;
@@ -31,12 +27,16 @@ import org.gradle.api.plugins.quality.Checkstyle;
 import org.gradle.api.plugins.quality.CheckstyleExtension;
 import org.gradle.api.plugins.quality.CheckstylePlugin;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+
 /**
  * {@link Plugin} that applies conventions for checkstyle.
  *
  * @author Brian Clozel
  */
-public class CheckstyleConventions {
+public class SpotlessConventions {
 
 	/**
 	 * Applies the Spring Java Format and Checkstyle plugins with the project conventions.
@@ -47,7 +47,6 @@ public class CheckstyleConventions {
 			if (project.getRootProject() == project) {
 				configureNoHttpPlugin(project);
 			}
-			project.getPlugins().apply(CheckstylePlugin.class);
 			project.getPlugins().apply(SpotlessPlugin.class);
 			project.getTasks().withType(Checkstyle.class).forEach(checkstyle -> checkstyle.getMaxHeapSize().set("1g"));
 			CheckstyleExtension checkstyle = project.getExtensions().getByType(CheckstyleExtension.class);
