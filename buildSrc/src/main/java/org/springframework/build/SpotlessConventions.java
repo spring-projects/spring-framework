1ee4bb30d745e656e16c95583fe8292534e4ad57
Vincent Potucek <vpotucek@me.com>
2025-06-01 10:50:26 +0200

    spotless

--- buildSrc/src/main/java/org/springframework/build/SpotlessConventions.java
+++ buildSrc/src/main/java/org/springframework/build/SpotlessConventions.java
@@ -41,36 +41,9 @@ public class SpotlessConventions {
 	 * @param project the current project
 	 */
 	public void apply(Project project) {
-		project.getPlugins().withType(JavaBasePlugin.class, (java) -> {
-			if (project.getRootProject() == project) {
-				configureNoHttpPlugin(project);
-			}
+		project.getPlugins().withType(JavaBasePlugin.class, (_) -> {
 			project.getPlugins().apply(com.diffplug.gradle.spotless.SpotlessPlugin.class);
-			project.getTasks().withType(Spotless.class).forEach(checkstyle -> checkstyle.getMaxHeapSize().set("1g"));
-			CheckstyleExtension checkstyle = project.getExtensions().getByType(CheckstyleExtension.class);
-			checkstyle.setToolVersion("10.23.1");
-			checkstyle.getConfigDirectory().set(project.getRootProject().file("src/checkstyle"));
-			String version = SpringJavaFormatPlugin.class.getPackage().getImplementationVersion();
-			DependencySet checkstyleDependencies = project.getConfigurations().getByName("checkstyle").getDependencies();
-			checkstyleDependencies.add(
-					project.getDependencies().create("io.spring.javaformat:spring-javaformat-checkstyle:" + version));
-		});
-	}
-
-	private static void configureNoHttpPlugin(Project project) {
-		project.getPlugins().apply(NoHttpPlugin.class);
-		NoHttpExtension noHttp = project.getExtensions().getByType(NoHttpExtension.class);
-		noHttp.setAllowlistFile(project.file("src/nohttp/allowlist.lines"));
-		noHttp.getSource().exclude("**/test-output/**", "**/.settings/**", "**/.classpath",
-				"**/.project", "**/.gradle/**", "**/node_modules/**", "**/spring-jcl/**", "buildSrc/build/**");
-		List<String> buildFolders = List.of("bin", "build", "out");
-		project.allprojects(subproject -> {
-			Path rootPath = project.getRootDir().toPath();
-			Path projectPath = rootPath.relativize(subproject.getProjectDir().toPath());
-			for (String buildFolder : buildFolders) {
-				Path innerBuildDir = projectPath.resolve(buildFolder);
-				noHttp.getSource().exclude(innerBuildDir + File.separator + "**");
-			}
+			project.getTasks().withType(Spotless.class).forEach(spotless -> spotless.getMaxHeapSize().set("1g"));
 		});
 	}
 
