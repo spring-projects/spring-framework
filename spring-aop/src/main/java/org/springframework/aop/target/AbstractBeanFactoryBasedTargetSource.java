bb168618c179144ad2ba8b4f4dbd4e3f144d8d22
chenggwang <90715678+chenggwang@users.noreply.github.com>
2025-07-11 02:06:41 +0800

    Change the modifier of targetBeanName from private to protected
    Change the modifier of targetBeanName from private to protected to facilitate direct access through this.targetBeanName where no assertion is needed.
    
    Signed-off-by: chenggwang <90715678+chenggwang@users.noreply.github.com>

--- spring-aop/src/main/java/org/springframework/aop/target/AbstractBeanFactoryBasedTargetSource.java
+++ spring-aop/src/main/java/org/springframework/aop/target/AbstractBeanFactoryBasedTargetSource.java
@@ -60,7 +60,7 @@ public abstract class AbstractBeanFactoryBasedTargetSource implements TargetSour
 	protected final transient Log logger = LogFactory.getLog(getClass());
 
 	/** Name of the target bean we will create on each invocation. */
-	private @Nullable String targetBeanName;
+	protected @Nullable String targetBeanName;
 
 	/** Class of the target. */
 	private volatile @Nullable Class<?> targetClass;
@@ -91,17 +91,6 @@ public abstract class AbstractBeanFactoryBasedTargetSource implements TargetSour
 	 */
 	public String getTargetBeanName() {
 		Assert.state(this.targetBeanName != null, "Target bean name not set");
-		return this.targetBeanName;
-	}
-
-	/**
- 	* Return the name of the target bean in the factory.
- 	* If the configuration has not been completed yet, null can be returned
- 	* If it's just for obtaining simple basic information, using it is very friendly.
- 	* For example, toString().However, if strict non-null judgment is required,
- 	* please use {@link #getTargetBeanName}
- 	*/
-	public String getPlainTargetBeanName() {
 		return this.targetBeanName;
 	}
 	
