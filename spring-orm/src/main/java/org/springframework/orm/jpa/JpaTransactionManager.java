9d28e4542a379204d5f998f4d9e99502d541cf16
Giheon Do <dgh0001@naver.com>
2025-07-21 15:43:20 +0900

    Clarify limitations of nested transactions in Javadoc
    Signed-off-by: Giheon Do <dgh0001@naver.com>

--- spring-orm/src/main/java/org/springframework/orm/jpa/JpaTransactionManager.java
+++ spring-orm/src/main/java/org/springframework/orm/jpa/JpaTransactionManager.java
@@ -93,12 +93,14 @@ import org.springframework.util.CollectionUtils;
  *
  * <p>This transaction manager supports nested transactions via JDBC Savepoints.
  * The {@link #setNestedTransactionAllowed "nestedTransactionAllowed"} flag defaults
- * to {@code true} though, since nested transactions will just apply to the JDBC
+ * to {@code true}, but nested transactions will just apply to the JDBC
  * Connection, not to the JPA EntityManager and its cached entity objects and related
  * context. You can manually set the flag to {@code false} if you want to disallow nested
  * transactions for JDBC access code which participates in JPA transactions (provided
  * that your JDBC driver supports Savepoints). <i>Note that JPA itself does not support
- * nested transactions! Hence, do not expect JPA access code to semantically
+ * nested transactions! Furthermore, as most JPA providers (e.g., Hibernate)
+ * do not support savepoints, {@code Propagation.NESTED} will not work and will typically
+ * result in an exception. Hence, do not expect JPA access code to semantically
  * participate in a nested transaction.</i>
  *
  * @author Juergen Hoeller
