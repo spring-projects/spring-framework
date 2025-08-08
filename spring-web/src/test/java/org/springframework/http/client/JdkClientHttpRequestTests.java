ffc785471bbe579aaef282720baef00a44d46435
rstoyanchev <rossen.stoyanchev@broadcom.com>
2025-08-08 12:31:13 +0100

    Fix checkstyle error

--- spring-web/src/test/java/org/springframework/http/client/JdkClientHttpRequestTests.java
+++ spring-web/src/test/java/org/springframework/http/client/JdkClientHttpRequestTests.java
@@ -81,7 +81,7 @@ class JdkClientHttpRequestTests {
 	}
 
 	private JdkClientHttpRequest createRequest(Duration timeout) {
-		return new JdkClientHttpRequest(client, URI.create("http://abc.com"), HttpMethod.GET, executor, timeout);
+		return new JdkClientHttpRequest(client, URI.create("https://abc.com"), HttpMethod.GET, executor, timeout);
 	}
 
 }
