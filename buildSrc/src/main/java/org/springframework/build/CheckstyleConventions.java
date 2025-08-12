9f9b33c2ac2736a516ccf8e5d5457afbcc54f675
Juergen Hoeller <juergen.hoeller@broadcom.com>
2025-08-13 00:48:37 +0200

    Upgrade to Reactor 2025.0.0-M6, Micrometer 1.16.0-M2, Jetty 12.1.0.beta3
    Includes Checkstyle 11.0, Groovy 5.0 RC1, JRuby 10.0.2, MockK 1.14.5
    
    Closes gh-35310
    Closes gh-35311
    Closes gh-35233

--- buildSrc/src/main/java/org/springframework/build/CheckstyleConventions.java
+++ buildSrc/src/main/java/org/springframework/build/CheckstyleConventions.java
@@ -50,7 +50,7 @@ public class CheckstyleConventions {
 			project.getPlugins().apply(CheckstylePlugin.class);
 			project.getTasks().withType(Checkstyle.class).forEach(checkstyle -> checkstyle.getMaxHeapSize().set("1g"));
 			CheckstyleExtension checkstyle = project.getExtensions().getByType(CheckstyleExtension.class);
-			checkstyle.setToolVersion("10.26.1");
+			checkstyle.setToolVersion("11.0.0");
 			checkstyle.getConfigDirectory().set(project.getRootProject().file("src/checkstyle"));
 			String version = SpringJavaFormatPlugin.class.getPackage().getImplementationVersion();
 			DependencySet checkstyleDependencies = project.getConfigurations().getByName("checkstyle").getDependencies();
