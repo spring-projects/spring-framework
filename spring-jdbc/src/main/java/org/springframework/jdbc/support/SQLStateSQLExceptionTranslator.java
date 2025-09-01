1df688ac13abad3cc97849bc14f2260ecfff2137
Lukáš Kvídera <kvideral@qwsome.eu>
2025-09-01 13:05:31 +0200

    Added Informix equivalent of DuplicateKeyException code
    Signed-off-by: Lukáš Kvídera <kvideral@qwsome.eu>

--- spring-jdbc/src/main/java/org/springframework/jdbc/support/SQLStateSQLExceptionTranslator.java
+++ spring-jdbc/src/main/java/org/springframework/jdbc/support/SQLStateSQLExceptionTranslator.java
@@ -95,7 +95,8 @@ public class SQLStateSQLExceptionTranslator extends AbstractFallbackSQLException
 			301,   // SAP HANA
 			1062,  // MySQL/MariaDB
 			2601,  // MS SQL Server
-			2627   // MS SQL Server
+			2627,  // MS SQL Server
+			-268   // Informix
 		);
 
 
