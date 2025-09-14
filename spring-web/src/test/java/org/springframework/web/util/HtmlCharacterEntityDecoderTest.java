a6efa2aec7418bd3838ab70ade6eace4a194ff4f
potato <65760583+juntae6942@users.noreply.github.com>
2025-09-14 13:38:46 +0900

    Test: Add case for basic HTML entities in HtmlUtils
    Signed-off-by: potato <65760583+juntae6942@users.noreply.github.com>

--- spring-web/src/test/java/org/springframework/web/util/HtmlCharacterEntityDecoderTest.java
+++ spring-web/src/test/java/org/springframework/web/util/HtmlCharacterEntityDecoderTest.java
@@ -23,4 +23,18 @@ class HtmlCharacterEntityDecoderTest {
 		assertEquals(expectedCharacter, actualResultFromDecimal, "Decimal entity was not converted correctly.");
 		assertEquals(expectedCharacter, actualResultFromHex, "Hexadecimal entity was not converted correctly.");
 	}
+
+	@Test
+	@DisplayName("Should correctly unescape basic and named HTML entities")
+	void unescapeHandlesBasicEntities() {
+		// Arrange
+		String input = "&lt;p&gt;Tom &amp; Jerry&#39;s &quot;Show&quot;&lt;/p&gt;";
+		String expectedOutput = "<p>Tom & Jerry's \"Show\"</p>";
+
+		// Act
+		String actualOutput = HtmlUtils.htmlUnescape(input);
+
+		// Assert
+		assertEquals(expectedOutput, actualOutput, "Basic HTML entities were not unescaped correctly.");
+	}
 }
