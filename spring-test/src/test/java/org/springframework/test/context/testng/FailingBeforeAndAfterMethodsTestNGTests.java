5cd2cb38e1b1ed3e1951499d9b5ad63372dd7e3e
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2025-09-23 13:07:22 +0200

    Make AbstractTestNGSpringContextTests thread-safe regarding tracked exceptions
    Prior to this commit, AbstractTestNGSpringContextTests was not
    thread-safe with regard to tracked exceptions.
    
    To address that, AbstractTestNGSpringContextTests now tracks the test
    exception via a ThreadLocal.
    
    Closes gh-35528

--- spring-test/src/test/java/org/springframework/test/context/testng/FailingBeforeAndAfterMethodsTestNGTests.java
+++ spring-test/src/test/java/org/springframework/test/context/testng/FailingBeforeAndAfterMethodsTestNGTests.java
@@ -64,10 +64,10 @@ class FailingBeforeAndAfterMethodsTestNGTests {
 
 		String name = clazz.getSimpleName();
 
-		assertThat(listener.testStartCount).as("tests started for [" + name + "] ==> ").isEqualTo(expectedTestStartCount);
-		assertThat(listener.testSuccessCount).as("successful tests for [" + name + "] ==> ").isEqualTo(expectedTestSuccessCount);
-		assertThat(listener.testFailureCount).as("failed tests for [" + name + "] ==> ").isEqualTo(expectedFailureCount);
-		assertThat(listener.failedConfigurationsCount).as("failed configurations for [" + name + "] ==> ").isEqualTo(expectedFailedConfigurationsCount);
+		assertThat(listener.testStartCount.get()).as("tests started for [" + name + "] ==> ").isEqualTo(expectedTestStartCount);
+		assertThat(listener.testSuccessCount.get()).as("successful tests for [" + name + "] ==> ").isEqualTo(expectedTestSuccessCount);
+		assertThat(listener.testFailureCount.get()).as("failed tests for [" + name + "] ==> ").isEqualTo(expectedFailureCount);
+		assertThat(listener.failedConfigurationsCount.get()).as("failed configurations for [" + name + "] ==> ").isEqualTo(expectedFailedConfigurationsCount);
 	}
 
 	static List<Arguments> testData() {
