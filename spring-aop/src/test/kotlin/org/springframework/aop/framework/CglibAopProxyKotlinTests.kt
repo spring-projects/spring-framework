cb849a7071d39e6e474bfd977db71b445ae702c0
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-10-01 19:21:40 +0200

    Add a test for CGLIB Enhancer Kotlin refinements
    This commit adds a reproducer for the change of behavior introduced via
    https://youtrack.jetbrains.com/issue/KT-76667. The test is only broken
    with Kotlin 2.2.20+ without the related fix (see previous commit).
    
    Closes gh-35487

--- spring-aop/src/test/kotlin/org/springframework/aop/framework/CglibAopProxyKotlinTests.kt
+++ spring-aop/src/test/kotlin/org/springframework/aop/framework/CglibAopProxyKotlinTests.kt
@@ -19,6 +19,7 @@ package org.springframework.aop.framework
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 
 /**
  * Tests for Kotlin support in [CglibAopProxy].
@@ -48,6 +49,13 @@ class CglibAopProxyKotlinTests {
 		assertThatThrownBy { proxy.checkedException() }.isInstanceOf(CheckedException::class.java)
 	}
 
+	@Test // gh-35487
+	fun jvmDefault() {
+		val proxyFactory = ProxyFactory()
+		proxyFactory.setTarget(AddressRepo())
+		proxyFactory.proxy
+	}
+
 
 	open class MyKotlinBean {
 
@@ -63,4 +71,24 @@ class CglibAopProxyKotlinTests {
 	}
 
 	class CheckedException() : Exception()
+
+	open class AddressRepo(): CrudRepo<Address, Int>
+
+	interface CrudRepo<E : Any, ID : Any> {
+		fun save(e: E): E {
+			return e
+		}
+		fun delete(id: ID): Long {
+			return 0L
+		}
+	}
+
+	data class Address(
+		val id: Int = 0,
+		val street: String,
+		val version: Int = 0,
+		val createdAt: LocalDateTime? = null,
+		val updatedAt: LocalDateTime? = null,
+	)
+
 }
