a32d0b17d13ac43fe327a490f625e1944b63b63f
Wars <wars@wars.cat>
2025-10-05 13:34:55 +0800

    Remove unused application startup call
    AbstractApplicationContext.applicationStartup can only be injected after the instance construction. This metrics point has no practical effect at present and can be removed safely.
    
    Closes gh-35570
    Signed-off-by: Wars <wars@wars.cat>

--- spring-context/src/main/java/org/springframework/context/annotation/AnnotationConfigApplicationContext.java
+++ spring-context/src/main/java/org/springframework/context/annotation/AnnotationConfigApplicationContext.java
@@ -66,9 +66,7 @@ public class AnnotationConfigApplicationContext extends GenericApplicationContex
 	 * through {@link #register} calls and then manually {@linkplain #refresh refreshed}.
 	 */
 	public AnnotationConfigApplicationContext() {
-		StartupStep createAnnotatedBeanDefReader = getApplicationStartup().start("spring.context.annotated-bean-reader.create");
 		this.reader = new AnnotatedBeanDefinitionReader(this);
-		createAnnotatedBeanDefReader.end();
 		this.scanner = new ClassPathBeanDefinitionScanner(this);
 	}
 
