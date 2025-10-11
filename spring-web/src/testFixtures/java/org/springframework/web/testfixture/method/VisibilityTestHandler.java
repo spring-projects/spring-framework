a2d706c0097d57dddbb0de189d17279b9d594274
yongjunhong <yongjunh@apache.org>
2025-10-11 22:14:10 +0900

    Remove unused annotation
    Signed-off-by: yongjunhong <yongjunh@apache.org>

--- spring-web/src/testFixtures/java/org/springframework/web/testfixture/method/VisibilityTestHandler.java
+++ spring-web/src/testFixtures/java/org/springframework/web/testfixture/method/VisibilityTestHandler.java
@@ -19,7 +19,6 @@ package org.springframework.web.testfixture.method;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 public class VisibilityTestHandler {
 
 	@Controller
