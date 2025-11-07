06ae7fb2b5a3404d23e763e5e8f0ce1e33d6c2b0
Brian Clozel <brian.clozel@broadcom.com>
2025-11-07 14:05:40 +0100

    Add jar validation and fix multi-release jar
    See gh-35768
    
    Signed-off-by: Clayton Walker <clayton.m.walker@gmail.com>

--- buildSrc/src/main/java/org/springframework/build/multirelease/MultiReleaseJarPlugin.java
+++ buildSrc/src/main/java/org/springframework/build/multirelease/MultiReleaseJarPlugin.java
@@ -27,6 +27,9 @@ import org.gradle.api.plugins.ExtensionContainer;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.tasks.TaskContainer;
+import org.gradle.api.tasks.TaskProvider;
+import org.gradle.api.tasks.bundling.AbstractArchiveTask;
+import org.gradle.jvm.tasks.Jar;
 import org.gradle.jvm.toolchain.JavaToolchainService;
 
 /**
@@ -57,5 +60,9 @@ public class MultiReleaseJarPlugin implements Plugin<Project> {
 				tasks,
 				dependencies,
 				objects);
+		TaskProvider<MultiReleaseJarValidateTask> validateJarTask = tasks.register("validateJar", MultiReleaseJarValidateTask.class, (task) -> {
+			task.getJar().set(tasks.named("jar", Jar.class).flatMap(AbstractArchiveTask::getArchiveFile));
+		});
+		tasks.named("check", task -> task.dependsOn(validateJarTask));
 	}
 }
