f62380cc7bcac3b4c2df11293b9a802e3c6e4033
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2025-11-12 17:05:05 +0100

    Make WebSocketHttpHeaders compatible with HttpHeaders APIs
    Prior to this commit (and despite the changes made in commit
    4593f877dd), WebSocketHttpHeaders was not compatible with the
    HttpHeaders(HttpHeaders) constructor or the copyOf(HttpHeaders) and
    readOnlyHttpHeaders(HttpHeaders) factory methods.
    
    To address that, this commit revises the implementation of
    WebSocketHttpHeaders so that it only extends HttpHeaders, analogous to
    ReadOnlyHttpHeaders. In other words, WebSocketHttpHeaders no longer
    stores or delegates to a local HttpHeaders instance.
    
    Closes gh-35792

--- spring-websocket/src/test/java/org/springframework/web/socket/handler/WebSocketHttpHeadersTests.java
+++ spring-websocket/src/test/java/org/springframework/web/socket/handler/WebSocketHttpHeadersTests.java
@@ -25,7 +25,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.web.socket.WebSocketHttpHeaders;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.entry;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+import static org.springframework.web.socket.WebSocketHttpHeaders.SEC_WEBSOCKET_EXTENSIONS;
 
 /**
  * Tests for {@link WebSocketHttpHeaders}.
@@ -35,13 +42,101 @@ import static org.assertj.core.api.Assertions.entry;
  */
 class WebSocketHttpHeadersTests {
 
-	private WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+	private final WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
 
+
+	@Test  // gh-35792
+	void constructorCopiesHeaders() {
+		headers.setContentType(APPLICATION_JSON);
+		assertThat(headers.getContentType()).isEqualTo(APPLICATION_JSON);
+
+		var copy = new WebSocketHttpHeaders(headers);
+		assertThat(copy.getContentType()).isEqualTo(APPLICATION_JSON);
+
+		copy.setContentType(TEXT_PLAIN);
+
+		assertThat(copy.getContentType()).isEqualTo(TEXT_PLAIN);
+		// The WebSocketHttpHeaders "copy constructor" creates an WebSocketHttpHeaders
+		// instance that mutates the state of the original WebSocketHttpHeaders instance.
+		assertThat(headers.getContentType()).isEqualTo(TEXT_PLAIN);
+	}
+
+	@Test  // gh-35792
+	void constructorUnwrapsReadonly() {
+		headers.setContentType(APPLICATION_JSON);
+
+		var readOnly = HttpHeaders.readOnlyHttpHeaders(headers);
+		assertThat(readOnly.getContentType()).isEqualTo(APPLICATION_JSON);
+
+		var writable = new WebSocketHttpHeaders(readOnly);
+		writable.setContentType(TEXT_PLAIN);
+
+		// content-type value is cached by ReadOnlyHttpHeaders
+		assertThat(readOnly.getContentType()).isEqualTo(APPLICATION_JSON);
+		assertThat(writable.getContentType()).isEqualTo(TEXT_PLAIN);
+		assertThat(headers.getContentType()).isEqualTo(TEXT_PLAIN);
+	}
+
+	@Test  // gh-35792
+	void copyOf() {
+		headers.setContentType(APPLICATION_JSON);
+		headers.set("X-Project", "Spring Framework");
+
+		assertThat(headers.getContentType()).isEqualTo(APPLICATION_JSON);
+		assertThat(headers.toSingleValueMap()).containsOnly(
+			entry(CONTENT_TYPE, APPLICATION_JSON_VALUE),
+			entry("X-Project", "Spring Framework")
+		);
+
+		var copy = HttpHeaders.copyOf(headers);
+
+		assertThat(copy.getContentType()).isEqualTo(APPLICATION_JSON);
+		assertThat(copy.toSingleValueMap()).containsOnly(
+			entry(CONTENT_TYPE, APPLICATION_JSON_VALUE),
+			entry("X-Project", "Spring Framework")
+		);
+
+		copy.setContentType(TEXT_PLAIN);
+		copy.set("X-Project", "Project X");
+
+		assertThat(headers.getContentType()).isEqualTo(APPLICATION_JSON);
+		assertThat(headers.toSingleValueMap()).containsOnly(
+			entry(CONTENT_TYPE, APPLICATION_JSON_VALUE),
+			entry("X-Project", "Spring Framework")
+		);
+		assertThat(copy.getContentType()).isEqualTo(TEXT_PLAIN);
+		assertThat(copy.toSingleValueMap()).containsOnly(
+			entry(CONTENT_TYPE, TEXT_PLAIN_VALUE),
+			entry("X-Project", "Project X")
+		);
+	}
+
+	@Test  // gh-35792
+	void readOnlyHttpHeaders() {
+		headers.setContentType(APPLICATION_JSON);
+		headers.add("X-Project", "Spring Framework");
+
+		assertThat(headers.getContentType()).isEqualTo(APPLICATION_JSON);
+		assertThat(headers.toSingleValueMap()).containsOnly(
+			entry(CONTENT_TYPE, APPLICATION_JSON_VALUE),
+			entry("X-Project", "Spring Framework")
+		);
+
+		var readOnly = HttpHeaders.readOnlyHttpHeaders(headers);
+		assertThat(readOnly.getContentType()).isEqualTo(APPLICATION_JSON);
+		assertThat(readOnly.toSingleValueMap()).containsOnly(
+			entry(CONTENT_TYPE, APPLICATION_JSON_VALUE),
+			entry("X-Project", "Spring Framework")
+		);
+
+		assertThatExceptionOfType(UnsupportedOperationException.class)
+				.isThrownBy(() -> readOnly.setContentType(TEXT_PLAIN));
+	}
 
 	@Test
 	void parseWebSocketExtensions() {
 		var extensions = List.of("x-foo-extension, x-bar-extension", "x-test-extension");
-		this.headers.put(WebSocketHttpHeaders.SEC_WEBSOCKET_EXTENSIONS, extensions);
+		this.headers.put(SEC_WEBSOCKET_EXTENSIONS, extensions);
 
 		var parsedExtensions = this.headers.getSecWebSocketExtensions();
 		assertThat(parsedExtensions).hasSize(3);
