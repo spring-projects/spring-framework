ab96576e6753bb7c1ffa7c4c3ff9738bbe3cbefd
Juergen Hoeller <juergen.hoeller@broadcom.com>
2025-11-26 15:08:24 +0100

    Expose non-existent resources at the end of the sorted result
    Closes gh-35895
    
    (cherry picked from commit c1b6bfb6818a91ec2b7ec16a73562b57c8283b01)

--- spring-jdbc/src/test/java/org/springframework/jdbc/config/JdbcNamespaceIntegrationTests.java
+++ spring-jdbc/src/test/java/org/springframework/jdbc/config/JdbcNamespaceIntegrationTests.java
@@ -21,6 +21,7 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.PropertyValue;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
@@ -32,6 +33,7 @@ import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.AbstractDriverBasedDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactoryBean;
+import org.springframework.jdbc.datasource.init.CannotReadScriptException;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.lang.Nullable;
 
@@ -65,6 +67,13 @@ class JdbcNamespaceIntegrationTests {
 	@Test
 	void createWithResourcePattern() {
 		assertCorrectSetup("jdbc-config-pattern.xml", "dataSource");
+	}
+
+	@Test
+	void createWithNonExistentResource() {
+		assertThatExceptionOfType(BeanCreationException.class)
+				.isThrownBy(() -> assertCorrectSetup("jdbc-config-nonexistent.xml", "dataSource"))
+				.withCauseInstanceOf(CannotReadScriptException.class);
 	}
 
 	@Test
