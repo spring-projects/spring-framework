a91402dcc6402e7d0e291fabc38d22fd86da3519
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2026-01-19 14:20:44 +0100

    Extract code snippets from multipart.adoc
    See gh-36175

---
+++ framework-docs/src/main/kotlin/org/springframework/docs/web/webmvc/mvcservlet/mvcmultipartresolverstandard/AppInitializer.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2002-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.docs.web.webmvc.mvcservlet.mvcmultipartresolverstandard
+
+import jakarta.servlet.MultipartConfigElement
+import jakarta.servlet.ServletRegistration
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer
+
+// tag::snippet[]
+class AppInitializer : AbstractAnnotationConfigDispatcherServletInitializer() {
+
+	// @fold:on
+	override fun getServletMappings(): Array<out String> {
+		/**/TODO("Not yet implemented")
+	}
+
+	override fun getRootConfigClasses(): Array<out Class<*>>? {
+		/**/TODO("Not yet implemented")
+	}
+
+	override fun getServletConfigClasses(): Array<out Class<*>>? {
+		/**/TODO("Not yet implemented")
+	}
+
+	// @fold:off
+	override fun customizeRegistration(registration: ServletRegistration.Dynamic) {
+
+		// Optionally also set maxFileSize, maxRequestSize, fileSizeThreshold
+		registration.setMultipartConfig(MultipartConfigElement("/tmp"))
+	}
+
+}
+// end::snippet[]
