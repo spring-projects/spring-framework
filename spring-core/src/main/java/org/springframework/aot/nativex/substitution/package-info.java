59a9d0fcb9be6c3d4843bf512802b1fd4a3635b5
Brian Clozel <brian.clozel@broadcom.com>
2026-02-04 14:52:40 +0100

    Remove outdated GraalVM substitutions
    Closes gh-36257

--- spring-core/src/main/java/org/springframework/aot/nativex/substitution/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * GraalVM native image substitutions, not part of Spring Framework public API.
- */
-@NullUnmarked
-package org.springframework.aot.nativex.substitution;
-
-import org.jspecify.annotations.NullUnmarked;
