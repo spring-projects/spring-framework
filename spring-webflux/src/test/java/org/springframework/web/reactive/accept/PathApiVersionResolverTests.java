c004010c6f5c94063e34e17056dda92bbfc85196
Martin Mois <martin.mois@gmail.com>
2026-02-27 22:19:27 +0100

    new method usePathSegment(int index, Predicate<RequestPath> excludePath) in ApiVersionConfigurer to exclude certain paths

--- spring-webflux/src/test/java/org/springframework/web/reactive/accept/PathApiVersionResolverTests.java
+++ spring-webflux/src/test/java/org/springframework/web/reactive/accept/PathApiVersionResolverTests.java
@@ -18,10 +18,13 @@ package org.springframework.web.reactive.accept;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.http.server.PathContainer;
 import org.springframework.web.accept.InvalidApiVersionException;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.testfixture.http.server.reactive.MockServerHttpRequest;
 import org.springframework.web.testfixture.server.MockServerWebExchange;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -29,6 +32,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 /**
  * Unit tests for {@link org.springframework.web.accept.PathApiVersionResolver}.
  * @author Rossen Stoyanchev
+ * @author Martin Mois
  */
 public class PathApiVersionResolverTests {
 
@@ -41,6 +45,39 @@ public class PathApiVersionResolverTests {
 	@Test
 	void insufficientPathSegments() {
 		assertThatThrownBy(() -> testResolve(0, "/", "1.0")).isInstanceOf(InvalidApiVersionException.class);
+	}
+
+	@Test
+	void excludePathTrue() {
+		String requestUri = "/v3/api-docs";
+		testResolveWithExcludePath(requestUri, null);
+	}
+
+	@Test
+	void excludePathFalse() {
+		String requestUri = "/app/1.0/path";
+		testResolveWithExcludePath(requestUri, "1.0");
+	}
+
+	@Test
+	void excludePathFalseShortPath() {
+		String requestUri = "/app";
+		assertThatThrownBy(() -> testResolveWithExcludePath(requestUri, null)).isInstanceOf(InvalidApiVersionException.class);
+	}
+
+	private static void testResolveWithExcludePath(String requestUri, String expected) {
+		ServerWebExchange exchange = MockServerWebExchange.from(MockServerHttpRequest.get(requestUri));
+		String actual = new PathApiVersionResolver(1, requestPath -> {
+			List<PathContainer.Element> elements = requestPath.elements();
+			if (elements.size() < 4) {
+				return false;
+			}
+			return elements.get(0).value().equals("/") &&
+					elements.get(1).value().equals("v3") &&
+					elements.get(2).value().equals("/") &&
+					elements.get(3).value().equals("api-docs");
+		}).resolveVersion(exchange);
+		assertThat(actual).isEqualTo(expected);
 	}
 
 	private static void testResolve(int index, String requestUri, String expected) {
