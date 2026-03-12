19ab92ef5941383327df79e7752804a06040f2e3
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2026-03-12 08:53:54 +0100

    Restore ScriptTemplateViewTests
    Restore both WebMVC and WebFlux variants that were deleted
    by mistake in commit 4db2f8ea1bc72b09980852660d2a96f44d66b31b.
    
    This commit also removes the empty resource loader path, as it is not
    needed for the main WEB-INF/ use case that is typically configured
    explicitly by the user, and not needed to pass the restored tests.
    
    Closes gh-36456

--- spring-webmvc/src/main/java/org/springframework/web/servlet/view/script/ScriptTemplateView.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/view/script/ScriptTemplateView.java
@@ -185,14 +185,13 @@ public class ScriptTemplateView extends AbstractUrlBasedView {
 	 */
 	public void setResourceLoaderPath(String resourceLoaderPath) {
 		String[] paths = StringUtils.commaDelimitedListToStringArray(resourceLoaderPath);
-		this.resourceLoaderPaths = new String[paths.length + 1];
-		this.resourceLoaderPaths[0] = "";
+		this.resourceLoaderPaths = new String[paths.length];
 		for (int i = 0; i < paths.length; i++) {
 			String path = paths[i];
 			if (!path.endsWith("/") && !path.endsWith(":")) {
 				path = path + "/";
 			}
-			this.resourceLoaderPaths[i + 1] = path;
+			this.resourceLoaderPaths[i] = path;
 		}
 	}
 
