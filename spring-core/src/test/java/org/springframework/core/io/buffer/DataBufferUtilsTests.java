80534f9df6aaa6b155ef3fb8494c057b0d4f268f
Brian Clozel <brian.clozel@broadcom.com>
2026-06-04 12:09:52 +0200

    Polishing contribution
    This commit adds further fixes in the same area, since there were
    similar bugs in the WriteCompletionHandler:
    * databuffers were not always emitted when fully read in the onNext hook
    * on completion, the iterator was closed too early, before it was fully
      read
    * on completion, writing the next bytebuffers from the iterator would
      always reuse the first one and not update the attachment
    
    Closes gh-36714

--- spring-core/src/test/java/org/springframework/core/io/buffer/DataBufferUtilsTests.java
+++ spring-core/src/test/java/org/springframework/core/io/buffer/DataBufferUtilsTests.java
@@ -467,6 +467,27 @@ class DataBufferUtilsTests extends AbstractDataBufferAllocatingTests {
 	}
 
 	@ParameterizedDataBufferAllocatingTest
+	void writeAsynchronousFileChannelWithJoinedBuffer(DataBufferFactory bufferFactory) throws Exception {
+		super.bufferFactory = bufferFactory;
+
+		DataBuffer foo = stringBuffer("foo");
+		DataBuffer bar = stringBuffer("bar");
+		DataBuffer joined = bufferFactory.join(List.of(foo, bar));
+
+		AsynchronousFileChannel channel = AsynchronousFileChannel.open(tempFile, StandardOpenOption.WRITE);
+
+		Flux<DataBuffer> writeResult = DataBufferUtils.write(Flux.just(joined), channel);
+		StepVerifier.create(writeResult)
+				.consumeNextWith(stringConsumer("foobar"))
+				.verifyComplete();
+
+		String result = String.join("", Files.readAllLines(tempFile));
+
+		assertThat(result).isEqualTo("foobar");
+		channel.close();
+	}
+
+	@ParameterizedDataBufferAllocatingTest
 	void writeAsynchronousFileChannelErrorInFlux(DataBufferFactory bufferFactory) throws Exception {
 		super.bufferFactory = bufferFactory;
 
