8d8b0e77db1b40575348e50a595980734ac3ae04
zanarelli <zanarelli.dev@gmail.com>
2026-08-01 01:03:08 -0300

    Preserve I/O errors in RestClient body extraction
    readWithMessageConverters wrapped IOException and UncheckedIOException
    in a RestClientException whose message blamed content-type extraction,
    hiding timeouts and other I/O failures. Re-throw those as
    ResourceAccessException (still a RestClientException subclass).
    
    Closes gh-37078
    
    Signed-off-by: zanarelli <zanarelli.dev@gmail.com>

--- spring-web/src/main/java/org/springframework/web/client/DefaultRestClient.java
+++ spring-web/src/main/java/org/springframework/web/client/DefaultRestClient.java
@@ -270,6 +270,10 @@ final class DefaultRestClient implements RestClient {
 			else {
 				cause = exc;
 			}
+			if (cause instanceof IOException ioException) {
+				throw new ResourceAccessException("I/O error while extracting response for type [" +
+						ResolvableType.forType(bodyType) + "]", ioException);
+			}
 			throw new RestClientException("Error while extracting response for type [" +
 					ResolvableType.forType(bodyType) + "] and content type [" + contentType + "]", cause);
 		}
