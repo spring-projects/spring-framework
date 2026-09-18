d9b99678e0614732e22267cab98d2bf71934264c
안승현 <abeb3@naver.com>
2026-09-18 14:27:53 +0900

    Parse multiple Sec-WebSocket-Protocol header values
    Signed-off-by: 안승현 <abeb3@naver.com>

--- spring-websocket/src/main/java/org/springframework/web/socket/WebSocketHttpHeaders.java
+++ spring-websocket/src/main/java/org/springframework/web/socket/WebSocketHttpHeaders.java
@@ -160,16 +160,7 @@ public class WebSocketHttpHeaders extends HttpHeaders {
 	 * @return the value of the header
 	 */
 	public List<String> getSecWebSocketProtocol() {
-		List<String> values = get(SEC_WEBSOCKET_PROTOCOL);
-		if (CollectionUtils.isEmpty(values)) {
-			return Collections.emptyList();
-		}
-		else if (values.size() == 1) {
-			return getValuesAsList(SEC_WEBSOCKET_PROTOCOL);
-		}
-		else {
-			return values;
-		}
+		return getValuesAsList(SEC_WEBSOCKET_PROTOCOL);
 	}
 
 	/**
